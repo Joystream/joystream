b90b5c8a70516977680b5c1e39a384a992f755fc
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-02-12 17:27:10 +0100

    - Make project-level requires easier - Add pagination 'middleware' by adding its parameter and response object definitions - Add final handler for serializing errors (if any get through); this is the path taken when request validation fails.

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 'use strict';
 
+// Node requires
+const fs = require('fs');
+const path = require('path');
+
+// npm requires
 const express = require('express');
 const openapi = require('express-openapi');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const yaml = require('js-yaml');
-const fs = require('fs');
-const path = require('path');
 
-const validateResponses = require('./lib/middleware/validate_responses');
-const fileUploads = require('./lib/middleware/file_uploads');
+// Allow require relative to project root
+require.main.paths.push(path.join(__dirname, 'lib'))
+
+// Project requires
+const validateResponses = require('middleware/validate_responses');
+const fileUploads = require('middleware/file_uploads');
+const pagination = require('middleware/pagination');
 
 // Configure app
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 // FIXME app.use(bodyParser.urlencoded({ extended: true }));
-//
+
+// Load & extend/configure API docs
 var api = yaml.safeLoad(fs.readFileSync('./api-base.yml'));
 api['x-express-openapi-additional-middleware'] = [validateResponses];
 api['x-express-openapi-validation-strict'] = true;
+
+api = pagination.openapi(api);
 
 openapi.initialize({
   apiDoc: api,
@@ -30,6 +40,12 @@ openapi.initialize({
   consumesMiddleware: {
     'multipart/form-data': fileUploads
   },
+});
+
+// If no other handler gets triggered (errors), respond with the
+// error serialized to JSON.
+app.use(function(err, req, res, next) {
+  res.status(err.status).json(err);
 });
 
 module.exports = app;
