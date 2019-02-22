84600669f612a0ad492b7218dea31a4dd932ec50
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-02-22 15:05:31 +0100

    Walk a file system hierarchy.

---
+++ test/util/fswalk.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const mocha = require('mocha');
+const expect = require('chai').expect;
+
+const fswalk = require.main.require('util/fswalk');
+
+describe('fswalk', function()
+{
+  it('reports all files in a file system hierarchy', function(done)
+  {
+    var results = new Map();
+    fswalk('./testdata/template', (err, relname, stat, linktarget) => {
+      expect(err).to.be.null;
+
+      results.set(relname, [stat, linktarget]);
+      if (results.size == 4) {
+
+        const entries = Array.from(results.keys());
+        expect(entries).to.include('/foo');
+        expect(results.get('/foo')[0].isDirectory()).to.be.true;
+
+        expect(entries).to.include('/bar');
+        expect(results.get('/bar')[0].isFile()).to.be.true;
+
+        expect(entries).to.include('/quux');
+        expect(results.get('/quux')[0].isSymbolicLink()).to.be.true;
+        expect(results.get('/quux')[1]).to.equal('foo/baz');
+
+        expect(entries).to.include('/foo/baz');
+        expect(results.get('/foo/baz')[0].isFile()).to.be.true;
+
+        done();
+      }
+    });
+  });
+});
