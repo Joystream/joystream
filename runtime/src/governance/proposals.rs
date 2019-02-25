01d1e3df91a78cf07d0359710460a2f104c7303f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-02-25 13:37:37 +0200

    proposal veto

--- runtime/src/governance/proposals.rs
+++ runtime/src/governance/proposals.rs
@@ -143,6 +143,9 @@ decl_event!(
 
         /// * Hash - hash of wasm code of runtime update.
         RuntimeUpdated(u32, Hash),
+
+        /// Root cancelled proposal
+        ProposalVeto(u32),
     }
 );
 
@@ -314,6 +317,20 @@ decl_module! {
             if let Err(e) = Self::end_block(n) {
                 print(e);
             }
+        }
+
+        /// Cancel a proposal and return stake without slashing
+        fn veto(proposal_id: u32) -> Result {
+            ensure!(<Proposals<T>>::exists(proposal_id), MSG_PROPOSAL_NOT_FOUND);
+            let proposal = Self::proposals(proposal_id);
+            ensure!(proposal.status == Active, MSG_PROPOSAL_FINALIZED);
+
+            let _ = T::Currency::unreserve(&proposal.proposer, proposal.stake);
+
+            Self::_update_proposal_status(proposal_id, Cancelled)?;
+
+            Self::deposit_event(RawEvent::ProposalVeto(proposal_id));
+            Ok(())
         }
     }
 }
