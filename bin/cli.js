020e0c8f9aa7e60dd814478a725f6a75aa38b0bf
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-02-26 14:33:01 +0100

    Add a list CLI command for listing repos and repo contents.

--- bin/cli.js
+++ bin/cli.js
@@ -28,6 +28,9 @@ const cli = meow(`
     create            Create a repository in the configured storage location.
                       If a second argument is given, it is a directory from which
                       the repository will be populated.
+    list              Output a list of storage entries. If an argument is given,
+                      it is interpreted as a repo ID, and the contents of the
+                      repo are listed instead.
 
   Options:
     --config=PATH, -c PATH  Configuration file path. Defaults to
@@ -84,6 +87,72 @@ function start_app(project_root, config, flags)
   console.log('API server started; API docs at http://localhost:' + port + '/swagger.json');
 }
 
+// Get an initialized storage instance
+function get_storage(config, flags)
+{
+  const store_path = flags.storage || config.get('storage') || './storage';
+  const store_type = flags['storage-type'] || config.get('storage-type') || 'hyperdrive';
+
+  const storage = require('joystream/core/storage');
+
+  const store = new storage.Storage(store_path, storage.DEFAULT_POOL_SIZE,
+      store_type == "fs");
+
+  return store;
+}
+
+// List repos in a storage
+function list_repos(store)
+{
+  console.log('Repositories in storage:');
+  store.repos((err, id) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`  ${id}`);
+  });
+}
+
+// List repository contents
+function list_repo(store, repo_id)
+{
+  console.log(`Contents of repository "${repo_id}":`);
+  const repo = store.get(repo_id);
+  const fswalk = require('joystream/util/fs/walk');
+  const siprefix = require('si-prefix');
+
+  fswalk('/', repo.archive, (err, relname, stat) => {
+    if (err) {
+      throw err;
+    }
+    if (!relname) {
+      return;
+    }
+
+    var line = stat.ctime.toUTCString() + '  ';
+    if (stat.isDirectory()) {
+      line += 'D  ';
+    }
+    else {
+      line += 'F  ';
+    }
+
+    var size = '-';
+    if (stat.isFile()) {
+      var info = siprefix.byte.convert(stat.size);
+      size = `${Math.ceil(info[0])} ${info[1]}`;
+    }
+    while (info.length < 8) {
+      info = ' ' + info;
+    }
+
+    line += size + '  ' + relname;
+
+    console.log(line);
+  });
+}
+
 // Simple CLI commands
 var command = cli.input[0];
 if (!command) {
@@ -97,19 +166,25 @@ const commands = {
   },
   'create': () => {
     const cfg = create_config(pkg.name, cli.flags);
+    const store = get_storage(cfg, cli.flags);
 
-    const store_path = cli.flags.storage || cfg.get('storage') || './storage';
-    const store_type = cli.flags['storage-type'] || cfg.get('storage-type') || 'hyperdrive';
-
-    const storage = require('joystream/core/storage');
-
-    const store = new storage.Storage(store_path, storage.DEFAULT_POOL_SIZE,
-        store_type == "fs");
     if (store.new) {
       console.log('Storage created.');
     }
     else {
       console.log('Storage already existed, not created.');
+    }
+  },
+  'list': () => {
+    const cfg = create_config(pkg.name, cli.flags);
+    const store = get_storage(cfg, cli.flags);
+
+    const repo_id = cli.input[1];
+    if (repo_id) {
+      list_repo(store, repo_id);
+    }
+    else {
+      list_repos(store);
     }
   },
 };
