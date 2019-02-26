7698362b06201a1be02e2711adbefadaca31012b
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-02-26 13:17:29 +0100

    Modify fswalk to also work on other, non-filesystem objects with a similar API (i.e. hyperdrive repos). This includes dealing better with asynchronous access.

--- test/util/fs/walk.js
+++ test/util/fs/walk.js
@@ -2,38 +2,64 @@
 
 const mocha = require('mocha');
 const expect = require('chai').expect;
+const temp = require('temp').track();
+
+const fs = require('fs');
 
 const fswalk = require('joystream/util/fs/walk');
+
+const repository = require('joystream/core/repository');
+
+function walktest(archive, base, done)
+{
+  var results = new Map();
+
+  fswalk(base, archive, (err, relname, stat, linktarget) => {
+    expect(err).to.be.null;
+
+    if (relname) {
+      results.set(relname, [stat, linktarget]);
+      return;
+    }
+
+    // End of data, do testing
+    const entries = Array.from(results.keys());
+    expect(entries).to.include('foo');
+    expect(results.get('foo')[0].isDirectory()).to.be.true;
+
+    expect(entries).to.include('bar');
+    expect(results.get('bar')[0].isFile()).to.be.true;
+
+    if (archive === fs) {
+      expect(entries).to.include('quux');
+      expect(results.get('quux')[0].isSymbolicLink()).to.be.true;
+      expect(results.get('quux')[1]).to.equal('foo/baz');
+    }
+
+    expect(entries).to.include('foo/baz');
+    expect(results.get('foo/baz')[0].isFile()).to.be.true;
+
+    done();
+  });
+}
 
 describe('fs/walk', function()
 {
   it('reports all files in a file system hierarchy', function(done)
   {
-    var results = new Map();
-    fswalk('./testdata/template', (err, relname, stat, linktarget) => {
-      expect(err).to.be.null;
+    walktest(fs, './testdata/template', done)
+  });
 
-      if (relname) {
-        results.set(relname, [stat, linktarget]);
-        return;
-      }
-
-      // End of data, do testing
-      const entries = Array.from(results.keys());
-      expect(entries).to.include('foo');
-      expect(results.get('foo')[0].isDirectory()).to.be.true;
-
-      expect(entries).to.include('bar');
-      expect(results.get('bar')[0].isFile()).to.be.true;
-
-      expect(entries).to.include('quux');
-      expect(results.get('quux')[0].isSymbolicLink()).to.be.true;
-      expect(results.get('quux')[1]).to.equal('foo/baz');
-
-      expect(entries).to.include('foo/baz');
-      expect(results.get('foo/baz')[0].isFile()).to.be.true;
-
-      done();
+  it('reports all files in a repo created from a file system hierarchy', function(done)
+  {
+    var prefix = temp.mkdirSync('joystream-walk-test');
+    var repo = new repository.Repository(prefix); // hyperdrive
+    repo.on('ready', () => {
+      repo.populate('./testdata/template', (err) => {
+        // When the repository is populated, walking it should be much the same
+        // as walking the original file system.
+        walktest(repo.archive, '/', done);
+      });
     });
   });
 });
