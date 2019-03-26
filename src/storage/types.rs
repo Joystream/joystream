61dfc3ca442a48833cb04d93637b30f47744f7e5
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-03-26 14:11:26 +0100

    Fixes #14

--- src/storage/types.rs
+++ src/storage/types.rs
@@ -17,9 +17,9 @@ pub trait Trait: system::Trait + MaybeDebug
 }
 
 
-static MSG_REQUIRE_NEW_DOT: &str = "New Data Object Type required; the provided one seems to be in use already!";
-static MSG_DOT_NOT_FOUND: &str = "Data Object Type with the given ID not found!";
-static MSG_REQUIRE_DOT_ID: &str = "Can only update Data Object Types that are already registered (with an ID)!";
+static MSG_REQUIRE_NEW_DO_TYPE: &str = "New Data Object Type required; the provided one seems to be in use already!";
+static MSG_DO_TYPE_NOT_FOUND: &str = "Data Object Type with the given ID not found!";
+static MSG_REQUIRE_DO_TYPE_ID: &str = "Can only update Data Object Types that are already registered (with an ID)!";
 
 const DEFAULT_FIRST_DATA_OBJECT_TYPE_ID: u64 = 1;
 
@@ -68,7 +68,7 @@ impl<T: Trait> traits::IsActiveDataObjectType<T> for Module<T>
     {
         match Self::ensure_data_object_type(*which)
         {
-            Ok(dot) => dot.active,
+            Ok(do_type) => do_type.active,
             Err(_err) => false
         }
     }
@@ -83,33 +83,33 @@ decl_module! {
         pub fn register_data_object_type(origin, data_object_type: DataObjectType<T>)
         {
             ensure_root(origin)?;
-            ensure!(data_object_type.id.is_none(), MSG_REQUIRE_NEW_DOT);
+            ensure!(data_object_type.id.is_none(), MSG_REQUIRE_NEW_DO_TYPE);
 
-            let new_dot_id = Self::next_data_object_type_id();
-            let dot: DataObjectType<T> = DataObjectType {
-                id: Some(new_dot_id),
+            let new_do_type_id = Self::next_data_object_type_id();
+            let do_type: DataObjectType<T> = DataObjectType {
+                id: Some(new_do_type_id),
                 description: data_object_type.description.clone(),
                 active: data_object_type.active,
             };
 
-            <DataObjectTypeMap<T>>::insert(new_dot_id, dot);
+            <DataObjectTypeMap<T>>::insert(new_do_type_id, do_type);
             <NextDataObjectTypeID<T>>::mutate(|n| { *n += T::DataObjectTypeID::sa(1); });
 
-            Self::deposit_event(RawEvent::DataObjectTypeAdded(new_dot_id));
+            Self::deposit_event(RawEvent::DataObjectTypeAdded(new_do_type_id));
         }
 
         pub fn update_data_object_type(origin, data_object_type: DataObjectType<T>)
         {
             ensure_root(origin)?;
-            ensure!(data_object_type.id.is_some(), MSG_REQUIRE_DOT_ID);
+            ensure!(data_object_type.id.is_some(), MSG_REQUIRE_DO_TYPE_ID);
 
             let id = data_object_type.id.unwrap();
-            let mut dot = Self::ensure_data_object_type(id)?;
+            let mut do_type = Self::ensure_data_object_type(id)?;
 
-            dot.description = data_object_type.description.clone();
-            dot.active = data_object_type.active;
+            do_type.description = data_object_type.description.clone();
+            do_type.active = data_object_type.active;
 
-            <DataObjectTypeMap<T>>::insert(id, dot);
+            <DataObjectTypeMap<T>>::insert(id, do_type);
 
             Self::deposit_event(RawEvent::DataObjectTypeUpdated(id));
         }
@@ -117,11 +117,11 @@ decl_module! {
         pub fn activate_data_object_type(origin, id: T::DataObjectTypeID, active: bool)
         {
             ensure_root(origin)?;
-            let mut dot = Self::ensure_data_object_type(id)?;
+            let mut do_type = Self::ensure_data_object_type(id)?;
 
-            dot.active = active;
+            do_type.active = active;
 
-            <DataObjectTypeMap<T>>::insert(id, dot);
+            <DataObjectTypeMap<T>>::insert(id, do_type);
 
             Self::deposit_event(RawEvent::DataObjectTypeUpdated(id));
         }
@@ -132,7 +132,7 @@ impl <T: Trait> Module<T>
 {
     fn ensure_data_object_type(id: T::DataObjectTypeID) -> Result<DataObjectType<T>, &'static str>
     {
-        let dot = Self::data_object_type(&id).ok_or(MSG_DOT_NOT_FOUND)?;
-        Ok(dot)
+        let do_type = Self::data_object_type(&id).ok_or(MSG_DO_TYPE_NOT_FOUND)?;
+        Ok(do_type)
     }
 }
