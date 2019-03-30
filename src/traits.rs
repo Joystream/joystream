afc031bbf5fab851d24dc69f3874414dd36f010a
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-03-30 15:29:34 +0100

    Add more traits for storage

--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use crate::storage::{data_object_type_registry, data_directory};
+use crate::storage::{data_object_type_registry, data_directory, data_object_storage_registry};
 use system;
+
+// Storage
 
 pub trait IsActiveDataObjectType<T: data_object_type_registry::Trait> {
     fn is_active_data_object_type(which: &T::DataObjectTypeId) -> bool {
@@ -13,7 +15,23 @@ pub trait ContentIdExists<T: data_directory::Trait> {
     fn has_content(which: &T::ContentId) -> bool {
         false
     }
+
+    fn get_data_object(which: &T::ContentId) -> Result<data_directory::DataObject<T>,  &'static str> {
+        Err("not implemented")
+    }
 }
+
+pub trait ContentHasStorage<T: data_object_storage_registry::Trait> {
+    fn has_storage_provider(which: &T::ContentId) -> bool {
+        false
+    }
+
+    fn is_ready_at_storage_provider(which: &T::ContentId, provider: &T::AccountId) -> bool {
+        false
+    }
+}
+
+// Membership
 
 pub trait IsActiveMember<T: system::Trait> {
     fn is_active_member(account_id: &T::AccountId) -> bool {
