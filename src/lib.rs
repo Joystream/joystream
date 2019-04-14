a789a9c4c06c46c0933c15c3f199ea7d8f843fe7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-04-14 18:49:44 +0300

    set runtime and package version to v5.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -127,8 +127,8 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node"),
     impl_name: create_runtime_str!("joystream-node"),
-    authoring_version: 4,
-    spec_version: 3,
+    authoring_version: 5,
+    spec_version: 1,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
 };
