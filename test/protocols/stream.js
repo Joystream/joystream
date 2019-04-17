02f1eb336c5a4591cb16881c7bdbabe24795a20a
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-04-17 14:38:21 +0200

    Adjust streams test to new keys

--- test/protocols/stream.js
+++ test/protocols/stream.js
@@ -23,33 +23,51 @@ const expect = require('chai').expect;
 
 const net = require('net');
 
+const { Keyring } = require('joystream/crypto/keyring');
+const { ALICE_SEED, BOB_SEED } = require('../common');
+
 const protostream = require('joystream/protocols/stream');
 const mutual = require('joystream/protocols/mutual');
 const envelope = require('joystream/protocols/envelope');
-const keys = require('joystream/crypto/keys');
 
 describe('protocols/stream', function()
 {
+  var alice, bob, alice_pub, bob_pub;
+
+  before(async () => {
+    const kr = await Keyring.create();
+    const alice_p = kr.from_seed('ed25519', ALICE_SEED);
+    alice = kr.convert_keypair(alice_p);
+    const bob_p = kr.from_seed('ed25519', BOB_SEED);
+    bob = kr.convert_keypair(bob_p);
+
+    bob_pub = {};
+    Object.assign(bob_pub, bob);
+    bob_pub.secretKey = undefined;
+
+    alice_pub = {};
+    Object.assign(alice_pub, alice);
+    alice_pub.secretKey = undefined;
+  });
+
+
   it('can stream mutual authentication in object mode', function(done)
   {
-    const key1 = keys.key_pair();
-    const key2 = keys.key_pair();
-
-    const auth1 = new mutual.MutualAuthenticator(key1, key2.pubKey, 8);
-    const auth2 = new mutual.MutualAuthenticator(key2, key1.pubKey, 8);
+    const auth1 = new mutual.MutualAuthenticator(alice, bob_pub, 8);
+    const auth2 = new mutual.MutualAuthenticator(bob, alice_pub, 8);
 
     const proto1 = new protostream.ProtocolStream(auth1, { objectMode: true });
     const proto2 = new protostream.ProtocolStream(auth2, { objectMode: true });
 
     // Precondition
-    expect(auth1.peer_authenticated).to.be.false;
-    expect(auth2.peer_authenticated).to.be.false;
+    expect(auth1.peer_authenticated()).to.be.false;
+    expect(auth2.peer_authenticated()).to.be.false;
 
     proto1.pipe(proto2).pipe(proto1);
     proto2.on('finish', () => {
       // Postcondition
-      expect(auth1.peer_authenticated).to.be.true;
-      expect(auth2.peer_authenticated).to.be.true;
+      expect(auth1.peer_authenticated()).to.be.true;
+      expect(auth2.peer_authenticated()).to.be.true;
       done();
     });
     proto1.initiate();
@@ -57,11 +75,8 @@ describe('protocols/stream', function()
 
   it('can stream mutual authentication with envelopes', function(done)
   {
-    const key1 = keys.key_pair();
-    const key2 = keys.key_pair();
-
-    const auth1 = new mutual.MutualAuthenticator(key1, key2.pubKey, 8);
-    const auth2 = new mutual.MutualAuthenticator(key2, key1.pubKey, 8);
+    const auth1 = new mutual.MutualAuthenticator(alice, bob_pub, 8);
+    const auth2 = new mutual.MutualAuthenticator(bob, alice_pub, 8);
 
     const ser1 = new envelope.Serializer();
     const des1 = new envelope.Deserializer();
@@ -72,14 +87,14 @@ describe('protocols/stream', function()
     const proto2 = new protostream.ProtocolStream(auth2);
 
     // Precondition
-    expect(auth1.peer_authenticated).to.be.false;
-    expect(auth2.peer_authenticated).to.be.false;
+    expect(auth1.peer_authenticated()).to.be.false;
+    expect(auth2.peer_authenticated()).to.be.false;
 
     proto1.pipe(ser1).pipe(des2).pipe(proto2).pipe(ser2).pipe(des1).pipe(proto1);
     proto2.on('finish', () => {
       // Postcondition
-      expect(auth1.peer_authenticated).to.be.true;
-      expect(auth2.peer_authenticated).to.be.true;
+      expect(auth1.peer_authenticated()).to.be.true;
+      expect(auth2.peer_authenticated()).to.be.true;
       done();
     });
     proto1.initiate();
@@ -87,11 +102,8 @@ describe('protocols/stream', function()
 
   it('can stream mutual authentication through localhost', function(done)
   {
-    const key1 = keys.key_pair();
-    const key2 = keys.key_pair();
-
-    const auth1 = new mutual.MutualAuthenticator(key1, key2.pubKey, 8);
-    const auth2 = new mutual.MutualAuthenticator(key2, key1.pubKey, 8);
+    const auth1 = new mutual.MutualAuthenticator(alice, bob_pub, 8);
+    const auth2 = new mutual.MutualAuthenticator(bob, alice_pub, 8);
 
     // Serializer and Deserializer
     const ser1 = new envelope.Serializer();
@@ -103,8 +115,8 @@ describe('protocols/stream', function()
     const proto2 = new protostream.ProtocolStream(auth2);
 
     // Precondition
-    expect(auth1.peer_authenticated).to.be.false;
-    expect(auth2.peer_authenticated).to.be.false;
+    expect(auth1.peer_authenticated()).to.be.false;
+    expect(auth2.peer_authenticated()).to.be.false;
 
     const server = net.createServer((socket) => {
       socket.pipe(des1).pipe(proto1).pipe(ser1).pipe(socket);
@@ -116,8 +128,8 @@ describe('protocols/stream', function()
 
     proto2.on('finish', () => {
       // Postcondition
-      expect(auth1.peer_authenticated).to.be.true;
-      expect(auth2.peer_authenticated).to.be.true;
+      expect(auth1.peer_authenticated()).to.be.true;
+      expect(auth2.peer_authenticated()).to.be.true;
 
       server.close();
       done();
