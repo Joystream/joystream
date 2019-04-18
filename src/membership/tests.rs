6f55005ea4f7bd6b9e2ff1bba9feea0e8651d2e6
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-04-18 18:57:54 +0300

    add GPLv3 License

--- src/membership/tests.rs
+++ src/membership/tests.rs
@@ -1,3 +1,31 @@
+// Copyright 2019 Joystream Contributors
+// This file is part of Joystream runtime
+
+// Joystream runtime is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Joystream runtime is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// === Substrate ===
+// Copyright 2017-2019 Parity Technologies (UK) Ltd.
+// Substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Substrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this software If not, see <http://www.gnu.org/licenses/>.
+
 #![cfg(test)]
 
 use super::mock::*;
