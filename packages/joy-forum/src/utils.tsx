736969fd345dcbd5bb30a6d98d8d10a04ea36721
Alex Siman <aleksandr.siman@gmail.com>
2019-05-21 00:49:35 +0300

    Show threads pagination only if pages > 1

--- packages/joy-forum/src/utils.tsx
+++ packages/joy-forum/src/utils.tsx
@@ -32,7 +32,7 @@ export const Pagination = (p: PaginationProps) => {
   const { currentPage = 1, itemsPerPage = 20 } = p;
   const totalPages = Math.floor(p.totalItems / itemsPerPage);
 
-  return (
+  return totalPages <= 1 ? null : (
     <SuiPagination
       firstItem={null}
       lastItem={null}
