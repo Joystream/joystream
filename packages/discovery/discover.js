956d64b227579d1fc0c7187825d723351582ec80
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-06-27 17:28:20 +0300

    better concurrent discovery handling

--- packages/discovery/discover.js
+++ packages/discovery/discover.js
@@ -1,12 +1,13 @@
 const axios = require('axios')
 const debug = require('debug')('discovery::discover')
-const AsyncLock = require('async-lock');
+
+var ipfs = require('ipfs-http-client')('localhost', '5001', { protocol: 'http' })
 
 function inBrowser() {
     return typeof window !== 'undefined'
 }
 
-const discoveryLock = new AsyncLock();
+var activeDiscoveries = {};
 var accountInfoCache = {};
 const CACHE_TTL = 60 * 60 * 1000;
 
@@ -71,14 +72,14 @@ async function discover_over_local_ipfs_node(actorAccountId, runtimeApi) {
 
     const ipns_address = `/ipns/${identity}/`
 
-    var ipfs = require('ipfs-http-client')('localhost', '5001', { protocol: 'http' })
-
+    debug('resolved ipns to ipfs object')
     let ipfs_name = await ipfs.name.resolve(ipns_address, {
         recursive: false, // there should only be one indirection to service info file
         nocache: false,
-    })
+    }) // this can hang forever!? can we set a timeout?
 
-    let data = await ipfs.get(ipfs_name)
+    debug('getting ipfs object', ipfs_name)
+    let data = await ipfs.get(ipfs_name) // this can sometimes hang forever!?! can we set a timeout?
 
     // there should only be one file published under the resolved path
     let content = data[0].content
@@ -92,48 +93,71 @@ async function discover_over_local_ipfs_node(actorAccountId, runtimeApi) {
 
 async function discover (actorAccountId, runtimeApi, useCachedValue = false, maxCacheAge = 0) {
     const id = actorAccountId.toString();
+    const cached = accountInfoCache[id];
 
-    return discoveryLock.acquire(id, async () => {
-        const cached = accountInfoCache[id];
-
-        if (cached && useCachedValue) {
-            if (maxCacheAge > 0) {
-                // get latest value
-                if (Date.now() > (cached.updated + maxCacheAge)) {
-                    return _discover(actorAccountId, runtimeApi);
-                }
+    if (cached && useCachedValue) {
+        if (maxCacheAge > 0) {
+            // get latest value
+            if (Date.now() > (cached.updated + maxCacheAge)) {
+                return _discover(actorAccountId, runtimeApi);
             }
-            // refresh if cache is stale, new value returned on next cached query
-            if (Date.now() > (cached.updated + CACHE_TTL)) {
-                _discover(actorAccountId, runtimeApi);
-            }
-            // return best known value
-            return cached.value;
-        } else {
-            return _discover(actorAccountId, runtimeApi);
         }
+        // refresh if cache is stale, new value returned on next cached query
+        if (Date.now() > (cached.updated + CACHE_TTL)) {
+            _discover(actorAccountId, runtimeApi);
+        }
+        // return best known value
+        return cached.value;
+    } else {
+        return _discover(actorAccountId, runtimeApi);
+    }
+}
+
+function createExternallyControlledPromise() {
+    let resolve, reject;
+    const promise = new Promise((_resolve, _reject) => {
+        resolve = _resolve;
+        reject = _reject;
     });
+    return ({ resolve, reject, promise });
 }
 
 async function _discover(actorAccountId, runtimeApi) {
-    let result = null;
+    const id = actorAccountId.toString();
 
+    const discoveryResult = activeDiscoveries[id];
+    if (discoveryResult) {
+        debug('discovery in progress waiting for result for',id);
+        return discoveryResult
+    }
+
+    debug('starting new discovery for', id);
+    const deferredDiscovery = createExternallyControlledPromise();
+    activeDiscoveries[id] = deferredDiscovery.promise;
+
+    let result;
     try {
         if (inBrowser()) {
             result = await discover_over_joystream_discovery_service(actorAccountId, runtimeApi)
         } else {
             result = await discover_over_local_ipfs_node(actorAccountId, runtimeApi)
         }
-
-        accountInfoCache[actorAccountId.toString()] = {
-            value: JSON.stringify(result),
+        debug(result)
+        result = JSON.stringify(result)
+        accountInfoCache[id] = {
+            value: result,
             updated: Date.now()
         };
+
+        deferredDiscovery.resolve(result);
+        delete activeDiscoveries[id];
+        return result;
     } catch (err) {
         debug(err.message);
+        deferredDiscovery.reject(err);
+        delete activeDiscoveries[id];
+        throw err;
     }
-
-    return result;
 }
 
 module.exports = {
