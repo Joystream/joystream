777b731e9f768202d7cb1718cb121b4d9f8473cb
Alex Siman <aleksandr.siman@gmail.com>
2019-08-08 11:58:16 +0300

    Add CRUD functions

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,8 @@
 use serde_derive::{Serialize, Deserialize};
 
 use rstd::prelude::*;
-use parity_codec::Codec;
 use parity_codec_derive::{Decode, Encode};
-use srml_support::{decl_event, decl_module, decl_storage, dispatch, ensure, fail, Parameter, StorageValue, StorageMap};
-use runtime_primitives::traits::{SimpleArithmetic, As, Member, MaybeDebug, MaybeSerializeDebug};
-use system::{ensure_signed};
+use srml_support::{decl_event, decl_module, decl_storage, dispatch, ensure, StorageValue, StorageMap};
 use system;
 
 // mod mock;
@@ -30,32 +27,38 @@ mod tests;
 /// Constants
 /////////////////////////////////////////////////////////////////
 
-const MAX_NUM_OF_PROPS_PER_CLASS: u16 = 30;
+// Error messages for dispatchables
 
-/// Error messages for dispatchables
-const ERROR_NOT_IMPLEMENTED: &str = "Not implemented yet";
+const ERROR_CLASS_NOT_FOUND: &str = "Class was not found by id";
+const ERROR_CLASS_EMPTY_SCHEMAS: &str = "Class cannot have an empty list of schemas";
+const ERROR_CLASS_EMPTY_PROPS: &str = "Class cannot have an empty list of properties";
+const ERROR_CLASS_EMPTY_NAME: &str = "Class cannot have an empty name";
+const ERROR_CLASS_EMPTY_DESCRIPTION: &str = "Class cannot have an empty description";
+const ERROR_NO_PROPS_IN_CLASS_SCHEMA: &str = "Cannot add a class schema with an empty list of properties";
+const ERROR_SAME_PROPS_IN_CLASS_SCHEMA: &str = "Cannot add a class schema with the same list of properties as in the last schema of this class";
 
-const MAX_NAME_LENGTH: u16 = 100;
-const MAX_DESCRIPTION_LENGTH: u16 = 1000;
+const ERROR_ENTITY_NOT_FOUND: &str = "Entity was not found by id";
+const ERROR_ENTITY_EMPTY_SCHEMAS: &str = "Entity cannot have an empty list of schema indicies";
+const ERROR_ENTITY_EMPTY_PROPS: &str = "Entity cannot have an empty list of property values";
+const ERROR_ENTITY_EMPTY_NAME: &str = "Entity cannot have an empty name";
+const ERROR_NOTHING_TO_UPDATE_IN_ENTITY: &str = "Nothing to update in this entity";
+const ERROR_ENTITY_ALREADY_DELETED: &str = "Entity is already deleted";
+
+// const MAX_NUM_OF_PROPS_PER_CLASS: u16 = 30;
+// const MAX_NAME_LENGTH: u16 = 100;
+// const MAX_DESCRIPTION_LENGTH: u16 = 1000;
 
 pub type ClassId = u64;
 pub type EntityId = u64;
-
-/// Convenient composite time stamp 
-// #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
-// #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
-// pub struct BlockAndTime<BlockNumber, Moment> {
-//     block: BlockNumber,
-//     time: Moment
-// }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct Class {
     id: ClassId,
 
+    /// All properties that has been used on this class across different class schemas.
+    /// Unlikely to be more than roughly 20 properties per class, often less.
     /// For Person, think "height", "weight", etc.
-    /// Unlikely to be more than roughly 20ish, often less.
     properties: Vec<Property>,
 
     /// All scehmas that are available for this class, think v0.0 Person, v.1.0 Person, etc.
@@ -74,7 +77,7 @@ pub struct Entity {
     class_id: ClassId,
 
     /// What schemas under which this entity of a class is available, think
-    /// v.2.0 Person schema for John , v3.0 Person schema for John
+    /// v.2.0 Person schema for John, v3.0 Person schema for John
     /// Unlikely to be more than roughly 20ish, assuming schemas for a given class eventually stableize, or that very old schema are eventually removed.
     schemas: Vec<u16>, // indices of schema in corresponding class
 
@@ -90,8 +93,8 @@ pub struct Entity {
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct ClassSchema {
-    version: u16,
-    properties: Vec<u8> // indices into properties vector for the corresponding class
+    /// Indices into properties vector for the corresponding class.
+    properties: Vec<u16>,
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
@@ -170,13 +173,17 @@ decl_event!(
     where
         <T as system::Trait>::AccountId,
     {
-        ClassCreated(AccountId, ClassId),
-        ClassPropertyAdded(AccountId, ClassId),
-        ClassSchemaAdded(AccountId, ClassId),
+        ClassCreated(ClassId),
+        ClassPropertyAdded(ClassId, u16),
+        ClassPropertyUpdated(ClassId, u16),
+        ClassSchemaAdded(ClassId, u16),
 
-        EntityCreated(AccountId, EntityId),
-        EntityUpdated(AccountId, EntityId),
-        EntityDeleted(AccountId, EntityId),
+        EntityCreated(EntityId),
+        EntityUpdated(EntityId),
+        EntityDeleted(EntityId),
+
+        /// This is a fake event that uses AccountId type just to make Rust compiler happy to compile this module.
+        FixCompilation(AccountId),
     }
 );
 
@@ -188,50 +195,170 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
 
-    pub fn create_class(name: Vec<u8>, description: Vec<u8>, properties: Vec<Property>, schemas: Vec<ClassSchema>) -> Result<ClassId, &'static str> {
-        // TODO impl
+    pub fn create_class(
+        properties: Vec<Property>,
+        schemas: Vec<ClassSchema>,
+        name: Vec<u8>,
+        description: Vec<u8>
+    ) -> Result<ClassId, &'static str> {
+        
+        ensure!(schemas.len() > 0, ERROR_CLASS_EMPTY_SCHEMAS);
+        ensure!(properties.len() > 0, ERROR_CLASS_EMPTY_PROPS);
+        ensure!(name.len() > 0, ERROR_CLASS_EMPTY_NAME);
+        ensure!(description.len() > 0, ERROR_CLASS_EMPTY_DESCRIPTION);
 
-        // let class_id = <NextClassId<T>>::get();
+        let class_id = <NextClassId<T>>::get();
 
-        // // Create new category
-        // let new_class = Class {
-        //     // TODO finish
-        // };
+        let new_class = Class {
+            id: class_id,
+            properties,
+            schemas,
+            name,
+            description,
+        };
 
-        // // Insert category in map
-        // <ClassById<T>>::insert(new_class.id, new_class);
+        // Save newly created class:
+        <ClassById<T>>::insert(class_id, new_class);
 
-        // // Update other things
-        // <NextClassId<T>>::put(class_id + 1);
+        // Increment the next class id:
+        <NextClassId<T>>::put(class_id + 1);
 
-        // // Generate event
-        // Self::deposit_event(RawEvent::ClassCreated(account.clone(), class_id));
-
-        Err(ERROR_NOT_IMPLEMENTED)
+        Self::deposit_event(RawEvent::ClassCreated(class_id));
+        Ok(class_id)
     }
 
     pub fn add_class_property(class_id: ClassId, property: Property) -> Result<u16, &'static str> {
-        // TODO impl
-        Err(ERROR_NOT_IMPLEMENTED)
+
+        ensure!(<ClassById<T>>::exists(class_id), ERROR_CLASS_NOT_FOUND);
+
+        let mut class = <ClassById<T>>::get(class_id);
+        let prop_idx = class.properties.len() as u16;
+        class.properties.push(property);
+        <ClassById<T>>::insert(class_id, class);
+
+        Self::deposit_event(RawEvent::ClassPropertyAdded(class_id, prop_idx));
+        Ok(prop_idx)
     }
 
-    pub fn add_class_schema(class_id: ClassId, schema: ClassSchema) -> dispatch::Result {
-        // TODO impl
-        Err(ERROR_NOT_IMPLEMENTED)
+    pub fn add_class_schema(class_id: ClassId, schema: ClassSchema) -> Result<u16, &'static str> {
+        
+        ensure!(<ClassById<T>>::exists(class_id), ERROR_CLASS_NOT_FOUND);
+
+        ensure!(schema.properties.len() > 0, ERROR_NO_PROPS_IN_CLASS_SCHEMA);
+        
+        let mut class = <ClassById<T>>::get(class_id);
+
+        if let Some(prev_schema) = class.schemas.last() {
+            // Check that prev schema has a different set of props:
+            ensure!(prev_schema.properties != schema.properties, ERROR_SAME_PROPS_IN_CLASS_SCHEMA);
+        }
+
+        // TODO check that schema contains props that are in the list of class props.
+
+        let schema_idx = class.schemas.len() as u16;
+        class.schemas.push(schema);
+        <ClassById<T>>::insert(class_id, class);
+
+        Self::deposit_event(RawEvent::ClassSchemaAdded(class_id, schema_idx));
+        Ok(schema_idx)
     }
 
-    pub fn create_entity(name: Vec<u8>, class_id: ClassId, schema_indices: Vec<u16>, property_values: Vec<(u16, PropertyValue)>) -> Result<EntityId, &'static str>{
-        // TODO impl
-        Err(ERROR_NOT_IMPLEMENTED)
+    pub fn create_entity(
+        class_id: ClassId,
+        schema_indices: Vec<u16>,
+        property_values: Vec<(u16, PropertyValue)>,
+        name: Vec<u8>
+    ) -> Result<EntityId, &'static str>{
+        
+        ensure!(<ClassById<T>>::exists(class_id), ERROR_CLASS_NOT_FOUND);
+
+        ensure!(schema_indices.len() > 0, ERROR_ENTITY_EMPTY_SCHEMAS);
+        ensure!(property_values.len() > 0, ERROR_ENTITY_EMPTY_PROPS);
+        ensure!(name.len() > 0, ERROR_ENTITY_EMPTY_NAME);
+
+        // TODO check that every schema_indicies[i] < Class.schemas.size
+
+        // TODO check that every property_values[i][0] < Class.properties.size
+
+        // TODO check name length
+
+        let entity_id = <NextEntityId<T>>::get();
+
+        let new_entity = Entity {
+            id: entity_id,
+            class_id,
+            schemas: schema_indices,
+            values: property_values,
+            name,
+            deleted: false,
+        };
+
+        // Save newly created entity:
+        <EntityById<T>>::insert(entity_id, new_entity);
+
+        // Increment the next entity id:
+        <NextEntityId<T>>::put(entity_id + 1);
+
+        Self::deposit_event(RawEvent::EntityCreated(entity_id));
+        Ok(entity_id)
     }
 
-    pub fn update_entity(entity_id: EntityId, schema_indices: Vec<u16>, property_values: Vec<(u16, PropertyValue)>) -> dispatch::Result {
-        // TODO impl
-        Err(ERROR_NOT_IMPLEMENTED)
+    pub fn update_entity(
+        entity_id: EntityId,
+        schema_indices: Vec<u16>,
+        property_values: Vec<(u16, PropertyValue)>,
+        name_opt: Option<Vec<u8>>
+    ) -> dispatch::Result {
+
+        ensure!(<EntityById<T>>::exists(entity_id), ERROR_ENTITY_NOT_FOUND);
+
+        let mut entity = <EntityById<T>>::get(entity_id);
+        
+        let mut fields_updated = 0;
+
+        if let Some(name) = name_opt {
+            if entity.name != name {
+
+                // TODO check name length
+
+                entity.name = name;
+                fields_updated += 1;
+            }
+        }
+
+        if schema_indices.len() > 0 && entity.schemas != schema_indices {
+
+            // TODO validate schema_indicies
+
+            entity.schemas = schema_indices;
+            fields_updated += 1;
+        }
+
+        if property_values.len() > 0 && entity.values != property_values {
+
+            // TODO validate property_values
+            // TODO replace only passed properties, don't do a full replacement of all prop values on entity?
+
+            entity.values = property_values;
+            fields_updated += 1;
+        }
+
+        ensure!(fields_updated > 0, ERROR_NOTHING_TO_UPDATE_IN_ENTITY);
+        Self::deposit_event(RawEvent::EntityUpdated(entity_id));
+        Ok(())
     }
 
     pub fn delete_entity(entity_id: EntityId) -> dispatch::Result {
-        // TODO impl
-        Err(ERROR_NOT_IMPLEMENTED)
+        ensure!(<EntityById<T>>::exists(entity_id), ERROR_ENTITY_NOT_FOUND);
+
+        let is_deleted = <EntityById<T>>::get(entity_id).deleted;
+        ensure!(!is_deleted, ERROR_ENTITY_ALREADY_DELETED);
+
+        <EntityById<T>>::mutate(entity_id, |x| {
+            x.deleted = true;
+        });
+
+        Self::deposit_event(RawEvent::EntityDeleted(entity_id));
+        Ok(())
     }
 }
