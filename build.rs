dfaa91176588976ee670e3c1e7bc1750cfd1b907
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-08-29 15:09:30 +0300

    rustfmt

--- build.rs
+++ build.rs
@@ -33,12 +33,11 @@ seems `rls run ...` is being run instead of `rustup run ...`
 */
 
 fn main() {
-	build_current_project_with_rustflags(
-		"wasm_binary.rs",
-		WasmBuilderSource::Crates("1.0.4"),
-		// This instructs LLD to export __heap_base as a global variable, which is used by the
-		// external memory allocator.
-		"-Clink-arg=--export=__heap_base",
-	);
+    build_current_project_with_rustflags(
+        "wasm_binary.rs",
+        WasmBuilderSource::Crates("1.0.4"),
+        // This instructs LLD to export __heap_base as a global variable, which is used by the
+        // external memory allocator.
+        "-Clink-arg=--export=__heap_base",
+    );
 }
-
