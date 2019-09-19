01ea15b4b9c9df6353bb30b9bda01d3a604c08e9
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-09-19 10:28:35 +0300

    setup stake pool module funds

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,26 @@
 use rstd::prelude::*;
 
 use codec::{Decode, Encode};
+use runtime_primitives::traits::{AccountIdConversion, Zero};
+use runtime_primitives::ModuleId;
+use srml_support::traits::{Currency, Get};
 use srml_support::{
     decl_event, decl_module, decl_storage, dispatch, ensure, StorageMap, StorageValue,
 };
-
-use minting;
 use system;
 
-pub trait Trait: system::Trait + minting::Trait + Sized {}
+pub type BalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+
+pub trait Trait: system::Trait + Sized {
+    /// The currency that is managed by the module
+    type Currency: Currency<Self::AccountId>;
+
+    type StakePoolId: Get<[u8; 8]>;
+}
 
 decl_storage! {
-    trait Store for Module<T: Trait> as RecurringReward {
+    trait Store for Module<T: Trait> as StakePool {
 
     }
 }
@@ -26,4 +35,17 @@ decl_module! {
     }
 }
 
-impl<T: Trait> Module<T> {}
+impl<T: Trait> Module<T> {
+    /// The account ID of the stake pool.
+    ///
+    /// This actually does computation. If you need to keep using it, then make sure you cache the
+    /// value and only call this once.
+    pub fn account_id() -> T::AccountId {
+        // MODULE_ID.into_account()
+        ModuleId(T::StakePoolId::get()).into_account()
+    }
+
+    pub fn balance() -> BalanceOf<T> {
+        T::Currency::free_balance(&Self::account_id())
+    }
+}
