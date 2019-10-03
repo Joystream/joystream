aae8140812d666d61185882aead18d8c31eab9f6
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-10-03 12:48:13 +0300

    better layout of all error types

--- src/errors.rs
+++ src/errors.rs
@@ -4,12 +4,6 @@ pub enum StakeActionError<ErrorType> {
     Error(ErrorType),
 }
 
-impl<ErrorType> From<ErrorType> for StakeActionError<ErrorType> {
-    fn from(e: ErrorType) -> StakeActionError<ErrorType> {
-        StakeActionError::Error(e)
-    }
-}
-
 #[derive(Debug, Eq, PartialEq)]
 pub enum TransferFromAccountError {
     InsufficientBalance,
@@ -28,22 +22,6 @@ pub enum StakingFromAccountError {
     InsufficientBalanceInSourceAccount,
 }
 
-impl From<StakingError> for StakeActionError<StakingFromAccountError> {
-    fn from(e: StakingError) -> StakeActionError<StakingFromAccountError> {
-        StakeActionError::Error(StakingFromAccountError::StakingError(e))
-    }
-}
-
-impl From<TransferFromAccountError> for StakeActionError<StakingFromAccountError> {
-    fn from(e: TransferFromAccountError) -> StakeActionError<StakingFromAccountError> {
-        match e {
-            TransferFromAccountError::InsufficientBalance => {
-                StakeActionError::Error(StakingFromAccountError::InsufficientBalanceInSourceAccount)
-            }
-        }
-    }
-}
-
 #[derive(Debug, Eq, PartialEq)]
 pub enum IncreasingStakeError {
     NotStaked,
@@ -55,37 +33,6 @@ pub enum IncreasingStakeError {
 pub enum IncreasingStakeFromAccountError {
     IncreasingStakeError(IncreasingStakeError),
     InsufficientBalanceInSourceAccount,
-}
-
-impl From<IncreasingStakeError> for StakeActionError<IncreasingStakeFromAccountError> {
-    fn from(e: IncreasingStakeError) -> StakeActionError<IncreasingStakeFromAccountError> {
-        StakeActionError::Error(IncreasingStakeFromAccountError::IncreasingStakeError(e))
-    }
-}
-
-impl From<TransferFromAccountError> for StakeActionError<IncreasingStakeFromAccountError> {
-    fn from(e: TransferFromAccountError) -> StakeActionError<IncreasingStakeFromAccountError> {
-        match e {
-            TransferFromAccountError::InsufficientBalance => StakeActionError::Error(
-                IncreasingStakeFromAccountError::InsufficientBalanceInSourceAccount,
-            ),
-        }
-    }
-}
-
-impl From<StakeActionError<IncreasingStakeError>>
-    for StakeActionError<IncreasingStakeFromAccountError>
-{
-    fn from(
-        e: StakeActionError<IncreasingStakeError>,
-    ) -> StakeActionError<IncreasingStakeFromAccountError> {
-        match e {
-            StakeActionError::StakeNotFound => StakeActionError::StakeNotFound,
-            StakeActionError::Error(increasing_stake_error) => StakeActionError::Error(
-                IncreasingStakeFromAccountError::IncreasingStakeError(increasing_stake_error),
-            ),
-        }
-    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -146,4 +93,57 @@ pub enum ResumeUnstakingError {
     NotUnstaking,
     NotPaused,
     SlashPeriodShouldBeGreaterThanZero,
+}
+
+impl<ErrorType> From<ErrorType> for StakeActionError<ErrorType> {
+    fn from(e: ErrorType) -> StakeActionError<ErrorType> {
+        StakeActionError::Error(e)
+    }
+}
+
+impl From<StakingError> for StakeActionError<StakingFromAccountError> {
+    fn from(e: StakingError) -> StakeActionError<StakingFromAccountError> {
+        StakeActionError::Error(StakingFromAccountError::StakingError(e))
+    }
+}
+
+impl From<TransferFromAccountError> for StakeActionError<StakingFromAccountError> {
+    fn from(e: TransferFromAccountError) -> StakeActionError<StakingFromAccountError> {
+        match e {
+            TransferFromAccountError::InsufficientBalance => {
+                StakeActionError::Error(StakingFromAccountError::InsufficientBalanceInSourceAccount)
+            }
+        }
+    }
+}
+
+impl From<IncreasingStakeError> for StakeActionError<IncreasingStakeFromAccountError> {
+    fn from(e: IncreasingStakeError) -> StakeActionError<IncreasingStakeFromAccountError> {
+        StakeActionError::Error(IncreasingStakeFromAccountError::IncreasingStakeError(e))
+    }
+}
+
+impl From<TransferFromAccountError> for StakeActionError<IncreasingStakeFromAccountError> {
+    fn from(e: TransferFromAccountError) -> StakeActionError<IncreasingStakeFromAccountError> {
+        match e {
+            TransferFromAccountError::InsufficientBalance => StakeActionError::Error(
+                IncreasingStakeFromAccountError::InsufficientBalanceInSourceAccount,
+            ),
+        }
+    }
+}
+
+impl From<StakeActionError<IncreasingStakeError>>
+    for StakeActionError<IncreasingStakeFromAccountError>
+{
+    fn from(
+        e: StakeActionError<IncreasingStakeError>,
+    ) -> StakeActionError<IncreasingStakeFromAccountError> {
+        match e {
+            StakeActionError::StakeNotFound => StakeActionError::StakeNotFound,
+            StakeActionError::Error(increasing_stake_error) => StakeActionError::Error(
+                IncreasingStakeFromAccountError::IncreasingStakeError(increasing_stake_error),
+            ),
+        }
+    }
 }
