056cb23aa2576d9225e2eb174ec1f125aa2730ae
Bedeho Mender <bedeho@users.noreply.github.com>
2019-10-30 17:45:16 +0100

    wip

--- src/content-working-group/lib.rs
+++ src/content-working-group/lib.rs
@@ -1,10 +1,7 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "std")]
-//use serde_derive::{Deserialize, Serialize};
 use rstd::prelude::*;
-
 use codec::{Codec, Decode, Encode};
 use runtime_primitives::traits::{MaybeSerializeDebug, Member, One, SimpleArithmetic};
 use srml_support::traits::Currency;
@@ -12,10 +9,6 @@ use srml_support::{
     decl_module, decl_storage, ensure, EnumerableStorageMap, Parameter, StorageMap, StorageValue,
 };
 use std::iter::Iterator;
-
-use runtime_primitives::traits::Zero;
-
-//use crate::sr_api_hidden_includes_decl_storage::hidden_include::traits::Imbalance;
 
 use rstd::collections::btree_map::BTreeMap;
 use rstd::collections::btree_set::BTreeSet;
@@ -25,136 +18,239 @@ mod macroes;
 mod mock;
 mod test;
 
-use hiring::*;
+pub use types;
+
 use system;
-
-use stake;
-
-/// Module configuration trait for this Substrate module.
-pub trait Trait: system::Trait + minting::Trait + RecurringReward::Trait + stake::Trait + Hiring::Trait + VersionedStorePermissions::Trait + Membership::Trait + Sized {
-
-    /// Type for identifier for curators.
-    type CuratorId: Parameter
-        + Member
-        + SimpleArithmetic
-        + Codec
-        + Default
-        + Copy
-        + MaybeSerializeDebug
-        + PartialEq;
-
-    /// Type for identifier for channels.
-    type ChannelId: Parameter
-        + Member
-        + SimpleArithmetic
-        + Codec
-        + Default
-        + Copy
-        + MaybeSerializeDebug
-        + PartialEq;
-
-    /// Type for identifier for dynamic version store credential.
-    type DynamicVersionedStoreCredentialId: Parameter
-        + Member
-        + SimpleArithmetic
-        + Codec
-        + Default
-        + Copy
-        + MaybeSerializeDebug
-        + PartialEq;
-}
-
-pub type BalanceOf<T> =
-    <<T as stake::Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
-
-pub type NegativeImbalance<T> =
-    <<T as stake::Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::NegativeImbalance;
 
 decl_storage! {
     trait Store for Module<T: Trait> as ContentWorkingGroup {
 
-        /// 
-        /// 
+        /// The mint currently funding the rewards for this module.
+        pub Mint get(mint) config(): T::TokenMintId; 
 
-        /// Whether
-        pub LeadCredential get(lead_credential): BuiltInVersionStoreCredential;
+        /// The current lead.
+        pub CurrentLead get(current_lead) config(): Option<T::LeadId>;
+
+        /// Maps identifier to corresponding lead.
+        pub LeadById get(lead_by_id) config(): linked_map T::LeadId => Lead<T::AccountId, RewardRelationshipId, T::BlockNumber>;
+
+        /// Next identifier for new current lead.
+        pub NextLeadId get(next_lead_id) config(): T::LeadId;
+
+        /// Set of identifiers for all openings originated from this group.
+        /// Using map to model a set.
+        pub Openings get(openings) config(): linked_map T::OpeningId => ();
+
+        /// Maps identifier to corresponding curator.
+        pub CuratorById get(curator_by_id) config(): linked_map T::CuratorId => Curator<T>;
+        
+        /// Next identifier for new curator.
+        pub NextCuratorId get(next_curator_id) config(): T::CuratorId;
+
+        /// The constraints lead must respect when creating a new curator opening.
+        /// Lack of policy is interpreted as blocking any new openings at all.
+        pub OpeningPolicy get(opening_policy) config(): Option<OpeningPolicy<T::BlockNumber, hiring::StakingPolicy<BalanceOf<T>, T::BlockNumber>>>;
+
+        /// Credentials for built in roles.
+        pub LeadCredential get(lead_credential) config(): LeadCredential;
+
+        /// The "any curator" credential.
+        pub AnyCuratorCredential get(any_curator_credential) config(): AnyCuratorCredential;
+
+        /// The "any member" credential.
+        pub AnyMemberCredential get(any_member_credential) config(): AnyMemberCredential;
+
+        /// Maps dynamic credential by
+        pub DynamicCredentialById get(dynamic_credential_by_id) config(): linked_map DynamicCredentialId => DynamicCredential<T::CuratorId, T::ChannelId>;
 
         /// ...
-        pub DynamicVersionedStoreCredentialById get(dynamic_versioned_store_credential_by_id): linked_map T::DynamicVersionedStoreCredentialId => DynamicVersionedStoreCredential<T>
+        pub NextDynamicCredentialId get(next_dynamic_credential_id) config(): T::DynamicCredentialId;
 
-        /// ...
-        pub NextDynamicVersionedStoreCredentialId: T::DynamicVersionedStoreCredentialId;
+
+        // Input guards
+
+        // TODO: use proper input constraint types
+
+        /// Upper bound for character length of description field of any new or updated PermissionGroup 
+        pub MaxPermissionGroupDescriptionLength get(max_permission_group_description_length) config(): u16;
+
+        /// Upper bound for character length of the rationale_text field of any new CuratorRoleStage.
+        pub MaxCuratorExitRationaleTextLength get(max_curator_exit_rationale_text_length) config(): u16;
 
     }
 }
+        /*
+/// Substrate module events.
+decl_event! {
+    pub enum Event<T> where
+      <T as system::Trait>::AccountId,
+      <T as Trait>::MemberId,
+      <T as Trait>::ActorId, 
+      {
+
+        LeadSet
+        LeadUnset
+        OpeningPolicySet
+        LeadRewardUpdated
+        LeadRoleAccountUpdated
+        LeadRewardAccountUpdated
+        PermissionGroupAdded
+        PermissionGroupUpdated
+        CuratorOpeningAdded
+        AcceptedCuratorApplications
+        BeganCuratorApplicationReview
+        CuratorOpeningFilled
+        CuratorSlashed
+        TerminatedCurator
+        AppliedOnCuratorOpening
+        CuratorRewardUpdated
+        CuratorRoleAccountUpdated
+        CuratorRewardAccountUpdated
+        CuratorExited
+        
+    }
+}
+*/
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-        /*
+        fn deposit_event() = default;
+
+
 
         /// ...
-        pub fn create_channel(type: ChannelType, owner: Trait::MemberId);
+        pub fn create_channel(type: ChannelType, owner: Trait::MemberId) {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn transfer_channel_ownerhsip(channel_id: T::ChannelId);
+        pub fn transfer_channel_ownerhsip(channel_id: T::ChannelId) {
+
+            // DONE
+            Ok(())
+        }
 
         // perhaps curation can be done here in one go.
 
-        /// ...
-        pub fn update_channel_as_owner();
+        
 
         /// ...
-        pub fn update_channel_as_curator();
+        pub fn update_channel_as_owner() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn update_lead_role_account();
+        pub fn update_channel_as_curator() {
+
+            // DONE
+            Ok(())
+        }
+
+        /// ..
+        pub fn create_version_store_credential()  {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn update_lead_reward_account();
+        pub fn update_lead_role_account() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn add_permission_group();
+        pub fn update_lead_reward_account()  {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn update_permission_group();
+        pub fn add_curator_opening()  {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn add_curator_opening();
+        pub fn accept_curator_applications()  {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn accept_curator_applications();
+        pub fn begin_curator_applicant_review() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn begin_curator_applicant_review();
+        pub fn fill_curator_opening() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn fill_curator_opening();
+        pub fn update_curator_reward() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn update_curator_reward();
+        pub fn slash_curator() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn slash_curator();
+        pub fn terminate_curator() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn terminate_curator();
+        pub fn apply_on_curator_opening() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn apply_on_curator_opening();
+        pub fn update_curator_role_account() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn update_curator_role_account();
+        pub fn update_curator_reward_account() {
+
+            // DONE
+            Ok(())
+        }
 
         /// ...
-        pub fn update_curator_reward_account();
+        pub fn exit_curator_role() {
 
-        /// ...
-        pub fn exit_curator_role();
-        */
-
+            // DONE
+            Ok(())
+        }
 
         fn on_finalize(now: T::BlockNumber) {
+
         }
     }
 }
@@ -185,23 +281,24 @@ impl<T: Trait> Module<T> {
  *  ======== ======== ======== ======== =======
  */
 
-enum GetCredentialFromIdResult {
-    InvalidId
-    BuiltIn(BuiltInVersionStoreCredential),
+/// ...
+enum Credential {
+    Lead(LeadCredential),
+    AnyCurator(AnyCuratorCredential),
+    AnyMember(AnyMemberCredential)
     Dynamic(DynamicVersionedStoreCredential)
 }
 
 impl<T: Trait> Module<T> {
     
-
-
-    fn get_credential_from_id(credential_id: VersionedStorePermissions::Trait::CredentialId) -> GetCredentialFromIdResult {
+    /// .
+    fn credential_from_id(credential_id: VersionedStorePermissions::Trait::CredentialId) -> Option<Credential> {
 
         let  = credential_id_to_built_in_credential_holder(credential_id);
 
         // 2. 
 
-        
+
 
     }
     
--- src/content-working-group/types.rs
+++ src/content-working-group/types.rs
@@ -1,23 +1,63 @@
 use codec::{Decode, Encode};
-//use rstd::collections::btree_map::BTreeMap;
+use rstd::collections::btree_map::BTreeMap;
 use rstd::collections::btree_set::BTreeSet;
 
-use crate::{Trait};
+use minting;
+use recurringrewards;
+use stake;
+use hiring;
+use versioned-store-permissions;
 
+/// Module configuration trait for this Substrate module.
+pub trait Trait: system::Trait + minting::Trait + RecurringReward::Trait + stake::Trait + Hiring::Trait + VersionedStorePermissions::Trait + Membership::Trait + Sized {
+
+    /// Type for identifier for curators.
+    type CuratorId: Parameter
+        + Member
+        + SimpleArithmetic
+        + Codec
+        + Default
+        + Copy
+        + MaybeSerializeDebug
+        + PartialEq;
+
+    /// Type for identifier for channels.
+    type ChannelId: Parameter
+        + Member
+        + SimpleArithmetic
+        + Codec
+        + Default
+        + Copy
+        + MaybeSerializeDebug
+        + PartialEq;
+}
+
+/// Type for identifier for dynamic version store credential.
+pub type DynamicCredentialId = VersionedStorePermissions::Trait::CredentialId;
+
+/// Balance type of runtime
+pub type BalanceOf<T> =
+    <<T as stake::Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+
+/// Negative imbalance of runtime.
+pub type NegativeImbalance<T> =
+    <<T as stake::Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::NegativeImbalance;
+
+/// The exit stage of a lead involvement in the working group.
 pub struct ExitedLeadRole<BlockNumber> {
 
-    /// ...
+    /// When exit was initiated.
     pub initiated_at_block_number: BlockNumber
 }
 
-/// ...
+/// The stage of the involvement of a lead in the working group.
 #[derive(Encode, Decode, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
 pub enum LeadRoleState<BlockNumber> {
 
-    /// ...
+    /// Currently active.
     Active,
-    
-    /// ...
+
+    /// No longer active, for some reason
     Exited(ExitedLeadRole<BlockNumber>)
 }
 
@@ -33,48 +73,49 @@ pub struct Lead<AccountId, RewardRelationshipId, BlockNumber> {
     pub reward_relationship: Option<RewardRelationshipId>,
 
     /// When was inducted
-    /// TODO: Add richer information about circumstances of induction
+    /// TODO: Add richer information about circumstances of induction, like referencing a council proposal?
     pub inducted: BlockNumber,
 
-    /// ... 
+    /// The stage of the involvement of this lead in the working group.
     pub stage: LeadRoleState<BlockNumber>
 }
 
-/// Represents ...
+/// Origin of exit initiation on behalf of a curator.'
 #[derive(Encode, Decode, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
 pub enum CuratorExitInitiationOrigin {
 
-    /// ...
+    /// Lead is origin.
     Lead,
 
-    /// ...
+    /// The curator exiting is the origin.
     Curator
 }
 
+/// The exit stage of a curators involvement in the working group.
 pub struct ExitedCuratorRoleStage<BlockNumber> {
 
-    /// ...
+    /// Origin for exit.
     pub origin: CuratorExitInitiationOrigin,
 
-    /// ...
+    /// When exit was initiated.
     pub initiated_at_block_number: BlockNumber,
 
-    /// ...
-    pub rationale_text: Vec<u8> // <== needs constrainst in extrinsics
+    /// Explainer for why exit was initited.
+    pub rationale_text: Vec<u8>
 }
 
-/// Represents ...
+/// The stage of the involvement of a curator in the working group.
 #[derive(Encode, Decode, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
 pub enum CuratorRoleStage<BlockNumber> {
 
-    /// ...
+    /// Currently active.
     Active,
 
-    /// ...
+    /// No longer active, for some reason
     Exited(ExitedCuratorRoleStage<BlockNumber>)
 }
 
-/// Represents ...
+/// The induction of a curator in the working group.
 pub struct CuratorInduction<LeadId, ApplicationId, BlockNumber> {
 
     /// Lead responsible
@@ -87,8 +128,8 @@ pub struct CuratorInduction<LeadId, ApplicationId, BlockNumber> {
     pub at_block: BlockNumber // wasn't there some composte type here?
 }
 
-// Working group participant: curator
-// This role can be staked, have reward and be inducted through the hiring module.
+/// Working group participant: curator
+/// This role can be staked, have reward and be inducted through the hiring module.
 #[derive(Encode, Decode, Default, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
 pub struct Curator<AccountId, RewardRelationshipId, StakeId> {
 
@@ -101,155 +142,184 @@ pub struct Curator<AccountId, RewardRelationshipId, StakeId> {
     /// Whether participant is staked, and if so, the identifier for this staking in the staking module.
     pub stake: Option<StakeId>,
 
-    /// ...
+    /// The stage of this curator in the working group.
     pub stage: CuratorRoleStage<BlockNumber>,
 
-    /// ..
+    /// How the curator was inducted into the working group.
     pub induction: CuratorInduction<T>
 }
 
-///
-pub enum ChannelType {
+/// Type of channel content.
+pub enum ChannelContentType {
     Video,
     Music,
     Ebook
 }
 
-/// 
+/// Status of channel, as set by the owner.
+/// Is only meant to affect visibility, mutation of channel and child content
+/// is unaffected on runtime.
 pub enum ChannelPublishingStatus {
+
+    /// Compliant UIs should render.
     Published,
+    
+    /// Compliant UIs should not render it or any child content.
     NotPublished
 }
 
-///
+/// Status of channel, as set by curators.
+/// Is only meant to affect visibility currently, but in the future
+/// it will also gate publication of new child content,
+/// editing properties, revenue flows, etc. 
 pub enum ChannelCurationStatus {
     Normal,
     Censored
 }
 
-/// 
+/// A channel for publishing content.
 pub struct Channel<BlockNumber> {
 
-    ///
-    pub handle: , 
+    /// Unique human readble channel handle.
+    pub handle: Vec<u8>, 
 
-    /// 
-    pub description: ,
+    /// Whether channel has been verified, in the normal Web2.0 platform sense of being authenticated.
+    pub verified: bool,
 
-    /// The type of channel
-    pub type: ChannelType,
+    /// Human readable description of channel purpose and scope.
+    pub description: Vec<u8>,
 
-    /// Member who owns channel
+    /// The type of channel.
+    pub content: ChannelContentType,
+
+    /// Member who owns channel.
     pub owner: Trait::MemberId,
 
     /// Account used to authenticate as owner.
     /// Can be updated through membership role key.
     pub role_account: AccountId,
 
-    ///
-    pub publishing_status: 
+    /// Publication status of channel.
+    pub publishing_status: ChannelPublishingStatus,
 
-    ///
+    /// Curation status of channel.
     pub curation_status: ChannelCurationStatus,
 
-    /// When channel was established
+    /// When channel was established.
     pub created: BlockNumber
 
 }
 
-/// 
+/// The types of built in credential holders.
 pub enum BuiltInCredentialHolder {
 
-    /// 
-    CurrentLead,
+    /// Cyrrent working group lead.
+    Lead,
     
-    ///
+    /// Any active urator in the working group.
     AnyCurator,
 
-    /// 
+    /// Any active member in the membership registry.
     AnyMember
 }
 
-
-/// ...
-pub static CURRENT_LEAD_CREDENTIAL_ID: VersionedStorePermissions::Trait::CredentialId = VersionedStorePermissions::Trait::CredentialId::from(0);
+/// Credential identifiers for built in credential holder types.
+pub static LEAD_CREDENTIAL_ID: VersionedStorePermissions::Trait::CredentialId = VersionedStorePermissions::Trait::CredentialId::from(0);
 pub static ANY_CURATOR_CREDENTIAL_ID: VersionedStorePermissions::Trait::CredentialId = VersionedStorePermissions::Trait::CredentialId::from(1);
 pub static ANY_MEMBER_CREDENTIAL_ID: VersionedStorePermissions::Trait::CredentialId = VersionedStorePermissions::Trait::CredentialId::from(2);
 
-/// ...
+/// Holder of dynamic credential.
 #[derive(Encode, Decode, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
 pub enum DynamicCredentialHolder<CuratorId, ChannelId> {
 
-    ///
+    /// Sets of curators.
     Curators(BTreeSet<CuratorId>),
 
-    /// 
+    /// Owner of a channel.
     ChannelOwner(ChannelId),
 }
 
-/*
-/// ...
+/// Holder of a credential.
 enum CredentialHolder {
 
-    /// ..
-    BuiltInHolder(BuiltInHolder),
-
-    /// ..
-    DynamicHolder(DynamicCredentialHolder)
-}
-*/
-
-enum CredentialIdToHolderMappingResult {
-
-    ///
+    /// Built in credential holder.
     BuiltInCredentialHolder(BuiltInCredentialHolder),
 
-    /// ...
-    CandidateDynamicVersionedStoreCredentialId(VersionedStorePermissions::Trait::CredentialId)
+    /// A possible dynamic credendtial holder.
+    CandidateDynamicCredentialId(DynamicCredentialId)
 }
 
-/// ...
-fn credential_id_to_built_in_credential_holder(credential_id: VersionedStorePermissions::Trait::CredentialId) -> CredentialIdToHolderMappingResult {
+/// Maps a permission module credential identifier to a credential holder.
+/// 
+/// **CRITICAL**: 
+/// 
+/// Credential identifiers are stored in the permissions module, this means that
+/// the mapping in this function _must_ not disturb how it maps any id that is actually in use
+/// across runtime upgrades, _unless_ one is also prepared to make the corresponding migrations
+/// in the permissions module. Best to keep mapping stable.
+/// 
+/// In practice the only way one may want augment this map is to support new
+/// built in credentials. In this case, the mapping has to be written and deployed while
+/// no new dynamic credentials are created, and a new case of the form below must be introcued
+/// in the match: CandidateDynamicCredentialId(credential_id - X), where X = #ChannelIds mapped so far.
+fn credential_id_to_holder(credential_id: VersionedStorePermissions::Trait::CredentialId) -> CredentialHolder {
 
     match credential_id {
 
-        CURRENT_LEAD_CREDENTIAL_ID: BuiltInCredentialHolder(BuiltInCredentialHolder::CurrentLead),
+        LEAD_CREDENTIAL_ID: BuiltInCredentialHolder(BuiltInCredentialHolder::Lead),
         ANY_CURATOR_CREDENTIAL_ID: BuiltInCredentialHolder(BuiltInCredentialHolder::AnyCurator),
         ANY_MEMBER_CREDENTIAL_ID: BuiltInCredentialHolder(BuiltInCredentialHolder::AnyMember),
-        _ => CandidateDynamicVersionedStoreCredentialId(credential_id - ANY_MEMBER_CREDENTIAL_ID)
+        _ => CandidateDynamicCredentialId(credential_id - 3) // will map first dynamic id to 0
+
+        /*
+         Add new built in credentials here below
+        */
     }
 }
 
+/// Represents credential for authenticating as "the current lead".
+pub struct LeadCredential {
 
-pub struct BuiltInVersionStoreCredential {
-
-    /// Whether it is currently possible to authenticate with this credential,
-    /// and also join.
+    /// Whether it is currently possible to authenticate with this credential.
     pub is_active: bool
 }
 
-/// Represents a group as understood by the VersionedStorePermissions module
-#[derive(Encode, Decode, Default, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
-pub struct DynamicVersionedStoreCredential<T: Trait> {
+/// Represents credential for authenticating as "any curator".
+pub struct AnyCuratorCredential {
 
-    /// ..
-    pub holder: DynamicCredentialHolder<,>,
+    /// Whether it is currently possible to authenticate with this credential.
+    pub is_active: bool
+}
 
-    /// Whether it is currently possible to authenticate with this credential,
-    /// and also join.
+/// Represents credential for authenticating as "any member".
+pub struct AnyMemberCredential {
+
+    /// Whether it is currently possible to authenticate with this credential.
+    pub is_active: bool
+}
+
+/// Represents credential to be referenced from the version store.
+/// It is dynamic in the sense that these can be created on the fly.
+#[derive(Encode, Decode, Default, Debug, Eq, PartialEq, Clone, PartialOrd)]
+pub struct DynamicCredential<CuratorId, ChannelIdt> {
+
+    /// Who holds this credential, meaning they can successfully authenticate with this credential.
+    pub holder: DynamicCredentialHolder<CuratorId, ChannelId>,
+
+    /// Whether it is currently possible to authenticate with this credential.
     pub is_active: bool,
 
-    /// ..
+    /// When it was created.
     pub created: T::BlockNumber,
 
-    /// ..
+    /// Human readable description of credential.
     pub description: Vec<u8>
 }
 
 /// Policy governing any curator opening which can be made by lead.
 /// Be aware that all limits are forward looking in constrainign future extrinsics or method calls.
 /// Updating them has no side-effects beyond changing the limit.
-#[derive(Encode, Decode, Default, Debug, Eq, PartialEq, Clone, PartialOrd, Ord)]
+#[derive(Encode, Decode, Default, Debug, Eq, PartialEq, Clone, PartialOrd)]
 pub struct OpeningPolicy<BlockNumber, StakingPolicy> {
 
     /// Limits the total number of curators which can be active, or possibly active through an active opening. 
