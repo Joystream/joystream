2acc83e526e316d845aa8b6bfe58372ac3e42ded
Paul M Fox <iam@yourheropaul.com>
2019-10-31 08:40:35 +0000

    Added mock router
    This allows the use of <Link> elements

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import '@storybook/addon-console';
+import StoryRouter from 'storybook-react-router';
+ 
+addDecorator(StoryRouter());
 
 configure(require.context('../packages', true, /\.stories\.tsx?$/), module)
