82e365a8ae9aa9b4b248f6b12784a5984e9aacc5
Paul M Fox <iam@yourheropaul.com>
2019-10-31 11:03:24 +0000

    Introduce global padding div

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import { configure, addDecorator } from '@storybook/react';
 import '@storybook/addon-console';
 import StoryRouter from 'storybook-react-router';
  
 addDecorator(StoryRouter());
+
+addDecorator(story => (
+  <div style={{padding: '1em'}}>{story()}</div>
+));
 
 configure(require.context('../packages', true, /\.stories\.tsx?$/), module)
