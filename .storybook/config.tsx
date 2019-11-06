1f583435d490ba393def522201e8f7416795db4c
Alex Siman <aleksandr.siman@gmail.com>
2019-11-06 11:49:11 +0200

    Import Semantic UI styles in Storybook config

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { configure, addDecorator } from '@storybook/react';
 import '@storybook/addon-console';
 import StoryRouter from 'storybook-react-router';
- 
+
+import 'semantic-ui-css/semantic.min.css'
+import './style.css'
+
 addDecorator(StoryRouter());
 
 addDecorator(story => (
-  <div style={{padding: '1em'}}>{story()}</div>
+  <div className='StorybookRoot'>{story()}</div>
 ));
 
 configure(require.context('../packages', true, /\.stories\.tsx?$/), module)
