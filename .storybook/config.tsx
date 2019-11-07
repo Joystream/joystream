62353fd3d5ceacc1cc9d816d8cf9fa068e46d0dd
Alex Siman <aleksandr.siman@gmail.com>
2019-11-07 19:12:26 +0200

    Add Joystream CSS to storybook

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -3,13 +3,17 @@ import { configure, addDecorator } from '@storybook/react';
 import '@storybook/addon-console';
 import StoryRouter from 'storybook-react-router';
 
+import GlobalStyle from '@polkadot/react-components/styles';
 import 'semantic-ui-css/semantic.min.css'
 import './style.css'
 
 addDecorator(StoryRouter());
 
 addDecorator(story => (
-  <div className='StorybookRoot'>{story()}</div>
+  <div className='StorybookRoot'>
+    <GlobalStyle />
+    {story()}
+  </div>
 ));
 
 configure(require.context('../packages', true, /\.stories\.tsx?$/), module)
