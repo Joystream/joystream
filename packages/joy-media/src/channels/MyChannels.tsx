dfacd87927b1547cb87b6ed594f7bc0e3cb180ae
Alex Siman <aleksandr.siman@gmail.com>
2019-11-22 00:21:08 +0200

    Add filtering tabs to My channels page

--- packages/joy-media/src/channels/MyChannels.tsx
+++ packages/joy-media/src/channels/MyChannels.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { Button, Segment, Statistic, Message, Icon, Label, SemanticICONS, SemanticCOLORS } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Segment, Statistic, Icon, Label, SemanticICONS, SemanticCOLORS, Button, Tab } from 'semantic-ui-react';
 import { ChannelEntity } from '../entities/MusicChannelEntity';
 import { YouHaveNoChannels } from './YouHaveNoChannels';
 import Section from '@polkadot/joy-utils/Section';
-import { onImageError } from '../utils';
 import { formatNumber } from '@polkadot/util';
 
 type Props = {
@@ -11,75 +10,126 @@ type Props = {
   channels?: ChannelEntity[]
 };
 
-export function MyChannels (props: Props) {
-  const { suspended = false, channels = [] } = props;
+const TabsAndChannels = (props: Props) => {
+  const { channels: allChannels = [] } = props;
+  const [ channels, setChannels ] = useState(allChannels);
 
-  const renderChannelPreview = (channel: ChannelEntity) => {
-    const avatarStyle = {
-      backgroundImage: `url(${channel.avatarUrl})`
+  let videoChannelsCount = 0;
+  let musicChannelsCount = 0;
+  allChannels.forEach(x => {
+    if (x.contentType === 'video') {
+      videoChannelsCount++;
+    } else if (x.contentType === 'music') {
+      musicChannelsCount++;
     }
+  });
 
-    const statSize = 'tiny';
+  const panes = [
+    { menuItem: `All (${allChannels.length})` },
+    { menuItem: `Video (${videoChannelsCount})` },
+    { menuItem: `Music (${musicChannelsCount})` }
+  ];
 
-    let itemsPublishedLabel = ''
-    if (channel.contentType === 'video') {
-      itemsPublishedLabel = 'Videos'
-    } else if (channel.contentType === 'music') {
-      itemsPublishedLabel = 'Music tracks'
+  const contentTypeByTabIndex = [ undefined, 'video', 'music' ];
+
+  const switchTab = (activeIndex: number) => {
+    const activeContentType = contentTypeByTabIndex[activeIndex];
+    if (activeContentType === undefined) {
+      setChannels(allChannels)
+    } else {
+      setChannels(allChannels.filter(
+        x => x.contentType === activeContentType)
+      )
     }
+  }
 
-    let visibilityIcon: SemanticICONS = 'eye';
-    let visibilityColor: SemanticCOLORS = 'green';
-    if (channel.visibility === 'Unlisted') {
-      visibilityIcon = 'eye slash';
-      visibilityColor = 'orange'
-    }
-  
-    return <Segment padded>
-      <div className='ChannelPreview'>
-        <div className='ChannelAvatar' style={avatarStyle} />
+  return <>
+    <Tab
+      panes={panes}
+      menu={{ secondary: true }}
+      style={{ display: 'inline-flex', margin: '1rem 2rem 1rem 0' }}
+      onTabChange={(_e, data) => switchTab(data.activeIndex as number)}
+    />
+    <Button color='blue' icon='plus' content='Create Channel' />
+    {channels.map(x => <ChannelPreview channel={x} />)}
+  </>
+}
 
-        <div className='ChannelDetails'>
-          <h2 className='ChannelTitle'>{channel.title}</h2>
-          <p>{channel.description}</p>
+type ChannelPreviewProps = {
+  channel: ChannelEntity
+};
 
-          <Label basic size='large' color={visibilityColor} style={{ marginRight: '1rem' }}>
-            <Icon name={visibilityIcon} />
-            {channel.visibility}
-          </Label>
+const ChannelPreview = (props: ChannelPreviewProps) => {
+  const { channel } = props;
 
-          {channel.blocked && <Label basic size='large' color='red'>
-            <Icon name='dont' />
-            Channel blocked
-            {' '}<Icon name='question circle outline' size='small' />
-          </Label>}
+  const avatarStyle = {
+    backgroundImage: `url(${channel.avatarUrl})`
+  }
+
+  const statSize = 'tiny';
+
+  let itemsPublishedLabel = ''
+  if (channel.contentType === 'video') {
+    itemsPublishedLabel = 'Videos'
+  } else if (channel.contentType === 'music') {
+    itemsPublishedLabel = 'Music tracks'
+  }
+
+  let visibilityIcon: SemanticICONS = 'eye';
+  let visibilityColor: SemanticCOLORS = 'green';
+  if (channel.visibility === 'Unlisted') {
+    visibilityIcon = 'eye slash';
+    visibilityColor = 'orange'
+  }
+
+  return <Segment padded>
+    <div className='ChannelPreview'>
+      <div className='ChannelAvatar' style={avatarStyle} />
+
+      <div className='ChannelDetails'>
+        <h2 className='ChannelTitle'>{channel.title}</h2>
+        <p>{channel.description}</p>
+
+        <Label basic size='large' color={visibilityColor} style={{ marginRight: '1rem' }}>
+          <Icon name={visibilityIcon} />
+          {channel.visibility}
+        </Label>
+
+        {channel.blocked && <Label basic size='large' color='red'>
+          <Icon name='dont' />
+          Channel blocked
+          {' '}<Icon name='question circle outline' size='small' />
+        </Label>}
+      </div>
+
+      <div className='ChannelStats'>
+        <div>
+          <Statistic size={statSize}>
+            <Statistic.Label>Reward earned</Statistic.Label>
+            <Statistic.Value>
+              {formatNumber(channel.rewardEarned)}
+              &nbsp;<span style={{ fontSize: '1.5rem' }}>JOY</span>
+            </Statistic.Value>
+          </Statistic>
         </div>
 
-        <div className='ChannelStats'>
-          <div>
-            <Statistic size={statSize}>
-              <Statistic.Label>Reward earned</Statistic.Label>
-              <Statistic.Value>
-                {formatNumber(channel.rewardEarned)}
-                &nbsp;<span style={{ fontSize: '1.5rem' }}>JOY</span>
-              </Statistic.Value>
-            </Statistic>
-          </div>
-
-          <div style={{ marginTop: '1rem' }}>
-            <Statistic size={statSize}>
-              <Statistic.Label>{itemsPublishedLabel}</Statistic.Label>
-              <Statistic.Value>{formatNumber(channel.contentItemsCount)}</Statistic.Value>
-            </Statistic>
-          </div>
+        <div style={{ marginTop: '1rem' }}>
+          <Statistic size={statSize}>
+            <Statistic.Label>{itemsPublishedLabel}</Statistic.Label>
+            <Statistic.Value>{formatNumber(channel.contentItemsCount)}</Statistic.Value>
+          </Statistic>
         </div>
       </div>
-    </Segment>
-  }
+    </div>
+  </Segment>
+}
+
+export function MyChannels (props: Props) {
+  const { suspended = false, channels = [] } = props;
 
   return <Section title='My Channels' className='JoyChannels'>
     {!channels.length
       ? <YouHaveNoChannels suspended={suspended} />
-      : channels.map(renderChannelPreview)
+      : <TabsAndChannels {...props} />
     }</Section>;
 }
