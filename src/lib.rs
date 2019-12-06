0c7b8ab2305bd06fb52c9884697673993e4fc67b
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2019-12-06 17:15:22 +0200

    integrate new modules

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,8 @@ pub use runtime_primitives::BuildStorage;
 pub use runtime_primitives::{Perbill, Permill};
 
 pub use srml_support::{
-    construct_runtime, parameter_types, traits::Randomness, StorageMap, StorageValue,
+    construct_runtime, parameter_types, traits::Currency, traits::Imbalance, traits::Randomness,
+    StorageLinkedMap, StorageMap, StorageValue,
 };
 pub use staking::StakerStatus;
 pub use timestamp::Call as TimestampCall;
@@ -406,12 +407,16 @@ pub use forum;
 use membership::members;
 
 mod content_working_group;
-
+use content_working_group::lib as content_wg;
 mod migration;
 mod roles;
 mod service_discovery;
+use hiring;
+use minting;
+use recurringrewards;
 use roles::actors;
 use service_discovery::discovery;
+use stake;
 
 /// Alias for ContentId, used in various places.
 pub type ContentId = primitives::H256;
@@ -422,7 +427,7 @@ impl versioned_store::Trait for Runtime {
 
 impl versioned_store_permissions::Trait for Runtime {
     type Credential = u64;
-    type CredentialChecker = SudoKeyHasAllCredentials;
+    type CredentialChecker = (SudoKeyHasAllCredentials, ContentWorkingGroup);
     type CreateClassPermissionsChecker = SudoKeyCanCreateClasses;
 }
 
@@ -444,6 +449,106 @@ impl versioned_store_permissions::CreateClassPermissionsChecker<Runtime>
     fn account_can_create_class_permissions(account: &AccountId) -> bool {
         <sudo::Module<Runtime>>::key() == *account
     }
+}
+
+impl hiring::Trait for Runtime {
+    type OpeningId = u64;
+    type ApplicationId = u64;
+    type ApplicationDeactivatedHandler = (); // TODO - what needs to happen?
+}
+
+impl minting::Trait for Runtime {
+    type Currency = <Self as currency::GovernanceCurrency>::Currency;
+    type MintId = u64;
+}
+
+impl recurringrewards::Trait for Runtime {
+    type PayoutStatusHandler = (); // TODO - deal with successful and failed payouts
+    type RecipientId = u64;
+    type RewardRelationshipId = u64;
+}
+
+parameter_types! {
+    pub const StakePoolId: [u8; 8] = *b"joystake";
+}
+
+impl stake::Trait for Runtime {
+    type Currency = <Self as currency::GovernanceCurrency>::Currency;
+    type StakePoolId = StakePoolId;
+    type StakingEventsHandler = ContentWorkingGroupStakingEventHandler;
+    type StakeId = u64;
+    type SlashId = u64;
+}
+
+pub struct ContentWorkingGroupStakingEventHandler {}
+impl stake::StakingEventsHandler<Runtime> for ContentWorkingGroupStakingEventHandler {
+    fn unstaked(
+        stake_id: &<Runtime as stake::Trait>::StakeId,
+        _unstaked_amount: stake::BalanceOf<Runtime>,
+        remaining_imbalance: stake::NegativeImbalance<Runtime>,
+    ) -> stake::NegativeImbalance<Runtime> {
+        if !hiring::ApplicationIdByStakingId::<Runtime>::exists(stake_id) {
+            // Stake not related to a staked role managed by the hiring module
+            return remaining_imbalance;
+        }
+
+        let application_id = hiring::ApplicationIdByStakingId::<Runtime>::get(stake_id);
+
+        if !content_wg::CuratorApplicationById::<Runtime>::exists(application_id) {
+            // Stake not for a Curator
+            return remaining_imbalance;
+        }
+
+        // Notify the Hiring module - is there a potential re-entrancy bug if
+        // instant unstaking is occuring?
+        hiring::Module::<Runtime>::unstaked(*stake_id);
+
+        // Only notify working group module if non instantaneous unstaking occured
+        if content_wg::UnstakerByStakeId::<Runtime>::exists(stake_id) {
+            content_wg::Module::<Runtime>::unstaked(*stake_id);
+        }
+
+        // Determine member id of the curator
+        let curator_application =
+            content_wg::CuratorApplicationById::<Runtime>::get(application_id);
+        let member_id = curator_application.member_id;
+
+        // get member's profile
+        let member_profile = membership::members::MemberProfile::<Runtime>::get(member_id).unwrap();
+
+        // deposit funds to member's root_account
+        // The application doesn't recorded the original source_account from which staked funds were
+        // provided, so we don't really have another option at the moment.
+        <Runtime as stake::Trait>::Currency::resolve_creating(
+            &member_profile.root_account,
+            remaining_imbalance,
+        );
+
+        stake::NegativeImbalance::<Runtime>::zero()
+    }
+
+    // Handler for slashing event
+    fn slashed(
+        _id: &<Runtime as stake::Trait>::StakeId,
+        _slash_id: &<Runtime as stake::Trait>::SlashId,
+        _slashed_amount: stake::BalanceOf<Runtime>,
+        _remaining_stake: stake::BalanceOf<Runtime>,
+        remaining_imbalance: stake::NegativeImbalance<Runtime>,
+    ) -> stake::NegativeImbalance<Runtime> {
+        // Check if the stake is associated with a hired curator or applicant
+        // if their stake goes below minimum required for the role,
+        // they should get deactivated.
+        // Since we don't currently implement any slash initiation in working group,
+        // there is nothing to do for now.
+
+        // Not interested in transfering the slashed amount anywhere for now,
+        // so return it to next handler.
+        remaining_imbalance
+    }
+}
+
+impl content_wg::Trait for Runtime {
+    type Event = Event;
 }
 
 impl currency::GovernanceCurrency for Runtime {
@@ -632,6 +737,11 @@ construct_runtime!(
         Discovery: discovery::{Module, Call, Storage, Event<T>},
         VersionedStore: versioned_store::{Module, Call, Storage, Event<T>, Config},
         VersionedStorePermissions: versioned_store_permissions::{Module, Call, Storage},
+        Stake: stake::{Module, Call, Storage},
+        Minting: minting::{Module, Call, Storage},
+        RecurringRewards: recurringrewards::{Module, Call, Storage},
+        Hiring: hiring::{Module, Call, Storage},
+        ContentWorkingGroup: content_wg::{Module, Call, Storage, Event<T>, Config<T>},
 	}
 );
 
