46781016c5a1caae39f770a36463ff8485f3a9f8
Shamil Gadelshin <shamilgadelshin@gmail.com>
2019-12-23 18:04:08 +0300

    Clean some comments
    - clean comments in tests/mod.rs
    - add future test TODOs

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -6,81 +6,29 @@ mod begin_accepting_applications;
 mod begin_review;
 mod smoke;
 
+/*
+Add tests:
+- cancel_opening:
+.cancel_opening_success
+.cancel_opening_fails_due_to_too_short_application_unstaking_period
+.cancel_opening_fails_due_to_too_short_role_unstaking_period
+.cancel_opening_fails_due_to_opening_not_existing
+
+- ensure_can_add_application
+- fill_opening
+- deactive_application
+- unstaked
+- on_finalize
+
+
+Non existing API:
+- remove_application
+- remove_opening
+*/
+
 use super::*;
 
 ////Debug test object content
 //pub fn debug_print<T: rstd::fmt::Debug>(obj: T) {
 //    println!("{:?}", obj);
 //}
-
-// cancel_opening
-
-//#[test]
-//fn cancel_opening_success() {
-//    build_test_externalities().execute_with(|| {});
-//}
-//
-//#[test]
-//fn cancel_opening_fails_due_to_too_short_application_unstaking_period() {
-//    build_test_externalities().execute_with(|| {});
-//}
-//
-//#[test]
-//fn cancel_opening_fails_due_to_too_short_role_unstaking_period() {
-//    build_test_externalities().execute_with(|| {});
-//}
-//
-//#[test]
-//fn cancel_opening_fails_due_to_opening_not_existing() {
-//    build_test_externalities().execute_with(|| {});
-//}
-
-/*
- * remove_opening
- *
- *
- */
-
-/*
- * begin_accepting_applications
- *
- * begin_accepting_applications_succeeded
- * begin_accepting_applications_fails_due_to_invalid_opening_id
- * begin_accepting_applications_fails_due_to_opening_not_being_in_waiting_to_begin_stage
- */
-
-/*
- * begin_review
- *
- * begin_review
- */
-
-/*
- * fill_opening
- *
- * fill_opening
- */
-
-/*
- * add_application
- *
- * add_application
- */
-
-/*
- * deactive_application
- *
- * deactive_application
- */
-
-/*
- * remove_application
- *
- * remove_application
- */
-
-/*
- * unstaked
- *
- * unstaked
- */
