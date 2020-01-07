a00fd906af08577c7473a37c303130235a159033
Paul M Fox <iam@yourheropaul.com>
2020-01-07 13:51:31 +0000

    add small timeout to simulate API request overhead

--- packages/joy-utils/src/Transport.ts
+++ packages/joy-utils/src/Transport.ts
@@ -1,6 +1,9 @@
 export abstract class Transport {
-  protected promise<T>(value: T): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
+  protected async promise<T>(value: T, timeout:number = 1): Promise<T> {
+    return new Promise<T>(async (resolve, reject) => {
+      if (timeout > 0) {
+        await new Promise(r => setTimeout(r, timeout));
+      }
       resolve(value)
     })
   }
