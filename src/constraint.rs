16c068976fd4a7a692c2a35661fba2298267ee41
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-01-07 16:04:33 +0200

    improved comments for ReferenceConstraint

--- src/constraint.rs
+++ src/constraint.rs
@@ -1,23 +1,23 @@
 use codec::{Decode, Encode};
 use rstd::collections::btree_set::BTreeSet;
 
-/// Pointer to a specific property of a specific class.
+/// Reference to a specific property of a specific class.
 #[derive(Encode, Decode, Eq, PartialEq, Ord, PartialOrd, Clone, Debug)]
 pub struct PropertyOfClass<ClassId, PropertyIndex> {
     pub class_id: ClassId,
     pub property_index: PropertyIndex,
 }
 
-/// The type of constraint on what entities can reference instances of a class through an Internal property type.
+/// The type of constraint imposed on referencing a class via class property of type "Internal".
 #[derive(Encode, Decode, Eq, PartialEq, Clone, Debug)]
 pub enum ReferenceConstraint<ClassId: Ord, PropertyIndex: Ord> {
-    /// No Entity can reference the class.
+    /// No property can reference the class.
     NoReferencingAllowed,
 
-    /// Any entity may reference the class.
+    /// Any property of any class may reference the class.
     NoConstraint,
 
-    /// Only a set of entities of type ClassId and from the specified property index can reference the class.
+    /// Only a set of properties of specific classes can reference the class.
     Restricted(BTreeSet<PropertyOfClass<ClassId, PropertyIndex>>),
 }
 
