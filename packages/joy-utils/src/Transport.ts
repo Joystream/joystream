f760b363befd6d2ae10251547d18255f7bb03997
Paul M Fox <iam@yourheropaul.com>
2020-01-08 09:05:35 +0000

    Remove default timeout
    Co-Authored-By: Alex Siman <aleksandr.siman@gmail.com>

--- packages/joy-utils/src/Transport.ts
+++ packages/joy-utils/src/Transport.ts
@@ -1,5 +1,7 @@
 export abstract class Transport {
-  protected async promise<T>(value: T, timeout:number = 1): Promise<T> {
+  protected async promise<T>(value: T, timeout?: number): Promise<T> {
+    // ...
+    if (timeout) { // means: timeout !== undefined && timeout > 0
     return new Promise<T>(async (resolve, reject) => {
       if (timeout > 0) {
         await new Promise(r => setTimeout(r, timeout));
@@ -8,5 +10,4 @@ export abstract class Transport {
     })
   }
 }
-
 
