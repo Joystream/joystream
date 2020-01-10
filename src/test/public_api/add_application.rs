428aa97cc82eaf181e9ef16cf46381a0b53b7d22
Shamil Gadelshin <shamilgadelshin@gmail.com>
2020-01-10 18:53:11 +0300

    Add ‘would_application_get_added’ tests
    - add ‘would_application_get_added’ tests
    - add test with mocks to ‘add_application’ test pack

--- src/test/public_api/add_application.rs
+++ src/test/public_api/add_application.rs
@@ -303,3 +303,74 @@ fn add_application_succeeds_with_created_application_stake() {
         assert_eq!(stake, expected_stake);
     });
 }
+
+#[test]
+fn add_application_succeeds_with_crowding_out_with_staking_mocks() {
+    handle_mock(true, || {
+        build_test_externalities().execute_with(|| {
+            let mock = {
+                let mut mock = crate::MockStakeHandler::<Test>::new();
+
+                mock.expect_stake().times(1).returning(|_, _| Ok(()));
+                mock.expect_create_stake().times(1).returning(|| 0);
+
+                Rc::new(rstd::cell::RefCell::new(mock))
+            };
+
+            set_stake_handler_impl(mock.clone());
+            let mut opening_fixture = AddOpeningFixture::default();
+            opening_fixture.application_rationing_policy =
+                Some(hiring::ApplicationRationingPolicy {
+                    max_active_applicants: 1,
+                });
+            opening_fixture.application_staking_policy = Some(StakingPolicy {
+                amount: 100,
+                amount_mode: StakingAmountLimitMode::AtLeast,
+                crowded_out_unstaking_period_length: None,
+                review_period_expired_unstaking_period_length: None,
+            });
+
+            let add_opening_result = opening_fixture.add_opening();
+            let opening_id = add_opening_result.unwrap();
+
+            let mut application_fixture = AddApplicationFixture::default_for_opening(opening_id);
+            application_fixture.opt_application_stake_imbalance =
+                Some(stake::NegativeImbalance::<Test>::new(100));
+
+            assert!(application_fixture.add_application().is_ok());
+            mock.borrow_mut().checkpoint();
+
+            application_fixture.opt_application_stake_imbalance =
+                Some(stake::NegativeImbalance::<Test>::new(101));
+
+            let mock2 = {
+                let mut mock = crate::MockStakeHandler::<Test>::new();
+                mock.expect_stake_exists().returning(|_| true);
+
+                mock.expect_stake().times(1).returning(|_, _| Ok(()));
+
+                mock.expect_initiate_unstaking()
+                    .times(3)
+                    .returning(|_, _| Ok(()));
+
+                mock.expect_create_stake().times(1).returning(|| 1);
+
+                mock.expect_get_stake().returning(|_| stake::Stake {
+                    created: 1,
+                    staking_status: stake::StakingStatus::Staked(stake::StakedState {
+                        staked_amount: 100,
+                        staked_status: stake::StakedStatus::Normal,
+                        next_slash_id: 0,
+                        ongoing_slashes: BTreeMap::new(),
+                    }),
+                });
+
+                Rc::new(RefCell::new(mock))
+            };
+
+            set_stake_handler_impl(mock2.clone());
+
+            assert!(application_fixture.add_application().is_ok());
+        });
+    });
+}
--- src/test/would_application_get_added.rs
+++ src/test/would_application_get_added.rs
@@ -1,8 +1,135 @@
 use super::*;
 use crate::mock::*;
 
+#[derive(Default)]
+struct WouldApplicationGetAddedFixture {
+    possible_opening_application_rationing_policy: Option<ApplicationRationingPolicy>,
+    opening_applicants: BTreeSet<ApplicationId>,
+    opt_role_stake_balance: Option<Balance>,
+    opt_application_stake_balance: Option<Balance>,
+}
+
+impl WouldApplicationGetAddedFixture {
+    fn call_and_assert(&self, expected_result: ApplicationWouldGetAddedEvaluation<Test>) {
+        let wagaf_result = self.would_application_get_added();
+
+        assert_eq!(wagaf_result, expected_result);
+    }
+
+    fn would_application_get_added(&self) -> ApplicationWouldGetAddedEvaluation<Test> {
+        Hiring::would_application_get_added(
+            &self.possible_opening_application_rationing_policy,
+            &self.opening_applicants,
+            &self.opt_role_stake_balance,
+            &self.opt_application_stake_balance,
+        )
+    }
+}
+
 #[test]
-fn add_application_succeeds_with_crowding_out() {
+fn would_application_get_added_with_no_rationing_policy() {
+    build_test_externalities().execute_with(|| {
+        let wagaf = WouldApplicationGetAddedFixture::default();
+
+        wagaf.call_and_assert(ApplicationWouldGetAddedEvaluation::Yes(
+            ApplicationAddedSuccess::Unconditionally,
+        ));
+    });
+}
+
+#[test]
+fn would_application_get_added_with_zero_opening_applicants() {
+    build_test_externalities().execute_with(|| {
+        let mut wagaf = WouldApplicationGetAddedFixture::default();
+        wagaf.possible_opening_application_rationing_policy =
+            Some(hiring::ApplicationRationingPolicy {
+                max_active_applicants: 1,
+            });
+
+        wagaf.call_and_assert(ApplicationWouldGetAddedEvaluation::Yes(
+            ApplicationAddedSuccess::Unconditionally,
+        ));
+    });
+}
+
+#[test]
+fn would_application_get_added_with_too_low_stake() {
+    build_test_externalities().execute_with(|| {
+        let mut opening_fixture = AddOpeningFixture::default();
+        opening_fixture.application_rationing_policy = Some(hiring::ApplicationRationingPolicy {
+            max_active_applicants: 1,
+        });
+        opening_fixture.application_staking_policy = Some(StakingPolicy {
+            amount: 100,
+            amount_mode: StakingAmountLimitMode::AtLeast,
+            crowded_out_unstaking_period_length: None,
+            review_period_expired_unstaking_period_length: None,
+        });
+
+        let add_opening_result = opening_fixture.add_opening();
+        let opening_id = add_opening_result.unwrap();
+
+        let mut application_fixture = AddApplicationFixture::default_for_opening(opening_id);
+        application_fixture.opt_application_stake_imbalance =
+            Some(stake::NegativeImbalance::<Test>::new(100));
+
+        let app_application_result = application_fixture.add_application();
+        let application_id = app_application_result.unwrap().application_id_added;
+
+        let mut wagaf = WouldApplicationGetAddedFixture::default();
+        wagaf.possible_opening_application_rationing_policy =
+            Some(hiring::ApplicationRationingPolicy {
+                max_active_applicants: 1,
+            });
+        wagaf.opening_applicants.insert(application_id);
+        wagaf.opt_application_stake_balance = Some(99);
+
+        wagaf.call_and_assert(ApplicationWouldGetAddedEvaluation::No);
+    });
+}
+
+#[test]
+fn would_application_get_added_with_crowding_out() {
+    build_test_externalities().execute_with(|| {
+        let mut opening_fixture = AddOpeningFixture::default();
+        opening_fixture.application_rationing_policy = Some(hiring::ApplicationRationingPolicy {
+            max_active_applicants: 1,
+        });
+        opening_fixture.application_staking_policy = Some(StakingPolicy {
+            amount: 100,
+            amount_mode: StakingAmountLimitMode::AtLeast,
+            crowded_out_unstaking_period_length: None,
+            review_period_expired_unstaking_period_length: None,
+        });
+
+        let add_opening_result = opening_fixture.add_opening();
+        let opening_id = add_opening_result.unwrap();
+
+        let mut application_fixture = AddApplicationFixture::default_for_opening(opening_id);
+        application_fixture.opt_application_stake_imbalance =
+            Some(stake::NegativeImbalance::<Test>::new(100));
+
+        let app_application_result = application_fixture.add_application();
+        let application_id = app_application_result.unwrap().application_id_added;
+
+        let mut wagaf = WouldApplicationGetAddedFixture::default();
+        wagaf.possible_opening_application_rationing_policy =
+            Some(hiring::ApplicationRationingPolicy {
+                max_active_applicants: 1,
+            });
+        wagaf.opening_applicants.insert(application_id);
+        wagaf.opt_application_stake_balance = Some(101);
+
+        wagaf.call_and_assert(ApplicationWouldGetAddedEvaluation::Yes(
+            ApplicationAddedSuccess::CrowdsOutExistingApplication(application_id),
+        ));
+    });
+}
+
+/*
+
+#[test]
+fn add_application_succeeds_with_crowding_out_with_staking_mocks() {
     handle_mock(true, || {
         build_test_externalities().execute_with(|| {
             let mock = {
@@ -71,3 +198,5 @@ fn add_application_succeeds_with_crowding_out() {
         });
     });
 }
+
+*/
