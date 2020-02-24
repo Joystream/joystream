ffacfa46954aefe650a27111ef3cd2d18a06b0ac
Alex Siman <aleksandr.siman@gmail.com>
2020-02-24 19:51:38 +0200

    Implement plainToSubstrate on EntityCodec

--- packages/joy-types/src/versioned-store/EntityCodec.ts
+++ packages/joy-types/src/versioned-store/EntityCodec.ts
@@ -2,6 +2,7 @@ import camelCase from 'lodash/camelCase';
 import BN from 'bn.js';
 import { Text, bool, Vec, u16 } from '@polkadot/types';
 import { Codec } from '@polkadot/types/types';
+import * as PV from './PropertyValue';
 import { PropertyValue } from './PropertyValue';
 import { Class, Entity, VecClassPropertyValue, ClassPropertyValue } from '.';
 import PropertyTypeName from './PropertyTypeName';
@@ -30,13 +31,67 @@ function substrateToPlain<T> (x: Codec): T | undefined {
 
 /**
  * Convert a plain JavaScript value of such type as string, number, boolean 
- * to Substrate equivalent.
+ * to Substrate equivalent in Versioned Store module.
  * 
  * Based on code of transformPropertyValue from another Joystream repo:
  * /versioned-store-js/src/transformPropertyValue.ts
  */
-export function plainToSubstrate(propType: PropertyTypeName, value?: any): PropertyValue {
-  return new PropertyValue({ [propType]: value });
+function plainToSubstrate(propType: string, value: any): PropertyValue {
+
+  const ok = (typeEnum: PV.PropertyValueEnum) => {
+    return new PropertyValue({ [propType]: typeEnum })
+  }
+
+  const valueAsBool = (): boolean => {
+    if (typeof value === 'string' || typeof value === 'number') {
+      value = value.toString().toLowerCase()
+      if (['true', 'yes', '1'].indexOf(value) >= 0) {
+        return true
+      }
+      return false
+    } else if (typeof value === 'boolean') {
+      return value
+    } else {
+      throw new Error('Unsupported representation of a boolean value: ' + value)
+    }
+  }
+
+  const valueAsBoolArr = (): boolean[] => {
+    return (value as []).map(valueAsBool)
+  }
+
+  switch (propType) {
+
+    // Primitives:
+
+    case 'None':        return ok(new PV.None())
+    case 'Bool':        return ok(new PV.Bool(valueAsBool()))
+    case 'Uint16':      return ok(new PV.Uint16(value as string))
+    case 'Uint32':      return ok(new PV.Uint32(value as string))
+    case 'Uint64':      return ok(new PV.Uint64(value as string))
+    case 'Int16':       return ok(new PV.Int16(value as string))
+    case 'Int32':       return ok(new PV.Int32(value as string))
+    case 'Int64':       return ok(new PV.Int64(value as string))
+    case 'Text':        return ok(new PV.Text(value as string))
+    case 'Internal':    return ok(new PV.Internal(value as string))
+
+    // Vectors:
+
+    case 'BoolVec':     return ok(new PV.BoolVec(valueAsBoolArr()))
+    case 'Uint16Vec':   return ok(new PV.Uint16Vec(value as string[]))
+    case 'Uint32Vec':   return ok(new PV.Uint32Vec(value as string[]))
+    case 'Uint64Vec':   return ok(new PV.Uint64Vec(value as string[]))
+    case 'Int16Vec':    return ok(new PV.Int16Vec(value as string[]))
+    case 'Int32Vec':    return ok(new PV.Int32Vec(value as string[]))
+    case 'Int64Vec':    return ok(new PV.Int64Vec(value as string[]))
+    case 'TextVec':     return ok(new PV.TextVec(value as string[]))
+    case 'InternalVec': return ok(new PV.InternalVec(value as string[]))
+
+    default: {
+      console.log(`Unknown property type name: ${propType}`)
+      return undefined
+    }
+  }
 }
 
 function propNameToJsFieldStyle (humanFriendlyPropName: string): string {
@@ -97,7 +152,7 @@ export abstract class EntityCodec<T extends PlainEntity> {
    * that can be passed to the extrinsic `update_entity_property_values`
    * of Substrate runtime module `substrate-versioned-store`.
    */
-  toSubstrateUpdate (updatedProps: Partial<T>): VecClassPropertyValue {
+  toSubstrateUpdate (updatedProps: Partial<{ [propName: string]: any }>): VecClassPropertyValue {
 
     // TODO check required fields! save prop metadata in constructor?
 
