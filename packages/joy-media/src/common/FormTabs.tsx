6550dd5c6b4d77278ed7513ddcdc2a9d50e32043
Alex Siman <aleksandr.siman@gmail.com>
2020-02-26 14:51:02 +0200

    Hide error counter on edit form tabs because its markup is broken

--- packages/joy-media/src/common/FormTabs.tsx
+++ packages/joy-media/src/common/FormTabs.tsx
@@ -37,13 +37,19 @@ export function FormTabs <FormValues> (props: FormTabsProps<FormValues>) {
         }
       })
     
+      // Currently we don't show error counter because it's markup is broken:
+      // a red circle with a number is shifted quite far from the right border of its tab.
+      const showErrorCounter = false
+
       const errCount = tabErrors.length;
       const errTooltip = 'Number of errors on this tab';
 
       const menuItem =
         <Menu.Item key={id}>
           {renderTitle()}
-          {errCount > 0 && <Label color='red' circular floating title={errTooltip}>{errCount}</Label>}
+          {showErrorCounter && errCount > 0 &&
+            <Label color='red' circular floating title={errTooltip}>{errCount}</Label>
+          }
         </Menu.Item>;
 
       return { menuItem, render };
