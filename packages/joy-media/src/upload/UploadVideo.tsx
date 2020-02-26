e42c62ffbc0f5b325a342318897633a282132861
Alex Siman <aleksandr.siman@gmail.com>
2020-02-26 14:33:19 +0200

    Fix Edit Video Entity form

--- packages/joy-media/src/upload/UploadVideo.tsx
+++ packages/joy-media/src/upload/UploadVideo.tsx
@@ -19,6 +19,8 @@ import { Class } from '@joystream/types/versioned-store';
 import { TxCallback } from '@polkadot/react-components/Status/types';
 import { SubmittableResult } from '@polkadot/api';
 import { findFirstParamOfSubstrateEvent, nonEmptyStr } from '@polkadot/joy-utils/';
+import { u16, Option } from '@polkadot/types';
+import { isInternalProp } from '@joystream/types/versioned-store/EntityCodec';
 
 /** Example: "2019-01-23" -> 1548201600 */
 function humanDateToUnixTs(humanFriendlyDate: string): number | undefined {
@@ -66,23 +68,57 @@ const InnerForm = (props: MediaFormProps<OuterProps, FormValues>) => {
     values,
     dirty,
     errors,
-    // isValid,
+    isValid,
     isSubmitting,
     setSubmitting,
     resetForm
   } = props;
 
-  const [ entityId, setEntityId ] = useState<EntityId | undefined>(id)
-  const { thumbnail } = values;
-  const withCredential = new Credential(2)
-  // const isNew = !entity;
+  const initialSupportedSchemaIds: number[] = entity?.inClassSchemaIndexes || []
 
-  const getValuesOfUpdatedFields = (): Partial<FormValues> => {
+  const [ entityId, setEntityId ] = useState<EntityId | undefined>(id)
+  const [ supportsSchema, setSupportsSchema ] = useState<boolean>(initialSupportedSchemaIds.length > 0)
+  const { thumbnail } = values
+
+  const withCredential = new Option(Credential, new Credential(2))
+  const asEntityMaintainer = false
+  const firstSchemaId = new u16(0)
+  const codec = new VideoCodec(entityClass)
+
+  const getFieldsValues = (): Partial<FormValues> => {
     const res: Partial<FormValues> = {}
+
+    // TODO return media object id here, if entity doesn't have media object id yet
+
     Object.keys(values).forEach((prop) => {
       const fieldName = prop as VideoPropId
-      if (isFieldChanged(fieldName)) {
-        let fieldValue = values[fieldName] as any
+      const field = Fields[fieldName]
+      let fieldValue = values[fieldName] as any
+
+      let shouldIncludeValue = true
+      if (entity) {
+        // If we updating existing entity, then update only changed props:
+        shouldIncludeValue = isFieldChanged(fieldName)
+      } else if (field.required !== true) {
+        // If we creating a new entity, then provide all required props
+        // plus non empty non required props:
+        if (isInternalProp(field)) {
+          shouldIncludeValue = fieldValue > 0
+        } else if (typeof fieldValue === 'string') {
+          shouldIncludeValue = nonEmptyStr(fieldValue)
+        } else if (Array.isArray(fieldValue) && fieldValue.length === 0) {
+          shouldIncludeValue = false
+        }
+      }
+
+      // For debugging:
+      // const propForLog: any = { fieldName, fieldValue }
+      // if (shouldIncludeValue) {
+      //   propForLog.shouldIncludeValue = shouldIncludeValue
+      // }
+      // console.log(propForLog)
+      
+      if (shouldIncludeValue) {
         if (typeof fieldValue === 'string') {
           fieldValue = fieldValue.trim()
         }
@@ -97,25 +133,35 @@ const InnerForm = (props: MediaFormProps<OuterProps, FormValues>) => {
 
   // TODO Batch create + update entity operations with versionedStorePermissions.transaction function
 
-  const buildCreateTxParams = () => {
+  const buildCreateEntityTxParams = () => {
     return [
       withCredential,
       entityClass.id
     ]
   }
 
-  const buildUpdateTxParams = () => {
-    const asEntityMaintainer = false
-
-    const codec = new VideoCodec(entityClass)
-    const updatedProps = codec.toSubstrateUpdate(getValuesOfUpdatedFields())
-    // console.log('buildUpdateTxParams updatedProps:', updatedProps)
+  const buildAddSchemaSupportTxParams = () => {
+    const propValues = codec.toSubstrateUpdate(getFieldsValues())
+    // console.log('buildAddSchemaSupportTxParams propValues:', propValues)
 
     return [
       withCredential,
       asEntityMaintainer,
       entityId,
-      updatedProps
+      firstSchemaId,
+      propValues
+    ]
+  }
+
+  const buildUpdateEntityTxParams = () => {
+    const updatedPropValues = codec.toSubstrateUpdate(getFieldsValues())
+    // console.log('buildUpdateEntityTxParams updatedPropValues:', updatedPropValues)
+
+    return [
+      withCredential,
+      asEntityMaintainer,
+      entityId,
+      updatedPropValues
     ]
   };
 
@@ -129,14 +175,21 @@ const InnerForm = (props: MediaFormProps<OuterProps, FormValues>) => {
     console.log('New video entity id:', newId && newId.toString())
   }
 
+  const redirectToPlaybackPage = () => {
+    if (history && entityId) {
+      history.push('/media/video/' + entityId.toString())
+    }
+  }
+
+  const onAddSchemaSupportSuccess: TxCallback = (_txResult: SubmittableResult) => {
+    setSubmitting(false)
+    setSupportsSchema(true)
+    redirectToPlaybackPage()
+  }
+
   const onUpdateEntitySuccess: TxCallback = (_txResult: SubmittableResult) => {
     setSubmitting(false)
-    if (!history) return
-
-    // Redirect to playback video page:
-    if (entity) {
-      history.push('/media/video/' + entity.id.toString())
-    }
+    redirectToPlaybackPage()
   }
 
   const basicInfoTab = () => <Tab.Pane as='div'>
@@ -195,23 +248,36 @@ const InnerForm = (props: MediaFormProps<OuterProps, FormValues>) => {
     <TxButton
       type='submit'
       size='large'
-      isDisabled={!dirty || isSubmitting}
+      isDisabled={!(dirty && isValid && !isSubmitting)}
       label='Create video entity'
       tx='versionedStorePermissions.createEntity'
-      params={buildCreateTxParams()}
+      params={buildCreateEntityTxParams()}
       onClick={newOnSubmit}
       txFailedCb={onTxFailed}
       txSuccessCb={onCreateEntitySuccess}
+    />
+
+  const AddSchemaSupportButton = () =>
+    <TxButton
+      type='submit'
+      size='large'
+      isDisabled={!(dirty && isValid && !isSubmitting)}
+      label='Add schema support'
+      tx='versionedStorePermissions.addSchemaSupportToEntity'
+      params={buildAddSchemaSupportTxParams()}
+      onClick={newOnSubmit}
+      txFailedCb={onTxFailed}
+      txSuccessCb={onAddSchemaSupportSuccess}
     />
 
   const UpdateEntityButton = () =>
     <TxButton
       type='submit'
       size='large'
-      isDisabled={!dirty || isSubmitting}
+      isDisabled={!(dirty && isValid && !isSubmitting)}
       label='Update video entity'
       tx='versionedStorePermissions.updateEntityPropertyValues'      
-      params={buildUpdateTxParams()}
+      params={buildUpdateEntityTxParams()}
       onClick={newOnSubmit}
       txFailedCb={onTxFailed}
       txSuccessCb={onUpdateEntitySuccess}
@@ -227,8 +293,15 @@ const InnerForm = (props: MediaFormProps<OuterProps, FormValues>) => {
       {tabs}
 
       <LabelledField style={{ marginTop: '1rem' }} {...props}>
-        <CreateEntityButton />
-        <UpdateEntityButton />
+        {!entity && !entityId &&
+          <CreateEntityButton />
+        }
+        {entityId && !supportsSchema &&
+          <AddSchemaSupportButton />
+        }
+        {entityId && supportsSchema &&
+          <UpdateEntityButton />
+        }
         <Button
           type='button'
           size='large'
