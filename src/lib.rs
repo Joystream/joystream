63da40df9979fabfbdcbc9687a25e5cd29e1e66e
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-02-29 12:21:18 +0400

    bump libp2p

--- src/lib.rs
+++ src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node"),
     impl_name: create_runtime_str!("joystream-node"),
     authoring_version: 6,
-    spec_version: 1,
+    spec_version: 2,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
 };
