f07169ac5f37bf69c1745f1974edd7fb50f446d5
Alex Siman <aleksandr.siman@gmail.com>
2020-03-11 00:06:43 +0200

    Show all video metadata below a description on Playback page

--- packages/joy-media/src/entities/EntityHelpers.ts
+++ packages/joy-media/src/entities/EntityHelpers.ts
@@ -1,4 +1,6 @@
 import moment from 'moment';
+import ISO6391 from 'iso-639-1';
+import { LanguageType } from '../schemas/general/Language';
 
 export function printExplicit(explicit?: boolean): string {
   return explicit === true ? 'Yes' : 'No'
@@ -6,4 +8,8 @@ export function printExplicit(explicit?: boolean): string {
 
 export function printReleaseDate(linuxTimestamp?: number): string {
   return !linuxTimestamp ? '' : moment(linuxTimestamp * 1000).format('YYYY-MM-DD')
+}
+
+export function printLanguage(language?: LanguageType): string {
+  return !language ? '' : ISO6391.getName(language.value)
 }
