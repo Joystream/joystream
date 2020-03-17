5482842e26ea78aedcb9ee135ec3cd2b7d7daa55
Alex Siman <aleksandr.siman@gmail.com>
2020-03-17 16:40:55 +0200

    Add different status messages: info, success, warning, error

--- packages/joy-utils/src/JoyWarn.tsx
+++ packages/joy-utils/src/JoyWarn.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import { Message } from 'semantic-ui-react';
+import { nonEmptyStr } from '.';
 
-type Props = {
-  title: React.ReactNode
+type BaseProps = {
+  title?: React.ReactNode
   children?: React.ReactNode
-};
+}
 
-export const JoyWarn = ({ title, children }: Props) => (
-  <Message warning className='JoyMainStatus'>
-    <Message.Header>{title}</Message.Header>
-    {children && <div style={{ marginTop: '1rem' }}>
-      {children}
-    </div>}
-  </Message>
-);
+type Variants = {
+  info?: boolean
+  success?: boolean
+  warning?: boolean
+  error?: boolean
+}
+
+type Props = BaseProps & Variants
+
+export const JoyStatus = (props: Props) => {
+  const { title, children, ...variants } = props
+
+  return (
+    <Message className='JoyMainStatus' {...variants}>
+      {nonEmptyStr(title) &&
+        <Message.Header>{title}</Message.Header>
+      }
+      {children &&
+        <div style={{ marginTop: '1rem' }}>
+          {children}
+        </div>
+      }
+    </Message>
+  )
+}
+
+export const JoyInfo = (props: BaseProps) => <JoyStatus {...props} info />
+
+export const JoySuccess = (props: BaseProps) => <JoyStatus {...props} success />
+
+export const JoyWarn = (props: BaseProps) => <JoyStatus {...props} warning />
+
+export const JoyError = (props: BaseProps) => <JoyStatus {...props} error />
