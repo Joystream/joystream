c7f263fcffaeb92df1ec5b68f17f90154155de3e
Gamaranto <gamarantor@gmail.com>
2020-03-18 14:11:42 +0100

    Add Simple React App

---
+++ packages/app/__tests__/app.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const app = require('..');
+
+describe('app', () => {
+    it('needs tests');
+});
---
+++ packages/app/src/components/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div>Hello Atlas</div>;
+}
