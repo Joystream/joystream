922547a0c59f7d5ca3ec6df7632d6719f69b9627
Gamaranto <gamarantor@gmail.com>
2020-03-20 16:38:16 +0100

    Add Pages Folder To App

--- packages/app/src/components/App.tsx
+++ packages/app/src/components/App.tsx
@@ -1,25 +1,6 @@
 import React from "react";
-import { Block } from "components";
-
-const blockData = {
-  blockNum: 1854,
-  hash: "0x080d672d268a72cae5b255918c1c832439869d6b9b36933734612ba7cd53f2db",
-
-  parentHash:
-    "0xdfe6522b146213accfb65c16ba82ad96e0cc737abe21eb28bbea80135e08e2d4",
-
-  stateRoot:
-    "0xb0486392387dc820afb96ecd3b0d8f129538b789d811d0e82d86607d45424664",
-
-  extrinsictRoot:
-    "0x3f4378235b086fd65dd5d3650c8ef82e1b75297e884389ad1037fcc966a9ae05",
-};
+import Explorer from "./pages/Explorer";
 
 export default function App() {
-  return (
-    <div>
-      Hello Atlas
-      <Block {...blockData}></Block>
-    </div>
-  );
+  return <Explorer />;
 }
