999b1627af07366c46281937150d2e90943520f6
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-03-21 03:09:27 +0400

    remove unnecessary steps in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,10 @@
 
 set -e
 
-# Install OS dependencies
+# If OS is supported will install:
+#  - build tools and any other dependencies required for rust
+#  - rustup - rust insaller
+#  - rust compiler and toolchains
+#  - skips installing substrate and subkey
 curl https://getsubstrate.io -sSf | bash -s -- --fast
 
-echo "*** Initialising WASM build environment"
-
-if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
-fi
-
-rustup target add wasm32-unknown-unknown --toolchain nightly
-
-# Install wasm-gc. It's useful for stripping slimming down wasm binaries.
-command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
