28f0e20588acb86f944421163763235b13bb75ef
Gamaranto <gamarantor@gmail.com>
2020-03-20 13:58:14 +0100

    Restore Storybook Styling

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,18 +1,25 @@
 module.exports = {
-  stories: ["../stories/**/*.stories.{js,jsx,ts,tsx}"],
+  stories: ["../stories/**/*.stories.(js|jsx|ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-knobs",
     "@storybook/addon-storysource",
     "storybook-addon-jsx/register",
+    "@storybook/addon-docs",
   ],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
         {
-          loader: require.resolve("ts-loader"),
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [
+              ["react-app", { flow: false, typescript: true }],
+              require.resolve("@emotion/babel-preset-css-prop"),
+            ],
+          },
         },
         // Optional
         {
