328e488224128795d700ac4c04ea1b2cfb265329
Gamaranto <gamarantor@gmail.com>
2020-03-21 15:53:06 +0100

    Remove Merge conflict

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import Block from "./components/Block";
-
-import { blockData } from "../stories/1-Block.stories";
-
-export default function App() {
-  return (
-    <div>
-      <div>Here is a simple block information</div>
-      <Block {...blockData}></Block>
-    </div>
-  );
-}
-=======
 export { default as Block } from "./components/Block";
 export { default as BlockDetails } from "./components/BlockDetails";
 export { default as Card } from "./components/Card";
@@ -23,4 +9,3 @@ export { default as Label } from "./components/Label";
 export { default as SearchBar } from "./components/SearchBar";
 export { default as TabLink } from "./components/TabLink";
 export { default as TabNavigation } from "./components/TabNavigation";
->>>>>>> develop
