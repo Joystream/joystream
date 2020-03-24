e37875c6747fe77be7e2f440d5093c9c2293cec7
Gamaranto <gamarantor@gmail.com>
2020-03-24 16:07:57 +0100

    Fixes Storybook Not Displaying Styles

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,11 +1,2 @@
-export { default as Block } from "./components/Block";
-export { default as BlockDetails } from "./components/BlockDetails";
-export { default as Card } from "./components/Card";
-export { default as CardStack } from "./components/CardStack";
 export { default as ContentLabel } from "./components/ContentLabel";
-export { default as Grid } from "./components/Grid";
-export { default as GridItem } from "./components/GridItem";
-export { default as Label } from "./components/Label";
 export { default as SearchBar } from "./components/SearchBar";
-export { default as TabLink } from "./components/TabLink";
-export { default as TabNavigation } from "./components/TabNavigation";
