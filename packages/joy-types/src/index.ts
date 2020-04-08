cb013d051efd7f493724df4cffe0470f4ce87db9
Gamaranto <gamarantor@gmail.com>
2020-04-08 17:12:45 +0200

    Add Proposal Types

--- packages/joy-types/src/index.ts
+++ packages/joy-types/src/index.ts
@@ -1,21 +1,22 @@
-import { Enum, Option, Struct, Vec } from '@polkadot/types/codec';
-import { getTypeRegistry, Text } from '@polkadot/types';
-import { BlockNumber, AccountId, Balance, Hash } from '@polkadot/types/interfaces';
-import { u32, bool } from '@polkadot/types/primitive';
-import { Codec } from '@polkadot/types/types';
+import { Enum, Option, Struct, Vec } from "@polkadot/types/codec";
+import { getTypeRegistry, Text } from "@polkadot/types";
+import { BlockNumber, AccountId, Balance, Hash } from "@polkadot/types/interfaces";
+import { u32, bool } from "@polkadot/types/primitive";
+import { Codec } from "@polkadot/types/types";
 
-import { registerForumTypes } from './forum';
-import { registerMediaTypes } from './media';
-import { registerMembershipTypes } from './members';
-import { registerRolesTypes } from './roles';
-import { registerDiscoveryTypes } from './discovery';
-import { registerHiringTypes } from './hiring';
-import { registerVersionedStoreTypes } from './versioned-store';
-import { registerVersionedStorePermissionsTypes } from './versioned-store/permissions';
-import { registerStakeTypes } from './stake';
-import { registerMintTypes } from './mint';
-import { registerRecurringRewardsTypes } from './recurring-rewards';
-import { registerContentWorkingGroupTypes } from './content-working-group';
+import { registerForumTypes } from "./forum";
+import { registerMediaTypes } from "./media";
+import { registerMembershipTypes } from "./members";
+import { registerRolesTypes } from "./roles";
+import { registerDiscoveryTypes } from "./discovery";
+import { registerHiringTypes } from "./hiring";
+import { registerVersionedStoreTypes } from "./versioned-store";
+import { registerVersionedStorePermissionsTypes } from "./versioned-store/permissions";
+import { registerStakeTypes } from "./stake";
+import { registerMintTypes } from "./mint";
+import { registerRecurringRewardsTypes } from "./recurring-rewards";
+import { registerContentWorkingGroupTypes } from "./content-working-group";
+import { registerProposalsTypes } from "./proposals";
 
 export function getTextPropAsString(struct: Struct, fieldName: string): string {
   return (struct.get(fieldName) as Text).toString();
@@ -25,14 +26,11 @@ export function getBoolPropAsBoolean(struct: Struct, fieldName: string): boolean
   return (struct.get(fieldName) as bool).valueOf();
 }
 
-export function getOptionPropOrUndefined<T extends Codec>
-  (struct: Struct, fieldName: string): T | undefined {
-
+export function getOptionPropOrUndefined<T extends Codec>(struct: Struct, fieldName: string): T | undefined {
   return (struct.get(fieldName) as Option<T>).unwrapOr(undefined);
 }
 
 export class OptionText extends Option.with(Text) {
-
   static none(): OptionText {
     return new Option(Text, null);
   }
@@ -43,64 +41,64 @@ export class OptionText extends Option.with(Text) {
 }
 
 export type TransferableStake = {
-  seat: Balance,
-  backing: Balance
+  seat: Balance;
+  backing: Balance;
 };
 
 export type Stake = {
-  new: Balance,
-  transferred: Balance
+  new: Balance;
+  transferred: Balance;
 };
 
 export type Backer = {
-  member: AccountId,
-  stake: Balance
+  member: AccountId;
+  stake: Balance;
 };
 
 export type Seat = {
-  member: AccountId,
-  stake: Balance,
-  backers: Backer[]
+  member: AccountId;
+  stake: Balance;
+  backers: Backer[];
 };
 
 export type SealedVote = {
-  voter: AccountId,
-  commitment: Hash,
-  stake: Stake,
-  vote: Option<AccountId>
+  voter: AccountId;
+  commitment: Hash;
+  stake: Stake;
+  vote: Option<AccountId>;
 };
 
 // Note: this could be named 'RuntimeUpgradeProposal' (as it is in Rust),
 // but not a big deal here in JS.
 export type Proposal = {
-  id: u32,
-  proposer: AccountId,
-  stake: Balance,
-  name: Text, // or AnyU8a?
-  description: Text,
-  wasm_hash: Hash,
-  proposed_at: BlockNumber,
-  status: ProposalStatus
+  id: u32;
+  proposer: AccountId;
+  stake: Balance;
+  name: Text; // or AnyU8a?
+  description: Text;
+  wasm_hash: Hash;
+  proposed_at: BlockNumber;
+  status: ProposalStatus;
 };
 
 export type ProposalVote = {
-  voter: AccountId,
-  kind: VoteKind
+  voter: AccountId;
+  kind: VoteKind;
 };
 
 export type TallyResult = {
-  proposal_id: u32,
-  abstentions: u32,
-  approvals: u32,
-  rejections: u32,
-  slashes: u32,
-  status: ProposalStatus,
-  finalized_at: BlockNumber
+  proposal_id: u32;
+  abstentions: u32;
+  approvals: u32;
+  rejections: u32;
+  slashes: u32;
+  status: ProposalStatus;
+  finalized_at: BlockNumber;
 };
 
-export class Announcing extends u32 { };
-export class Voting extends u32 { };
-export class Revealing extends u32 { };
+export class Announcing extends u32 {}
+export class Voting extends u32 {}
+export class Revealing extends u32 {}
 
 export class ElectionStage extends Enum {
   constructor(value?: any, index?: number) {
@@ -110,22 +108,24 @@ export class ElectionStage extends Enum {
         Voting,
         Revealing
       },
-      value, index);
+      value,
+      index
+    );
   }
 
   /** Create a new Announcing stage. */
   static Announcing(endsAt: BlockNumber | number): ElectionStage {
-    return this.newElectionStage('Announcing', endsAt);
+    return this.newElectionStage("Announcing", endsAt);
   }
 
   /** Create a new Voting stage. */
   static Voting(endsAt: BlockNumber | number): ElectionStage {
-    return this.newElectionStage('Voting', endsAt);
+    return this.newElectionStage("Voting", endsAt);
   }
 
   /** Create a new Revealing stage. */
   static Revealing(endsAt: BlockNumber | number): ElectionStage {
-    return this.newElectionStage('Revealing', endsAt);
+    return this.newElectionStage("Revealing", endsAt);
   }
 
   static newElectionStage(stageName: string, endsAt: BlockNumber | number) {
@@ -136,49 +136,37 @@ export class ElectionStage extends Enum {
 export type AnyElectionStage = Announcing | Voting | Revealing;
 
 export const ProposalStatuses: { [key: string]: string } = {
-  Active: 'Active',
-  Cancelled: 'Cancelled',
-  Expired: 'Expired',
-  Approved: 'Approved',
-  Rejected: 'Rejected',
-  Slashed: 'Slashed'
+  Active: "Active",
+  Cancelled: "Cancelled",
+  Expired: "Expired",
+  Approved: "Approved",
+  Rejected: "Rejected",
+  Slashed: "Slashed"
 };
 
 export class ProposalStatus extends Enum {
   constructor(value?: any) {
-    super([
-      'Active',
-      'Cancelled',
-      'Expired',
-      'Approved',
-      'Rejected',
-      'Slashed'
-    ], value);
+    super(["Active", "Cancelled", "Expired", "Approved", "Rejected", "Slashed"], value);
   }
 }
 
 export const VoteKinds: { [key: string]: string } = {
-  Abstain: 'Abstain',
-  Approve: 'Approve',
-  Reject: 'Reject',
-  Slash: 'Slash'
+  Abstain: "Abstain",
+  Approve: "Approve",
+  Reject: "Reject",
+  Slash: "Slash"
 };
 
 export class VoteKind extends Enum {
   constructor(value?: any) {
-    super([
-      'Abstain',
-      'Approve',
-      'Reject',
-      'Slash'
-    ], value);
+    super(["Abstain", "Approve", "Reject", "Slash"], value);
   }
 }
 
 export type ProposalVotes = [AccountId, VoteKind][];
 
 // Treat a BTreeSet as a Vec since it is encoded in the same way.
-export class BTreeSet<T extends Codec> extends Vec<T> { };
+export class BTreeSet<T extends Codec> extends Vec<T> {}
 
 // TODO Refactor: split this function and move to corresponding modules: election and proposals.
 function registerElectionAndProposalTypes() {
@@ -190,7 +178,7 @@ function registerElectionAndProposalTypes() {
     });
 
     typeRegistry.register({
-      MemoText: 'Text'
+      MemoText: "Text"
     });
     // Register parametrized enum ElectionStage:
     typeRegistry.register({
@@ -201,52 +189,52 @@ function registerElectionAndProposalTypes() {
       VoteKind
     });
     typeRegistry.register({
-      'ElectionStake': {
-        'new': 'Balance',
-        'transferred': 'Balance'
+      ElectionStake: {
+        new: "Balance",
+        transferred: "Balance"
       },
-      'Backer': {
-        member: 'AccountId',
-        stake: 'Balance'
+      Backer: {
+        member: "AccountId",
+        stake: "Balance"
       },
-      'Seat': {
-        member: 'AccountId',
-        stake: 'Balance',
-        backers: 'Vec<Backer>'
+      Seat: {
+        member: "AccountId",
+        stake: "Balance",
+        backers: "Vec<Backer>"
       },
-      'Seats': 'Vec<Seat>',
-      'SealedVote': {
-        'voter': 'AccountId',
-        'commitment': 'Hash',
-        'stake': 'ElectionStake',
-        'vote': 'Option<AccountId>'
+      Seats: "Vec<Seat>",
+      SealedVote: {
+        voter: "AccountId",
+        commitment: "Hash",
+        stake: "ElectionStake",
+        vote: "Option<AccountId>"
       },
-      'TransferableStake': {
-        'seat': 'Balance',
-        'backing': 'Balance'
+      TransferableStake: {
+        seat: "Balance",
+        backing: "Balance"
       },
-      'RuntimeUpgradeProposal': {
-        'id': 'u32',
-        'proposer': 'AccountId',
-        'stake': 'Balance',
-        'name': 'Text',
-        'description': 'Text',
-        'wasm_hash': 'Hash',
-        'proposed_at': 'BlockNumber',
-        'status': 'ProposalStatus'
+      RuntimeUpgradeProposal: {
+        id: "u32",
+        proposer: "AccountId",
+        stake: "Balance",
+        name: "Text",
+        description: "Text",
+        wasm_hash: "Hash",
+        proposed_at: "BlockNumber",
+        status: "ProposalStatus"
       },
-      'TallyResult<BlockNumber>': {
-        'proposal_id': 'u32',
-        'abstentions': 'u32',
-        'approvals': 'u32',
-        'rejections': 'u32',
-        'slashes': 'u32',
-        'status': 'ProposalStatus',
-        'finalized_at': 'BlockNumber'
+      "TallyResult<BlockNumber>": {
+        proposal_id: "u32",
+        abstentions: "u32",
+        approvals: "u32",
+        rejections: "u32",
+        slashes: "u32",
+        status: "ProposalStatus",
+        finalized_at: "BlockNumber"
       }
     });
   } catch (err) {
-    console.error('Failed to register custom types of Joystream node', err);
+    console.error("Failed to register custom types of Joystream node", err);
   }
 }
 
@@ -256,6 +244,7 @@ export function registerJoystreamTypes() {
   registerMediaTypes();
   registerForumTypes();
   registerElectionAndProposalTypes();
+  registerProposalsTypes();
   registerDiscoveryTypes();
   registerVersionedStoreTypes();
   registerVersionedStorePermissionsTypes();
