4cd3c45e73a852aac5ddde5f17ffb5d6545edbf6
Gamaranto <gamarantor@gmail.com>
2020-04-13 09:10:54 +0200

    Add and Fix Stories

--- packages/joy-proposals/src/Proposal/ProposalPreviewList.tsx
+++ packages/joy-proposals/src/Proposal/ProposalPreviewList.tsx
@@ -1,54 +1,54 @@
-import React, { useState } from "react";
-import { Card, Menu, Container } from "semantic-ui-react";
+import React, { useState } from 'react';
+import { Card, Menu, Container } from 'semantic-ui-react';
 
-import { ProposalProps } from "./ProposalDetails";
-import ProposalPreview from "./ProposalPreview";
+import { ProposalProps } from './ProposalDetails';
+import ProposalPreview from './ProposalPreview';
 
-type ProposalFilter = "all" | "active" | "withdrawn" | "approved" | "rejected" | "slashed";
+type ProposalFilter = 'all' | 'active' | 'withdrawn' | 'approved' | 'rejected' | 'slashed';
 
 export default function ProposalPreviewList({ proposals }: { proposals: ProposalProps[] }) {
-    let [activeFilter, setActiveFilter] = useState<ProposalFilter>("all");
-    let proposalsMap = new Map();
+    const [activeFilter, setActiveFilter] = useState<ProposalFilter>('all');
+    const proposalsMap = new Map();
 
-    proposalsMap.set("all", proposals);
-    proposalsMap.set("withdrawn", filterProposals("withdrawn", proposals));
-    proposalsMap.set("active", filterProposals("withdrawn", proposals));
-    proposalsMap.set("approved", filterProposals("approved", proposals));
-    proposalsMap.set("rejected", filterProposals("rejected", proposals));
-    proposalsMap.set("slashed", filterProposals("slashed", proposals));
+    proposalsMap.set('all', proposals);
+    proposalsMap.set('withdrawn', filterProposals('withdrawn', proposals));
+    proposalsMap.set('active', filterProposals('withdrawn', proposals));
+    proposalsMap.set('approved', filterProposals('approved', proposals));
+    proposalsMap.set('rejected', filterProposals('rejected', proposals));
+    proposalsMap.set('slashed', filterProposals('slashed', proposals));
 
     return (
         <Container className="Proposal">
             <Menu tabular className="list-menu">
                 <Menu.Item
-                    name={`all - ${proposalsMap.get("withdrawn").length} `}
-                    active={activeFilter === "all"}
-                    onClick={() => setActiveFilter("all")}
+                    name={`all - ${proposalsMap.get('withdrawn').length} `}
+                    active={activeFilter === 'all'}
+                    onClick={() => setActiveFilter('all')}
                 />
                 <Menu.Item
-                    name={`withdrawn (${proposalsMap.get("withdrawn").length})`}
-                    active={activeFilter === "withdrawn"}
-                    onClick={() => setActiveFilter("withdrawn")}
+                    name={`withdrawn (${proposalsMap.get('withdrawn').length})`}
+                    active={activeFilter === 'withdrawn'}
+                    onClick={() => setActiveFilter('withdrawn')}
                 />
                 <Menu.Item
-                    name={`active (${proposalsMap.get("active").length})`}
-                    active={activeFilter === "active"}
-                    onClick={() => setActiveFilter("active")}
+                    name={`active (${proposalsMap.get('active').length})`}
+                    active={activeFilter === 'active'}
+                    onClick={() => setActiveFilter('active')}
                 />
                 <Menu.Item
-                    name={`approved (${proposalsMap.get("approved").length})`}
-                    active={activeFilter === "approved"}
-                    onClick={() => setActiveFilter("approved")}
+                    name={`approved (${proposalsMap.get('approved').length})`}
+                    active={activeFilter === 'approved'}
+                    onClick={() => setActiveFilter('approved')}
                 />
                 <Menu.Item
-                    name={`rejected (${proposalsMap.get("rejected").length})`}
-                    active={activeFilter === "rejected"}
-                    onClick={() => setActiveFilter("rejected")}
+                    name={`rejected (${proposalsMap.get('rejected').length})`}
+                    active={activeFilter === 'rejected'}
+                    onClick={() => setActiveFilter('rejected')}
                 />
                 <Menu.Item
-                    name={`slashed (${proposalsMap.get("slashed").length})`}
-                    active={activeFilter === "slashed"}
-                    onClick={() => setActiveFilter("slashed")}
+                    name={`slashed (${proposalsMap.get('slashed').length})`}
+                    active={activeFilter === 'slashed'}
+                    onClick={() => setActiveFilter('slashed')}
                 />
             </Menu>
 
@@ -67,10 +67,10 @@ export default function ProposalPreviewList({ proposals }: { proposals: Proposal
 }
 
 function filterProposals(filter, proposals) {
-    if (filter === "all") {
+    if (filter === 'all') {
         return proposals;
-    } else if (filter === "active") {
-        return proposals.filter((prop: any) => prop.details.stage === "active");
+    } else if (filter === 'active') {
+        return proposals.filter((prop: any) => prop.details.stage === 'active');
     }
 
     return proposals.filter((prop: any) => prop.finalized === filter);
