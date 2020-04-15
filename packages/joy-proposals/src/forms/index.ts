e2bced809b1876a3608b5c14802592870bac94c3
Gamaranto <gamarantor@gmail.com>
2020-04-15 19:12:39 +0200

    Add Signal Form

---
+++ packages/joy-proposals/src/forms/index.ts
@@ -0,0 +1,1 @@
+export { default as SignalForm } from "./SignalForm";
--- packages/joy-proposals/src/stories/ProposalForms.stories.tsx
+++ packages/joy-proposals/src/stories/ProposalForms.stories.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import '../index.css';
-import { EditForm } from '../forms/GenericProposalForm';
-import { ProposalId, ProposalType, ProposalFormValues, ProposalGenericProp, ProposalValidationConstraints } from '..s/forms/ProposalTypes';
+import React from "react";
+import "../index.css";
+import { SignalForm } from "../forms/";
 
 export default {
-	title: 'Proposals | Forms',
+  title: "Proposals | Forms"
 };
 
-const MockGenericProposal = {
-	title: 'Please send me some tokens for coffee',
-	description: 'I am a good guy and deserve this reward.'
-}
-
-const MockProposalConstraints = {
-	title: { min: 1, max: 50 },
-	description: { min: 1, max: 50 }
-}
-
-export const GenericForm = () =>
-	<EditForm
-		entity={MockGenericProposal}
-		constraints={MockProposalConstraints} 
-	/>
+export const Signal = () => <SignalForm />;
