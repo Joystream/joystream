209689c5009154790757e8bd8be22fdfcd111b4b
metmirr <metmirr@gmail.com>
2020-04-20 15:52:03 +0300

    Accept schema path from command line, throw error on invalid input type

--- joystream-query-node/cli.ts
+++ joystream-query-node/cli.ts
@@ -1,15 +1,26 @@
+import 'reflect-metadata';
+import * as fs from 'fs';
+import { extname } from 'path';
+import { deserializeArray, Type } from 'class-transformer';
 const cli = require('warthog/dist/cli/cli');
-const definations: Input[] = require('./schema.json');
 
-interface Input {
-	name: String;
-	fields: [Field];
+const defaultSchemaPath = './schema.json';
+
+class Input {
+	name!: string;
+
+	@Type(() => Field)
+	fields!: [Field];
+
+	getFormattedFields() {
+		return this.fields.map(f => `${f.name}:${f.type}`).join(' ');
+	}
 }
 
-interface Field {
-	name: string;
-	type: string;
-	description: string;
+class Field {
+	name!: string;
+	type!: string;
+	description?: string;
 }
 
 /**
@@ -22,32 +33,56 @@ function init() {
 /**
  * Generate model/resolver/service for input types in schema.json
  */
-function generate() {
-	const command = 'generate';
+function generate(inputs: Input[]) {
 	// Make arguments ready for "generate" command
-	const commands = definations.map(input => {
-		// e.g name:string! age:int!
-		const fields = input.fields.map(f => `${f.name}:${f.type}`).join(' ');
+	const commands = inputs.map(input => {
+		if (!input.name) logErrorAndExit('A type must have "name" property');
+		if (!input.fields)
+			logErrorAndExit(`A defined type must have at least one field. Got "${input.fields}"`);
 
 		// e.g generate user name:string! age:int!
-		return [command, input.name, fields].join(' ');
+		return ['generate', input.name, input.getFormattedFields()].join(' ');
 	});
 
 	// Execute commands
-	commands.forEach(command => cli.run(command));
+	commands.forEach(command => (command ? cli.run(command) : ''));
+}
+
+function prepareInputData(schemaPath: string): Input[] {
+	if (!fs.existsSync(schemaPath)) {
+		logErrorAndExit(`File does not exists! ${schemaPath}`);
+	}
+	if (!extname(schemaPath)) {
+		logErrorAndExit('Schema file must be a JSON file!');
+	}
+	const data = fs.readFileSync(schemaPath, 'utf8');
+	return deserializeArray(Input, data);
+}
+
+function logErrorAndExit(message: string) {
+	console.error(message);
+	process.exit(1);
 }
 
 function main() {
-	const command = process.argv[2];
+	if (process.argv.length === 2) {
+		logErrorAndExit('Expected at least one argument!');
+	}
+
+	const commandIndex = 2;
+	const command = process.argv[commandIndex];
+
 	switch (command) {
 		case 'new':
 			init();
 			break;
 		case 'generate':
-			generate();
+			let schemaPath = process.argv[commandIndex + 1];
+			schemaPath = schemaPath ? schemaPath : defaultSchemaPath;
+			generate(prepareInputData(schemaPath));
 			break;
 		default:
-			throw new Error('Missing command! Available commands [init, generate]');
+			logErrorAndExit('Expected at least one argument!');
 	}
 }
 
