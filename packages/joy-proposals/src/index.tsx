b60885fcafec4caf2b228c1c6351de9ae4f2d4c9
Gamaranto <gamarantor@gmail.com>
2020-04-21 18:55:26 +0200

    Add Basic Index To Proposals

---
+++ packages/joy-proposals/src/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Route, Switch } from "react-router";
+
+import { AppProps, I18nProps } from "@polkadot/react-components/types";
+import Tabs, { TabItem } from "@polkadot/react-components/Tabs";
+import { ApiProps } from "@polkadot/react-api/types";
+
+import "./index.css";
+
+import translate from "./translate";
+import NotDone from "./NotDone";
+import { ProposalPreviewList } from "./Proposal";
+
+type Props = AppProps & I18nProps & ApiProps;
+
+function App(props: Props) {
+  const { t, basePath } = props;
+
+  // TODO: Can use semantic here?
+  const tabs: TabItem[] = [
+    {
+      isRoot: true,
+      name: "proposals",
+      text: t("Proposals")
+    },
+    {
+      name: "proposals/new",
+      text: t("New Proposal")
+    }
+  ];
+
+  return (
+    <main className="proposal--App">
+      <header>
+        <Tabs basePath={basePath} items={tabs} />
+      </header>
+      <Switch>
+        <Route path={`${basePath}/proposals/new`} component={NotDone} />
+        <Route path={`${basePath}/active`} component={NotDone} />
+        <Route path={`${basePath}/finalized`} component={NotDone} />
+        <Route path={`${basePath}/new`} component={NotDone} />
+        <Route path={`${basePath}/:id`} component={NotDone} />
+        <Route component={ProposalPreviewList} />
+      </Switch>
+    </main>
+  );
+}
+
+export default translate(App);
