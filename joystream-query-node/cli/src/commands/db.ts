46f53e0473fe65b595cd012fae0472273084496e
metmirr <metmirr@gmail.com>
2020-04-22 23:55:23 +0300

    Add db commands create, migrate, generatemigration

---
+++ joystream-query-node/cli/src/commands/db.ts
@@ -0,0 +1,27 @@
+const cli = require('warthog/dist/cli/cli');
+import { Command, flags } from '@oclif/command';
+
+export default class DB extends Command {
+  static flags = {
+    create: flags.boolean({ char: 'c', description: 'Create database' }),
+    migrate: flags.boolean({ char: 'm', description: 'Run database migration' }),
+    generatemigration: flags.string({ char: 'g', description: 'Generate database migration' }),
+  };
+
+  async run() {
+    const { flags } = this.parse(DB);
+
+    if (flags.create) {
+      cli.run('db:create');
+      this.exit(0);
+    }
+    if (flags.migrate) {
+      cli.run('db:migrate');
+      this.exit(0);
+    }
+    if (flags.generatemigration) {
+      cli.run(`db:migrate:generate --name=${flags.generatemigration}`);
+      this.exit(0);
+    }
+  }
+}
