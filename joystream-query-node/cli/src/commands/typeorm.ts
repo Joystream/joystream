cda379615d237bd06535261200c6d5238f668fe3
metmirr <metmirr@gmail.com>
2020-04-22 21:56:44 +0300

    Set typeorm-model-generator options from ormconfig.json file

--- joystream-query-node/cli/src/commands/typeorm.ts
+++ joystream-query-node/cli/src/commands/typeorm.ts
@@ -1,20 +1,61 @@
+import * as fs from 'fs';
+import * as path from 'path';
+//const { exec } = require('child_process');
 import { Command, flags } from '@oclif/command';
-
-import { initConfig } from '../helpers/initConfig';
-import { createEntities } from '../helpers/createEntities';
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 export default class Typeorm extends Command {
   static description = 'Typeorm commands';
+  static typeormModelGenerator = 'typeorm-model-generator';
 
   static args = [{ name: 'generate', options: ['generate'] }];
   static flags = {
-    env: flags.string({ char: 'e', description: 'dotenv file path', required: true })
+    ormconfig: flags.string({
+      char: 'c',
+      description: 'ormconfig.json file',
+      required: true,
+      default: './ormconfig.json',
+    }),
+    config: flags.string({ char: 'c', default: 'default', description: 'Typeorm config name' }),
+    output: flags.string({ char: 'o', description: 'Generated entity path', default: '.' }),
   };
-  async run() {
-    initConfig();
-    if (!process.env.TYPEORM_CLI)
-      this.error('typeorm-model-generator path is not defined in .env file');
 
-    createEntities();
+  async run() {
+    const { flags } = this.parse(Typeorm);
+
+    // Get ormconfig.json file path
+    const configFilePath = path.resolve(process.cwd(), flags.ormconfig);
+    // Read content of ormconfig.json file
+    const configData = fs.readFileSync(configFilePath, 'utf8');
+
+    if (configData.length <= 1) this.error('Empty config file');
+
+    let configs;
+    try {
+      configs = JSON.parse(configData);
+    } catch (error) {
+      this.error('Invalid JSON data');
+    }
+
+    // Find config by name
+    const config = configs.find((c: any) => c.name === flags.config);
+    if (typeof config !== 'object') this.error('Not an object');
+
+    const command = [
+      Typeorm.typeormModelGenerator,
+      `-h ${config.host}`,
+      `-d ${config.database}`,
+      `-e ${config.type}`,
+      `-o ${flags.output}`,
+      `-u ${config.username}`,
+      `-p ${config.password}`,
+    ].join(' ');
+
+    exec(command, function (error: any, stdout: any, stderr: any) {
+      // command output is in stdout
+      console.log(stdout, stderr);
+      if (error) console.log(error);
+    });
   }
 }
