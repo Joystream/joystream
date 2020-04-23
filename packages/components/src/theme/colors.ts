df908525aff2c977d89549486b080615d47de638
Pedro Semeano <semeano@gmail.com>
2020-04-23 10:37:03 +0100

    Add colors from design specifications.

--- packages/components/src/theme/colors.ts
+++ packages/components/src/theme/colors.ts
@@ -16,4 +16,23 @@ export default {
     danger: "#EF6236",
     warning: "#FEEB83",
   },
+  black: {
+    base: "#000",
+    medium: "#121519",
+    light: "1f252e"
+  },
+  blue: {
+    darker: "#261ee4",
+    dark: "#2f2ff4",
+    base: "#4038ff",
+    light: "#5252ff",
+    lighter: "#6c6cff"
+  },
+  grey: {
+    darker: "#a7aeb7",
+    dark: "#d3d8e0",
+    base: "#e8edf6",
+    light: "#f2f5f9"
+  },
+  white: "#fff"
 };
