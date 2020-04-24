4841a73c15ee6b89aa467b49f60151735793145c
metmirr <metmirr@gmail.com>
2020-04-24 14:56:15 +0300

    Set database name for substrate-query-node

--- joystream-query-node/cli/src/commands/new.ts
+++ joystream-query-node/cli/src/commands/new.ts
@@ -1,15 +1,16 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
 const cli = require('warthog/dist/cli/cli');
 import { Command } from '@oclif/command';
 
-import { ormconfig } from '../helpers/ormconfig';
 import { createDir, createFile } from '../utils/utils';
+import { formatWithPrettier } from '../helpers/formatter';
 
 export default class New extends Command {
   static description = 'Create grapqhl server and event processor';
   static joystreamQueryNode = 'joystream-query-node';
   static substrateQueryNode = 'substrate-query-node';
   static schemaFile = 'schema.json';
-  static ormconfigFile = 'ormconfig.json';
 
   static args = [
     {
@@ -29,7 +30,7 @@ export default class New extends Command {
     createDir(New.joystreamQueryNode);
     createDir(New.substrateQueryNode);
 
-    this.generateSubstrateQueryNodeDirs();
+    this.generateSubstrateQueryNodeDirs(project_name);
 
     // Change working to joystream-query-node
     process.chdir(New.joystreamQueryNode);
@@ -39,12 +40,29 @@ export default class New extends Command {
     cli.run(`new ${project_name}`);
   }
 
-  generateSubstrateQueryNodeDirs() {
+  generateSubstrateQueryNodeDirs(projectName: string) {
     // substrate-query-node/src
     const srcDir = [New.substrateQueryNode, 'src'].join('/');
-    const ormConfigFile = [New.substrateQueryNode, New.ormconfigFile].join('/');
 
     createDir(srcDir);
-    createFile(ormConfigFile, JSON.stringify(ormconfig));
+
+    let ormconfigTemplate: any;
+    ormconfigTemplate = fs.readFileSync(
+      path.resolve(__dirname, '..', 'helpers', 'templates', 'ormconfig.json'),
+      'utf8'
+    );
+    try {
+      ormconfigTemplate = JSON.parse(ormconfigTemplate);
+
+      ormconfigTemplate[0]['database'] = projectName;
+    } catch (error) {
+      this.error('ormconfig.json template is not a valid json file.');
+    }
+
+    const ormconfigFilePath = path.resolve(
+      path.resolve(process.cwd(), New.substrateQueryNode, 'ormconfig.json')
+    );
+    ormconfigTemplate = formatWithPrettier(JSON.stringify(ormconfigTemplate));
+    createFile(ormconfigFilePath, ormconfigTemplate);
   }
 }
