73e465b05d8bb2bbe760b23d7b529e7162cc7875
Pedro Semeano <semeano@gmail.com>
2020-04-27 10:48:44 +0100

    Add VideoPlayer component.

--- packages/app/src/views/VideoView/VideoView.tsx
+++ packages/app/src/views/VideoView/VideoView.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from "react"
 
 import {
-  Video,
+  VideoPlayer,
   GenericSection,
   ChannelSummary,
   DetailsTable,
-} from "components";
-import { useParams, RouteComponentProps } from "@reach/router";
+} from "components"
+import { useParams, RouteComponentProps, navigate } from "@reach/router"
 
 type VideoViewProps = {
-  video: any;
-  channel: any;
-};
+  video: any
+  channel: any
+}
 function VideoViewComponent({ video, channel }: VideoViewProps) {
   return (
     <>
       <GenericSection>
-        <Video src={video.src} poster={video.poster} />
+        <VideoPlayer src={video.src} poster={video.poster} />
       </GenericSection>
       <GenericSection
         topDivider
@@ -26,7 +26,6 @@ function VideoViewComponent({ video, channel }: VideoViewProps) {
         <ChannelSummary
           isPublic={channel.isPublic}
           img={channel.img}
-          channelUrl="/"
           name={channel.name}
           isVerified={channel.isVerified}
           description={video.description}
@@ -41,17 +40,17 @@ function VideoViewComponent({ video, channel }: VideoViewProps) {
         <DetailsTable details={video.details} />
       </GenericSection>
     </>
-  );
+  )
 }
 
 type RouteProps = {
-  videos: any;
-  channels: any;
-} & RouteComponentProps;
+  videos: any
+  channels: any
+} & RouteComponentProps
 
 export default function VideoView({ videos, channels }: RouteProps) {
-  let params = useParams();
-  let video = Object.values(videos).flat()[params.idx];
-  let channel = channels[video.channel];
-  return <VideoViewComponent video={video} channel={channel} />;
+  let params = useParams()
+  let video = Object.values(videos).flat()[params.idx]
+  let channel = channels[video.channel]
+  return <VideoViewComponent video={video} channel={channel} />
 }
