7b6d0b3368e794ea4d6ac37ea0786855baf9c1e0
Pedro Semeano <semeano@gmail.com>
2020-04-27 17:51:33 +0100

    Fixing type declarations.

--- packages/app/src/store/index.ts
+++ packages/app/src/store/index.ts
@@ -3,7 +3,7 @@ import rootReducer from "./reducers"
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  (<any>window).__REDUX_DEVTOOLS_EXTENSION__ && (<any>window).__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
