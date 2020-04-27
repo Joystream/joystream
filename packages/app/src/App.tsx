c92bdf206f3801bb0e9b069035f2d074271a879b
Pedro Semeano <semeano@gmail.com>
2020-04-27 09:56:40 +0100

    Scroll restoration when navigating.

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,30 +1,33 @@
-import React from "react";
-import { Router } from "@reach/router";
-import { Provider } from "react-redux";
+import React from "react"
+import { Router } from "@reach/router"
+import { Provider } from "react-redux"
 
 import store from "./store"
-import data from "../staticData";
+import data from "../staticData"
 
-import ChannelView from "./views/ChannelView";
-import ExploreView from "./views/ExploreView";
-import VideoView from "./views/VideoView";
+import ChannelView from "./views/ChannelView"
+import ExploreView from "./views/ExploreView"
+import VideoView from "./views/VideoView"
+import ScrollToTop from "./components/ScrollToTop"
 
-let { channels, videos } = data;
+let { channels, videos } = data
 
 export default function App() {
   return (
     <main className="main-section">
       <Provider store={store}>
-        <Router>
-          <ExploreView path="/" channels={channels} videos={videos} />
-          <ChannelView
-            path="/channels/:channelName"
-            channels={channels}
-            videos={videos}
-          />
-          <VideoView path="/videos/:idx" channels={channels} videos={videos} />
+        <Router primary={false}>
+          <ScrollToTop path="/">
+            <ExploreView path="/" channels={channels} videos={videos} />
+            <ChannelView
+              path="/channels/:channelName"
+              channels={channels}
+              videos={videos}
+            />
+            <VideoView path="/videos/:idx" channels={channels} videos={videos} />
+          </ScrollToTop>
         </Router>
       </Provider>
     </main>
-  );
+  )
 }
