a9c49834292d41d66d2480307fcf584d117631dc
Gamaranto <gamarantor@gmail.com>
2020-04-30 18:38:46 +0200

    Make Validation Values Dynamic

--- packages/joy-proposals/src/validationSchema.ts
+++ packages/joy-proposals/src/validationSchema.ts
@@ -1,6 +1,59 @@
-import { ProposalType } from "./runtime";
 import * as Yup from "yup";
 import { StringSchema, NumberSchema } from "@types/yup";
+import { checkAddress } from "@polkadot/util-crypto";
+
+const ANNOUNCING_PERIOD_MAX = 23000;
+const ANNOUNCING_PERIOD_MIN = 48000;
+const VOTING_PERIOD_MIN = 23000;
+const VOTING_PERIOD_MAX = 48000;
+const MIN_VOTING_STAKE_MIN = 23000;
+const MIN_VOTING_STAKE_MAX = 49000;
+const REVEALING_PERIOD_MIN = 23000;
+const REVEALING_PERIOD_MAX = 48000;
+const MIN_COUNCIL_STAKE_MIN = 1000000;
+const MIN_COUNCIL_STAKE_MAX = 10000000;
+const NEW_TERM_DURATION_MIN = 23000;
+const NEW_TERM_DURATION_MAX = 46000;
+const CANDIDACY_LIMIT_MIN = 23000;
+const CANDIDACY_LIMIT_MAX = 45000;
+const COUNCIL_SIZE_MAX = 23;
+const COUNCIL_SIZE_MIN = 5;
+const MIN_STAKE_MIN = 1;
+const MIN_STAKE_MAX = 1000;
+const MIN_ACTORS_MIN = 1;
+const MIN_ACTORS_MAX = 20;
+const MAX_ACTORS_MIN = 1;
+const MAX_ACTORS_MAX = 40;
+const REWARD_MIN = 100;
+const REWARD_MAX = 1000;
+const REWARD_PERIOD_MIN = 2000;
+const REWARD_PERIOD_MAX = 42000;
+const BONDING_PERIOD_MIN = 43000;
+const BONDING_PERIOD_MAX = 56000;
+const UNBONDING_PERIOD_MIN = 34000;
+const UNBONDING_PERIOD_MAX = 36000;
+const MIN_SERVICE_PERIOD_MIN = 23000;
+const MIN_SERVICE_PERIOD_MAX = 26000;
+const STARTUP_GRACE_PERIOD_MIN = 4000;
+const STARTUP_GRACE_PERIOD_MAX = 8000;
+const ENTRY_REQUEST_FEE_MIN = 400;
+const ENTRY_REQUEST_FEE_MAX = 2000;
+
+/*
+Validation is used to validate a proposal form.
+Each proposal type should validate the fields of his form, anything is valid as long as it fits in a Yup Schema.
+In a form, validation should be injected in the Yup Schema just by accessing it in this object.
+Ex:
+// EvictStorageProvider Form
+
+import Validation from 'path/to/validationSchema'
+...
+  validationSchema: Yup.object().shape({
+    ...genericFormDefaultOptions.validationSchema,
+    storageProvider: Validation.EvictStorageProvider.storageProvider
+  }),
+
+*/
 
 type ValidationType = {
   All: {
@@ -50,21 +103,7 @@ type ValidationType = {
     entry_request_fee: NumberSchema<number>;
   };
 };
-/*
-Validation is used to validate a proposal form.
-Each proposal type should validate the fields of his form, anything is valid as long as it fits in a Yup Schema.
-In a form, validation should be injected in the Yup Schema just by accessing it in this object.
-Ex:
-// EvictStorageProvider Form
 
-import Validation from 'path/to/validationSchema'
-...
-  validationSchema: Yup.object().shape({
-    ...genericFormDefaultOptions.validationSchema,
-    storageProvider: Validation.EvictStorageProvider.storageProvider
-  }),
-
-*/
 const Validation: ValidationType = {
   All: {
     title: Yup.string().required("Title is required!"),
@@ -77,18 +116,44 @@ const Validation: ValidationType = {
     WASM: Yup.string().required("The file is empty!")
   },
   SetElectionParameters: {
-    announcingPeriod: Yup.number().required("All fields must be filled!"),
-    votingPeriod: Yup.number().required("All fields must be filled!"),
-    minVotingStake: Yup.number().required("All fields must be filled!"),
-    revealingPeriod: Yup.number().required("All fields must be filled!"),
-    minCouncilStake: Yup.number().required("All fields must be filled!"),
-    newTermDuration: Yup.number().required("All fields must be filled!"),
-    candidacyLimit: Yup.number().required("All fields must be filled!"),
-    councilSize: Yup.number().required("All fields must be filled!")
+    announcingPeriod: Yup.number()
+      .required("All fields must be filled!")
+      .min(ANNOUNCING_PERIOD_MIN)
+      .max(ANNOUNCING_PERIOD_MAX),
+    votingPeriod: Yup.number()
+      .required("All fields must be filled!")
+      .min(VOTING_PERIOD_MIN)
+      .max(VOTING_PERIOD_MAX),
+    minVotingStake: Yup.number()
+      .required("All fields must be filled!")
+      .min(MIN_VOTING_STAKE_MIN)
+      .max(MIN_VOTING_STAKE_MAX),
+    revealingPeriod: Yup.number()
+      .required("All fields must be filled!")
+      .min(REVEALING_PERIOD_MIN)
+      .max(REVEALING_PERIOD_MAX),
+    minCouncilStake: Yup.number()
+      .required("All fields must be filled!")
+      .min(MIN_COUNCIL_STAKE_MIN)
+      .max(MIN_COUNCIL_STAKE_MAX),
+    newTermDuration: Yup.number()
+      .required("All fields must be filled!")
+      .min(NEW_TERM_DURATION_MIN)
+      .max(NEW_TERM_DURATION_MAX),
+    candidacyLimit: Yup.number()
+      .required("All fields must be filled!")
+      .min(CANDIDACY_LIMIT_MIN)
+      .max(CANDIDACY_LIMIT_MAX),
+    councilSize: Yup.number()
+      .required("All fields must be filled!")
+      .min(COUNCIL_SIZE_MIN)
+      .max(COUNCIL_SIZE_MAX)
   },
   Spending: {
     tokens: Yup.number().required("You need to specify an amount of tokens."),
-    destinationAccount: Yup.string().required("Select a destination account!")
+    destinationAccount: Yup.string()
+      .required("Select a destination account!")
+      .test("address-test", "${account} is not a valid address", account => !!checkAddress(account, 5))
   },
   SetLead: {
     workingGroupLead: Yup.string().required("Select a proposed lead!")
@@ -103,16 +168,47 @@ const Validation: ValidationType = {
     maxValidatorCount: Yup.number().required("Enter the max validator count")
   },
   SetStorageRoleParameters: {
-    min_stake: Yup.number().required("All parameters are required"),
-    min_actors: Yup.number().required("All parameters are required"),
-    max_actors: Yup.number().required("All parameters are required"),
-    reward: Yup.number().required("All parameters are required"),
-    reward_period: Yup.number().required("All parameters are required"),
-    bonding_period: Yup.number().required("All parameters are required"),
-    unbonding_period: Yup.number().required("All parameters are required"),
-    min_service_period: Yup.number().required("All parameters are required"),
-    startup_grace_period: Yup.number().required("All parameters are required"),
-    entry_request_fee: Yup.number().required("All parameters are required")
+    min_stake: Yup.number()
+      .required("All parameters are required")
+      .min(MIN_STAKE_MIN)
+      .max(MIN_STAKE_MAX),
+    min_actors: Yup.number()
+      .required("All parameters are required")
+      .min(MIN_ACTORS_MIN)
+      .max(MIN_ACTORS_MAX),
+    max_actors: Yup.number()
+      .required("All parameters are required")
+      .min(MAX_ACTORS_MIN)
+      .max(MAX_ACTORS_MAX),
+    reward: Yup.number()
+      .required("All parameters are required")
+      .positive()
+      .min(REWARD_MIN)
+      .max(REWARD_MAX),
+    reward_period: Yup.number()
+      .required("All parameters are required")
+      .min(REWARD_PERIOD_MIN)
+      .max(REWARD_PERIOD_MAX),
+    bonding_period: Yup.number()
+      .required("All parameters are required")
+      .min(BONDING_PERIOD_MIN)
+      .max(BONDING_PERIOD_MAX),
+    unbonding_period: Yup.number()
+      .required("All parameters are required")
+      .min(UNBONDING_PERIOD_MIN)
+      .max(UNBONDING_PERIOD_MAX),
+    min_service_period: Yup.number()
+      .required("All parameters are required")
+      .min(MIN_SERVICE_PERIOD_MIN)
+      .max(MIN_SERVICE_PERIOD_MAX),
+    startup_grace_period: Yup.number()
+      .required("All parameters are required")
+      .min(STARTUP_GRACE_PERIOD_MIN)
+      .max(STARTUP_GRACE_PERIOD_MAX),
+    entry_request_fee: Yup.number()
+      .required("All parameters are required")
+      .min(ENTRY_REQUEST_FEE_MIN)
+      .max(ENTRY_REQUEST_FEE_MAX)
   }
 };
 
