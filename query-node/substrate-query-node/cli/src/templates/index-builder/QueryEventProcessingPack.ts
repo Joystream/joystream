c3b266be62655b20c5ba33886bb5cf635b1cdede
metmirr <metmirr@gmail.com>
2020-04-30 18:08:01 +0300

    Remove unused code from templates

--- query-node/substrate-query-node/cli/src/templates/index-builder/QueryEventProcessingPack.ts
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import QueryEvent from './QueryEvent'
-
-export type QueryEventProcessorResult = void | Promise<void>
-
-export default interface QueryEventProcessingPack {
-    [index: string] : (query_event: QueryEvent) => QueryEventProcessorResult
-}
--- query-node/substrate-query-node/cli/src/templates/query-node/index.ts
+++
@@ -1,8 +0,0 @@
-import QueryNode, {QueryNodeState} from './QueryNode'
-import QueryNodeManager from './QueryNodeManager'
-
-export {
-    QueryNode,
-    QueryNodeState,
-    QueryNodeManager
-}
