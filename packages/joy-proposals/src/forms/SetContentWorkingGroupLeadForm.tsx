061f55130cbcd378ad39915253b4230f7565d208
Leszek Wiesner <l.wiesner@adawards.pl>
2020-05-05 13:11:32 +0200

    Set lead default & info

--- packages/joy-proposals/src/forms/SetContentWorkingGroupLeadForm.tsx
+++ packages/joy-proposals/src/forms/SetContentWorkingGroupLeadForm.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Dropdown, Label, Loader } from "semantic-ui-react";
+import React, { useEffect } from "react";
+import { Dropdown, Label, Loader, Message, Icon } from "semantic-ui-react";
 import { getFormErrorLabelsProps } from "./errorHandling";
 import * as Yup from "yup";
 import {
@@ -18,6 +18,7 @@ import { withFormContainer } from "./FormContainer";
 import { useTransport } from "../runtime";
 import { usePromise } from "../utils";
 import { Profile } from "@joystream/types/members";
+import PromiseComponent from "../Proposal/PromiseComponent";
 import "./forms.css";
 
 type FormValues = GenericFormValues & {
@@ -34,52 +35,77 @@ type ExportComponentProps = ProposalFormExportProps<FormAdditionalProps, FormVal
 type FormContainerProps = ProposalFormContainerProps<ExportComponentProps>;
 type FormInnerProps = ProposalFormInnerProps<FormContainerProps, FormValues>;
 
+function memberOptionKey(id: number, profile: Profile) {
+  return `${id}:${profile.root_account.toString()}`;
+}
+
+function membersToOptions(members: { id: number, profile: Profile }[]) {
+  return members.map(({ id, profile }) => ({
+    key: profile.handle,
+    text: profile.handle,
+    value: memberOptionKey(id, profile),
+    image: profile.avatar_uri.toString() ? { avatar: true, src: profile.avatar_uri } : null
+  }));
+}
+
 const SetContentWorkingGroupsLeadForm: React.FunctionComponent<FormInnerProps> = props => {
-  const { handleChange, errors, touched, values } = props;
+  const { handleChange, errors, touched, values, setFieldValue } = props;
   const errorLabelsProps = getFormErrorLabelsProps<FormValues>(errors, touched);
   const transport = useTransport();
-  const [members /* error */, , loading] = usePromise<{ id: number; profile: Profile }[]>(
+  const [members, /* error */, loading] = usePromise<{ id: number; profile: Profile }[]>(
     () => transport.membersExceptCouncil(),
     []
   );
-  const membersOptions = members.map(({ id, profile }) => ({
-    key: profile.handle,
-    text: profile.handle,
-    value: `${id}:${profile.root_account.toString()}`,
-    image: profile.avatar_uri.toString() ? { avatar: true, src: profile.avatar_uri } : null
-  }));
+  const membersOptions = membersToOptions(members);
+  const [currentLead, clError, clLoading] = usePromise<{ id: number; profile: Profile } | null>(
+    () => transport.WGLead(),
+    null
+  );
+  useEffect(() => {
+    if (currentLead) {
+      setFieldValue('workingGroupLead', memberOptionKey(currentLead.id, currentLead.profile));
+    }
+  }, [currentLead])
   return (
-    <GenericProposalForm
-      {...props}
-      txMethod="createSetLeadProposal"
-      requiredStakePercent={0.25}
-      submitParams={[props.myMemberId, values.title, values.rationale, "{STAKE}", values.workingGroupLead.split(":")]}
-    >
-      {loading ? (
-        <>
-          <Loader active inline style={{ marginRight: "5px" }} /> Fetching members...
-        </>
-      ) : (
-        <FormField
-          error={errorLabelsProps.workingGroupLead}
-          label="New Content Working Group Lead"
-          help="The member you propose to set as a new Content Working Group Lead"
-        >
-          <Dropdown
-            clearable
-            search
-            name="workingGroupLead"
-            placeholder="Select a member"
-            fluid
-            selection
-            options={membersOptions}
-            onChange={handleChange}
-            value={values.workingGroupLead}
-          />
-          {errorLabelsProps.workingGroupLead && <Label {...errorLabelsProps.workingGroupLead} prompt />}
-        </FormField>
-      )}
-    </GenericProposalForm>
+    <PromiseComponent error={clError} loading={clLoading} message="Fetching current lead...">
+      <GenericProposalForm
+        {...props}
+        txMethod="createSetLeadProposal"
+        requiredStakePercent={0.25}
+        submitParams={[props.myMemberId, values.title, values.rationale, "{STAKE}", values.workingGroupLead.split(":")]}
+      >
+        {loading ? (
+          <>
+            <Loader active inline style={{ marginRight: "5px" }} /> Fetching members...
+          </>
+        ) : (<>
+          <FormField
+            error={errorLabelsProps.workingGroupLead}
+            label="New Content Working Group Lead"
+            help="The member you propose to set as a new Content Working Group Lead"
+          >
+            <Dropdown
+              clearable
+              search
+              name="workingGroupLead"
+              placeholder="Select a member"
+              fluid
+              selection
+              options={membersOptions}
+              onChange={handleChange}
+              value={values.workingGroupLead}
+            />
+            {errorLabelsProps.workingGroupLead && <Label {...errorLabelsProps.workingGroupLead} prompt />}
+          </FormField>
+          <Message info active>
+            <Message.Content>
+              <Icon name="info circle"/>
+              Current Content Working Group lead: <b>{ (currentLead && currentLead.profile.handle) || 'NONE' }</b>
+            </Message.Content>
+          </Message>
+        </>)}
+      </GenericProposalForm>
+    </PromiseComponent>
   );
 };
 
--- packages/joy-proposals/src/runtime/transport.substrate.ts
+++ packages/joy-proposals/src/runtime/transport.substrate.ts
@@ -1,15 +1,16 @@
 import { Transport, ParsedProposal, ProposalType, ProposalTypes, ParsedMember, ProposalVote } from "./transport";
 import { Proposal, ProposalId, Seats, VoteKind, ElectionParameters } from "@joystream/types/proposals";
-import { MemberId, Profile } from "@joystream/types/members";
+import { MemberId, Profile, ActorInRole } from "@joystream/types/members";
 import { ApiProps } from "@polkadot/react-api/types";
 import { u32, u128, Vec, Option } from "@polkadot/types/";
 import { Balance, Moment, AccountId, BlockNumber } from "@polkadot/types/interfaces";
 import { ApiPromise } from "@polkadot/api";
-import { RoleKeys } from "@joystream/types/members";
+import { RoleKeys, Role } from "@joystream/types/members";
 import { FIRST_MEMBER_ID } from '@polkadot/joy-members/constants';
 
 import { includeKeys, calculateStake, calculateMetaFromType, splitOnUpperCase } from "../utils";
 import { MintId, Mint } from "@joystream/types/mint";
+import { LeadId } from "@joystream/types/content-working-group";
 
 export class SubstrateTransport extends Transport {
   protected api: ApiPromise;
@@ -276,5 +277,21 @@ export class SubstrateTransport extends Transport {
     const WGMintId = await this.contentWorkingGroup.mint() as MintId;
     const WGMint = await this.minting.mints(WGMintId) as Vec<Mint>;
     return (WGMint[0].get('capacity') as u128).toNumber();
+  }
+
+  async WGLead(): Promise<{ id: number, profile: Profile } | null> {
+    const optLeadId = (await this.contentWorkingGroup.currentLeadId()) as Option<LeadId>;
+    const leadId = optLeadId.unwrapOr(null);
+
+    if (!leadId) return null;
+
+    const actorInRole = new ActorInRole({
+      role: new Role(RoleKeys.CuratorLead),
+      actor_id: leadId
+    })
+    const memberId = await this.members.membershipIdByActorInRole(actorInRole) as MemberId;
+    const profile = (await this.memberProfile(memberId)).unwrapOr(null);
+
+    return profile && { id: memberId.toNumber(), profile };
   }
 }
