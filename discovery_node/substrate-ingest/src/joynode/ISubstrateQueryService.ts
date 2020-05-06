03dae24a43dbfe491457ee7a68037a24539c4f2d
dzhelezov <dzhelezov@gmail.com>
2020-05-06 14:08:14 +0300

    make QueryBlockProducer follow the head of the chain and wait for new blocks

--- discovery_node/substrate-ingest/src/joynode/ISubstrateQueryService.ts
+++ discovery_node/substrate-ingest/src/joynode/ISubstrateQueryService.ts
@@ -11,10 +11,12 @@ export default interface ISubstrateQueryService {
     getFinalizedHead(): Promise<Hash>;
     getHeader(hash?: Hash | Uint8Array | string): Promise<Header>;
     subscribeNewHeads(v: Callback<Header> ): UnsubscribePromise;
+    subscribeFinalizedHeads(v: Callback<Header>): UnsubscribePromise;
     getBlockHash(blockNumber?: BlockNumber | Uint8Array | number | string): Promise<Hash>;
     getBlock(hash?: Hash | Uint8Array | string): Promise<SignedBlock>;
     // Cut down from at: (hash: Hash | Uint8Array | string, ...args: Parameters<F>) => PromiseOrObs<ApiType, ObsInnerType<ReturnType<F>>>;
     eventsAt(hash: Hash | Uint8Array | string): Promise<EventRecord[] & Codec>;
+    substrateAPI(): ApiPromise;
     //eventsRange()
     //events()   
 }
@@ -25,8 +27,10 @@ export function makeQueryService(api: ApiPromise): ISubstrateQueryService {
         getHeader: (hash?: Hash | Uint8Array | string) => { return api.rpc.chain.getHeader(hash)},
         getFinalizedHead: () => { return api.rpc.chain.getFinalizedHead();}, 
         subscribeNewHeads: (v: Callback<Header> ) => { return api.rpc.chain.subscribeNewHeads(v); },
+        subscribeFinalizedHeads: (v: Callback<Header> ) => { return api.rpc.chain.subscribeFinalizedHeads(v); },
         getBlockHash: (blockNumber?: BlockNumber | Uint8Array | number | string) => { return api.rpc.chain.getBlockHash(blockNumber); },
         getBlock: (hash?: Hash | Uint8Array | string) => { return api.rpc.chain.getBlock(hash); },
-        eventsAt: (hash: Hash | Uint8Array | string) => { return api.query.system.events.at(hash); }
-     } as ISubstrateQueryService;
+        eventsAt: (hash: Hash | Uint8Array | string) => { return api.query.system.events.at(hash); },
+        substrateAPI: () => { return api; }
+    } as ISubstrateQueryService;
 }
--- discovery_node/substrate-ingest/src/joynode/QueryBlockProducer.ts
+++ discovery_node/substrate-ingest/src/joynode/QueryBlockProducer.ts
@@ -1,11 +1,14 @@
 import ISubstrateQueryService from './ISubstrateQueryService';
 import QueryEvent from './QueryEvent';
 import QueryEventBlock from './QueryEventBlock';
-import { Header, Extrinsic, EventRecord } from '@polkadot/types/interfaces';
+import { Header, Extrinsic, EventRecord, SignedBlock, Hash } from '@polkadot/types/interfaces';
+import { Codec } from '@polkadot/types/types';
 import { EventEmitter } from 'events';
 import * as assert from 'assert';
 const logger = require('log4js').getLogger('producer');
 
+// TODO: read off the config
+const NEW_BLOCK_POLL_INTERVAL_MS = 5000;
 
 export default class QueryBlockProducer extends EventEmitter {
     private _started: boolean;
@@ -54,12 +57,16 @@ export default class QueryBlockProducer extends EventEmitter {
             if (this._height_of_chain < at_block) throw Error(`Provided block is ahead of chain.`);
         }
 
-        //this._new_heads_unsubscriber = await this._query_service.subscribeNewHeads((header) => {
-        //    this._OnNewHeads(header);
-        //});
+        
+        this._new_heads_unsubscriber = await this._query_service.subscribeNewHeads((header) => {
+            this._height_of_chain = header.number.toNumber();
+            logger.debug(`New block found at height #${this._height_of_chain}`)
+        });
 
         // Start producing blocks right away
-        if (!this._producing_blocks_blocks) this._produce_blocks();
+        //if (!this._producing_blocks_blocks) this._produce_blocks();
+        // starts the loop producing new blocks
+        await this._produce_next_block();
     }
 
     async stop() {
@@ -72,54 +79,59 @@ export default class QueryBlockProducer extends EventEmitter {
         this._started = false;
     }
 
-    private async _OnNewHeads(header: Header) {
-        assert(this._started, 'Has to be started to process new heads.');
+    // private _OnNewHeads(header: Header): void {
+    //      assert(this._started, 'Has to be started to process new heads.');
 
-        this._height_of_chain = header.number.toNumber();
+    //      this._height_of_chain = header.number.toNumber();
 
-        logger.debug(`New block found at height #${this._height_of_chain}`);
+    //      logger.debug(`New block found at height #${this._height_of_chain}`);
 
-        if (!this._producing_blocks_blocks) await this._produce_blocks();
-    }
+    // }
 
-    private async _produce_blocks() {
-        if (this._producing_blocks_blocks) {
-            logger.warn("Already producing new blocks");
+    private async _produce_next_block() { 
+        if (!this._started) {
+            logger.info("Block producer is not started");
             return;
         }
 
-        this._producing_blocks_blocks = true;
+        let height = this._block_to_be_produced_next;
+        let block_hash_of_target = await this.getBlockHashOrWait(height);
+        let records = await this._query_service.eventsAt(block_hash_of_target);
+        logger.debug(`\tRead ${records.length} events of the block at height ${height}.`);
 
-        // Continue as long as we know there are outstanding blocks.
-        while (this._block_to_be_produced_next <= this._height_of_chain) {
-            logger.debug(`Fetching block #${this._block_to_be_produced_next}`);
+        let signed_block = await this._query_service.getBlock(block_hash_of_target);
+        logger.debug(`\tFetched full block at height ${height}.`);    
+        
+        this.publish_block_envent(signed_block, records);
+        this._block_to_be_produced_next++;
+        
+        await this._produce_next_block();
+             
+    }
 
-            try {
-                let hash = this._query_service.getBlockHash(this._block_to_be_produced_next);
-                await hash.then((hash) => console.log(hash));
-                //logger.debug(`\tHash ${block_hash_of_target.toString()}.`);
-            } catch (e) {
-                logger.error(e);
-                continue;
+    private async getBlockHashOrWait(height: number): Promise<Hash> {
+        return new Promise<Hash>((resolve, reject) => {
+            if (!this._started) {
+                reject("The block producer is stopped")
             }
-           
-            let block_hash_of_target = await this._query_service.getBlockHash(this._block_to_be_produced_next);;
-            
-            
-            let records = await this._query_service.eventsAt(block_hash_of_target);
-            // TODO: CATCH HERE
+            let checkHeight = () => {
+                if (this._height_of_chain >= height) {
+                    this._query_service.getBlockHash(height)
+                        .then((h: Hash) => resolve(h))
+                        .catch((e) => reject(e));
+                } else {
+                    logger.debug(`Current chain height: ${this._height_of_chain}, waiting for: ${height}`);
+                    setTimeout(checkHeight, NEW_BLOCK_POLL_INTERVAL_MS);
+                }    
 
-            logger.debug(`\tRead ${records.length} events.`);
+            }
+            checkHeight();
+        });
+    }
 
-            // Since there is at least 1 event, we will fetch block.
-            let signed_block = await this._query_service.getBlock(block_hash_of_target);
-            // TODO: CATCH HERE
-
-            logger.debug(`\tFetched full block.`);
-
-            let extrinsics_array = signed_block.block.extrinsics.toArray();
-
-            let query_events: QueryEvent[] = records.map(
+    private publish_block_envent(signed_block: SignedBlock, records: EventRecord[] & Codec):void {
+        let extrinsics_array = signed_block.block.extrinsics.toArray();
+        let query_events: QueryEvent[] = records.map(
                 (record, index): QueryEvent => {
                     // Extract the phase, event
                     const { phase } = record;
@@ -141,14 +153,10 @@ export default class QueryBlockProducer extends EventEmitter {
             );
 
             let query_block = new QueryEventBlock(this._block_to_be_produced_next, query_events);
-
             this.emit('QueryEventBlock', query_block);
-
             logger.debug(`\tEmitted query event block.`);
-
-            this._block_to_be_produced_next++;
-        }
-
-        this._producing_blocks_blocks = false;
     }
+
+
+    
 }
