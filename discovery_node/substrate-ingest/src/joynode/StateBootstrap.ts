0842c3605cc2c17ec267799c928a935fa043117a
dzhelezov <dzhelezov@gmail.com>
2020-05-08 19:17:27 +0300

    produce members from the block at given height as async generator

--- discovery_node/substrate-ingest/src/joynode/StateBootstrap.ts
+++ discovery_node/substrate-ingest/src/joynode/StateBootstrap.ts
@@ -1,14 +1,22 @@
 import ISubstrateQueryService from './ISubstrateQueryService';
 import { ApiPromise } from '@polkadot/api';
 
-import { Profile } from '@joystream/types/lib/members';
+import { IProfile } from '@joystream/types/lib/members';
 import { Hash } from '@polkadot/types/interfaces';
 import { Codec } from '@polkadot/types/types';
 import { EventEmitter } from 'events';
 import Config from '../Config';
+import { JoyStruct } from '@joystream/types/lib/JoyStruct';
 const logger = require('log4js').getLogger('state-boot');
 
 const DEFAULT_BOOTSTRAP_HEIGHT = 0;
+
+export interface JoyEntity {
+    entity: Object,
+    type: string,
+    blockNumber: number,
+    id: any
+}
 
 export default class StateBootstrap extends EventEmitter {
     private _queryService: ISubstrateQueryService;
@@ -20,22 +28,31 @@ export default class StateBootstrap extends EventEmitter {
         this._queryService = queryService;
         this._api = queryService.substrateAPI();
         this._blockHeight = config.get().bootstrap?.block || DEFAULT_BOOTSTRAP_HEIGHT;
-        logger.debug(`Bootstrap at height ${this._blockHeight}`);
+        logger.debug(`Bootstrap from block at height ${this._blockHeight}`);
     }      
 
-    public async fetch() {
-        this.fetchMembers(this._blockHeight);
+    public async * fetch(): AsyncGenerator<JoyEntity> {
+        yield* this.members(this._blockHeight);
         // TODO: fetch other stuff like content etc
     }
     
-    private async fetchMembers(height: number) {
-        let genesis: Hash = await this._queryService.getBlockHash(height);
-        let ids: Codec = await this._api.query.members.membersCreated.at(genesis);
+    public async * members(height: number): AsyncGenerator<JoyEntity> {
+        let blkHash: Hash = await this._queryService.getBlockHash(height);
+        let ids: Codec = await this._api.query.members.membersCreated.at(blkHash);
         let num: number = parseInt(ids.toString())
         for (let i = 0; i < num; i++) {
-            let profile = await this._api.query.members.memberProfile.at(genesis, i) as Profile;
-            this.emit('BootstrapEntity', profile);
+            let profile = await this._api.query.members.memberProfile.at(blkHash, i) as JoyStruct<IProfile>;
+            // TODO: this looks ugly, what is the proper way of type casting?
+            let profileJSON = profile.toJSON() as Object;
             logger.trace(`BootstrapEnitity: ${JSON.stringify(profile, null, 2)}`);
+            yield { 
+                entity: profileJSON as IProfile, 
+                type: "profile",
+                blockNumber: height,
+                id: (profileJSON as IProfile).handle
+            };
         }
     }
+
+    
 }
