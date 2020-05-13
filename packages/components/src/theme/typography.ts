3f0ae2ad187e7b2531b7bf95fed7b833c53d78b0
Pedro Semeano <semeano@gmail.com>
2020-05-13 16:47:28 +0100

    Added typography sizes accordingly with the design specs.

--- packages/components/src/theme/typography.ts
+++ packages/components/src/theme/typography.ts
@@ -11,12 +11,30 @@ export default {
     bold: "700",
   },
   sizes: {
-    xxsmall: "0.7rem",
-    xsmall: "0.8rem",
-    small: "1rem",
-    normal: "1.3rem",
-    medium: "2rem",
-    large: "3rem",
-    xlarge: "4rem",
-  },
+    hero: "4rem",
+    h1: "3rem",
+    h2: "2.5rem",
+    h3: "2rem",
+    h4: "1.5rem",
+    h5: "1.1rem",
+    h6: "0.9rem",
+    subtitle1: "1.1rem",
+    subtitle2: "0.9rem",
+    body1: "1.1rem",
+    body2: "0.9rem",
+    caption: "0.8rem",
+    overhead: "0.8rem",
+    button: {
+      large: "1rem",
+      medium: "0.8rem",
+      small: "0.7rem"
+    },
+    icon: {
+      xxlarge: "2rem",
+      xlarge: "1.5rem",
+      large: "1.2rem",
+      medium: "1rem",
+      small: "0.8rem"
+    }
+  }
 };
