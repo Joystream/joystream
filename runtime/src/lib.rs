23bf6308b9745001b29dc9c2c56f519e7fdc9ecb
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-05-19 14:13:48 +0300

    runtime: reward-curve adjusted for Constantinople incentive model

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -358,8 +358,8 @@ impl session::historical::Trait for Runtime {
 srml_staking_reward_curve::build! {
     const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
         min_inflation: 0_025_000,
-        max_inflation: 0_100_000,
-        ideal_stake: 0_500_000,
+        max_inflation: 0_300_000,
+        ideal_stake: 0_300_000,
         falloff: 0_050_000,
         max_piece_count: 40,
         test_precision: 0_005_000,
