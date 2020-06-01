5ecff8517290ecd09b908aa59a38622555882a05
Gleb Urvanov <gleb.urvanov@hotovo.org>
2020-06-01 16:17:17 +0200

    wrapped content of setTimeout function in tap.test

--- tests/network-tests/src/constantinople/tests/impl/setTimeout.ts
+++ tests/network-tests/src/constantinople/tests/impl/setTimeout.ts
@@ -2,8 +2,8 @@ import tap from 'tap';
 import { ApiWrapper } from '../../utils/apiWrapper';
 
 export async function setTimeout(apiWrapper: ApiWrapper, durationInBlocks: number) {
-  // tap.test('retrieving time necessary for the test', async () => {
-  const durationInMillis = (await apiWrapper.getBlockDuration()).muln(durationInBlocks).toNumber();
-  tap.setTimeout(durationInMillis);
-  // });
+  tap.test('retrieving time necessary for the test', async () => {
+    const durationInMillis = (await apiWrapper.getBlockDuration()).muln(durationInBlocks).toNumber();
+    tap.setTimeout(durationInMillis);
+  });
 }
