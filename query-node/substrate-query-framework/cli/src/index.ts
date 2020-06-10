906003e1a931ca33d33cb13be04cf5a8fe4e757d
dzhelezov <dzhelezov@gmail.com>
2020-06-10 16:18:39 +0300

    replace yarn call with a direct api call

--- query-node/substrate-query-framework/cli/src/index.ts
+++ query-node/substrate-query-framework/cli/src/index.ts
@@ -1,1 +1,10 @@
-export {run} from '@oclif/command'
+import { build } from 'gluegun';
+
+export const cli = build()
+    .brand('warthog')
+    .src(`${__dirname}/../node_modules/warthog/dist/cli`)
+    .help() // provides default for help, h, --help, -h
+    .version() // provides default for version, v, --version, -v
+    .create();
+
+export { run } from '@oclif/command'
