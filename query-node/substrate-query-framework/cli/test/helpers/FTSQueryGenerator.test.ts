5f438dd86d4e9e5e9484891879004239c245f36b
dzhelezov <dzhelezov@gmail.com>
2020-06-10 16:24:53 +0300

    fix lint errors

--- query-node/substrate-query-framework/cli/test/helpers/FTSQueryGenerator.test.ts
+++ query-node/substrate-query-framework/cli/test/helpers/FTSQueryGenerator.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
