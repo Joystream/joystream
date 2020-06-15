033815f60d4dc218ec8e95ce3e180de9b4518ed7
metmirr <metmirr@gmail.com>
2020-06-15 10:52:27 +0300

    feat(mappings): add handlers for avatar and handle

--- query-node/joystream-query-node/mappings/members.ts
+++ query-node/joystream-query-node/mappings/members.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import { CheckedUserInfo } from '@joystream/types/lib/members';
 
 import { Member } from '../generated/graphql-server/src/modules/member/member.model';
 import { DB, SubstrateEvent } from '../generated/indexer';
@@ -6,9 +7,17 @@ import { DB, SubstrateEvent } from '../generated/indexer';
 export async function handleMemberRegistered(db: DB, event: SubstrateEvent) {
   const { AccountId, MemberId } = event.event_params;
 
+  // Not safe type casting!
+  const userInfo = (event.extrinsic?.args[1].toJSON() as unknown) as CheckedUserInfo;
+
   let member = new Member();
-  member.rootAccount = Buffer.from(AccountId);
+  member.registeredAtBlock = event.block_number.toString();
   member.memberId = MemberId.toString();
+  member.rootAccount = Buffer.from(AccountId);
+  member.controllerAccount = Buffer.from(AccountId);
+  member.handle = userInfo.handle.toString();
+  member.avatarUri = userInfo.avatar_uri.toString();
+  member.about = userInfo.about.toString();
 
   db.save<Member>(member);
 }
@@ -22,3 +31,30 @@ export async function handleMemberUpdatedAboutText(db: DB, event: SubstrateEvent
   member.updatedAt = new Date();
   db.save<Member>(member);
 }
+
+export async function handleMemberUpdatedAvatar(db: DB, event: SubstrateEvent) {
+  const { MemberId } = event.event_params;
+  const member = await db.get(Member, { where: { memberId: MemberId.toString() } });
+  console.log('member', member);
+  assert(member);
+
+  // Not safe type casting!
+  const userInfo = (event.extrinsic?.args[1].toJSON() as unknown) as CheckedUserInfo;
+  member.avatarUri = userInfo.avatar_uri.toString();
+
+  db.save<Member>(member);
+}
+
+export async function handleMemberUpdatedHandle(db: DB, event: SubstrateEvent) {
+  const { MemberId } = event.event_params;
+  const member = await db.get(Member, { where: { memberId: MemberId.toString() } });
+
+  assert(member);
+
+  // Not safe type casting!
+  const userInfo = (event.extrinsic?.args[1].toJSON() as unknown) as CheckedUserInfo;
+  member.handle = userInfo.handle.toString();
+
+  db.save<Member>(member);
+}
+
