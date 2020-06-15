16eedf169cd1eb2d8a1e5f8354434aec126244ed
Francesco Baccetti <gamarantor@gmail.com>
2020-06-15 17:31:35 +0200

    Add Chromia And Partial Jest Setup

--- atlas/jest.config.js
+++ atlas/jest.config.js
@@ -1,18 +1,30 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
 module.exports = {
-  preset: "ts-jest",
-  setupFiles: [
-    "<rootDir>/enzyme.config.js"
-  ],
-  moduleFileExtensions: [
-    "js",
-    "ts",
-    "tsx"
-  ],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "babel-jest"
-  },
-  snapshotSerializers: [
-    "enzyme-to-json/serializer"
-  ]
-}
+	// The directory where Jest should store its cached dependency information
+	cacheDirectory: ".jest-cache",
+
+	// Automatically clear mock calls and instances between every test
+	clearMocks: true,
+
+	// The directory where Jest should output its coverage files
+	coverageDirectory: ".coverage",
+
+	// An array of regexp pattern strings used to skip coverage collection
+	coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/packages/(?:.+?)/dist/"],
+
+	// An array of directory names to be searched recursively up from the requiring module's location
+	moduleDirectories: ["node_modules", "packages"],
+
+	// An array of file extensions your modules use
+	moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+	// Run tests from one or more projects
+	projects: ["<rootDir>", "<rootDir>/packages/*"],
+
+	// A list of paths to modules that run some code to configure or set up the testing framework before each test
+	setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+
+	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+	testPathIgnorePatterns: ["/node_modules/", "<rootDir>/packages/(?:.+?)/dist/"],
+};
--- atlas/packages/components/__tests__/Button.test.js
+++ atlas/packages/components/__tests__/Button.test.js
@@ -1,15 +1,13 @@
-import React from "react"
-import { mount } from "enzyme"
-import Button from "./../src/components/Button"
+import React from "react";
+import { mount } from "enzyme";
+import Button from "./../src/components/Button";
 
 describe("Button component", () => {
+	it("Should render default button correctly", () => {
+		expect(mount(<Button>Click me!</Button>)).toMatchSnapshot();
+	});
 
-  it("Should render default button correctly", () => {
-    expect(mount(<Button>Click me!</Button>)).toMatchSnapshot()
-  })
-
-  it("Should render custom button correctly", () => {
-    expect(mount(<Button size="large" color="success">Hello Atlas</Button>)).toMatchSnapshot()
-  })
-
-})
+	it("Should render custom button correctly", () => {
+		expect(mount(<Button size="large">Hello Atlas</Button>)).toMatchSnapshot();
+	});
+});
