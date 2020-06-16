ca8291c39d61d82d954e5c81caa1a121bf89e35e
dzhelezov <dzhelezov@gmail.com>
2020-06-16 14:38:45 +0300

    reformat code

--- query-node/substrate-query-framework/cli/src/parse/SchemaDirective.ts
+++ query-node/substrate-query-framework/cli/src/parse/SchemaDirective.ts
@@ -4,26 +4,25 @@ import { WarthogModel } from '../model';
 export const FULL_TEXT_SEARCHABLE_DIRECTIVE = 'fulltext';
 
 export interface DirectiveVisitor {
-    // directive name to watch
-    directiveName: string,
-    /**
-     * Generic visit function for the AST schema traversal. 
-     * Only ObjectTypeDefinition and FieldDefinition nodes are included in the path during the 
-     * traversal
-     * 
-     * May throw validation errors
-     * 
-     * @param path: BFS path in the schema tree ending at the directive node of interest
-     */
-    visit: (path: SchemaNode[]) => void;
+  // directive name to watch
+  directiveName: string;
+  /**
+   * Generic visit function for the AST schema traversal.
+   * Only ObjectTypeDefinition and FieldDefinition nodes are included in the path during the
+   * traversal
+   *
+   * May throw validation errors
+   *
+   * @param path: BFS path in the schema tree ending at the directive node of interest
+   */
+  visit: (path: SchemaNode[]) => void;
 }
 
-export interface SchemaDirective {  
-    // directive definition to be added to the
-    // schema preamble
-    preamble: string,
-    name: string,
-    validate: (path: SchemaNode[]) => void; 
-    generate: (path: SchemaNode[], model: WarthogModel) => WarthogModel,
+export interface SchemaDirective {
+  // directive definition to be added to the
+  // schema preamble
+  preamble: string;
+  name: string;
+  validate: (path: SchemaNode[]) => void;
+  generate: (path: SchemaNode[], model: WarthogModel) => WarthogModel;
 }
-
