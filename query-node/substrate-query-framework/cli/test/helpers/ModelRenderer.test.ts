5246ab1d7fa025135d6b97aa26358bcff21cfd0f
dzhelezov <dzhelezov@gmail.com>
2020-06-19 19:05:46 +0300

    add enum tests and fix old tests after refactoring

--- query-node/substrate-query-framework/cli/test/helpers/ModelRenderer.test.ts
+++ query-node/substrate-query-framework/cli/test/helpers/ModelRenderer.test.ts
@@ -1,9 +1,9 @@
-import { ModelRenderer } from "../../src/generate/ModelRenderer";
+import { ModelRenderer } from '../../src/generate/ModelRenderer';
 import { WarthogModel, Field, ObjectType } from '../../src/model';
 import { createModel, fromStringSchema } from './model';
 import * as fs from 'fs-extra';
 import { expect } from 'chai';
-import Debug from "debug";
+import Debug from 'debug';
 
 const debug = Debug('cli-test:model-renderer');
 
@@ -14,58 +14,58 @@ describe('ModelRenderer', () => {
 
   before(() => {
     // set timestamp in the context to make the output predictable
-    generator = new ModelRenderer();
     modelTemplate = fs.readFileSync('./src/templates/entities/model.ts.mst', 'utf-8');
-
-  })
+  });
 
   beforeEach(() => {
     warthogModel = createModel();
-  })
+  });
 
   it('should transform fields to camelCase', function () {
+    warthogModel.addField('Post', new Field('CamelCase', 'String'));
+    warthogModel.addField('Post', new Field('snake_case', 'String'));
+    warthogModel.addField('Post', new Field('kebab-case', 'String'));
 
-    warthogModel.addField("Post", new Field("CamelCase", "String"));
-    warthogModel.addField("Post", new Field("snake_case", "String"));
-    warthogModel.addField("Post", new Field("kebab-case", "String"));
+    generator = new ModelRenderer(warthogModel, warthogModel.lookupType('Post'));
 
-    const rendered = generator.generate(modelTemplate, warthogModel.lookupType("Post"));
+    const rendered = generator.render(modelTemplate);
 
     debug(`rendered: ${JSON.stringify(rendered, null, 2)}`);
 
     expect(rendered).to.include('camelCase?: string', 'Should camelCase correctly');
     expect(rendered).to.include('snakeCase?: string', 'Should camelCase correctly');
     expect(rendered).to.include('kebabCase?: string', 'Should camelCase correctly');
-
-  })
+  });
 
   it('should render ClassName', function () {
     warthogModel.addObjectType({
       name: `some_randomEntity`,
       isEntity: true,
-      fields: [new Field("a", "String")]
+      fields: [new Field('a', 'String')],
     } as ObjectType);
 
-    const rendered = generator.generate(modelTemplate, warthogModel.lookupType("some_randomEntity"));
+    generator = new ModelRenderer(warthogModel, warthogModel.lookupType('some_randomEntity'));
+
+    const rendered = generator.render(modelTemplate);
     debug(`rendered: ${JSON.stringify(rendered, null, 2)}`);
 
     expect(rendered).to.include('export class SomeRandomEntity extends BaseModel', 'Should render ClassName corretly');
-
-  })
+  });
 
   it('should include imports', function () {
+    warthogModel.addField('Post', new Field('a', 'ID'));
+    warthogModel.addField('Post', new Field('b', 'String'));
+    warthogModel.addField('Post', new Field('c', 'Int'));
+    warthogModel.addField('Post', new Field('d', 'Date'));
+    warthogModel.addField('Post', new Field('e', 'Float'));
+    warthogModel.addField('Post', new Field('f', 'BigInt'));
+    warthogModel.addField('Post', new Field('g', 'BigDecimal'));
+    warthogModel.addField('Post', new Field('h', 'Bytes'));
+    warthogModel.addField('Post', new Field('j', 'Boolean'));
 
-    warthogModel.addField("Post", new Field("a", 'ID'));
-    warthogModel.addField("Post", new Field("b", 'String'));
-    warthogModel.addField("Post", new Field("c", 'Int'));
-    warthogModel.addField("Post", new Field("d", "Date"));
-    warthogModel.addField("Post", new Field("e", "Float"));
-    warthogModel.addField("Post", new Field("f", "BigInt"));
-    warthogModel.addField("Post", new Field("g", "BigDecimal"));
-    warthogModel.addField("Post", new Field("h", "Bytes"));
-    warthogModel.addField("Post", new Field("j", "Boolean"));
+    generator = new ModelRenderer(warthogModel, warthogModel.lookupType('Post'));
 
-    const rendered = generator.generate(modelTemplate, warthogModel.lookupType("Post"));
+    const rendered = generator.render(modelTemplate);
 
     expect(rendered).to.include('BooleanField,', 'Should import BooleanField');
     expect(rendered).to.include('DateField,', 'Should import DateField');
@@ -73,11 +73,9 @@ describe('ModelRenderer', () => {
     expect(rendered).to.include('IntField,', 'Should import IntField');
     expect(rendered).to.include('NumericField,', 'Should import NumericField');
     expect(rendered).to.include('BytesField,', 'Should import BytesField');
+  });
 
-
-  })
-
-  it('should render otm types', function() {
+  it('should render otm types', function () {
     const model = fromStringSchema(`
     type Author @entity {
       posts: [Post!]
@@ -86,17 +84,19 @@ describe('ModelRenderer', () => {
     type Post @entity {
       title: String
       author: Author!
-    }`)
-    const rendered = generator.generate(modelTemplate, model.lookupType("Author"));
+    }`);
+
+    generator = new ModelRenderer(model, model.lookupType('Author'));
+
+    const rendered = generator.render(modelTemplate);
     debug(`rendered: ${JSON.stringify(rendered, null, 2)}`);
 
     expect(rendered).to.include(`import { Post } from '../post/post.model`, `Should render imports`);
     expect(rendered).to.include(`@OneToMany(() => Post, (post: Post) => post.author)`, 'Should render OTM decorator');
     expect(rendered).to.include(`posts?: Post[];`, 'Should render plural references');
-  
-  })
+  });
 
-  it('should render mto types', function() {
+  it('should render mto types', function () {
     const model = fromStringSchema(`
     type Author @entity {
       posts: [Post!]
@@ -104,33 +104,61 @@ describe('ModelRenderer', () => {
     
     type Post @entity {
       title: String
-      author: Author!
-    }`)
-    const rendered = generator.generate(modelTemplate, model.lookupType("Post"));
+      # FIXME: this causes a double field author: Author! 
+    }`);
+
+    generator = new ModelRenderer(model, model.lookupType('Post'));
+    const rendered = generator.render(modelTemplate);
     debug(`rendered: ${JSON.stringify(rendered, null, 2)}`);
 
     expect(rendered).to.include(`import { Author } from '../author/author.model`, `Should render imports`);
-    expect(rendered).to.include(`@ManyToOne(() => Author, (author: Author) => author.posts, { 
-      skipGraphQLField: true 
-    })`, 'Should render MTO decorator'); // nullable: true is not includered?
+    expect(rendered).to.include(
+      `@ManyToOne(() => Author, (author: Author) => author.posts, {
+    skipGraphQLField: true,
+  })`,
+      'Should render MTO decorator'
+    ); // nullable: true is not includered?
     expect(rendered).to.include(`author!: Author;`, 'Should render required referenced field');
-  
-  })
+  });
 
-  it('should renderer array types', function() {
+  it('should renderer array types', function () {
     const model = fromStringSchema(`
     type Author @entity {
       posts: [String]
-    }`)
+    }`);
 
-    const rendered = generator.generate(modelTemplate, model.lookupType("Author"));
+    generator = new ModelRenderer(model, model.lookupType('Author'));
+
+    const rendered = generator.render(modelTemplate);
     debug(`rendered: ${JSON.stringify(rendered, null, 2)}`);
     expect(rendered).to.include('CustomField,', 'Should import CustomField');
     expect(rendered).to.include(`@CustomField`, 'Should decorate arrays with @CustomField');
-    expect(rendered).to.include(`db: { type: 'text', array: true, nullable: true }`, 'Should add db option')
-    expect(rendered).to.include(`api: { type: 'string', nullable: true }`, 'Should inclued api option')
+    expect(rendered).to.include(`db: { type: 'text', array: true, nullable: true }`, 'Should add db option');
+    expect(rendered).to.include(`api: { type: 'string', nullable: true }`, 'Should inclued api option');
     expect(rendered).to.include('posts?: string[]', `should add an array field`);
-    
-  })
+  });
 
-})
+  it('should render enum types', function () {
+    const model = fromStringSchema(`
+      enum Episode {
+        NEWHOPE
+        EMPIRE
+        JEDI
+      }
+        
+      type Movie @entity {
+        episode: Episode
+      }`);
+
+    generator = new ModelRenderer(model, model.lookupType('Movie'));
+    const rendered = generator.render(modelTemplate);
+    debug(`rendered: ${JSON.stringify(rendered, null, 2)}`);
+
+    expect(rendered).to.include('EnumField,', 'Should import EnumField');
+    expect(rendered).to.include('export { Episode }', 'Should export enum');
+    // this will be generated in ../enums/enum.ts
+    //expect(rendered).to.include(`NEWHOPE = 'NEWHOPE'`, 'Should render enum values');
+    expect(rendered).to.include(`@EnumField`, 'Should decorate with @EnumField');
+    expect(rendered).to.include(`'Episode', Episode, { nullable: true }`, 'Should add enum decorator options');
+  });
+});
--- query-node/substrate-query-framework/cli/test/helpers/WarthogModel.test.ts
+++ query-node/substrate-query-framework/cli/test/helpers/WarthogModel.test.ts
@@ -1,33 +1,47 @@
-import { createModel, threadObjType, postObjType } from "./model";
+import { createModel, threadObjType, postObjType, fromStringSchema } from './model';
 import { expect } from 'chai';
 import { WarthogModel } from '../../src/model';
 
 describe('WarthogModel', () => {
-    let warthogModel: WarthogModel;
+  let warthogModel: WarthogModel;
 
-    beforeEach(() => {
-        warthogModel = createModel();
-    })
+  beforeEach(() => {
+    warthogModel = createModel();
+  });
 
-    it('Should lookup types', () => {
-        expect(warthogModel.lookupType("Thread")).eq(threadObjType, "Should find Thread type");
-        expect(warthogModel.lookupType("Post")).eq(postObjType, "Should find Post type");
+  it('Should lookup types', () => {
+    expect(warthogModel.lookupType('Thread')).eq(threadObjType, 'Should find Thread type');
+    expect(warthogModel.lookupType('Post')).eq(postObjType, 'Should find Post type');
+
+    expect(() => warthogModel.lookupType('NoSuchType')).throw('No ObjectType');
+  });
+
+  it('Should lookup fields', () => {
+    const field = warthogModel.lookupField('Thread', 'initial_body_text');
+    expect(field.type).equals('String', 'Should lookup the field');
+
+    expect(() => warthogModel.lookupField('Thread', 'noexistent')).throw('No field');
+  });
+
+  it('Should add queries', () => {
+    warthogModel.addQueryClause('test', 'initial_body_text', 'Thread');
+    warthogModel.addQueryClause('test', 'initial_body_text', 'Post');
+    expect(warthogModel.ftsQueries).length(1, 'Should add a query');
+    expect(warthogModel.ftsQueries[0].clauses).length(2, 'Should add two clauses');
+  });
+
+  it('Should add enums', () => {
+    const model = fromStringSchema(`
+        enum Episode {
+            NEWHOPE
+            EMPIRE
+            JEDI
+        }
         
-        expect(() => warthogModel.lookupType("NoSuchType")).throw("No ObjectType");
-
-    })
-
-    it('Should lookup fields', () => {
-        const field = warthogModel.lookupField("Thread", "initial_body_text")
-        expect(field.type).equals("String", "Should lookup the field")
-
-        expect(() => warthogModel.lookupField("Thread", "noexistent")).throw("No field");
-    })
-
-    it('Should add queries', () => {
-        warthogModel.addQueryClause("test", "initial_body_text", "Thread");
-        warthogModel.addQueryClause("test", "initial_body_text", "Post");
-        expect(warthogModel.ftsQueries).length(1, "Should add a query");
-        expect(warthogModel.ftsQueries[0].clauses).length(2, "Should add two clauses");
-    })
-})
+        type Movie @entity {
+            episode: Episode
+        }`);
+    expect(model.enums).length(1, 'Should add an enum');
+    expect(model.lookupEnum('Episode').name).eq('Episode', 'Should lookup by name');
+  });
+});
