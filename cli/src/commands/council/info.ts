81450c64629dfbc7929657630b3cefc7dd881dd7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-06-24 17:46:40 +0300

    update import path for @joystream/types in apps

--- cli/src/commands/council/info.ts
+++ cli/src/commands/council/info.ts
@@ -1,4 +1,4 @@
-import { ElectionStage } from '@joystream/types/lib/council';
+import { ElectionStage } from '@joystream/types/council';
 import { formatNumber, formatBalance } from '@polkadot/util';
 import { BlockNumber } from '@polkadot/types/interfaces';
 import { CouncilInfoObj, NameValueObj } from '../../Types';
