6768531005022b972d98cf0f3c5658ea758a93ae
iorveth <aenseidheiorweth@gmail.com>
2020-06-25 17:23:36 +0300

    Tests: set_curator_group_status failure cases added

--- runtime-modules/content-directory/src/tests/set_curator_group_status.rs
+++ runtime-modules/content-directory/src/tests/set_curator_group_status.rs
@@ -37,3 +37,55 @@ fn set_curator_group_status_success() {
         );
     })
 }
+
+#[test]
+fn set_curator_group_status_lead_auth_failed() {
+    with_test_externalities(|| {
+        // Add curator group
+        assert_ok!(add_curator_group(LEAD_ORIGIN));
+
+        // Runtime tested state before call
+
+        // Events number before tested call
+        let number_of_events_before_call = System::events().len();
+
+        // An attemt to change curator group status from non lead origin 
+        let set_curator_group_status_result = set_curator_group_status(
+            FIRST_MEMBER_ORIGIN,
+            FIRST_CURATOR_GROUP_ID,
+            true
+        );
+
+        // Failure checked
+        assert_failure(
+            set_curator_group_status_result,
+            ERROR_LEAD_AUTH_FAILED,
+            number_of_events_before_call,
+        );
+    })
+}
+
+#[test]
+fn set_curator_group_status_for_non_existent_curator_group() {
+    with_test_externalities(|| {
+
+        // Runtime tested state before call
+
+        // Events number before tested call
+        let number_of_events_before_call = System::events().len();
+
+        // An attemt to change curator group status for_non existent curator group
+        let set_curator_group_status_result = set_curator_group_status(
+            LEAD_ORIGIN,
+            FIRST_CURATOR_GROUP_ID,
+            true
+        );
+
+        // Failure checked
+        assert_failure(
+            set_curator_group_status_result,
+            ERROR_CURATOR_GROUP_DOES_NOT_EXIST,
+            number_of_events_before_call,
+        );
+    })
+}
