85fb7d44fc4a1e579368772d13f5c9cdc7e595e9
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-06-27 02:04:06 +0300

    storage-node: run tests when starting dev instance

--- storage-node/scripts/run-dev-instance.sh
+++ storage-node/scripts/run-dev-instance.sh
@@ -10,19 +10,30 @@ then
   exit 1
 fi
 
-script_path="$(dirname "${BASH_SOURCE[0]}")"
+SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 
 # stop prior run and clear volumes
-docker-compose -f ${script_path}/compose/devchain-and-ipfs-node/docker-compose.yaml down -v
+docker-compose -f ${SCRIPT_PATH}/compose/devchain-and-ipfs-node/docker-compose.yaml down -v
 
 # Run a development joystream-node chain and ipfs daemon in the background
 # Will use latest joystream/node images,
 # and will fetch from dockerhub if not found, so build them locally if
 # you need the version from the current branch
-docker-compose -f ${script_path}/compose/devchain-and-ipfs-node/docker-compose.yaml up -d
+docker-compose -f ${SCRIPT_PATH}/compose/devchain-and-ipfs-node/docker-compose.yaml up -d
 
 # configure the dev chain
 DEBUG=joystream:storage-cli:dev yarn storage-cli dev-init
 
+# Run the tests
+yarn workspace @joystream/storage-node test
+
+# Run the server in background
+# DEBUG=joystream:storage* yarn colossus --dev > ${SCRIPT_PATH}/colossus.log 2>&1 &
+# PID= ???
+# echo "Development storage node is running in the background process id: ${PID}""
+# prompt for pressing ctrl-c..
+# kill colossus and docker containers...
+# docker-compose -f ${SCRIPT_PATH}/compose/devchain-and-ipfs-node/docker-compose.yaml down -v
+
 # Run the server
-DEBUG=* yarn colossus --dev
+DEBUG=joystream:* yarn colossus --dev
