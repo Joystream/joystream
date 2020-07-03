4dcf537d3e1ffee2a6499f9ea962e1b49294c1e7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-07-04 02:05:49 +0300

    eslint-config: tweaking

--- devops/eslint-config/index.js
+++ devops/eslint-config/index.js
@@ -1,31 +1,34 @@
 module.exports = {
   env: {
-    es6: true,
+    es6: true
   },
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2019,
-    sourceType: "module",
+    sourceType: 'module'
   },
   extends: [
-    "plugin:react/recommended",
-    "standard",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
+    'plugin:react/recommended',
+    'standard',
+    'plugin:jsx-a11y/recommended',
+    // Disable eslint formatting related es-linting rules
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard'
   ],
-  plugins: ["react", "react-hooks"],
+  plugins: ['react', 'react-hooks'],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
   settings: {
-    version: "detect",
-  },
-};
+    version: 'detect'
+  }
+}
