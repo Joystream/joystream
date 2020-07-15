8a6f049d5d1a9c52c15291ecff08c11f9b5cf97d
Gleb Urvanov <gleb.urvanov@hotovo.org>
2020-07-15 17:38:28 +0200

    unstaking periods updated

--- tests/network-tests/src/nicaea/tests/proposals/impl/proposalsModule.ts
+++ tests/network-tests/src/nicaea/tests/proposals/impl/proposalsModule.ts
@@ -27,11 +27,11 @@ export async function createWorkingGroupLeaderOpening(
   opening.setMaxActiveApplicants(new BN(m1KeyPairs.length));
   opening.setMaxReviewPeriodLength(new BN(32));
   opening.setApplicationStakingPolicyAmount(new BN(applicationStake));
-  opening.setApplicationCrowdedOutUnstakingPeriodLength(new BN(0));
-  opening.setApplicationExpiredUnstakingPeriodLength(new BN(0));
+  opening.setApplicationCrowdedOutUnstakingPeriodLength(new BN(1));
+  opening.setApplicationExpiredUnstakingPeriodLength(new BN(1));
   opening.setRoleStakingPolicyAmount(new BN(roleStake));
-  opening.setRoleCrowdedOutUnstakingPeriodLength(new BN(0));
-  opening.setRoleExpiredUnstakingPeriodLength(new BN(0));
+  opening.setRoleCrowdedOutUnstakingPeriodLength(new BN(1));
+  opening.setRoleExpiredUnstakingPeriodLength(new BN(1));
   opening.setSlashableMaxCount(new BN(1));
   opening.setSlashableMaxPercentPtsPerTime(new BN(100));
   opening.setSuccessfulApplicantApplicationStakeUnstakingPeriod(new BN(1));
