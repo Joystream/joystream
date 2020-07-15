3f0d67a8930b2b8f7fb57cfc4f8ba5ec27b31314
metmirr <metmirr@gmail.com>
2020-07-15 23:05:03 +0300

    introduce QueryNodeStartUpOptions interface

---
+++ query-node/substrate-query-framework/index-builder/src/QueryNodeStartOptions.ts
@@ -0,0 +1,17 @@
+import * as BN from 'bn.js';
+import { QueryEventProcessingPack } from '.';
+import { BootstrapPack } from './bootstrap';
+
+export interface QueryNodeStartUpOptions {
+  // Web socket endpoint url
+  wsProviderURI: string;
+
+  // Event processors they are defined in mappings/bootstrap
+  processingPack: QueryEventProcessingPack | BootstrapPack;
+
+  // Custum type register function
+  typeRegistrator?: () => void;
+
+  // Block number that indexer will start from, default is 0
+  atBlock?: BN;
+}
