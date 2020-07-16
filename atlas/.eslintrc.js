ae189d95d09f1950264da759afd74fe9586815db
Klaudiusz Dembler <kdembler@gmail.com>
2020-07-16 11:52:30 +0200

    rebase atlas on common linting rules

--- atlas/.eslintrc.js
+++ atlas/.eslintrc.js
@@ -1,36 +1,12 @@
 module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		es6: true,
-		jest: true,
-	},
-	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly',
-	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2019,
-		sourceType: 'module',
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:prettier/recommended',
-	],
-	plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-	rules: {
-		'react/prop-types': 'off',
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['plugin:react-hooks/recommended', '../.eslintrc.js'],
+  rules: {
+    'react/prop-types': 'off',
+  },
 }
