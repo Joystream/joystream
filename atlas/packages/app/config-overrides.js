0f0a8f9ee1f8a026b235923450950afb044b814d
Klaudiusz Dembler <kdembler@gmail.com>
2020-07-28 13:32:07 +0200

    disable eslint warnings on CI build

--- atlas/packages/app/config-overrides.js
+++ atlas/packages/app/config-overrides.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
-const { override, addBabelPreset, addWebpackAlias } = require('customize-cra')
+const { override, addBabelPreset, addWebpackAlias, disableEsLint } = require('customize-cra')
 const eslintConfig = require('../../.eslintrc.js')
 
 const modifiedEslintConfig = {
@@ -13,29 +13,35 @@ const modifiedEslintConfig = {
 }
 
 // based on https://github.com/arackaf/customize-cra/issues/175#issuecomment-610023655
-const customEslintConfig = (configRules) => (config) => {
-  const updatedRules = config.module.rules.map((rule) => {
-    // Only target rules that have defined a `useEslintrc` parameter in their options
-    if (rule.use && rule.use.some((use) => use.options && use.options.useEslintrc !== undefined)) {
-      const ruleUse = rule.use[0]
-      const baseOptions = ruleUse.options
-      const baseConfig = baseOptions.baseConfig || {}
-      const newOptions = {
-        useEslintrc: false,
-        ignore: true,
-        baseConfig: { ...baseConfig, ...configRules },
+const customEslintConfig = (configRules) => {
+  if (process.env.CI) {
+    return disableEsLint()
+  }
+
+  return (config) => {
+    const updatedRules = config.module.rules.map((rule) => {
+      // Only target rules that have defined a `useEslintrc` parameter in their options
+      if (rule.use && rule.use.some((use) => use.options && use.options.useEslintrc !== undefined)) {
+        const ruleUse = rule.use[0]
+        const baseOptions = ruleUse.options
+        const baseConfig = baseOptions.baseConfig || {}
+        const newOptions = {
+          useEslintrc: false,
+          ignore: true,
+          baseConfig: { ...baseConfig, ...configRules },
+        }
+        ruleUse.options = newOptions
+        return rule
+
+        // Rule not using eslint. Do not modify.
+      } else {
+        return rule
       }
-      ruleUse.options = newOptions
-      return rule
+    })
 
-      // Rule not using eslint. Do not modify.
-    } else {
-      return rule
-    }
-  })
-
-  config.module.rules = updatedRules
-  return config
+    config.module.rules = updatedRules
+    return config
+  }
 }
 
 module.exports = {
