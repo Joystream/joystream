6a4e29a82b45c1ecbb5b24a4f73216e57b022e39
ondratra <ondratra@gmail.com>
2020-07-31 18:01:22 +0200

    referendum - module skelet III

--- runtime-modules/referendum/src/tests.rs
+++ runtime-modules/referendum/src/tests.rs
@@ -1,8 +1,9 @@
 #![cfg(test)]
 
-//use super::*;
+use super::*;
 use crate::mock::*;
 
+/////////////////// Lifetime - referendum start ////////////////////////////////
 #[test]
 fn referendum_start() {
     let config = default_genesis_config();
@@ -15,12 +16,41 @@ fn referendum_start() {
 }
 
 #[test]
+fn referendum_start_access_restricted() {
+    let config = default_genesis_config();
+
+    build_test_externalities(config).execute_with(|| {
+        let origin = OriginType::Signed(USER_REGULAR);
+
+        mock_start_referendum(origin, Err(Error::OriginNotSuperUser));
+    });
+}
+
+#[test]
+fn referendum_start_forbidden_after_start() {
+    let config = default_genesis_config();
+
+    build_test_externalities(config).execute_with(|| {
+        let origin = OriginType::Signed(USER_ADMIN);
+
+        mock_start_referendum(origin.clone(), Ok(()));
+        mock_start_referendum(origin.clone(), Err(Error::ReferendumAlreadyRunning));
+    });
+}
+
+/////////////////// Lifetime - voting finish ///////////////////////////////////
+
+#[test]
 #[ignore]
 fn finish_voting() {}
+
+/////////////////// Lifetime - start revealing /////////////////////////////////
 
 #[test]
 #[ignore]
 fn finish_revealing_period() {}
+
+/////////////////// Lifetime - complete ////////////////////////////////////////
 
 #[test]
 #[ignore]
