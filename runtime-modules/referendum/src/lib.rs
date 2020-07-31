3efdac8b474cbf74a0c1d0b8f142682df24a6f61
ondratra <ondratra@gmail.com>
2020-07-31 18:01:22 +0200

    referendum - module skelet III

--- runtime-modules/referendum/src/lib.rs
+++ runtime-modules/referendum/src/lib.rs
@@ -9,8 +9,9 @@
 // used dependencies
 use codec::{Decode, Encode};
 use srml_support::Parameter;
-use srml_support::{decl_event, decl_module, decl_storage, dispatch};
+use srml_support::{decl_error, decl_event, decl_module, decl_storage};
 use std::marker::PhantomData;
+use system::ensure_signed;
 
 //use sr_primitives::traits::{MaybeSerialize, Member, One, SimpleArithmetic};
 //use codec::{Codec};
@@ -25,14 +26,20 @@ mod tests;
 
 /////////////////// Data Structures ////////////////////////////////////////////
 
-#[derive(Encode, Decode)]
-pub enum ReferendumStage<BlockNumber> {
-    Void(BlockNumber),
-    Voting(BlockNumber),
-    Revealing(BlockNumber),
+#[derive(Encode, Decode, PartialEq, Eq, Debug)]
+pub enum ReferendumStage {
+    Void,
+    Voting,
+    Revealing,
 }
 
-/////////////////// Trait, Storage, and Events /////////////////////////////////
+impl Default for ReferendumStage {
+    fn default() -> ReferendumStage {
+        ReferendumStage::Void
+    }
+}
+
+/////////////////// Trait, Storage, Errors, and Events /////////////////////////
 
 pub trait Trait<I: Instance>: system::Trait {
     /// The overarching event type.
@@ -40,14 +47,16 @@ pub trait Trait<I: Instance>: system::Trait {
 
     // we need to use at least one type belogning to `Trait` to supress `decl_storage` macro's errors
     type TmpDummy: Parameter + Default;
+
+    fn is_super_user(account_id: &<Self as system::Trait>::AccountId) -> bool;
 }
 
 decl_storage! {
     trait Store for Module<T: Trait<I>, I: Instance> as Referendum {
         // Current stage if there is an election running
-        //Stage get(stage) config(): Option<ReferendumStage<T::BlockNumber>>;
+        Stage get(stage) config(): (ReferendumStage, T::BlockNumber);
 
-        Stage get(stage) config(): Option<T::TmpDummy>;
+        Tmp get(tmp) config(): Option<T::TmpDummy>;
     }
 
     /* This might be needed in some cases
@@ -70,25 +79,52 @@ decl_event! {
     }
 }
 
+decl_error! {
+    #[derive(Copy)]
+    /// Referendum errors
+    pub enum Error {
+        /// Origin doesn't correspond to any superuser
+        OriginNotSuperUser,
+
+        /// Referendum cannot run twice at the same time
+        ReferendumAlreadyRunning,
+    }
+}
+
+impl From<system::Error> for Error {
+    fn from(error: system::Error) -> Self {
+        match error {
+            system::Error::Other(msg) => Error::Other(msg),
+            system::Error::RequireRootOrigin => Error::OriginNotSuperUser,
+            _ => Error::Other(error.into()),
+        }
+    }
+}
+
 /////////////////// Module definition and implementation ///////////////////////
 
 decl_module! {
     pub struct Module<T: Trait<I>, I: Instance> for enum Call where origin: T::Origin {
+        /// Predefined errors
+        type Error = Error;
 
         /////////////////// Lifetime ///////////////////////////////////////////
 
         // start voting period
-        pub fn start_referendum(origin) -> dispatch::Result {
+        pub fn start_referendum(origin) -> Result<(), Error> {
             // prepare referendum
 
             //EnsureChecks<T>::can_start_referendum()?;
             EnsureChecks::<T, I>::can_start_referendum(origin)?;
 
+            // change referendum state
+            Stage::<T, I>::put((ReferendumStage::Voting, T::BlockNumber::from(0))); // TODO: get block number or remove it from stage storage
+
             Ok(())
         }
 
         // finish voting period
-        pub fn finish_voting(origin) -> dispatch::Result {
+        pub fn finish_voting(origin) -> Result<(), Error> {
             // do necessary actions to close voting
 
             // start revealing phase
@@ -97,7 +133,7 @@ decl_module! {
             Ok(())
         }
 
-        pub fn finish_revealing_period(origin) -> dispatch::Result {
+        pub fn finish_revealing_period(origin) -> Result<(), Error> {
             // do necessary actions to finish revealing phase
 
             Self::evaluate_referendum_results()?;
@@ -107,13 +143,13 @@ decl_module! {
 
         /////////////////// User actions ///////////////////////////////////////
 
-        pub fn vote(origin) -> dispatch::Result {
+        pub fn vote(origin) -> Result<(), Error> {
             // recieve user's commitment
 
             Ok(())
         }
 
-        pub fn reveal_vote(origin) -> dispatch::Result {
+        pub fn reveal_vote(origin) -> Result<(), Error> {
             // reveals user's commitment
 
             Ok(())
@@ -121,28 +157,38 @@ decl_module! {
     }
 }
 
-/////////////////// Inner logic ////////////////////////////////////////////
+/////////////////// Inner logic ////////////////////////////////////////////////
 impl<T: Trait<I>, I: Instance> Module<T, I> {
-    fn start_revealing_period() -> dispatch::Result {
+    fn start_revealing_period() -> Result<(), Error> {
         // do necessary actions to start commitment revealing phase
 
         Ok(())
     }
 
-    fn evaluate_referendum_results() -> dispatch::Result {
+    fn evaluate_referendum_results() -> Result<(), Error> {
         // evaluate results
 
         Ok(())
     }
 }
 
-/////////////////// Ensure checks //////////////////////////////////////////
+/////////////////// Ensure checks //////////////////////////////////////////////
 pub struct EnsureChecks<T: Trait<I>, I: Instance> {
     _dummy: PhantomData<(T, I)>, // 0-sized data meant only to bound generic parameters
 }
 
 impl<T: Trait<I>, I: Instance> EnsureChecks<T, I> {
-    fn can_start_referendum(origin: T::Origin) -> dispatch::Result {
+    fn can_start_referendum(origin: T::Origin) -> Result<(), Error> {
+        let who = ensure_signed(origin)?;
+
+        if !T::is_super_user(&who) {
+            return Err(Error::OriginNotSuperUser);
+        }
+
+        if Stage::<T, I>::get().0 != ReferendumStage::Void {
+            return Err(Error::ReferendumAlreadyRunning);
+        }
+
         Ok(())
     }
 }
--- runtime-modules/referendum/src/mock.rs
+++ runtime-modules/referendum/src/mock.rs
@@ -15,6 +15,7 @@ use srml_support::{impl_outer_origin, parameter_types};
 use crate::GenesisConfig;
 
 pub const USER_ADMIN: u64 = 1;
+pub const USER_REGULAR: u64 = 2;
 
 /////////////////// Runtime and Instances //////////////////////////////////////
 // Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
@@ -30,6 +31,10 @@ impl<I: Instance> Trait<I> for Runtime {
     type Event = Event<Self, I>;
 
     type TmpDummy = u64;
+
+    fn is_super_user(account_id: &<Self as system::Trait>::AccountId) -> bool {
+        *account_id == USER_ADMIN
+    }
 }
 
 pub type TestModule = Module<Runtime, Instance0>;
@@ -48,10 +53,12 @@ parameter_types! {
     pub const MinimumPeriod: u64 = 5;
 }
 
+// TODO: find a way to derive the trait
+#[allow(non_upper_case_globals)] // `decl_storage` macro defines this weird name
 impl Instance for Instance0 {
     const PREFIX: &'static str = "Instance0";
 
-    #[allow(non_upper_case_globals)] // `decl_storage` macro defines this weird name
+    const PREFIX_FOR_Tmp: &'static str = "Instance0_tmp";
     const PREFIX_FOR_Stage: &'static str = "Instance0_stage";
 }
 
@@ -73,6 +80,8 @@ impl system::Trait for Runtime {
     type Version = ();
 }
 
+/////////////////// Data structures ////////////////////////////////////////////
+
 #[allow(dead_code)]
 #[derive(Clone)]
 pub enum OriginType {
@@ -92,8 +101,8 @@ pub fn mock_origin(origin: OriginType) -> mock::Origin {
 
 pub fn default_genesis_config_generic<I: Instance>() -> GenesisConfig<Runtime, I> {
     GenesisConfig::<Runtime, I> {
-        //stage: ReferendumStage::Void,
-        stage: 0,
+        stage: (ReferendumStage::default(), 0),
+        tmp: 0,
     }
 }
 
@@ -115,7 +124,7 @@ pub fn build_test_externalities<I: Instance>(
 
 /////////////////// Mocks of Module's actions //////////////////////////////////
 
-pub fn mock_start_referendum(origin: OriginType, expected_result: dispatch::Result) {
+pub fn mock_start_referendum(origin: OriginType, expected_result: Result<(), Error>) -> () {
     assert_eq!(
         TestModule::start_referendum(mock_origin(origin),),
         expected_result,
