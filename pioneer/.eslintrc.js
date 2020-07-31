b29a3557954f932a6449967f6c229f6e7068acbf
Leszek Wiesner <l.wiesner@adawards.pl>
2020-07-31 16:50:55 +0200

    Adjustments following new apps introduction

--- pioneer/.eslintrc.js
+++ pioneer/.eslintrc.js
@@ -1,5 +1,5 @@
 // At some point don't depend on @polkadot rules and use @joystream/eslint-config
-const base = require('@polkadot/dev-react/config/eslint');
+const base = require('@polkadot/dev/config/eslint');
 
 // add override for any (a metric ton of them, initial conversion)
 module.exports = {
@@ -21,6 +21,7 @@ module.exports = {
     // why only required in VSCode!?!? is eslint plugin not working like eslint commandline?
     // Or are we having to add this because of new versions of eslint-config-* ?
     'no-console': 'off',
+    'header/header': 'off' // Temporary disable polkadot's rule
   },
   // isolate pioneer from monorepo eslint rules
   root: true
