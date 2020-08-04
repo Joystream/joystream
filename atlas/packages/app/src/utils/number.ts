079306486c34be0209b26dd323f31e104c032e51
Klaudiusz Dembler <kdembler@gmail.com>
2020-08-04 09:19:22 +0200

    improve formatting function

--- atlas/packages/app/src/utils/number.ts
+++ atlas/packages/app/src/utils/number.ts
@@ -6,10 +6,10 @@ export const formatNumberShort = (num: number): string => {
   let value = num
   let suffix = ''
 
-  if (num > 1000000) {
+  if (num >= 1000000) {
     value /= 1000000
     suffix = 'M'
-  } else if (num > 1000) {
+  } else if (num >= 1000) {
     value /= 1000
     suffix = 'K'
   }
