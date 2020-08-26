9caaedbb08f5874daf4740d99baac461277ad454
dzhelezov <dzhelezov@gmail.com>
2020-08-26 15:28:29 +0300

    convert BN -> number for QueryBlockProducer

--- query-node/substrate-query-framework/index-builder/src/QueryBlockProducer.ts
+++ query-node/substrate-query-framework/index-builder/src/QueryBlockProducer.ts
@@ -4,7 +4,6 @@ import QueryEventBlock from './QueryEventBlock';
 import { Header, Extrinsic } from '@polkadot/types/interfaces';
 import { EventEmitter } from 'events';
 import * as assert from 'assert';
-import * as BN from 'bn.js';
 
 import Debug from 'debug';
 import { UnsubscribePromise } from '@polkadot/api/types';
@@ -31,26 +30,18 @@ export default class QueryBlockProducer extends EventEmitter {
 
   private _backOffTime = DEFAULT_BACKOFF_TIME_MS;
 
-  private _producing_blocks_blocks: boolean;
-
   private readonly _query_service: ISubstrateQueryService;
 
   private _new_heads_unsubscriber: UnsubscribePromise | undefined;
 
-  private _block_to_be_produced_next: BN;
+  private _block_to_be_produced_next: number;
 
-  // Index of the last processed event
-  private _last_processed_event_index: BN;
-
-  private _at_block: BN;
-
-  private _height_of_chain: BN;
+  private _height_of_chain: number;
 
   constructor(query_service: ISubstrateQueryService) {
     super();
 
     this._started = false;
-    this._producing_blocks_blocks = false;
     this._query_service = query_service;
 
     // TODO
@@ -58,14 +49,12 @@ export default class QueryBlockProducer extends EventEmitter {
     // will be refactored
     this._new_heads_unsubscriber = undefined;
 
-    this._block_to_be_produced_next = new BN(0);
-    this._height_of_chain = new BN(0);
-    this._last_processed_event_index = new BN(0);
-    this._at_block = new BN(0);
+    this._block_to_be_produced_next = 0;
+    this._height_of_chain = 0;
   }
 
   // TODO: We cannot assume first block has events... we need more robust logic.
-  async start(at_block?: BN, at_event?: BN): Promise<void> {
+  async start(at_block?: number): Promise<void> {
     if (this._started) throw Error(`Cannot start when already started.`);
 
     // mark as started
@@ -74,17 +63,12 @@ export default class QueryBlockProducer extends EventEmitter {
     // Try to get initial header right away
     const finalizedHeadHash = await this._query_service.getFinalizedHead();
     const header = await this._query_service.getHeader(finalizedHeadHash);
-    this._height_of_chain = header.number.toBn() as BN;
+    this._height_of_chain = header.number.toNumber();
 
     if (at_block) {
       this._block_to_be_produced_next = at_block;
-      this._at_block = at_block;
-
-      if (at_block.gt(this._height_of_chain)) throw Error(`Provided block is ahead of chain.`);
-    }
-
-    if (at_event) {
-      this._last_processed_event_index = at_event;
+      
+      if (at_block > this._height_of_chain) throw Error(`Provided block is ahead of chain.`);
     }
 
     //
@@ -109,7 +93,7 @@ export default class QueryBlockProducer extends EventEmitter {
   private _OnNewHeads(header: Header) {
     assert(this._started, 'Has to be started to process new heads.');
 
-    this._height_of_chain = header.number.toBn() as BN;
+    this._height_of_chain = header.number.toNumber();
 
     debug(`New block found at height #${this._height_of_chain.toString()}`);
 
@@ -134,7 +118,7 @@ export default class QueryBlockProducer extends EventEmitter {
     ]).then(x => x as T);
   }
 
-  public async fetchBlock(height: BN = this._block_to_be_produced_next): Promise<QueryEventBlock> {
+  public async fetchBlock(height: number = this._block_to_be_produced_next): Promise<QueryEventBlock> {
     let qeb = undefined
     while (!qeb) {
       try {
@@ -161,7 +145,7 @@ export default class QueryBlockProducer extends EventEmitter {
    * It can throw errors which should be handled by the top-level code 
    * (in this case _produce_block())
    */
-  private async _doBlockProduce(height: BN = this._block_to_be_produced_next): Promise<QueryEventBlock> {
+  private async _doBlockProduce(height: number = this._block_to_be_produced_next): Promise<QueryEventBlock> {
     debug(`Fetching block #${height.toString()}`);
 
     const block_hash_of_target = await this._withTimeout(
@@ -195,7 +179,7 @@ export default class QueryBlockProducer extends EventEmitter {
           // is needed to avoid events from build config code in genesis, and possibly other cases.
         const extrinsic =
           phase.isApplyExtrinsic && extrinsics_array.length
-            ? extrinsics_array[(phase.asApplyExtrinsic.toBn() as BN).toNumber()]
+            ? extrinsics_array[Number.parseInt(phase.asApplyExtrinsic.toString())]
               : undefined;
 
         const query_event = new QueryEvent(record, height, index, extrinsic);
@@ -227,18 +211,18 @@ export default class QueryBlockProducer extends EventEmitter {
   private async checkHeightOrWait(): Promise<void> {
     return await waitFor(
       // when to resolve
-      () => this._block_to_be_produced_next.lte(this._height_of_chain),
+      () => this._block_to_be_produced_next <= this._height_of_chain,
       //exit condition
       () => !this._started )
     
   }
 
-  public async * blockHeights(): AsyncGenerator<BN> {
+  public async * blockHeights(): AsyncGenerator<number> {
     while (this._started) {
       await this.checkHeightOrWait();
       debug(`Yield: ${this._block_to_be_produced_next.toString()}`);
       yield this._block_to_be_produced_next;
-      this._block_to_be_produced_next = this._block_to_be_produced_next.addn(1);
+      this._block_to_be_produced_next++;
     }
   }
 
