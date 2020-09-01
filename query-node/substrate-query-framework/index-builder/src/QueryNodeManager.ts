8e831a4751b17adbf7603b83287ac37b12242ed8
dzhelezov <dzhelezov@gmail.com>
2020-09-01 13:41:45 +0300

    separate Indexer, Bootstrapper and Processor options

--- query-node/substrate-query-framework/index-builder/src/QueryNodeManager.ts
+++ query-node/substrate-query-framework/index-builder/src/QueryNodeManager.ts
@@ -1,7 +1,8 @@
 import QueryNode, { QueryNodeState } from './QueryNode';
 import { Bootstrapper } from './bootstrap';
-import { QueryNodeStartUpOptions, QueryEventProcessingPack } from '.';
 import MappingsProcessor from './processor/MappingsProcessor';
+import { IndexerOptions, BootstrapOptions, ProcessorOptions } from './QueryNodeStartOptions';
+import { createDBConnection } from './db';
 
 // Respondible for creating, starting up and shutting down the query node.
 // Currently this class is a bit thin, but it will almost certainly grow
@@ -21,14 +22,15 @@ export default class QueryNodeManager {
    * 
    * @param options options passed to create the indexer service
    */
-  async index(options: QueryNodeStartUpOptions): Promise<void> {
+  async index(options: IndexerOptions): Promise<void> {
     if (this._query_node) throw Error('Cannot start the same manager multiple times.');
+    await createDBConnection();
 
     this._query_node = await QueryNode.create(options);
     await this._query_node.start();
   }
 
-  async bootstrap(options: QueryNodeStartUpOptions): Promise<void> {
+  async bootstrap(options: BootstrapOptions): Promise<void> {
     const bootstrapper = await Bootstrapper.create(options);
     await bootstrapper.bootstrap();
   }
@@ -39,8 +41,10 @@ export default class QueryNodeManager {
    * 
    * @param options options passed to create the mappings
    */
-  async process(options: QueryNodeStartUpOptions): Promise<void> {
-    const processor =  MappingsProcessor.create(options.processingPack as QueryEventProcessingPack);
+  async process(options: ProcessorOptions): Promise<void> {
+    await createDBConnection();
+    
+    const processor =  MappingsProcessor.create(options.processingPack);
     await processor.start(options.atBlock);
   }
 
