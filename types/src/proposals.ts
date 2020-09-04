423b4ad8541dd0109a7bc0903c97fd49590a8c83
Leszek Wiesner <l.wiesner@adawards.pl>
2020-09-04 17:18:41 +0200

    Proposal error message hotfix

--- types/src/proposals.ts
+++ types/src/proposals.ts
@@ -86,7 +86,7 @@ export class ActiveStake extends JoyStructDecorated({ stake_id: StakeId, source_
   implements IActiveStake {}
 
 export class ExecutionFailedStatus extends JoyStructDecorated({
-  error: Vec.with(u8),
+  error: Bytes,
 }) {}
 
 export class ExecutionFailed extends ExecutionFailedStatus {}
