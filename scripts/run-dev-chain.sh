a4e5ba013df54a308ac78ccc1322684e68e18902
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-09-18 16:50:11 +0300

    update dev scripts

--- scripts/run-dev-chain.sh
+++ scripts/run-dev-chain.sh
@@ -3,10 +3,10 @@
 export WASM_BUILD_TOOLCHAIN=nightly-2020-05-23
 
 # Build release binary
-cargo build --release -p joystream-node
+cargo build --release
 
 # Purge existing local chain
-yes | cargo run --release -p joystream-node -- purge-chain --dev
+yes | cargo run --release -- purge-chain --dev
 
 # Run local development chain
-cargo run --release -p joystream-node -- --dev
+cargo run --release -- --dev
