209f90b608ede2f020658d3ee86c9b63734d131d
Leszek Wiesner <l.wiesner@adawards.pl>
2020-09-21 16:20:48 +0200

    Pioneer: Disable media app to allow build/start

--- pioneer/packages/apps-routing/src/index.ts
+++ pioneer/packages/apps-routing/src/index.ts
@@ -25,13 +25,11 @@ import { terms, privacyPolicy } from './joy-pages';
 import election from './joy-election';
 import proposals from './joy-proposals';
 import roles from './joy-roles';
-import media from './joy-media';
 import forum from './joy-forum';
 
 export default function create (t: <T = string> (key: string, text: string, options: { ns: string }) => T): Routes {
   return appSettings.uiMode === 'light'
     ? [
-      media(t),
       members(t),
       roles(t),
       election(t),
@@ -47,7 +45,6 @@ export default function create (t: <T = string> (key: string, text: string, opti
       privacyPolicy(t)
     ]
     : [
-      media(t),
       members(t),
       roles(t),
       election(t),
