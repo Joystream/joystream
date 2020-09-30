7873133e665348954ade5b3b5e14358e0d7d15aa
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-09-30 10:18:33 +0300

    factor out computing runtime shasum to a script file

---
+++ scripts/runtime-code-shasum.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Cargo workspace root
+export WORKSPACE_ROOT=`cargo metadata --offline --no-deps --format-version 1 | jq .workspace_root -r`
+
+cd ${WORKSPACE_ROOT}
+
+# srot/owner/group/mtime arguments only work with gnu version of tar.
+# So if you run this on Mac the default version of tar is `bsdtar`
+# and you will not get an idempotent result.
+# Install gnu-tar with brew
+#   brew install gnu-tar
+#   export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+tar -c --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2020-01-01' Cargo.lock Cargo.toml \
+    runtime runtime-modules utils/chain-spec-builder | shasum | cut -d " " -f 1
+
