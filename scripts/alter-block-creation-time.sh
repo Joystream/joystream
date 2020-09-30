bce4b6c0bfde836a846988eba0608005a6b43622
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-09-30 10:21:47 +0300

    scripts: cleanup

--- scripts/alter-block-creation-time.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-perl -i -pe's/pub const MILLISECS_PER_BLOCK: Moment = 6000;/pub const MILLISECS_PER_BLOCK: Moment = 2000;/' runtime/src/constants.rs
-perl -i -pe's/pub const SLOT_DURATION: Moment = 6000;/pub const SLOT_DURATION: Moment = 2000;/' runtime/src/constants.rs
--- scripts/raspberry-cross-build.sh
+++ scripts/raspberry-cross-build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# Run this script from the crago workspace root
-
 # Cross compiles release build of joystream-node
 # for Raspberry Pi - using docker
 
 # joystream/rust-raspberry image was built from:
-# https://github.com/mnaamani/rust-on-raspberry-docker/commit/8536508b743d55c8573043c4082c62da3b4fd3e2
+# https://github.com/mnaamani/rust-on-raspberry-docker/tree/add-wasm-toolchain-and-clang
+
+export WORKSPACE_ROOT=`cargo metadata --offline --no-deps --format-version 1 | jq .workspace_root -r`
 
 docker run \
-    --volume ${PWD}/:/home/cross/project \
+    -e WASM_BUILD_TOOLCHAIN=nightly-2020-05-23 \
+    --volume ${WORKSPACE_ROOT}/:/home/cross/project \
     --volume ${HOME}/.cargo/registry:/home/cross/.cargo/registry \
     joystream/rust-raspberry \
     build --release -p joystream-node
 
-# artifacts will be relative to the working directory:
-# target/arm-unknown-linux-gnueabihf/joystream-node
+ls -l target/arm-unknown-linux-gnueabihf/joystream-node
