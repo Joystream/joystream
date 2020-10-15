cdc0d08b6b9eb0182585da490134aee8367fe7a1
Leszek Wiesner <l.wiesner@adawards.pl>
2020-10-15 17:22:12 +0200

    Remove joy-media

--- pioneer/packages/joy-media/README.md
+++
@@ -1,3 +0,0 @@
-# Media content module for Joystream node
-
-This module works with multi-media content such as audio and video.
--- pioneer/packages/joy-media/src/stories/data/AccountIdSamples.ts
+++
@@ -1,7 +0,0 @@
-import { createType } from '@joystream/types';
-
-export const AccountIdSamples = {
-  Alice: createType('AccountId', '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY'),
-  Bob: createType('AccountId', '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'),
-  Charlie: createType('AccountId', '5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y')
-};
