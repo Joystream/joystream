33451b642ee3036db89a6d1ac57cd61dd085f32a
iorveth <aenseidheiorweth@gmail.com>
2020-11-09 14:03:09 +0200

    Query node integration tests: Update channel description and ensure query node successfully sync data

--- tests/network-tests/src/flows/contentDirectory/updatingChannel.ts
+++ tests/network-tests/src/flows/contentDirectory/updatingChannel.ts
@@ -2,11 +2,12 @@ import { QueryNodeApi, WorkingGroups } from '../../Api'
 import { UpdateChannelFixture } from '../../fixtures/contentDirectoryModule'
 import { ChannelEntity } from 'cd-schemas/types/entities/ChannelEntity'
 import { assert } from 'chai'
+import { Utils } from '../../utils'
 
-export function createUpdateChannelTitleFixture(api: QueryNodeApi): UpdateChannelFixture {
+export function createUpdateChannelDescriptionFixture(api: QueryNodeApi, description: string): UpdateChannelFixture {
   // Create partial channel entity, only containing the fields we wish to update
   const channelUpdateInput: Partial<ChannelEntity> = {
-    title: 'Updated channel title',
+    description,
   }
 
   const uniquePropVal: Record<string, any> = { title: 'Example channel' }
@@ -15,7 +16,26 @@ export function createUpdateChannelTitleFixture(api: QueryNodeApi): UpdateChanne
 }
 
 export default async function updateChannel(api: QueryNodeApi) {
-  const createVideoHappyCaseFixture = createUpdateChannelTitleFixture(api)
 
-  await createVideoHappyCaseFixture.runner(false)
+  const channelResult = await api.getChannelbyTitle('Example channel')
+  const channel = channelResult.data.channels[0]
+
+  const description = 'Updated description'
+  const createUpdateChannelDescriptionHappyCaseFixture = createUpdateChannelDescriptionFixture(api, description)
+
+  await createUpdateChannelDescriptionHappyCaseFixture.runner(false)
+
+  // Temporary solution (wait 2 minutes)
+  await Utils.wait(120000)
+
+  const channelAfterUpdateResult = await api.getChannelbyTitle('Example channel')
+  const channelAfterUpdate = channelAfterUpdateResult.data.channels[0]
+
+  assert(channelAfterUpdate.title === channel.title, 'Should be equal')
+  
+  // description field should be updated to provided one 
+  assert(channelAfterUpdate.description === description, 'Should be equal')
+  assert(channelAfterUpdate.coverPhotoUrl === channel.coverPhotoUrl, 'Should be equal')
+  assert(channelAfterUpdate.avatarPhotoUrl === channel.avatarPhotoURL, 'Should be equal')
+  assert(channelAfterUpdate.isPublic === channel.isPublic, 'Should be equal')
 }
