4c630afea7f3b42746577cdd8076970cead5bed6
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-11-16 09:32:49 +0400

    setup.sh: install docker

--- setup.sh
+++ setup.sh
@@ -22,8 +22,10 @@ rustup default 1.46.0
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     apt-get install -y coreutils clang jq curl gcc xz-utils sudo pkg-config unzip clang libc6-dev-i386
+    apt-get install -y docker.io docker-compose
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install b2sum gnu-tar jq curl
+    echo "It is recommended to setup Docker desktop from: https://www.docker.com/products/docker-desktop"
 fi
 
 # Volta nodejs, npm, yarn tools manager
