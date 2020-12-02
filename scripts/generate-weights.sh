3f650fccb0835dc682d0cc6b21b64b1d42b13a57
Shamil Gadelshin <shamilgadelshin@gmail.com>
2020-12-02 12:01:57 +0300

    Update ‘generate-weights.sh’ script: add constitution pallet.

--- scripts/generate-weights.sh
+++ scripts/generate-weights.sh
@@ -7,8 +7,12 @@ echo "Benchmarking proposals_discussion..."
 mv proposals_discussion.rs runtime/src/weights/
 echo "proposals_discussion benchmarked"
 
-
 echo "Benchmarking proposals_engine..."
 ./target/release/joystream-node benchmark --pallet=proposals_engine --extrinsic=* --chain=dev --steps=50 --repeat=20 --execution=wasm --output=. > /dev/null
 mv proposals_engine.rs runtime/src/weights/
 echo "proposals_engine benchmarked"
+
+echo "Benchmarking pallet_constitution..."
+./target/release/joystream-node benchmark --pallet=pallet_constitution --extrinsic=* --chain=dev --steps=50 --repeat=20 --execution=wasm --output=. > /dev/null
+mv pallet_constitution.rs runtime/src/weights/
+echo "pallet_constitution benchmarked"
