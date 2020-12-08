d7ca74fec42ad3bee30ad08728ac8dd2b0f725ae
conectado <gabrielalejandro7@gmail.com>
2020-12-08 05:30:45 -0300

    benchmarks: remove comment

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -17,10 +17,8 @@
 
 // FRAME pallets
 pub mod frame_system;
-//pub mod pallet_babe;
 pub mod pallet_balances;
 pub mod pallet_constitution;
-//pub mod pallet_grandpa;
 pub mod pallet_im_online;
 pub mod pallet_session;
 pub mod pallet_staking;
