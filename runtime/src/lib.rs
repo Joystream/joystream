544b6530e68fc6b235764ec84092a7b223168512
bwhm <martin@jsgenesis.com>
2020-12-08 10:10:48 +0100

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -75,7 +75,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node"),
     impl_name: create_runtime_str!("joystream-node"),
     authoring_version: 7,
-    spec_version: 7,
+    spec_version: 8,
     impl_version: 0,
     apis: crate::runtime_api::EXPORTED_RUNTIME_API_VERSIONS,
     transaction_version: 1,
