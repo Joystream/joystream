33dd60ace61a0584700a6d93f8f3be2b4fbbef93
conectado <gabrielalejandro7@gmail.com>
2020-12-10 07:32:16 -0300

    benchmark: script benchmark each pallet separately

--- runtime/src/runtime_api.rs
+++ runtime/src/runtime_api.rs
@@ -307,6 +307,8 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
             add_benchmark!(params, batches, pallet_session, SessionBench::<Runtime>);
             add_benchmark!(params, batches, pallet_im_online, ImOnline);
+
+            // Joystream Benchmarks
             add_benchmark!(params, batches, proposals_discussion, ProposalsDiscussion);
             add_benchmark!(params, batches, proposals_engine, ProposalsEngine);
             add_benchmark!(params, batches, pallet_constitution, Constitution);
--- scripts/generate-weights.sh
+++ scripts/generate-weights.sh
@@ -2,21 +2,41 @@
 
 # Executes and replaces all benchmarks with the new weights
 
-echo "Generating weights..."
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-$SCRIPT_DIR/../target/release/joystream-node benchmark \
-    --pallet=* \
-    --extrinsic=* \
-    --chain=dev \
-    --steps=50 \
-    --repeat=20 \
-    --execution=wasm \
-    --output=.  > /dev/null
 
-if [ $? -eq 0 ]; then
-    mv $SCRIPT_DIR/../*.rs $SCRIPT_DIR/../runtime/src/weights/
-    echo "Weights generated successfully"
-else
-    echo "There was a problem generating the new weights, check the error above"
-fi
+benchmark() {
+  echo "Generating weights for $1..."
+  start=`date +%s`
+  $SCRIPT_DIR/../target/release/joystream-node benchmark \
+      --pallet=$1 \
+      --extrinsic=* \
+      --chain=dev \
+      --steps=5 \
+      --repeat=2 \
+      --execution=wasm \
+      --output=.  > /dev/null
+
+  end=`date +%s`
+
+  if [ $? -eq 0 ]; then
+      mv $SCRIPT_DIR/../*.rs $SCRIPT_DIR/../runtime/src/weights/
+      echo "Weights generated successfully for $1"
+      echo "It took $((end-start)) seconds"
+  else
+      echo "There was a problem generating the weights for $1, check the error above"
+  fi
+}
+
+# FRAME benchmarks
+benchmark frame_system
+benchmark pallet_utility
+benchmark pallet_timestamp
+benchmark pallet_session
+benchmark pallet_im_online
+
+# Joystrem benchmarks
+benchmark proposals_discussion
+benchmark proposals_engine
+benchmark pallet_constitution
+benchmark working_group
