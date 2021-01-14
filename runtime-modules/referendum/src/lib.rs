8e9030117b1a0301325752770f44ad9abe73647f
conectado <gabrielalejandro7@gmail.com>
2021-01-14 12:45:40 -0300

    runtime: referendum: use reason for locking balance

--- runtime-modules/referendum/src/lib.rs
+++ runtime-modules/referendum/src/lib.rs
@@ -35,7 +35,7 @@
 // used dependencies
 use codec::{Codec, Decode, Encode};
 use core::marker::PhantomData;
-use frame_support::traits::{EnsureOrigin, Get};
+use frame_support::traits::{EnsureOrigin, Get, WithdrawReason};
 use frame_support::weights::Weight;
 use frame_support::{
     decl_error, decl_event, decl_module, decl_storage, ensure, error::BadOrigin, Parameter,
@@ -692,7 +692,7 @@ impl<T: Trait<I>, I: Instance> Mutations<T, I> {
         current_cycle_id: &u64,
     ) -> Result<(), Error<T, I>> {
         // Should call after `can_vote`
-        T::StakingHandler::lock(account_id, *stake);
+        T::StakingHandler::lock_with_reasons(account_id, *stake, WithdrawReason::Transfer.into());
 
         // store vote
         Votes::<T, I>::insert(
