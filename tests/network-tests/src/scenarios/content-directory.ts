236865243452d72481be76164f7ea9b492d7d151
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2021-02-26 18:12:11 +0200

    remove package dependencies on @joystream/cd-schemas and move init script to utils

--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -1,5 +1,4 @@
 import leaderSetup from '../flows/workingGroup/leaderSetup'
-import initializeContentDirectory from '../flows/contentDirectory/contentDirectoryInitialization'
 import createChannel from '../flows/contentDirectory/creatingChannel'
 import createVideo from '../flows/contentDirectory/creatingVideo'
 import updateChannel from '../flows/contentDirectory/updatingChannel'
@@ -7,7 +6,6 @@ import { scenario } from '../Scenario'
 
 scenario(async ({ job }) => {
   job('setup content lead', leaderSetup.content)
-    .then(job('init-content-dir', initializeContentDirectory))
     .then(job('create-channel', createChannel))
     .then(job('create-video', createVideo))
     .then(job('update-channel', updateChannel))
