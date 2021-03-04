92ecac0f50287b8f60e4c7838dcce14a8ce7d4cc
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-04 17:15:47 +0000

    creating getMemberById function

--- query-node/mappings/membership/mapping.ts
+++ query-node/mappings/membership/mapping.ts
@@ -7,3 +7,9 @@ import { DatabaseManager, SubstrateEvent } from '@dzlzv/hydra-indexer-lib/lib'
 import { Members } from '../generated/types'
 import { EntryMethod, Membership } from '../generated/graphql-server/src/modules/membership/membership.model'
 import { Block, Network } from '../generated/graphql-server/src/modules/block/block.model'
+
+async function getMemberById(db: DatabaseManager, id: MemberId): Promise<Membership> {
+    const member = await db.get(Membership, { where: { id: id.toString() } })
+    if (!member) throw Error(`Member(${id}) not found`)
+    return member
+  }
