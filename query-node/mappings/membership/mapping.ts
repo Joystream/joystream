3bdcdb2fc33050df7a5b9ad0a2eb3701469a588d
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-04 17:15:08 +0000

    adding additional imports

--- query-node/mappings/membership/mapping.ts
+++ query-node/mappings/membership/mapping.ts
@@ -3,3 +3,7 @@ import BN from 'bn.js'
 import { Bytes } from '@polkadot/types'
 import { MemberId } from '@joystream/types/members'
 import { DatabaseManager, SubstrateEvent } from '@dzlzv/hydra-indexer-lib/lib'
+
+import { Members } from '../generated/types'
+import { EntryMethod, Membership } from '../generated/graphql-server/src/modules/membership/membership.model'
+import { Block, Network } from '../generated/graphql-server/src/modules/block/block.model'
