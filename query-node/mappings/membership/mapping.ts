2cb7bece5c40ff3e90b8dfb617636065468b3fd5
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-04 17:09:20 +0000

    initiating the membership mappings

---
+++ query-node/mappings/membership/mapping.ts
@@ -0,0 +1,5 @@
+
+import BN from 'bn.js'
+import { Bytes } from '@polkadot/types'
+import { MemberId } from '@joystream/types/members'
+import { DatabaseManager, SubstrateEvent } from '@dzlzv/hydra-indexer-lib/lib'
