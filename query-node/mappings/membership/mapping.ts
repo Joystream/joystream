8d7fdd0d475f2a3e6bcaa362ed1ced7db6fcd169
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-04 17:17:10 +0000

    get registered members

--- query-node/mappings/membership/mapping.ts
+++ query-node/mappings/membership/mapping.ts
@@ -17,3 +17,19 @@ async function getMemberById(db: DatabaseManager, id: MemberId): Promise<Members
 function convertBytesToString(b: Bytes): string {
     return Buffer.from(b.toU8a(true)).toString()
 }
+
+export async function members_MemberRegistered(db: DatabaseManager, event_: SubstrateEvent): Promise<void> {
+    const { accountId, memberId } = new Members.MemberRegisteredEvent(event_).data
+    const { avatarUri, about, handle } = new Members.BuyMembershipCall(event_).args
+  
+    let block = await db.get(Block, { where: { block: event_.blockNumber } })
+  
+    if (!block) {
+      block = new Block({
+        network: Network.BABYLON,
+        block: event_.blockNumber,
+        // TODO: upgrade indexer-lib which support block timestamp: substrateEvent.timestamp
+        timestamp: new BN(Date.now()),
+      })
+      await db.save<Block>(block)
+    }
