cb4d02d2a7581dfee75eb3031c9d288bb62491a7
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-05 16:25:50 +0000

    updating file paths

--- query-node/mappings/membership/mapping.ts
+++ query-node/mappings/membership/mapping.ts
@@ -1,12 +1,11 @@
-
 import BN from 'bn.js'
 import { Bytes } from '@polkadot/types'
 import { MemberId } from '@joystream/types/members'
 import { DatabaseManager, SubstrateEvent } from '@dzlzv/hydra-indexer-lib/lib'
 
 import { Members } from '../generated/types'
-import { EntryMethod, Membership } from '../generated/graphql-server/src/modules/membership/membership.model'
-import { Block, Network } from '../generated/graphql-server/src/modules/block/block.model'
+import { EntryMethod, Membership } from '../../generated/graphql-server/src/modules/membership/membership.model'
+import { Block, Network } from '../../generated/graphql-server/src/modules/block/block.model'
 
 async function getMemberById(db: DatabaseManager, id: MemberId): Promise<Membership> {
     const member = await db.get(Membership, { where: { id: id.toString() } })
