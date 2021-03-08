77f84292fc599b870a6267fba3278dd0c7517706
iorveth <aenseidheiorweth@gmail.com>
2021-03-08 17:39:12 +0200

    Content cli: Add compiled proto for ChannelCreation

--- content-metadata-protobuf/src/index.ts
+++ content-metadata-protobuf/src/index.ts
@@ -8,3 +8,5 @@ export * from '../compiled/proto/Channel_pb'
 export * from '../compiled/proto/Person_pb'
 export * from '../compiled/proto/Playlist_pb'
 export * from '../compiled/proto/Series_pb'
+
+export * from '../compiled/proto/ChannelCreation_pb'
