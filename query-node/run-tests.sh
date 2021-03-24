aee01a9b44a486017045a35eecfaf2533cc53406
Leszek Wiesner <leszek@jsgenesis.com>
2021-03-24 12:57:23 +0100

    Query-node - separate start script

--- query-node/run-tests.sh
+++ query-node/run-tests.sh
@@ -23,28 +23,6 @@ trap cleanup EXIT
 # Clean start
 docker-compose down -v
 
-# Start the joystream-node first to allow fetching Olympia metadata during build (typegen)
-docker-compose up -d joystream-node
-
-# Only run codegen if no generated files found
-[ ! -d "generated/" ] && yarn build
-
-# Bring up db
-docker-compose up -d db
-
-# Make sure we use dev config for db migrations (prevents "Cannot create database..." and some other errors)
-yarn workspace query-node config:dev
-
-# Migrate the databases
-yarn workspace query-node-root db:prepare
-yarn workspace query-node-root db:migrate
-
-# Initialize databse (ie. membership module configuration)
-yarn workspace query-node-root db:init
-
-docker-compose up -d graphql-server-mnt
-
-# Starting up processor will bring up all services it depends on
-docker-compose up -d processor-mnt
+./start.sh
 
 time yarn workspace integration-tests run-test-scenario olympia
