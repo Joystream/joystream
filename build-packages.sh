81fc6d3361bba4fe62439dffa9180c4c40c27c4a
Leszek Wiesner <leszek@jsgenesis.com>
2021-03-24 12:36:28 +0100

    Add missing metadata-protobuf package build on postinstall

---
+++ build-packages.sh
@@ -0,0 +1,4 @@
+yarn workspace @joystream/types build
+yarn workspace @joystream/cd-schemas generate:all
+yarn workspace @joystream/cd-schemas build
+yarn workspace @joystream/metadata-protobuf build
