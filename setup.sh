7fbac8143f923c0ffc0e5744f75a2c4f3232e2be
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2021-03-26 17:27:35 +0400

    setup.sh: remove stale comments

--- setup.sh
+++ setup.sh
@@ -27,16 +27,9 @@ source ~/.cargo/env
 
 rustup component add rustfmt clippy
 
-# Current version of substrate requires an older version of nightly toolchain
-# to successfully compile the WASM runtime. We force install because rustfmt package
-# is not available for this nightly version.
-rustup install nightly-2020-10-06 --force
+rustup install nightly-2020-10-06
 rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
 
-# Latest clippy linter which comes with 1.47.0 fails on some subtrate modules
-# Also note combination of newer versions of toolchain with the above nightly
-# toolchain to build wasm seems to fail.
-# So we need to stick with an older version until we update substrate
 rustup install 1.47.0
 rustup default 1.47.0
 
