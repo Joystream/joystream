cf7aadb6db1bce1029ca43c758fff428d83b67cd
Shamil Gadelshin <shamilgadelshin@gmail.com>
2021-05-04 13:31:59 +0300

    runtime: proposals: Fix insufficient balance bug.
    https://github.com/Joystream/joystream/issues/2377

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -71,7 +71,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node"),
     impl_name: create_runtime_str!("joystream-node"),
     authoring_version: 7,
-    spec_version: 16,
+    spec_version: 17,
     impl_version: 0,
     apis: crate::runtime_api::EXPORTED_RUNTIME_API_VERSIONS,
     transaction_version: 1,
