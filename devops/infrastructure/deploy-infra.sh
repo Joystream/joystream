f7bb50b99d0dad76641444f7527fdc34ef4d9e2a
Anuj Bansal <highlightanuj@gmail.com>
2021-05-18 20:07:44 +0530

    Add deploy script, initial structure for CloudFormation template

---
+++ devops/infrastructure/deploy-infra.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+STACK_NAME=joystream-node
+REGION=us-east-1
+CLI_PROFILE=joystream-user
+
+EC2_INSTANCE_TYPE=t2.micro
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile $CLI_PROFILE \
+    --query "Exports[?Name=='InstanceDNS'].Value"
+fi
