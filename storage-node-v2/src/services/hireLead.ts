964676f2b5929234cf14cf604f217d0716cf2972
Shamil Gadelshin <shamilgadelshin@gmail.com>
2021-05-25 12:53:59 +0300

    storage-node-v2: Refactor ‘dev-init’ command.

--- storage-node-v2/src/services/hireLead.ts
+++ storage-node-v2/src/services/hireLead.ts
@@ -1,5 +1,4 @@
-import { SubmittableExtrinsic } from '@polkadot/api/types'
-import { createApi, getAlicePair, sendAndFollowTx } from './api'
+import { createApi, getAlicePair, sendAndFollowSudoNamedTx, sendAndFollowNamedTx } from './api'
 
 import { Option, Vec } from '@polkadot/types'
 import {
@@ -15,8 +14,6 @@ export async function hireStorageWorkingGroupLead(): Promise<void> {
   const SudoKeyPair = getAlicePair()
   const LeadKeyPair = getAlicePair()
 
-  const sudo = (tx: SubmittableExtrinsic<'promise'>) => api.tx.sudo.sudo(tx)
-
   // Create membership if not already created
   const members = (await api.query.members.memberIdsByControllerAccountId(
     LeadKeyPair.address
@@ -30,10 +27,12 @@ export async function hireStorageWorkingGroupLead(): Promise<void> {
     console.log('Preparing member account creation extrinsic...')
     memberId = ((await api.query.members.nextMemberId()) as MemberId).toBigInt()
 
-    await sendAndFollowTx(
+    await sendAndFollowNamedTx(
       api,
       LeadKeyPair,
-      api.tx.members.buyMembership(0, 'alice', null, null)
+      'members',
+      'buyMembership',
+      [0, 'alice', null, null]
     )
   }
 
@@ -53,55 +52,59 @@ export async function hireStorageWorkingGroupLead(): Promise<void> {
 
   // Create curator lead opening
   console.log('Preparing Create Storage Lead Opening extrinsic...')
-  await sendAndFollowTx(
+  await sendAndFollowSudoNamedTx(
     api,
     SudoKeyPair,
-    sudo(
-      api.tx.storageWorkingGroup.addOpening(
+    'storageWorkingGroup',
+    'addOpening',
+    [
         { CurrentBlock: null }, // activate_at
         // Disable lint because of the auto-generated types.
         // eslint-disable-next-line @typescript-eslint/camelcase
         { max_review_period_length: 9999 }, // OpeningPolicyCommitment
         'storage opening', // human_readable_text
         'Leader' // opening_type
-      )
-    )
+    ]  
   )
 
   // Apply to lead opening
   console.log('Preparing Apply to Storage Lead Opening extrinsic...')
-  await sendAndFollowTx(
+  await sendAndFollowNamedTx(
     api,
     LeadKeyPair,
-    api.tx.storageWorkingGroup.applyOnOpening(
+    'storageWorkingGroup',
+    'applyOnOpening',
+    [
       memberId, // member id
       newOpeningId, // opening id
       LeadKeyPair.address, // address
       null, // opt role stake
       null, // opt appl. stake
       'bootstrap opening' // human_readable_text
-    )
+    ]
   )
 
   // Begin review period
   console.log('Preparing Begin Applicant Review extrinsic...')
-  await sendAndFollowTx(
+  await sendAndFollowSudoNamedTx(
     api,
     SudoKeyPair,
-    sudo(api.tx.storageWorkingGroup.beginApplicantReview(newOpeningId))
+    'storageWorkingGroup',
+    'beginApplicantReview'
+    ,[newOpeningId]
   )
 
   // Fill opening
   console.log('Preparing Fill Opening extrinsic...')
-  await sendAndFollowTx(
+  await sendAndFollowSudoNamedTx(
     api,
     SudoKeyPair,
-    sudo(
-      api.tx.storageWorkingGroup.fillOpening(
+      'storageWorkingGroup',
+      'fillOpening',
+      [
         newOpeningId, // opening id
         api.createType('ApplicationIdSet', [newApplicationId]), // succesful applicants
         null // reward policy
-      )
-    )
+      ]
   )
 }
