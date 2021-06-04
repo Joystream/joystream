591bda039d9f9dff8ce97861fcd60d7a3d297d8d
Shamil Gadelshin <shamilgadelshin@gmail.com>
2021-06-04 16:59:28 +0300

    storage-node-v2: Add swagger editor script.

--- storage-node-v2/src/services/api/server.ts
+++ storage-node-v2/src/services/api/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import path from 'path'
+import cors from 'cors'
 import { Express } from 'express-serve-static-core'
 import * as OpenApiValidator from 'express-openapi-validator'
 
-// TODO: add swagger UI
+
+
 // TODO: custom errors (including validation errors)
 
 export async function createServer(
@@ -11,28 +13,10 @@ export async function createServer(
   uploadsDir: string
 ): Promise<Express> {
   const server = express()
+  server.use(cors())
   const spec = path.join(__dirname, './../../api-spec/openapi.yaml')
 
   if (devMode) {
-    // Form for the upload testing.
-    server.get('/', function (req, res) {
-      res.send(
-        `<html>
-          <head/>
-          <body>
-            <form method="POST" enctype="multipart/form-data" action="/api/v1/upload">
-              <h2>Api development form (upload)</h2>
-              <table>
-                <tr><td>DataObjectId</td><td><input name="dataObjectId" value="0"</td></tr>
-                <tr><td>WorkerId</td><td><input name="workerId" value="0"></td></tr>
-                <tr><td>StorageBucketId</td><td><input name="storageBucketId" value="0"></td></tr>
-                <tr><td><input type="file" name="recfile"><br /></td><td><input type="submit"></td></tr>
-            </form>
-          </body>
-        </html>`
-      )
-      res.end()
-    })
     // TODO: localhost only?
     server.use('/spec', express.static(spec))
   }
