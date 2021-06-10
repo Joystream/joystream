9f5d5c717c5a6df2771ce3abfd075dbe3c1e1dc4
Leszek Wiesner <leszek@jsgenesis.com>
2021-06-10 14:50:08 +0200

    Fix failing tests

--- tests/integration-tests/src/scenarios/full.ts
+++ tests/integration-tests/src/scenarios/full.ts
@@ -21,15 +21,18 @@ import { scenario } from '../Scenario'
 scenario(async ({ job }) => {
   // Membership:
   const membershipSystemJob = job('membership system', membershipSystem)
-  // All other membership jobs should be executed after, otherwise the varying membershipPrice etc. may break them
+
+  // All other jobs should be executed after membershipSystemJob,
+  // otherwise changing membershipPrice etc. may break them
   job('creating members', creatingMemberships).after(membershipSystemJob)
   job('updating member profile', updatingMemberProfile).after(membershipSystemJob)
   job('updating member accounts', updatingMemberAccounts).after(membershipSystemJob)
   job('inviting members', invitingMebers).after(membershipSystemJob)
   job('transferring invites', transferringInvites).after(membershipSystemJob)
   job('managing staking accounts', managingStakingAccounts).after(membershipSystemJob)
-  // Woring groups:
-  const sudoHireLead = job('sudo lead opening', leadOpening)
+
+  // Working groups:
+  const sudoHireLead = job('sudo lead opening', leadOpening).after(membershipSystemJob)
   job('openings and applications', openingsAndApplications).requires(sudoHireLead)
   job('upcoming openings', upcomingOpenings).requires(sudoHireLead)
   job('group status', groupStatus).requires(sudoHireLead)
