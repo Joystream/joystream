0e7ba9f59407cc85c4c72b03e911cfd105f24468
ignazio-bovo <10487328@polimi.it>
2021-06-24 10:55:33 +0200

    fixed macro errors

--- runtime-modules/content/src/lib.rs
+++ runtime-modules/content/src/lib.rs
@@ -126,7 +126,6 @@ pub trait Trait:
 
     /// Type of ReplyId
     type PostReactionId: NumericIdentifier;
-
 }
 
 /// Specifies how a new asset will be provided on creating and updating
@@ -604,8 +603,10 @@ decl_storage! {
         /// Map, representing  CuratorGroupId -> CuratorGroup relation
         pub CuratorGroupById get(fn curator_group_by_id): map hasher(blake2_128_concat) T::CuratorGroupId => CuratorGroup<T>;
 
-        pub PostById get(fn post_by_id) config() : map hasher(blake2_128_concat) T::PostId => Post<T>;
+        pub PostById get(fn post_by_id) config(): map hasher(blake2_128_concat) T::PostId => Post<T>;
         pub ReplyById get (fn reply_by_id): double_map hasher(blake2_128_concat) T::PostId, hasher(blake2_128_concat) T::ReplyId => Reply<T>;
+
+        pub PostCount get(fn post_count): T::PostId;
     }
 }
 
@@ -1372,76 +1373,78 @@ decl_module! {
         }
 
         #[weight = 10_000_000] // TODO: adjust weight
-        pub fn create_video_post(
+        pub fn create_post(
             _origin,
             _title: <T as frame_system::Trait>::Hash,
             _video: T::VideoId,
-        _category: T::VideoCategoryId,
+            _category: T::VideoCategoryId,
         ) {
             // ensure that origin is signed by the channel ownerr of the video
             Self::not_implemented()?;
         }
 
         #[weight = 10_000_000] // TODO: adjust weight
-      pub fn create_reply(
-      _origin,
-      _owner: PartecipantId<T>,
-      _post_id: T::PostId,
-      _reply_id: Option<T::ReplyId>,
-      _text: <T as frame_system::Trait>::Hash,
-      ) -> {
-          Self::not_implemented()?;
-      }
+        pub fn create_reply(
+            _origin,
+            _owner: PartecipantId<T>,
+            _post_id: T::PostId,
+            _reply_id: Option<T::ReplyId>,
+            _text: <T as frame_system::Trait>::Hash,
+        ) {
+           Self::not_implemented()?;
+        }
 
         #[weight = 10_000_000] // TODO: adjust weight
-    pub fn edit_post(
+        pub fn edit_post(
             _origin,
             _post_id: T::PostId,
-            _new_title: Option<<T as frame_system::Trait>::Hash>
-        _new_video: Option<T::VideoId>
-    ) {
+            _new_title: Option<<T as frame_system::Trait>::Hash>,
+            _new_video: Option<T::VideoId>,
+        ) {
+        // test
             Self::not_implemented()?;
-    }
+        }
 
         #[weight = 10_000_000] // TODO: adjust weight
-      pub fn edit_reply(
+        pub fn edit_reply(
           _origin,
           _owner: PartecipantId<T>,
           _post_id: T::PostId,
           _reply_id: T::ReplyId,
           _new_text: <T as frame_system::Trait>::Hash
-      ) {
+        ) {
           Self::not_implemented()?;
-      }
+        }
 
         #[weight = 10_000_000] // TODO: adjust weight
-    pub fn delete_post(
+        pub fn delete_post(
         _origin,
         _owner: ChannelOwner<
             <T as MembershipTypes>::MemberId,
             <T as ContentActorAuthenticator>::CuratorGroupId,
             <T as StorageOwnership>::DAOId,
         >,
-        _post: PostId,
-    ) {
-        Self::not_implemented()?;n
-    }
+        _post: T::PostId,
+        ) {
+            Self::not_implemented()?;
+        }
+
         #[weight = 10_000_000] // TODO: adjust weight
-      pub fn delete_replies(
-      origin,
-      owner: UserId,
-      reply: ReplyId,
-      ) {
-          Self::not_implemented()?;
-      }
+        pub fn delete_replies(
+        origin,
+        owner: PartecipantId<T>,
+        reply: T::ReplyId,
+        ) {
+            Self::not_implemented()?;
+        }
 
         #[weight = 10_000_000] // TODO: adjust weight
     fn react_post(
-        _origin,
-        _post: T::PostId,
-        _react: T::PostReactionId
+            _origin,
+            _post: T::PostId,
+            _react: T::PostReactionId
     ) {
-        Self::not_implemented()?;
+            Self::not_implemented()?;
     }
     }
 }
