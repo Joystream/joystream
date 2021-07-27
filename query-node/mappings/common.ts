f62c7dd1cfc59d437f05c9268fbee99260972f57
Leszek Wiesner <leszek@jsgenesis.com>
2021-07-27 16:31:59 +0200

    Distribution system initial schema and mappings

--- query-node/mappings/common.ts
+++ query-node/mappings/common.ts
@@ -1,20 +1,25 @@
 import { DatabaseManager } from '@joystream/hydra-common'
 import { BaseModel } from '@joystream/warthog'
 import { WorkingGroup } from '@joystream/types/augment/all'
+import { AnyMetadataClass, DecodedMetadataObject } from '@joystream/metadata-protobuf/types'
+import { metaToObject } from '@joystream/metadata-protobuf/utils'
+import { Bytes } from '@polkadot/types'
 
 type EntityClass<T extends BaseModel> = {
   new (): T
   name: string
 }
 
+type RelationsArr<T extends BaseModel> = Exclude<
+  keyof T & string,
+  { [K in keyof T]: T[K] extends BaseModel | undefined ? '' : T[K] extends BaseModel[] | undefined ? '' : K }[keyof T]
+>[]
+
 export async function getById<T extends BaseModel>(
   store: DatabaseManager,
   entityClass: EntityClass<T>,
   id: string,
-  relations?: Exclude<
-    keyof T & string,
-    { [K in keyof T]: T[K] extends BaseModel | undefined ? '' : T[K] extends BaseModel[] | undefined ? '' : K }[keyof T]
-  >[]
+  relations?: RelationsArr<T>
 ): Promise<T> {
   const result = await store.get(entityClass, { where: { id }, relations })
   if (!result) {
@@ -34,4 +39,16 @@ export function getWorkingGroupModuleName(group: WorkingGroup): WorkingGroupModu
   }
 
   throw new Error(`Unsupported working group encountered: ${group.type}`)
+}
+
+export function deserializeMetadata<T>(
+  metadataType: AnyMetadataClass<T>,
+  metadataBytes: Bytes
+): DecodedMetadataObject<T> | null {
+  try {
+    return metaToObject(metadataType, metadataType.decode(metadataBytes.toU8a(true)))
+  } catch (e) {
+    console.error(`Cannot deserialize ${metadataType.name}! Provided bytes: (${metadataBytes.toHex()})`)
+    return null
+  }
 }
