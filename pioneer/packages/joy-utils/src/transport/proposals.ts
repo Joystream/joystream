0c44b41134e1ec57192825e1eddbd4088747336d
Ricardo Maltez <ricardomaltez@gmail.com>
2021-08-06 02:30:48 +0100

    Replace voteExistsByProposalByVoter.size with voteExistsByProposalByVoter.entries for better performance getting member vote

--- pioneer/packages/joy-utils/src/transport/proposals.ts
+++ pioneer/packages/joy-utils/src/transport/proposals.ts
@@ -185,12 +185,16 @@ export default class ProposalsTransport extends BaseTransport {
       proposals
     };
   }
+  
+  async voteByProposalAndMember(proposalId: ProposalId, voterId: MemberId): Promise<VoteKind | null> {
+    const votesEntries = await this.api.query.proposalsEngine.voteExistsByProposalByVoter.entries(proposalId);
+    const voteEntry = votesEntries.find((voteEntry) => {
+      const memberId = voteEntry[0].args[1] as MemberId;
 
-  async voteByProposalAndMember (proposalId: ProposalId, voterId: MemberId): Promise<VoteKind | null> {
-    const vote = (await this.proposalsEngine.voteExistsByProposalByVoter(proposalId, voterId)) as VoteKind;
-    const hasVoted = (await this.api.query.proposalsEngine.voteExistsByProposalByVoter.size(proposalId, voterId)).toNumber();
+      return memberId.eq(voterId);
+    });
 
-    return hasVoted ? vote : null;
+    return voteEntry ? voteEntry[1] as VoteKind : null;
   }
 
   async votes (proposalId: ProposalId): Promise<ProposalVotes> {
