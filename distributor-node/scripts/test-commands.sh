71ccd6f266bcfc0bd643f2ec5c315137422502bc
Leszek Wiesner <leszek@jsgenesis.com>
2021-08-31 14:05:29 +0200

    New commands: remove-bucket-operator, set-bucket-family-metadata

--- distributor-node/scripts/test-commands.sh
+++ distributor-node/scripts/test-commands.sh
@@ -9,7 +9,7 @@ CLI=../bin/run
 
 ${CLI} dev:init
 ${CLI} leader:set-buckets-per-bag-limit -l 10
-FAMILY_ID=`${CLI} leader:create-bucket-family ${CONFIG}`
+FAMILY_ID=`${CLI} leader:create-bucket-family`
 BUCKET_ID=`${CLI} leader:create-bucket -f ${FAMILY_ID} -a yes`
 ${CLI} leader:update-bag -b static:council -f ${FAMILY_ID} -a ${BUCKET_ID}
 ${CLI} leader:update-bucket-status -f ${FAMILY_ID} -B ${BUCKET_ID}  --acceptingBags yes
@@ -20,10 +20,12 @@ ${CLI} leader:invite-bucket-operator -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0
 ${CLI} leader:cancel-invitation -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0
 ${CLI} leader:invite-bucket-operator -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0
 ${CLI} operator:accept-invitation -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0
-${CLI} operator:set-metadata -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0 -i ./example-metadata.json
+${CLI} operator:set-metadata -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0 -i ./data/operator-metadata.json
+${CLI} leader:remove-bucket-operator -f ${FAMILY_ID} -B ${BUCKET_ID} -w 0
+${CLI} leader:set-bucket-family-metadata -f ${FAMILY_ID} -i ./data/family-metadata.json
 
 # Deletion commands tested separately, since bucket operator removal is not yet supported
-FAMILY_TO_DELETE_ID=`${CLI} leader:create-bucket-family ${CONFIG}`
+FAMILY_TO_DELETE_ID=`${CLI} leader:create-bucket-family`
 BUCKET_TO_DELETE_ID=`${CLI} leader:create-bucket -f ${FAMILY_TO_DELETE_ID} -a yes`
 ${CLI} leader:delete-bucket -f ${FAMILY_TO_DELETE_ID} -B ${BUCKET_TO_DELETE_ID}
 ${CLI} leader:delete-bucket-family -f ${FAMILY_TO_DELETE_ID}
