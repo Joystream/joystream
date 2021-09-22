e630224d74def8be893249c3ff11522ab0ac6c47
Anuj Bansal <highlightanuj@gmail.com>
2021-09-22 18:13:58 +0530

    Update start scripts for new storage node

--- start.sh
+++ start.sh
@@ -23,28 +23,36 @@ trap down EXIT
 # Run a local development chain
 docker-compose up -d joystream-node
 
-## Storage Infrastructure
-# Configure a dev storage node and start storage node
-DEBUG=joystream:storage-cli:dev yarn storage-cli dev-init
-docker-compose up -d colossus
-# Create a new content directory lead
-yarn workspace api-scripts initialize-content-lead
-
-# Set sudo as the membership screening authority
-yarn workspace api-scripts set-sudo-as-screening-auth
-
 ## Query Node Infrastructure
 # Initialize a new database for the query node infrastructure
 docker-compose up -d db
+
+# Override DB_HOST for db setup
+export DB_HOST=localhost
+
+# Make sure we use dev config for db migrations (prevents "Cannot create database..." and some other errors)
+yarn workspace query-node config:dev
+
+# Migrate the databases
 yarn workspace query-node-root db:prepare
 yarn workspace query-node-root db:migrate
 
-# Startup all query-node infrastructure services
-export WS_PROVIDER_ENDPOINT_URI=ws://joystream-node:9944
-docker-compose up -d graphql-server
-docker-compose up -d processor
+# Set DB_HOST back to docker-service one
+export DB_HOST=db
 
-docker-compose up -d pioneer
+# Start processor and graphql server
+docker-compose up -d processor-mnt
+docker-compose up -d graphql-server-mnt
+
+## Storage Infrastructure
+docker-compose up -d colossus
+# # Create a new content directory lead
+# yarn workspace api-scripts initialize-content-lead
+
+# # Set sudo as the membership screening authority
+# yarn workspace api-scripts set-sudo-as-screening-auth
+
+# docker-compose up -d pioneer
 
 echo "use Ctrl+C to shutdown the development network."
 
