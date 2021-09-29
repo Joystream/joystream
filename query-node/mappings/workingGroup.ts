7d64c94667f72c1929287294692e36793ec7426a
Leszek Wiesner <leszek@jsgenesis.com>
2021-09-29 12:33:10 +0200

    Whitespace fix

--- query-node/mappings/workingGroup.ts
+++ query-node/mappings/workingGroup.ts
@@ -96,12 +96,12 @@ export async function workingGroup_TerminatedLeader({ event, store }: EventConte
 function getWorkerType(event: SubstrateEvent): WorkerType | null {
   if (event.section === 'storageWorkingGroup') {
     return WorkerType.STORAGE
-  } 
-  
+  }
+
   if (event.section === 'gatewayWorkingGroup') {
     return WorkerType.GATEWAY
   }
-  
+
   return null
 }
 
