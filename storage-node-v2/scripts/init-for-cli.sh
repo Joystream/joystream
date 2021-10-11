f3036feebd1ef2637b93fd4230fdae60ba704472
Leszek Wiesner <leszek@jsgenesis.com>
2021-10-11 10:49:07 +0200

    init-for-cli utility script for storage node

---
+++ storage-node-v2/scripts/init-for-cli.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Must be run on the clean development chain.
+
+SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
+cd $SCRIPT_PATH
+
+CLI=../bin/run
+
+${CLI} dev:init
+${CLI} leader:update-bag-limit -l 7 --dev
+${CLI} leader:update-voucher-limits -o 1000 -s 10000000000 --dev
+BUCKET_ID=`${CLI} leader:create-bucket -i=0 -a -n=1000 -s=10000000000  --dev`
+${CLI} operator:accept-invitation -w=0 -i=${BUCKET_ID} --dev
+${CLI} leader:update-bag -a=${BUCKET_ID} -i static:council --dev
+${CLI} leader:update-dynamic-bag-policy -n 1 -t Channel --dev
+${CLI} operator:set-metadata -w=0 -i=${BUCKET_ID} -m=http://localhost:3333/api/v1 --dev
+
+mkdir -p ~/tmp/uploads
+${CLI} server -w 0 -o 3333 -d ~/tmp/uploads --dev
