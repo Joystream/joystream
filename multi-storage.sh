bacf67a99b74c7ed585dbda497de84d9a0a6a9fe
Anuj Bansal <highlightanuj@gmail.com>
2021-10-11 12:47:51 +0530

    Add ports as env vars, update bash script

--- multi-storage.sh
+++ multi-storage.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+# Script to run a second storage node and distributor node on local
+# Make sure to run yarn start prior to running this script
+
+export COLOSSUS_PORT_2=3335
+export DISTRIBUTOR_PORT_2=3336
+
 set -a
 . .env
 set +a
@@ -9,15 +18,16 @@ function down()
     docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml rm -vsf colossus-2
 }
 
+down
+
 trap down EXIT
 
 docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml run -d --name \
-    colossus-2 --entrypoint sh colossus-2 -c "yarn storage-node dev:init --apiUrl ${WS_PROVIDER_ENDPOINT_URI} && \
-        yarn storage-node server --queryNodeHost ${QUERY_NODE_HOST} --port 3335 \
+    colossus-2 --entrypoint sh colossus-2 -c "yarn storage-node server --queryNodeHost ${QUERY_NODE_HOST} --port ${COLOSSUS_PORT_2} \
         --uploads /data --worker ${WORKER_ID} --apiUrl ${WS_PROVIDER_ENDPOINT_URI} --sync --syncInterval=1 \
         --keyFile=${ACCOUNT_KEYFILE} --elasticSearchHost=${ELASTIC_SEARCH_HOST}"
 
-docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml up -d distributor-node-2
+docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml run -d --name distributor-node-2 distributor-node-2
 
 echo "use Ctrl+C to shutdown the development network."
 
