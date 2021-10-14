1db2bebc673b1417a39c11f31fe904d730d4ce38
Anuj Bansal <highlightanuj@gmail.com>
2021-10-14 14:30:04 +0530

    Add buckets, keyfile and workers ids for distributor and storage

--- multi-storage.sh
+++ multi-storage.sh
@@ -4,12 +4,16 @@ set -e
 # Script to run a second storage node and distributor node on local
 # Make sure to run yarn start prior to running this script
 
-export COLOSSUS_PORT_2=3335
-export DISTRIBUTOR_PORT_2=3336
-
 set -a
 . .env
 set +a
+
+export COLOSSUS_PORT_2=3335
+export DISTRIBUTOR_PORT_2=3336
+export KEYS=[//Alice]
+export BUCKETS='all'
+export WORKER_ID=2
+export ACCOUNT_KEYFILE="./types/augment/all/defs.json"
 
 function down()
 {
@@ -22,10 +26,7 @@ down
 
 trap down EXIT
 
-docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml run -d --name \
-    colossus-2 --entrypoint sh colossus-2 -c "yarn storage-node server --queryNodeHost ${QUERY_NODE_HOST} --port ${COLOSSUS_PORT_2} \
-        --uploads /data --worker ${WORKER_ID} --apiUrl ${WS_PROVIDER_ENDPOINT_URI} --sync --syncInterval=1 \
-        --keyFile=${ACCOUNT_KEYFILE} --elasticSearchHost=${ELASTIC_SEARCH_HOST}"
+docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml run -d --name colossus-2 colossus-2
 
 docker-compose -f docker-compose.yml -f docker-compose.multi-storage.yml run -d --name distributor-node-2 distributor-node-2
 
