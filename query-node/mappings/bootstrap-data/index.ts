d6859818e16b66c5045678131d2759c79c712c73
Anuj Bansal <highlightanuj@gmail.com>
2021-10-20 22:48:28 +0530

    Update bootstrap script to upload data to hydra

--- query-node/mappings/bootstrap-data/index.ts
+++ query-node/mappings/bootstrap-data/index.ts
@@ -2,9 +2,11 @@ import { MemberJson, StorageSystemJson, WorkingGroupsJson } from './types'
 import storageSystemJson from './data/storageSystem.json'
 import membersJson from './data/members.json'
 import workingGroupsJson from './data/workingGroups.json'
+import channelCategoriesData from './data/channelCategories.json'
+import videoCategoriesData from './data/videoCategories.json'
 
 const storageSystemData: StorageSystemJson = storageSystemJson
 const membersData: MemberJson[] = membersJson
 const workingGroupsData: WorkingGroupsJson = workingGroupsJson
 
-export { storageSystemData, membersData, workingGroupsData }
+export { storageSystemData, membersData, workingGroupsData, channelCategoriesData, videoCategoriesData }
