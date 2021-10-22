0788cc82b440a888103623f3f822d74bf9874ffb
ignazio-bovo <10487328@polimi.it>
2021-10-22 14:06:44 +0200

    account import

--- tests/network-tests/pre_migration_hook.sh
+++ tests/network-tests/pre_migration_hook.sh
@@ -4,6 +4,8 @@ set -e
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 cd $SCRIPT_PATH
 
+echo "importing accounts"
+yarn joystream-cli account:import ./test-data/initial-members.json
 echo "creating 1 channel with 1 video in it"
 yarn joystream-cli content:createChannel -i ./assets/TestChannel.json
 yarn joystream-cli content:createChannel -c 0 -i ./assets/TestVideo.json
