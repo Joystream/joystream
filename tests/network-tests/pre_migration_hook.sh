88f6a4e3a8d82ca6b48120ee1682234182ede88e
ignazio-bovo <10487328@polimi.it>
2021-10-25 18:39:26 +0200

    edited assets input files

--- tests/network-tests/pre_migration_hook.sh
+++ tests/network-tests/pre_migration_hook.sh
@@ -4,8 +4,9 @@ set -e
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 cd $SCRIPT_PATH
 
-echo "importing accounts"
-yarn joystream-cli account:import ./test-data/initial-members.json
-echo "creating 1 channel with 1 video in it"
-yarn joystream-cli content:createChannel -i ./assets/TestChannel.json
-yarn joystream-cli content:createChannel -c 0 -i ./assets/TestVideo.json
+#echo "importing accounts"
+#yarn joystream-cli account:import ./test-data/initial-members.json
+echo "creating 1 channel"
+yarn joystream-cli content:createChannel --input=./assets/TestChannel.json --context=Member
+echo "adding 1 video to the above channel"
+yarn joystream-cli content:createVideo -c 0 --input=./assets/TestVideo.json
