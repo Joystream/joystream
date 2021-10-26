b6c5ff36f7cdf178ce708354d6b2dd82ce1bdcf7
ignazio-bovo <10487328@polimi.it>
2021-10-26 16:56:20 +0200

    pre migration hook fixed

--- tests/network-tests/pre_migration_hook.sh
+++ tests/network-tests/pre_migration_hook.sh
@@ -4,9 +4,8 @@ set -e
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 cd $SCRIPT_PATH
 
-#echo "importing accounts"
-#yarn joystream-cli account:import ./test-data/initial-members.json
+joystream-cli api:setUri ws://localhost:9944
 echo "creating 1 channel"
-yarn joystream-cli content:createChannel --input=./assets/TestChannel.json --context=Member
+joystream-cli content:createChannel --input=./assets/TestChannel.json
 echo "adding 1 video to the above channel"
-yarn joystream-cli content:createVideo -c 0 --input=./assets/TestVideo.json
+joystream-cli content:createVideo -c 0 --input=./assets/TestVideo.json
