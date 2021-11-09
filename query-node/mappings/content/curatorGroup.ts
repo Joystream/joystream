6b14c07b186700022eec5a9d57313a97a988149a
Leszek Wiesner <leszek@jsgenesis.com>
2021-11-09 15:05:20 +0100

    Update to Hydra 3.1.0-alpha.13 + related temporary fixes

--- query-node/mappings/content/curatorGroup.ts
+++ query-node/mappings/content/curatorGroup.ts
@@ -1,7 +1,7 @@
 /*
 eslint-disable @typescript-eslint/naming-convention
 */
-import { EventContext, StoreContext } from '@dzlzv/hydra-common'
+import { EventContext, StoreContext } from '@joystream/hydra-common'
 import { FindConditions } from 'typeorm'
 import { CuratorGroup } from 'query-node/dist/model'
 import { Content } from '../generated/types'
