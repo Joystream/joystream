145f3a78e230853eb1a407c4d7986fe6733a5373
ignazio <ignazio@hal.homenet.telecomitalia.it>
2021-12-09 14:11:44 +0100

    assertions fix

--- tests/network-tests/src/misc/postMigrationAssertionsFlow.ts
+++ tests/network-tests/src/misc/postMigrationAssertionsFlow.ts
@@ -11,22 +11,18 @@ export default async function postMigrationAssertions({ api }: FlowProps): Promi
     const channel_migration = await api.query.content.channelMigration();
     const video_migration = await api.query.content.videoMigration();
 
-    assert(channel_migration.current_id.toNumber() == channel_migration.final_id.toNumber())
-    assert(video_migration.current_id.toNumber() == video_migration.final_id.toNumber())
+    assert.equal(channel_migration.current_id.toNumber(), channel_migration.final_id.toNumber())
+    assert.equal(video_migration.current_id.toNumber(), video_migration.final_id.toNumber())
 
-    debug('Check all working groups have been correctly created')
+    debug('Check all new  working groups have been correctly initialized')
 
-    const wg_alpha = await api.query.operationsWorkingGroupAlpha.activeWorkerCount();
     const wg_beta = await api.query.operationsWorkingGroupBeta.activeWorkerCount();
     const wg_gamma = await api.query.operationsWorkingGroupGamma.activeWorkerCount();
     const wg_gateway = await api.query.gatewayWorkingGroup.activeWorkerCount();
-    const wg_content = await api.query.contentWorkingGroup.activeWorkerCount();
 
-    assert(wg_alpha.toNumber() === 0);
-    assert(wg_beta.toNumber() === 0);
-    assert(wg_gamma.toNumber() === 0);
-    assert(wg_gateway.toNumber() === 0);
-    assert(wg_content.toNumber() === 0);
+    assert.equal(wg_beta.toNumber(), 0);
+    assert.equal(wg_gamma.toNumber(), 0);
+    assert.equal(wg_gateway.toNumber(), 0);
 
     debug('Checking that Video, Channel, Categories  counters have not been re-set')
 
@@ -43,8 +39,8 @@ export default async function postMigrationAssertions({ api }: FlowProps): Promi
     const num_channels = await api.getNumberOfOutstandingChannels()
     const num_videos = await api.getNumberOfOutstandingVideos()
 
-    assert(num_channels === 0);
-    assert(num_videos === 0);
+    assert.equal(num_channels, 0);
+    assert.equal(num_videos, 0);
 
     debug('Done')
 }
