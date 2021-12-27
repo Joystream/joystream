d12ead57b30759e4591105c8e8c45b746ae2606d
Shamil Gadelshin <shamilgadelshin@gmail.com>
2021-12-27 16:12:52 +0300

    runtime: Fix Bounty LockID
    - add invitation lock compatibility

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -218,6 +218,7 @@ lazy_static! {
         ].to_vec()),
         // Bounty
         (BountyLockId::get(), [
+            InvitedMemberLockId::get(),
             VotingLockId::get(),
             StakingCandidateLockId::get(),
         ].to_vec()),
