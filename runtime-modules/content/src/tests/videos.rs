92bf6f327fa222e1c66e6e9ad8b7ad30e7930cc7
ignazio <ignazio@hal.homenet.telecomitalia.it>
2022-01-03 15:15:54 +0100

    unsuccessful video deletion by curator with auth failure

--- runtime-modules/content/src/tests/videos.rs
+++ runtime-modules/content/src/tests/videos.rs
@@ -1032,3 +1032,23 @@ fn unsuccessful_video_deletion_by_member_with_auth_failure() {
             .call_and_assert(Err(Error::<Test>::MemberAuthFailed.into()));
     })
 }
+
+#[test]
+fn unsuccessful_video_deletion_by_curator_with_auth_failure() {
+    with_default_mock_builder(|| {
+        run_to_block(1);
+
+        create_initial_storage_buckets_helper();
+        increase_account_balance_helper(DEFAULT_CURATOR_ACCOUNT_ID, INITIAL_BALANCE);
+        create_default_curator_owned_channel_with_video();
+
+        let default_curator_group_id = NextCuratorGroupId::<Test>::get() - 1;
+        DeleteVideoFixture::default()
+            .with_sender(UNAUTHORIZED_CURATOR_ACCOUNT_ID)
+            .with_actor(ContentActor::Curator(
+                default_curator_group_id,
+                DEFAULT_MEMBER_ACCOUNT_ID,
+            ))
+            .call_and_assert(Err(Error::<Test>::CuratorAuthFailed.into()));
+    })
+}
