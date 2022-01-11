7411516cfab812fd0f2d75886796c85fa16558c4
ignazio <ignazio@hal.homenet.telecomitalia.it>
2022-01-11 14:47:14 +0100

    unsucc post deletion with invalid post_id/video_id

--- runtime-modules/content/src/tests/posts.rs
+++ runtime-modules/content/src/tests/posts.rs
@@ -534,3 +534,28 @@ pub fn unsuccessful_post_deletion_with_lead_auth_failed() {
             .call_and_assert(Err(Error::<Test>::LeadAuthFailed.into()))
     })
 }
+
+#[test]
+pub fn unsuccessful_post_deletion_with_invalid_post_id() {
+    with_default_mock_builder(|| {
+        run_to_block(1);
+        increase_balance_helper(DEFAULT_MEMBER_ACCOUNT_ID, INITIAL_BALANCE);
+        create_default_member_channel_with_video_and_post();
+
+        DeletePostFixture::default()
+            .with_post_id(PostId::zero())
+            .call_and_assert(Err(Error::<Test>::PostDoesNotExist.into()))
+    })
+}
+#[test]
+pub fn unsuccessful_post_deletion_with_invalid_video_id() {
+    with_default_mock_builder(|| {
+        run_to_block(1);
+        increase_balance_helper(DEFAULT_MEMBER_ACCOUNT_ID, INITIAL_BALANCE);
+        create_default_member_channel_with_video_and_post();
+
+        DeletePostFixture::default()
+            .with_post_id(PostId::zero())
+            .call_and_assert(Err(Error::<Test>::PostDoesNotExist.into()))
+    })
+}
