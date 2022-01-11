f1ea4c59cf739a6c38102a21c90fb01ce1c730e2
ignazio <ignazio@hal.homenet.telecomitalia.it>
2022-01-11 14:33:28 +0100

    succ comment update by actor

--- runtime-modules/content/src/tests/posts.rs
+++ runtime-modules/content/src/tests/posts.rs
@@ -381,3 +381,60 @@ pub fn unsuccessful_comment_update_by_lead_not_author() {
             .call_and_assert(Err(Error::<Test>::ActorNotAuthorized.into()))
     })
 }
+
+#[test]
+pub fn successful_comment_update_by_member() {
+    with_default_mock_builder(|| {
+        run_to_block(1);
+        increase_balance_helper(DEFAULT_MEMBER_ACCOUNT_ID, INITIAL_BALANCE);
+        create_default_member_channel_with_video_and_comment();
+
+        EditPostTextFixture::default()
+            .with_post_id(PostId::from(2u64))
+            .call_and_assert(Ok(()))
+    })
+}
+
+#[test]
+pub fn successful_comment_update_by_curator() {
+    with_default_mock_builder(|| {
+        run_to_block(1);
+        increase_balance_helper(DEFAULT_CURATOR_ACCOUNT_ID, INITIAL_BALANCE);
+        create_default_curator_channel_with_video_and_comment();
+
+        let default_curator_group_id = Content::next_curator_group_id() - 1;
+        EditPostTextFixture::default()
+            .with_sender(DEFAULT_CURATOR_ACCOUNT_ID)
+            .with_actor(ContentActor::Curator(
+                default_curator_group_id,
+                DEFAULT_CURATOR_ID,
+            ))
+            .with_post_id(PostId::from(2u64))
+            .call_and_assert(Ok(()))
+    })
+}
+
+#[test]
+pub fn successful_comment_update_by_lead() {
+    with_default_mock_builder(|| {
+        run_to_block(1);
+        increase_balance_helper(DEFAULT_CURATOR_ACCOUNT_ID, INITIAL_BALANCE);
+        create_default_curator_channel_with_video_and_post();
+
+        increase_balance_helper(LEAD_ACCOUNT_ID, INITIAL_BALANCE);
+        CreatePostFixture::default()
+            .with_sender(LEAD_ACCOUNT_ID)
+            .with_actor(ContentActor::Lead)
+            .with_params(PostCreationParameters::<Test> {
+                post_type: PostType::<Test>::Comment(PostId::one()),
+                video_reference: VideoId::one(),
+            })
+            .call_and_assert(Ok(()));
+
+        EditPostTextFixture::default()
+            .with_sender(LEAD_ACCOUNT_ID)
+            .with_actor(ContentActor::Lead)
+            .with_post_id(PostId::from(2u64))
+            .call_and_assert(Ok(()))
+    })
+}
