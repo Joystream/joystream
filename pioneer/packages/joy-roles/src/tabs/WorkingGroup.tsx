08e8ffe692bc4f863c8d25a412689d91bca53c1e
Edvin Dzidic <dzidic@tugraz.student.at>
2022-01-31 14:25:47 +0100

    Add descriptions for operations groups roles

--- pioneer/packages/joy-roles/src/tabs/WorkingGroup.tsx
+++ pioneer/packages/joy-roles/src/tabs/WorkingGroup.tsx
@@ -76,6 +76,7 @@ type GroupOverviewProps = GroupOverviewOuterProps & {
 interface OperationsGroupProps extends GroupOverviewOuterProps{
   group: WorkingGroups;
   customGroupName?: string;
+  description: string | JSX.Element;
 }
 
 const GroupOverview = Loadable<GroupOverviewProps>(
@@ -149,16 +150,6 @@ export const StorageProviders = (props: GroupOverviewOuterProps) => (
 
 export const OperationsGroup = (props: OperationsGroupProps) => (
   <GroupOverview
-    description={
-      <span>
-        {"Operations Working Group encompases all the activites that don't require privilages on chain, for example:"}
-        <ul>
-          <li>Development</li>
-          <li>Management</li>
-          <li>Marketing</li>
-        </ul>
-      </span>
-    }
     {...props}
   />
 );
@@ -167,6 +158,12 @@ export const OperationsGroupAlpha = (props: GroupOverviewOuterProps) => (
   <OperationsGroup
     group={WorkingGroups.OperationsAlpha}
     customGroupName='Builders'
+    description={
+      <span>
+        The Builders (Operations Group Alpha) working group is comprised of a diverse set of contributors that facilitate
+        the evolution and maintenance of the platform.
+      </span>
+    }
     {...props}
   />
 );
@@ -175,6 +172,12 @@ export const OperationsGroupBeta = (props: GroupOverviewOuterProps) => (
   <OperationsGroup
     group={WorkingGroups.OperationsBeta}
     customGroupName='Human Resources'
+    description={
+      <span>
+        The Human Resources (Operations Group Beta) working group is responsible for the Human Resources tasks required
+        for the operation and growth of the platform and community.
+      </span>
+    }
     {...props}
   />
 );
@@ -183,6 +186,12 @@ export const OperationsGroupGamma = (props: GroupOverviewOuterProps) => (
   <OperationsGroup
     group={WorkingGroups.OperationsGamma}
     customGroupName='Marketing'
+    description={
+      <span>
+        The Marketing (Operations Group Gamma) working group is responsible for developing and executing strategies to
+        promote the Joystream platform.
+      </span>
+    }
     {...props}
   />
 );
