23a386b098b7bf240aff19b64560812f223b5a9c
Leszek Wiesner <leszek@jsgenesis.com>
2022-02-01 16:03:56 +0100

    Remove Pioneer

--- pioneer/packages/joy-utils/src/react/hooks/useMyAccount.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { MyAccountContext } from '../context/account';
-
-export default function useMyAccount () {
-  return useContext(MyAccountContext);
-}
--- pioneer/packages/joy-utils/src/react/hooks/useMyMembership.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { MyMembershipContext } from '../context';
-
-export default function useMyMembership () {
-  return useContext(MyMembershipContext);
-}
