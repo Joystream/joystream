c4a4090d411c0baeca3de1caa8188ccb04c5fa22
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-02-03 00:17:56 +0400

    update test scripts to explicitly define joystream-node image

--- query-node/run-tests.sh
+++ query-node/run-tests.sh
@@ -8,13 +8,15 @@ set -a
 . ../.env
 set +a
 
+export JOYSTREAM_NODE_TAG=${JOYSTREAM_NODE_TAG:=$(../scripts/runtime-code-shasum.sh)}
+
 function cleanup() {
     # Show tail end of logs for the processor and indexer containers to
     # see any possible errors
-    (echo "\n\n## Processor Logs ##" && docker logs joystream_processor_1 --tail 50) || :
-    (echo "\n\n## Indexer Logs ##" && docker logs joystream_indexer_1 --tail 50) || :
-    (echo "\n\n## Indexer API Gateway Logs ##" && docker logs joystream_hydra-indexer-gateway_1 --tail 50) || :
-    (echo "\n\n## Graphql Server Logs ##" && docker logs joystream_graphql-server_1 --tail 50) || :
+    (echo "\n\n## Processor Logs ##" && docker logs processor --tail 50) || :
+    (echo "\n\n## Indexer Logs ##" && docker logs indexer --tail 50) || :
+    (echo "\n\n## Indexer API Gateway Logs ##" && docker logs hydra-indexer-gateway --tail 50) || :
+    (echo "\n\n## Graphql Server Logs ##" && docker logs graphql-server --tail 50) || :
     docker-compose down -v
 }
 
