17ba9506e26302bebca96be453f20d2a035b12db
Leszek Wiesner <leszek@jsgenesis.com>
2022-02-17 16:08:05 +0100

    Integration-tests: Remaining bash script updates

--- tests/integration-tests/run-test-scenario.sh
+++ tests/integration-tests/run-test-scenario.sh
@@ -6,15 +6,8 @@ cd $SCRIPT_PATH
 
 # pass the scenario name without .ts extension
 SCENARIO=$1
-# fallback if scenario if not specified
+# fallback to full.ts scenario if not specified
 SCENARIO=${SCENARIO:=full}
-
-if [ -n "$RUNTIME_UPGRADE_TARGET_IMAGE_TAG" ]; then
-  export RUNTIME_UPGRADE_TARGET_WASM_PATH=${RUNTIME_UPGRADE_TARGET_WASM_PATH:="./target-runtime.wasm"}
-  id=`docker create joystream/node:${RUNTIME_UPGRADE_TARGET_IMAGE_TAG}`
-  docker cp $id:/joystream/runtime.compact.wasm $RUNTIME_UPGRADE_TARGET_WASM_PATH
-  docker rm $id
-fi
 
 # Execute the tests
 time DEBUG=integration-tests:* yarn workspace integration-tests node-ts-strict src/scenarios/${SCENARIO}.ts
