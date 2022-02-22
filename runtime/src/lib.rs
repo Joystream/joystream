69f4ea63c9792da580e495ab0ff17b2dd72a06d1
ignazio <bovoignazio.dev@gmail.com>
2022-02-22 08:45:55 +0100

    added bag deletion prize to runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -453,6 +453,7 @@ parameter_types! {
     pub const BloatBondCap: u32 = 1000;  // TODO: update
     pub const VideosMigrationsEachBlock: u64 = 100;
     pub const ChannelsMigrationsEachBlock: u64 = 25;
+    pub const BagDeletionPrize: Balance = 0; // TODO: update
 }
 
 impl content::Trait for Runtime {
@@ -477,6 +478,7 @@ impl content::Trait for Runtime {
     type ModuleId = ContentModuleId;
     type VideosMigrationsEachBlock = VideosMigrationsEachBlock;
     type ChannelsMigrationsEachBlock = ChannelsMigrationsEachBlock;
+    type BagDeletionPrize = BagDeletionPrize;
 }
 
 // The referendum instance alias.
