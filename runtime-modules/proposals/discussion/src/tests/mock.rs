220ba05e37ae7f7aec625250234f6ced5a0e2221
ignazio <bovoignazio.dev@gmail.com>
2022-03-02 16:22:53 +0100

    tests fixed weight info

--- runtime-modules/proposals/discussion/src/tests/mock.rs
+++ runtime-modules/proposals/discussion/src/tests/mock.rs
@@ -140,6 +140,9 @@ impl membership::WeightInfo for Weights {
     fn remove_staking_account() -> Weight {
         unimplemented!()
     }
+    fn member_remark() -> Weight {
+        unimplemented!()
+    }
 }
 
 impl balances::Trait for Test {
@@ -372,6 +375,12 @@ impl council::WeightInfo for CouncilWeightInfo {
         0
     }
     fn funding_request(_: u32) -> Weight {
+        0
+    }
+    fn councilor_remark() -> Weight {
+        0
+    }
+    fn candidate_remark() -> Weight {
         0
     }
 }
