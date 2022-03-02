7e706efb0536d2c150b3ab2c78904fb8a7fc5fe1
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-03-02 10:05:39 +0400

    keep updateWorkers flow in setupNewChain scenario

--- tests/integration-tests/src/scenarios/setupNewChain.ts
+++ tests/integration-tests/src/scenarios/setupNewChain.ts
@@ -1,4 +1,5 @@
 import leaderSetup from '../flows/working-groups/leadOpening'
+import updateAccountsFlow from '../misc/updateAllWorkerRoleAccountsFlow'
 import initFaucet from '../flows/faucet/initFaucet'
 import initStorage, { singleBucketConfig as defaultStorageConfig } from '../flows/storage/initStorage'
 import initDistribution, { singleBucketConfig as defaultDistributionConfig } from '../flows/storage/initDistribution'
@@ -6,11 +7,12 @@ import { scenario } from '../Scenario'
 
 scenario('Setup new chain', async ({ job }) => {
   const leads = job('Set WorkingGroup Leads', leaderSetup)
+  const updateAccounts = job('Update Worker Accounts', updateAccountsFlow).requires(leads)
 
-  job('Initialize Faucet', initFaucet).requires(leads)
+  job('Initialize Faucet', initFaucet).requires(updateAccounts)
 
   if (!process.env.SKIP_STORAGE_AND_DISTRIBUTION) {
-    job('initialize storage system', initStorage(defaultStorageConfig)).requires(leads)
-    job('initialize distribution system', initDistribution(defaultDistributionConfig)).requires(leads)
+    job('initialize storage system', initStorage(defaultStorageConfig)).requires(updateAccounts)
+    job('initialize distribution system', initDistribution(defaultDistributionConfig)).requires(updateAccounts)
   }
 })
