dbe71f3a1da8cb2d01dca83b8284de52afdb03da
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-03-08 12:29:18 +0400

    bump runtime spec and types versions for rhodes

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node"),
     impl_name: create_runtime_str!("joystream-node"),
     authoring_version: 10,
-    spec_version: 1,
+    spec_version: 3,
     impl_version: 0,
     apis: crate::runtime_api::EXPORTED_RUNTIME_API_VERSIONS,
     transaction_version: 1,
