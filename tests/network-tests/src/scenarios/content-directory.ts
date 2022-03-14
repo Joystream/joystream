f8661fcb6c122f86b5edb85c5ec08e4e9d95ab9e
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-03-14 13:02:45 +0500

    update scenarios

--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -5,8 +5,11 @@ import comments from '../flows/content/comments'
 import { scenario } from '../Scenario'
 
 scenario('Content directory', async ({ job }) => {
-  const leadSetupJob = job('Set content working group leads', leadOpening(true, ['contentWorkingGroup']))
+  const leadSetupJob = job(
+    'Set content working group leads',
+    leadOpening(true, ['contentWorkingGroup', 'storageWorkingGroup'])
+  )
   const videoCountersJob = job('check active video counters', activeVideoCounters).requires(leadSetupJob)
   job('nft auction and offers', nftAuctionAndOffers).after(videoCountersJob)
-  job('add comments on videos', comments)
+  job('add comments and reactions', comments).after(leadSetupJob)
 })
