1d4740953c6507eaf3a9fd19db864b52a002f507
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-03-15 12:32:24 +0000

    validate auction & bids in fixtures

--- tests/network-tests/src/fixtures/content/nft/englishAuctionWithExtension.ts
+++ tests/network-tests/src/fixtures/content/nft/englishAuctionWithExtension.ts
@@ -1,37 +1,34 @@
-import { assert } from 'chai'
+import BN from 'bn.js'
 import { Api } from '../../../Api'
 import { BaseQueryNodeFixture, FixtureRunner } from '../../../Fixture'
-import { JoystreamCLI } from '../../../cli/joystream'
 import { QueryNodeApi } from '../../../QueryNodeApi'
-import { IMember } from '../createMembers'
-import { BuyMembershipHappyCaseFixture } from '../../membership'
-import { PlaceBidsInAuctionFixture } from './placeBidsInAuction'
 import { Utils } from '../../../utils'
+import { IMember } from '../createMembers'
+import { PlaceBidsInAuctionFixture } from './placeBidsInAuction'
 import { assertNftOwner, assertAuctionAndBids } from './utils'
-import BN from 'bn.js'
 
-// settings
-const sufficientTopupAmount = new BN(1000000) // some very big number to cover fees of all transactions
-
-export class NftEnglishAuctionFixture extends BaseQueryNodeFixture {
-  private cli: JoystreamCLI
+export class NftEnglishAuctionWithExtensionFixture extends BaseQueryNodeFixture {
   private videoId: number
   private author: IMember
   private participants: IMember[]
+  private auctionDuration: number
+  private extensionPeriod: number
 
   constructor(
     api: Api,
     query: QueryNodeApi,
-    cli: JoystreamCLI,
     videoId: number,
     author: IMember,
-    participants: IMember[]
+    participants: IMember[],
+    auctionDuration: number,
+    extensionPeriod: number
   ) {
     super(api, query)
-    this.cli = cli
     this.videoId = videoId
     this.author = author
     this.participants = participants
+    this.auctionDuration = auctionDuration
+    this.extensionPeriod = extensionPeriod
   }
 
   /*
@@ -42,13 +39,13 @@ export class NftEnglishAuctionFixture extends BaseQueryNodeFixture {
     await this.api.issueNft(this.author.keyringPair.address, this.author.memberId.toNumber(), this.videoId)
 
     this.debug('Start NFT auction')
-    const {
-      auctionParams,
-      startingPrice,
-      minimalBidStep,
-      auctionDuration,
-      extensionPeriod,
-    } = await this.api.createAuctionParameters('English')
+    const { auctionParams, startingPrice, minimalBidStep } = await this.api.createAuctionParameters(
+      'English',
+      undefined,
+      new BN(this.auctionDuration),
+      new BN(this.extensionPeriod)
+    )
+
     await this.api.startNftAuction(
       this.author.keyringPair.address,
       this.author.memberId.toNumber(),
@@ -70,7 +67,9 @@ export class NftEnglishAuctionFixture extends BaseQueryNodeFixture {
     await new FixtureRunner(placeBidsFixture).run()
 
     this.debug('Wait for auction to end')
-    const waitBlocks = Math.min(auctionDuration.toNumber(), extensionPeriod.toNumber() + this.participants.length) + 1
+
+    const waitBlocks =
+      this.auctionDuration + 1 + (this.participants.length >= this.extensionPeriod ? this.extensionPeriod : 0)
     await Utils.wait(this.api.getBlockDuration().muln(waitBlocks).toNumber())
 
     this.debug('Check NFT Auction and bids')
--- tests/network-tests/src/fixtures/content/nft/utils.ts
+++ tests/network-tests/src/fixtures/content/nft/utils.ts
@@ -4,6 +4,24 @@ import { Utils } from '../../../utils'
 import { assert } from 'chai'
 import { OwnedNftFieldsFragment } from '../../../graphql/generated/queries'
 
+export async function assertAuctionAndBids(query: QueryNodeApi, videoId: number, lastBidder: IMember): Promise<void> {
+  await query.tryQueryWithTimeout(
+    () => query.ownedNftByVideoId(videoId.toString()),
+    (ownedNft) => {
+      Utils.assert(ownedNft, 'NFT not found')
+      Utils.assert(ownedNft.transactionalStatus.__typename === 'TransactionalStatusAuction', 'NFt not in Auction state')
+      Utils.assert(ownedNft.transactionalStatus.auction, 'NFT Auction not found')
+      Utils.assert(ownedNft.transactionalStatus.auction.bids, 'Bids not found')
+      Utils.assert(ownedNft.transactionalStatus.auction.lastBid, 'Last bid not found')
+      assert.equal(
+        ownedNft.transactionalStatus.auction.lastBid.bidder.id,
+        lastBidder.memberId.toString(),
+        'Invalid last bidder'
+      )
+    }
+  )
+}
+
 export async function assertNftOwner(
   query: QueryNodeApi,
   videoId: number,
