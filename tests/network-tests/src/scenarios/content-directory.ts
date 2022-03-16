a746c2c4f540466bafb9b269a7c5775e0eb9bd88
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-03-16 06:07:43 +0000

    revert content-directory scenario change

--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -4,10 +4,7 @@ import nftAuctionAndOffers from '../flows/content/nftAuctionAndOffers'
 import { scenario } from '../Scenario'
 
 scenario('Content directory', async ({ job }) => {
-  const leadSetupJob = job(
-    'Set content working group leads',
-    leadOpening(true, ['contentWorkingGroup', 'storageWorkingGroup'])
-  )
+  const leadSetupJob = job('Set content working group leads', leadOpening(true, ['contentWorkingGroup']))
   const videoCountersJob = job('check active video counters', activeVideoCounters).requires(leadSetupJob)
   job('nft auction and offers', nftAuctionAndOffers).after(videoCountersJob)
 })
