131a7fa02228eb616abb4196f3dd9e77502ddb7d
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-03-22 09:30:34 +0400

    bump runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -103,7 +103,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node-testing"),
     impl_name: create_runtime_str!("joystream-node"),
     authoring_version: 10,
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 0,
     apis: crate::runtime_api::EXPORTED_RUNTIME_API_VERSIONS,
     transaction_version: 1,
