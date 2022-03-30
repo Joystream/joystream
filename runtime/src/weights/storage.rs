74bf05308b38f1fd9ae0b4c9135edd2651d9e545
Shamil Gadelshin <shamilgadelshin@gmail.com>
2022-03-30 19:13:52 +0300

    runtime: storage: Update weights.

--- runtime/src/weights/storage.rs
+++ runtime/src/weights/storage.rs
@@ -8,52 +8,54 @@ use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 pub struct WeightInfo;
 impl storage::WeightInfo for WeightInfo {
     fn delete_storage_bucket() -> Weight {
-        (355_000_000 as Weight)
+        (293_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_uploading_blocked_status() -> Weight {
-        (208_000_000 as Weight)
+        (216_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_data_size_fee() -> Weight {
-        (232_000_000 as Weight)
+        (260_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_storage_buckets_per_bag_limit() -> Weight {
-        (217_000_000 as Weight)
+        (226_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_storage_buckets_voucher_max_limits() -> Weight {
-        (256_000_000 as Weight)
+        (226_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(2 as Weight))
     }
     fn update_number_of_storage_buckets_in_dynamic_bag_creation_policy() -> Weight {
-        (258_000_000 as Weight)
+        (260_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_blacklist(i: u32, j: u32) -> Weight {
-        (463_903_000 as Weight)
-            .saturating_add((76_353_000 as Weight).saturating_mul(i as Weight))
-            .saturating_add((36_361_000 as Weight).saturating_mul(j as Weight))
-            .saturating_add(DbWeight::get().reads(203 as Weight))
+        (34_250_806_000 as Weight)
+            .saturating_add((101_714_000 as Weight).saturating_mul(i as Weight))
+            .saturating_add((48_925_000 as Weight).saturating_mul(j as Weight))
+            .saturating_add(DbWeight::get().reads(3 as Weight))
+            .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
+            .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(j as Weight)))
             .saturating_add(DbWeight::get().writes(1 as Weight))
             .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(i as Weight)))
     }
     fn create_storage_bucket() -> Weight {
-        (309_000_000 as Weight)
+        (606_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(5 as Weight))
             .saturating_add(DbWeight::get().writes(2 as Weight))
     }
     fn update_storage_buckets_for_bag(i: u32, j: u32) -> Weight {
-        (506_116_000 as Weight)
-            .saturating_add((305_206_000 as Weight).saturating_mul(i as Weight))
-            .saturating_add((232_844_000 as Weight).saturating_mul(j as Weight))
+        (0 as Weight)
+            .saturating_add((393_338_000 as Weight).saturating_mul(i as Weight))
+            .saturating_add((324_358_000 as Weight).saturating_mul(j as Weight))
             .saturating_add(DbWeight::get().reads(4 as Weight))
             .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
             .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(j as Weight)))
@@ -62,76 +64,76 @@ impl storage::WeightInfo for WeightInfo {
             .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(j as Weight)))
     }
     fn cancel_storage_bucket_operator_invite() -> Weight {
-        (375_000_000 as Weight)
+        (386_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn invite_storage_bucket_operator() -> Weight {
-        (453_000_000 as Weight)
+        (599_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(4 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn remove_storage_bucket_operator() -> Weight {
-        (360_000_000 as Weight)
+        (489_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_storage_bucket_status() -> Weight {
-        (327_000_000 as Weight)
+        (376_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_storage_bucket_voucher_limits() -> Weight {
-        (359_000_000 as Weight)
+        (744_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(5 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn accept_storage_bucket_invitation() -> Weight {
-        (348_000_000 as Weight)
+        (454_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_storage_operator_metadata(i: u32) -> Weight {
-        (315_108_000 as Weight)
-            .saturating_add((178_000 as Weight).saturating_mul(i as Weight))
+        (325_027_000 as Weight)
+            .saturating_add((211_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(2 as Weight))
     }
     fn accept_pending_data_objects(i: u32) -> Weight {
-        (171_046_000 as Weight)
-            .saturating_add((150_478_000 as Weight).saturating_mul(i as Weight))
+        (852_583_000 as Weight)
+            .saturating_add((166_274_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
             .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(i as Weight)))
     }
     fn create_distribution_bucket_family() -> Weight {
-        (320_000_000 as Weight)
+        (304_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(4 as Weight))
             .saturating_add(DbWeight::get().writes(3 as Weight))
     }
     fn delete_distribution_bucket_family() -> Weight {
-        (381_000_000 as Weight)
+        (404_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(7 as Weight))
             .saturating_add(DbWeight::get().writes(2 as Weight))
     }
     fn create_distribution_bucket() -> Weight {
-        (316_000_000 as Weight)
+        (329_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(2 as Weight))
     }
     fn update_distribution_bucket_status() -> Weight {
-        (374_000_000 as Weight)
+        (382_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn delete_distribution_bucket() -> Weight {
-        (301_000_000 as Weight)
+        (315_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_distribution_buckets_for_bag(i: u32, j: u32) -> Weight {
-        (405_500_000 as Weight)
-            .saturating_add((148_368_000 as Weight).saturating_mul(i as Weight))
-            .saturating_add((158_703_000 as Weight).saturating_mul(j as Weight))
+        (9_111_681_000 as Weight)
+            .saturating_add((182_993_000 as Weight).saturating_mul(i as Weight))
+            .saturating_add((141_955_000 as Weight).saturating_mul(j as Weight))
             .saturating_add(DbWeight::get().reads(5 as Weight))
             .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
             .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(j as Weight)))
@@ -140,60 +142,60 @@ impl storage::WeightInfo for WeightInfo {
             .saturating_add(DbWeight::get().writes((1 as Weight).saturating_mul(j as Weight)))
     }
     fn update_distribution_buckets_per_bag_limit() -> Weight {
-        (213_000_000 as Weight)
+        (255_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_distribution_bucket_mode() -> Weight {
-        (337_000_000 as Weight)
+        (574_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn update_families_in_dynamic_bag_creation_policy(i: u32) -> Weight {
-        (411_271_000 as Weight)
-            .saturating_add((32_829_000 as Weight).saturating_mul(i as Weight))
+        (358_386_000 as Weight)
+            .saturating_add((46_414_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().reads((1 as Weight).saturating_mul(i as Weight)))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn invite_distribution_bucket_operator() -> Weight {
-        (503_000_000 as Weight)
+        (521_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(4 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn cancel_distribution_bucket_operator_invite() -> Weight {
-        (346_000_000 as Weight)
+        (565_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn remove_distribution_bucket_operator() -> Weight {
-        (385_000_000 as Weight)
+        (846_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(3 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_distribution_bucket_family_metadata(i: u32) -> Weight {
-        (261_309_000 as Weight)
-            .saturating_add((191_000 as Weight).saturating_mul(i as Weight))
+        (346_367_000 as Weight)
+            .saturating_add((180_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(3 as Weight))
     }
     fn accept_distribution_bucket_invitation() -> Weight {
-        (483_000_000 as Weight)
+        (1_464_000_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn set_distribution_operator_metadata(i: u32) -> Weight {
-        (300_323_000 as Weight)
-            .saturating_add((201_000 as Weight).saturating_mul(i as Weight))
+        (390_779_000 as Weight)
+            .saturating_add((149_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(2 as Weight))
     }
     fn storage_operator_remark(i: u32) -> Weight {
-        (291_098_000 as Weight)
-            .saturating_add((236_000 as Weight).saturating_mul(i as Weight))
+        (502_563_000 as Weight)
+            .saturating_add((151_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(2 as Weight))
     }
     fn distribution_operator_remark(i: u32) -> Weight {
-        (299_877_000 as Weight)
-            .saturating_add((195_000 as Weight).saturating_mul(i as Weight))
+        (374_922_000 as Weight)
+            .saturating_add((363_000 as Weight).saturating_mul(i as Weight))
             .saturating_add(DbWeight::get().reads(2 as Weight))
     }
 }
