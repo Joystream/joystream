79ae64eb243de73317fcbbce9d6f76996546b756
ignazio <bovoignazio.dev@gmail.com>
2022-04-18 15:35:43 +0200

    removed excess transfers tests for failure with invalid token_id & src

--- runtime-modules/project-token/src/tests/transfer.rs
+++ runtime-modules/project-token/src/tests/transfer.rs
@@ -18,7 +18,7 @@ macro_rules! outputs {
 
 // permissionless transfer tests
 #[test]
-fn permissionless_transfer_fails_with_non_existing_token() {
+fn transfer_fails_with_non_existing_token() {
     let token_id = token!(1);
     let config = GenesisConfigBuilder::new_empty().build();
     let origin = origin!(account!(1));
@@ -32,7 +32,7 @@ fn permissionless_transfer_fails_with_non_existing_token() {
 }
 
 #[test]
-fn permissionless_transfer_fails_with_non_existing_source() {
+fn transfer_fails_with_non_existing_source() {
     let token_id = token!(1);
     let origin = origin!(account!(1));
     let (dst, amount) = (account!(2), balance!(100));
@@ -312,46 +312,6 @@ fn permissionless_transfer_ok_without_change_in_token_supply() {
 }
 
 // multi output
-#[test]
-fn multiout_transfer_fails_with_non_existing_token() {
-    let token_id = token!(1);
-    let src = account!(1);
-    let (dst1, amount1) = (account!(2), balance!(1));
-    let (dst2, amount2) = (account!(3), balance!(1));
-    let outputs = outputs![(dst1, amount1), (dst2, amount2)];
-
-    let config = GenesisConfigBuilder::new_empty().build();
-
-    build_test_externalities(config).execute_with(|| {
-        let result = Token::transfer(origin!(src), token_id, outputs);
-
-        assert_noop!(result, Error::<Test>::TokenDoesNotExist);
-    })
-}
-
-#[test]
-fn multiout_transfer_fails_with_non_existing_source() {
-    let token_id = token!(1);
-    let token_info = TokenDataBuilder::new_empty()
-        .with_transfer_policy(Policy::Permissionless)
-        .build();
-    let src = account!(1);
-    let (dst1, amount1) = (account!(2), balance!(1));
-    let (dst2, amount2) = (account!(3), balance!(1));
-    let outputs = outputs![(dst1, amount1), (dst2, amount2)];
-
-    let config = GenesisConfigBuilder::new_empty()
-        .with_token(token_id, token_info)
-        .with_account(dst1, 0, 0)
-        .with_account(dst2, 0, 0)
-        .build();
-
-    build_test_externalities(config).execute_with(|| {
-        let result = Token::transfer(origin!(src), token_id, outputs);
-
-        assert_noop!(result, Error::<Test>::AccountInformationDoesNotExist);
-    })
-}
 
 #[test]
 fn multiout_transfer_ok_with_non_existing_destination() {
@@ -726,32 +686,6 @@ fn permissioned_transfer_ok_with_event_deposit() {
 }
 
 #[test]
-fn permissioned_transfer_fails_with_invalid_src() {
-    let token_id = token!(1);
-    let amount = balance!(100);
-    let src = account!(1);
-    let (dst1, dst2) = (account!(2), account!(3));
-    let commit = merkle_root![dst1, dst2];
-    let outputs = outputs![(dst1, amount)];
-
-    let token_data = TokenDataBuilder::new_empty()
-        .with_transfer_policy(Policy::Permissioned(commit))
-        .build();
-
-    let config = GenesisConfigBuilder::new_empty()
-        .with_token(token_id, token_data)
-        .with_account(dst1, 0, 0)
-        .with_account(dst2, 0, 0)
-        .build();
-
-    build_test_externalities(config).execute_with(|| {
-        let result = Token::transfer(origin!(src), token_id, outputs);
-
-        assert_noop!(result, Error::<Test>::AccountInformationDoesNotExist);
-    })
-}
-
-#[test]
 fn permissioned_transfer_fails_with_invalid_destination() {
     let token_id = token!(1);
     let amount = balance!(100);
@@ -858,54 +792,6 @@ fn permissioned_transfer_ok_without_change_in_token_supply() {
         let _ = Token::transfer(origin!(src), token_id, outputs);
 
         assert_eq!(Token::token_info_by_id(token_id).supply, balance!(amount),);
-    })
-}
-
-#[test]
-fn permissioned_multi_out_transfer_fails_with_invalid_token_id() {
-    let token_id = token!(1);
-    let src = account!(1);
-    let (dst1, amount1) = (account!(2), balance!(1));
-    let (dst2, amount2) = (account!(3), balance!(1));
-    let outputs = outputs![(dst1, amount1), (dst2, amount2)];
-
-    let config = GenesisConfigBuilder::new_empty()
-        .with_token(token_id, TokenDataBuilder::new_empty().build())
-        .with_account(src, amount1 + amount2, 0)
-        .with_account(dst1, 0, 0)
-        .with_account(dst2, 0, 0)
-        .build();
-
-    build_test_externalities(config).execute_with(|| {
-        let result = Token::transfer(origin!(src), token_id + 1, outputs);
-
-        assert_noop!(result, Error::<Test>::TokenDoesNotExist);
-    })
-}
-
-#[test]
-fn permissioned_multi_out_transfer_fails_with_invalid_source_account() {
-    let token_id = token!(1);
-    let src = account!(1);
-    let (dst1, amount1) = (account!(2), balance!(1));
-    let (dst2, amount2) = (account!(3), balance!(1));
-    let commit = merkle_root![dst1, dst2];
-    let outputs = outputs![(dst1, amount1), (dst2, amount2)];
-
-    let token_data = TokenDataBuilder::new_empty()
-        .with_transfer_policy(Policy::Permissioned(commit))
-        .build();
-
-    let config = GenesisConfigBuilder::new_empty()
-        .with_token(token_id, token_data)
-        .with_account(dst1, 0, 0)
-        .with_account(dst2, 0, 0)
-        .build();
-
-    build_test_externalities(config).execute_with(|| {
-        let result = Token::transfer(origin!(src), token_id, outputs);
-
-        assert_noop!(result, Error::<Test>::AccountInformationDoesNotExist);
     })
 }
 
