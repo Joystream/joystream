d9b4a8a8e3f643303d4d1fd7d96ad314c96a6ea1
ignazio <bovoignazio.dev@gmail.com>
2022-04-27 11:36:01 +0200

    max inflation set

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -385,7 +385,7 @@ impl pallet_session::historical::Trait for Runtime {
 pallet_staking_reward_curve::build! {
     const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
         min_inflation: 0_050_000,
-        max_inflation: 0_750_000,
+        max_inflation: 0_180_000,
         ideal_stake: 0_300_000,
         falloff: 0_050_000,
         max_piece_count: 100,
