96bbf70916c10ecf44e61d42c9b6b16ce2f73322
ignazio <bovoignazio.dev@gmail.com>
2022-05-02 13:38:59 +0200

    added post migration nft value assertions

--- tests/network-tests/src/scenarios/postRuntimeUpdate.ts
+++ tests/network-tests/src/scenarios/postRuntimeUpdate.ts
@@ -1,7 +1,21 @@
 import { scenario } from '../Scenario'
+import { extendDebug } from '../Debugger'
+import BN from 'bn.js'
+import { Utils } from '../utils'
 
 scenario('Post Runtime Upgrade', async ({ job }) => {
-  job('dummy', async () => {
-    return
-  })
+    job('dummy', async () => {
+        const debug = extendDebug('flow:postMigrationNftValuesAssertion')
+        debug('Started')
+
+        debug('Check that post migration NFT value are set')
+
+        let maxNftStartingPrice = await api.getMaxNftStartingPrice()
+        let maxNftBidStep = await api.getMaxNftBidStep()
+
+        assert.equal(maxNftStartingPrice, BN(1000000000000))
+        assert.equal(maxNftBidStep, BN(1000000000000))
+
+        debug('Done')
+    }
 })
