19019d9bd4f37d742bc70b6755b756072cfcea6a
ignazio <bovoignazio.dev@gmail.com>
2022-05-02 14:03:46 +0200

    run prettier

--- tests/network-tests/src/scenarios/postRuntimeUpdate.ts
+++ tests/network-tests/src/scenarios/postRuntimeUpdate.ts
@@ -2,5 +2,5 @@ import { scenario } from '../Scenario'
 import assertNftValues from '../flows/content/assertNftValues'
 
 scenario('Post Runtime Upgrade', async ({ job }) => {
-    job('Verify Nft Values', assertNftValues)
+  job('Verify Nft Values', assertNftValues)
 })
