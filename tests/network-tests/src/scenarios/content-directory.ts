1c7296d38d6bdea0a9a81bcb4d24e8dd179ddf62
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-05-04 00:51:47 +0500

    added playlist flow & scenario

--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -1,10 +1,12 @@
 import leadOpening from '../flows/working-groups/leadOpening'
 import activeVideoCounters from '../flows/content/activeVideoCounters'
 import nftAuctionAndOffers from '../flows/content/nftAuctionAndOffers'
+import chanelPlaylists from '../flows/content/channelPlaylists'
 import { scenario } from '../Scenario'
 
 scenario('Content directory', async ({ job }) => {
   const leadSetupJob = job('Set content working group leads', leadOpening(true, ['contentWorkingGroup']))
   const videoCountersJob = job('check active video counters', activeVideoCounters).requires(leadSetupJob)
-  job('nft auction and offers', nftAuctionAndOffers).after(videoCountersJob)
+  job('channel playlists', chanelPlaylists).after(videoCountersJob)
+  job('nft auction and offers', nftAuctionAndOffers).after(leadSetupJob)
 })
