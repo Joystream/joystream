4d90ce1be65fac80ed48ccb8abbf01a9911d1dd4
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-06-20 13:18:52 +0500

    update channel & video CLI commands

--- cli/src/commands/content/channels.ts
+++ cli/src/commands/content/channels.ts
@@ -1,6 +1,6 @@
 import ContentDirectoryCommandBase from '../../base/ContentDirectoryCommandBase'
 // import chalk from 'chalk'
-import { displayTable, shortAddress } from '../../helpers/display'
+import { displayTable } from '../../helpers/display'
 
 export default class ChannelsCommand extends ContentDirectoryCommandBase {
   static description = 'List existing content directory channels.'
@@ -17,10 +17,7 @@ export default class ChannelsCommand extends ContentDirectoryCommandBase {
         channels.map(([id, c]) => ({
           'ID': id.toString(),
           'Owner': JSON.stringify(c.owner.toJSON()),
-          'IsCensored': c.is_censored.toString(),
-          'RewardAccount': c.reward_account ? shortAddress(c.reward_account.toString()) : 'NONE',
           'Collaborators': c.collaborators.size,
-          'Moderators': c.moderators.size,
         })),
         3
       )
