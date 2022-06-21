849e371b353bc3709851ef4d40d9eb11666b80d3
L3pereira <pereiral3p@hotmail.com>
2022-06-21 12:47:43 +0100

    requests + types + metadata

--- types/augment/all/types.ts
+++ types/augment/all/types.ts
@@ -830,9 +830,6 @@ export interface OwnedNft extends Struct {
   readonly open_auctions_nonce: OpenAuctionId;
 }
 
-/** @name ParticipantId */
-export interface ParticipantId extends u64 {}
-
 /** @name PausableChannelFeature */
 export interface PausableChannelFeature extends Enum {
   readonly isChannelFundsTransfer: boolean;
@@ -1088,23 +1085,6 @@ export interface ReferendumStageVoting extends Struct {
   readonly current_cycle_id: u64;
 }
 
-/** @name Reply */
-export interface Reply extends Struct {
-  readonly text_hash: Hash;
-  readonly owner: ParticipantId;
-  readonly parent_id: PostId;
-}
-
-/** @name ReplyId */
-export interface ReplyId extends u64 {}
-
-/** @name ReplyToDelete */
-export interface ReplyToDelete extends Struct {
-  readonly post_id: PostId;
-  readonly reply_id: ReplyId;
-  readonly hide: bool;
-}
-
 /** @name RewardPaymentType */
 export interface RewardPaymentType extends Enum {
   readonly isMissedReward: boolean;
@@ -1235,9 +1215,6 @@ export interface ThreadOf extends Struct {
   readonly number_of_posts: u64;
 }
 
-/** @name Title */
-export interface Title extends Text {}
-
 /** @name TokenAllocation */
 export interface TokenAllocation extends Struct {
   readonly amount: u128;
@@ -1323,12 +1300,6 @@ export interface UpdateChannelPayoutsParameters extends Struct {
   readonly max_cashout_allowed: Option<u128>;
   readonly channel_cashouts_enabled: Option<bool>;
 }
-
-/** @name UpdatedBody */
-export interface UpdatedBody extends Option<Text> {}
-
-/** @name UpdatedTitle */
-export interface UpdatedTitle extends Option<Text> {}
 
 /** @name UploadContent */
 export interface UploadContent extends Struct {
