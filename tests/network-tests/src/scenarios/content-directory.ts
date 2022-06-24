95aa13faac1d15c86f5dfb239ad37212ed2319dc
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-06-24 13:34:43 +0500

    added curator moderation actions flow

--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -2,10 +2,12 @@ import leadOpening from '../flows/working-groups/leadOpening'
 import activeVideoCounters from '../flows/content/activeVideoCounters'
 import nftAuctionAndOffers from '../flows/content/nftAuctionAndOffers'
 import { scenario } from '../Scenario'
+import curatorModerationActions from '../flows/content/curatorModerationActions'
 
 scenario('Content directory', async ({ job }) => {
   const leadSetupJob = job('Set content working group leads', leadOpening(true, ['contentWorkingGroup']))
   // TODO: adjust test after https://github.com/Joystream/joystream/issues/3574 has been implemented
   // const videoCountersJob = job('check active video counters', activeVideoCounters).requires(leadSetupJob)
-  job('nft auction and offers', nftAuctionAndOffers).requires(leadSetupJob)
+  // job('nft auction and offers', nftAuctionAndOffers).requires(leadSetupJob)
+  job('curator moderation actions', curatorModerationActions).requires(leadSetupJob)
 })
