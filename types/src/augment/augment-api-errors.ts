b3272e03f1467fc597f054bccf6228ffb2093cd2
ignazio <bovoignazio.dev@gmail.com>
2022-07-01 16:42:00 +0200

    generate types and metadata

--- types/src/augment/augment-api-errors.ts
+++ types/src/augment/augment-api-errors.ts
@@ -227,6 +227,14 @@ declare module '@polkadot/api-base/types/errors' {
        **/
       ChannelStateBloatBondChanged: AugmentedError<ApiType>;
       /**
+       * Channel Transfers are blocked during revenue splits
+       **/
+      ChannelTransfersBlockedDuringRevenueSplits: AugmentedError<ApiType>;
+      /**
+       * Channel Transfers are blocked during token sales
+       **/
+      ChannelTransfersBlockedDuringTokenSales: AugmentedError<ApiType>;
+      /**
        * Creator token was already issued for this channel
        **/
       CreatorTokenAlreadyIssued: AugmentedError<ApiType>;
