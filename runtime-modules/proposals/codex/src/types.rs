e00243f5b178d57be3eb33b606d37effceb6cc2c
ignazio <bovoignazio.dev@gmail.com>
2022-07-04 16:16:41 +0200

    cargo format

--- runtime-modules/proposals/codex/src/types.rs
+++ runtime-modules/proposals/codex/src/types.rs
@@ -113,7 +113,6 @@ pub enum ProposalDetails<
     // TODO: enable after Carthage
     // /// `Update global NFT limit` proposal
     // UpdateGlobalNftLimit(NftLimitPeriod, u64),
-
     /// `Update Channel Payouts` proposal
     UpdateChannelPayouts(UpdateChannelPayoutsParameters),
 }
