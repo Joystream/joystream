870e44b61bfa6b4d04800c1ce18387b097d43810
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-07-23 21:12:06 +0300

    double semicolon

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -852,7 +852,7 @@ parameter_types! {
     pub const MaxSaltLength: u64 = 32;
     pub const VoteStageDuration: BlockNumber = 20;
     pub const RevealStageDuration: BlockNumber = 20;
-    pub const MinimumVotingStake: Balance = 10 * currency::DOLLARS;;
+    pub const MinimumVotingStake: Balance = 10 * currency::DOLLARS;
 
     // council parameteres
     pub const MinNumberOfExtraCandidates: u64 = 1;
