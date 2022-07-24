633f000138ed84f6408b97711e27f3c7355814f1
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-07-25 00:01:16 +0300

    content-directory scenario requires storage working group lead

--- tests/network-tests/src/scenarios/content-directory.ts
+++ tests/network-tests/src/scenarios/content-directory.ts
@@ -1,5 +1,4 @@
 import leadOpening from '../flows/working-groups/leadOpening'
-import activeVideoCounters from '../flows/content/activeVideoCounters'
 import nftAuctionAndOffers from '../flows/content/nftAuctionAndOffers'
 import commentsAndReactions from '../flows/content/commentsAndReactions'
 import { scenario } from '../Scenario'
@@ -7,7 +6,10 @@ import curatorModerationActions from '../flows/content/curatorModerationActions'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 scenario('Content directory', async ({ job }) => {
-  const leadSetupJob = job('Set content working group leads', leadOpening(true, ['contentWorkingGroup']))
+  const leadSetupJob = job(
+    'Set content working group leads',
+    leadOpening(true, ['contentWorkingGroup', 'storageWorkingGroup'])
+  )
   job('nft auction and offers', nftAuctionAndOffers).requires(leadSetupJob)
   job('curator moderation actions', curatorModerationActions).requires(leadSetupJob)
   job('video comments and reactions', commentsAndReactions).after(leadSetupJob)
