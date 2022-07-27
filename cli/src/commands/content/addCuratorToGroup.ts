6b7eafeb7d734803bb69b306f594e7f32066af20
Zeeshan Akram <97m.zeeshan@gmail.com>
2022-07-27 14:28:12 +0500

    update curator commands

--- cli/src/commands/content/addCuratorToGroup.ts
+++ cli/src/commands/content/addCuratorToGroup.ts
@@ -1,5 +1,8 @@
-import ContentDirectoryCommandBase from '../../base/ContentDirectoryCommandBase'
+import { createType, keysOf } from '@joystream/types'
+import { flags } from '@oclif/command'
+import { PalletContentChannelActionPermission as ChannelActionPermission } from '@polkadot/types/lookup'
 import chalk from 'chalk'
+import ContentDirectoryCommandBase from '../../base/ContentDirectoryCommandBase'
 
 export default class AddCuratorToGroupCommand extends ContentDirectoryCommandBase {
   static description = 'Add Curator to existing Curator Group.'
@@ -17,13 +20,32 @@ export default class AddCuratorToGroupCommand extends ContentDirectoryCommandBas
   ]
 
   static flags = {
+    permissions: flags.build({
+      parse: (value: string) => {
+        const arr = value.split(/[, ]+/).map((v) => {
+          const possibleActions = keysOf<ChannelActionPermission, 'PalletContentChannelActionPermission'>(
+            'PalletContentChannelActionPermission'
+          )
+          if (!possibleActions.includes(v as ChannelActionPermission['type'])) {
+            throw new Error(`Expected comma-separated permissions[${possibleActions}], but received: ${value}`)
+          }
+          return v as ChannelActionPermission['type']
+        })
+        return arr
+      },
+    })({
+      char: 'p',
+      description: `Comma separated list of permissions to associate with the curator`,
+      required: true,
+    }),
     ...ContentDirectoryCommandBase.flags,
   }
 
   async run(): Promise<void> {
-    // const lead = await this.getRequiredLeadContext()
+    const lead = await this.getRequiredLeadContext()
 
     let { groupId, curatorId } = this.parse(AddCuratorToGroupCommand).args
+    const { permissions } = this.parse(AddCuratorToGroupCommand).flags
 
     if (groupId === undefined) {
       groupId = await this.promptForCuratorGroup()
@@ -37,11 +59,11 @@ export default class AddCuratorToGroupCommand extends ContentDirectoryCommandBas
       await this.getCurator(curatorId)
     }
 
-    // TODO: fix in CLI Update work - https://github.com/Joystream/joystream/issues/3689
-    // await this.sendAndFollowNamedTx(await this.getDecodedPair(lead.roleAccount), 'content', 'addCuratorToGroup', [
-    //   groupId,
-    //   curatorId,
-    // ])
+    await this.sendAndFollowNamedTx(await this.getDecodedPair(lead.roleAccount), 'content', 'addCuratorToGroup', [
+      groupId,
+      curatorId,
+      createType('BTreeSet<PalletContentChannelActionPermission>', permissions),
+    ])
 
     console.log(
       chalk.green(
