939f57b572a2130fccccd2a2c988d91326cbbe8b
ignazio-bovo <bovoignazio.dev@gmail.com>
2022-07-28 16:33:20 +0200

    add channel payouts and withdrawals part

--- runtime-modules/content/src/benchmarks/benchmarking.rs
+++ runtime-modules/content/src/benchmarks/benchmarking.rs
@@ -3,13 +3,16 @@
 use crate::permissions::*;
 use crate::types::{
     ChannelOwner, ChannelTransferStatus, InitTransferParametersOf, PendingTransfer,
-    TransferCommitmentParameters, TransferCommitmentWitnessOf,
+    TransferCommitmentParameters, TransferCommitmentWitnessOf, ModuleAccount,
 };
 use crate::Module as Pallet;
+use storage::Pallet as Storage;
+use balances::Pallet as Balances;
 use crate::{Call, ChannelById, Config, Event};
 use frame_benchmarking::benchmarks;
 use frame_support::storage::StorageMap;
 use frame_support::traits::Get;
+use sp_runtime::traits::Hash;
 use frame_system::RawOrigin;
 use sp_arithmetic::traits::{One, Saturating};
 use sp_runtime::SaturatedConversion;
@@ -301,6 +304,67 @@ benchmarks! {
             ).into()
         );
     }
+
+    // ================================================================================
+    // ======================== CHANNEL PAYOUTS & WITHDRAWALS =========================
+    // ================================================================================
+
+    update_channel_payouts {
+        let origin = RawOrigin::Root;
+        let (account_id, _) = member_funded_account::<T>(1);
+        let hash = <<T as frame_system::Config>::Hashing as Hash>::hash(&"test".encode());
+        let params = crate::UpdateChannelPayoutsParameters::<T> {
+            commitment: Some(hash.clone()),
+            payload: Some(crate::ChannelPayoutsPayloadParameters::<T>{
+                uploader_account: account_id,
+                object_creation_params: storage::DataObjectCreationParameters {
+                    size: 1u64,
+                    ipfs_content_id: 1u32.to_be_bytes().as_slice().to_vec(),
+                },
+                expected_data_object_state_bloat_bond: Storage::<T>::data_object_state_bloat_bond_value(),
+                expected_data_size_fee: Storage::<T>::data_object_per_mega_byte_fee(),
+            }),
+            min_cashout_allowed: Some(<T as balances::Config>::Balance::one()),
+            max_cashout_allowed: Some(<T as balances::Config>::Balance::from(1_000_000u32)),
+            channel_cashouts_enabled: Some(true),
+        };
+    }: _ (origin, params)
+    verify {
+        assert_eq!(
+            Pallet::<T>::commitment(),
+            hash,
+        );
+    }
+
+    withdraw_from_channel_balance {
+        let lead_account = insert_content_leader::<T>();
+        let group_id = setup_worst_case_curator_group_with_curators::<T>(
+            T::MaxNumberOfCuratorsPerGroup::get()
+        )?;
+        let group = Pallet::<T>::curator_group_by_id(group_id);
+
+        let (channel_owner_account_id, channel_owner_member_id) = member_funded_account::<T>(DEFAULT_MEMBER_ID);
+        let origin = RawOrigin::Signed(channel_owner_account_id.clone());
+        let owner = crate::ChannelOwner::Member(channel_owner_member_id);
+        let channel_id = setup_worst_case_scenario_curator_channel::<T>(false);
+        Pallet::<T>::set_channel_paused_features_as_moderator(
+            RawOrigin::Signed(lead_account).into(),
+            crate::ContentActor::Lead,
+            channel_id,
+            super::all_channel_pausable_features_except(crate::PausableChannelFeature::ChannelFundsTransfer),
+            b"reason".to_vec(),
+        ).unwrap();
+
+        let channel_account_id = crate::ContentTreasury::<T>::account_for_channel(channel_id);
+        let amount = <T as balances::Config>::Balance::from(100u32);
+        let _ = Balances::<T>::deposit_creating(&channel_account_id, amount + T::ExistentialDeposit::get());
+
+        let balance_pre = Balances::<T>::usable_balance(&channel_owner_account_id);
+        let actor = crate::ContentActor::Member(channel_owner_member_id);
+    }: _ (origin, actor, channel_id, amount)
+        verify {
+            assert_eq!(Balances::<T>::usable_balance(channel_owner_account_id), balance_pre.saturating_add(amount));
+        }
 }
 
 #[cfg(test)]
@@ -370,4 +434,19 @@ pub mod tests {
             assert_ok!(Content::test_benchmark_accept_channel_transfer());
         })
     }
+
+    #[test]
+    fn update_channel_payouts() {
+        with_default_mock_builder(|| {
+            assert_ok!(Content::test_benchmark_update_channel_payouts());
+        })
+    }
+
+    #[test]
+    fn withdraw_from_channel_balance() {
+        with_default_mock_builder(|| {
+            assert_ok!(Content::test_benchmark_withdraw_from_channel_balance());
+        })
+    }
+
 }
