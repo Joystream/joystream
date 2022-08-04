f552e6e3c367a0783fccddad91b5513776ccb28f
Lezek123 <leszek@jsgenesis.com>
2022-08-04 11:09:30 +0200

    Use "our" weights for pallet_multisig

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1453,7 +1453,7 @@ impl pallet_multisig::Config for Runtime {
     type DepositBase = DepositBase;
     type DepositFactor = DepositFactor;
     type MaxSignatories = MaxSignatories;
-    type WeightInfo = pallet_multisig::weights::SubstrateWeight<Runtime>;
+    type WeightInfo = weights::pallet_multisig::SubstrateWeight<Runtime>;
 }
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
