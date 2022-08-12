4dfd99b336c4b3e98b3267060f7dd40463120765
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-08-12 17:16:21 +0300

    adjust validator's bonding duration

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -535,7 +535,7 @@ pallet_staking_reward_curve::build! {
 parameter_types! {
     pub const SessionsPerEra: sp_staking::SessionIndex = 6;
     pub const BondingDuration: sp_staking::EraIndex = BONDING_DURATION;
-    pub const SlashDeferDuration: sp_staking::EraIndex = BONDING_DURATION - 1;
+    pub const SlashDeferDuration: sp_staking::EraIndex = SLASH_DEFER_DURATION;
     pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
     pub const MaxNominatorRewardedPerValidator: u32 = 256;
     pub const OffendingValidatorsThreshold: Perbill = Perbill::from_percent(17);
