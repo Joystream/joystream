2380c1b0e1f88fde8e9f322badf2e39ea076b637
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-09-01 11:23:02 +0400

    configure BondingRestriction

--- node/src/chain_spec/mod.rs
+++ node/src/chain_spec/mod.rs
@@ -28,7 +28,7 @@ pub mod storage_config;
 use grandpa_primitives::AuthorityId as GrandpaId;
 
 use node_runtime::{
-    constants::currency::{ENDOWMENT, STASH},
+    constants::currency::{ENDOWMENT, MIN_NOMINATOR_BOND, MIN_VALIDATOR_BOND, STASH},
     wasm_binary_unwrap, AuthorityDiscoveryConfig, BabeConfig, BalancesConfig, Block, ContentConfig,
     GrandpaConfig, ImOnlineConfig, MaxNominations, SessionConfig, SessionKeys, StakerStatus,
     StakingConfig, StorageConfig, SudoConfig, SystemConfig, TransactionPaymentConfig,
@@ -209,6 +209,20 @@ pub fn testnet_genesis(
         }))
         .collect::<Vec<_>>();
 
+    // staking benchmakrs is not sensitive to actual value of min bonds so
+    // accounts are not funded with sufficient funds and fail with InsufficientBond err
+    // so for benchmarks we set min bond to zero.
+    let min_nominator_bond = if cfg!(feature = "runtime-benchmarks") {
+        0
+    } else {
+        MIN_NOMINATOR_BOND
+    };
+    let min_validator_bond = if cfg!(feature = "runtime-benchmarks") {
+        0
+    } else {
+        MIN_VALIDATOR_BOND
+    };
+
     GenesisConfig {
         system: SystemConfig {
             code: wasm_binary_unwrap().to_vec(),
@@ -243,6 +257,8 @@ pub fn testnet_genesis(
             invulnerables: initial_authorities.iter().map(|x| x.0.clone()).collect(),
             slash_reward_fraction: Perbill::from_percent(10),
             stakers,
+            min_nominator_bond,
+            min_validator_bond,
             ..Default::default()
         },
         sudo: SudoConfig {
