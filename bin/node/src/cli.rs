ef4614ab3442655d906c8d1020f744d68e2b0997
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-09-02 15:26:42 +0400

    remove TryRuntime subcommand

--- bin/node/src/cli.rs
+++ bin/node/src/cli.rs
@@ -52,14 +52,6 @@ pub enum Subcommand {
     #[clap(subcommand)]
     Benchmark(frame_benchmarking_cli::BenchmarkCmd),
 
-    /// Try some command against runtime state.
-    #[cfg(feature = "try-runtime")]
-    TryRuntime(try_runtime_cli::TryRuntimeCmd),
-
-    /// Try some command against runtime state. Note: `try-runtime` feature must be enabled.
-    #[cfg(not(feature = "try-runtime"))]
-    TryRuntime,
-
     /// Key management cli utilities
     #[clap(subcommand)]
     Key(sc_cli::KeySubcommand),
