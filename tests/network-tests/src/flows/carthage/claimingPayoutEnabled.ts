b8b53946aa46f23fff4db4626b89bb055a386011
ignazio-bovo <bovoignazio.dev@gmail.com>
2022-09-27 14:41:58 -0400

    fix: formatting

--- tests/network-tests/src/flows/carthage/claimingPayoutEnabled.ts
+++ tests/network-tests/src/flows/carthage/claimingPayoutEnabled.ts
@@ -18,34 +18,45 @@ export default async function claimingPayoutsEnabled({ api, query, env }: FlowPr
 
   // create n accounts
   const stakerAccounts = (await api.createKeyPairs(nAccounts)).map(({ key }) => key.address)
-  const authorities = await api.getSessionAuthorities()
-
-  // such accounts becomes stakers
-  (await Promise.all(
-    stakerAccounts.map(async (account) => {
-      const bondingSucceedsFixture = new BondingSucceedsFixture(api, {
-        stash: account,
-        controller: account,
-        bondAmount: bondAmount,
-      })
-      const fixtureRunner = new FixtureRunner(bondingSucceedsFixture)
-      fixtureRunner.run()
-    })
-  )).map(() => {})
+  const authorities = await api
+    .getSessionAuthorities()
+    (
+      // such accounts becomes stakers
+      await Promise.all(
+        stakerAccounts.map(async (account) => {
+          const bondingSucceedsFixture = new BondingSucceedsFixture(api, {
+            stash: account,
+            controller: account,
+            bondAmount: bondAmount,
+          })
+          const fixtureRunner = new FixtureRunner(bondingSucceedsFixture)
+          fixtureRunner.run()
+        })
+      )
+    )
+    .map(() => {})
 
   const previousBalances = await Promise.all(stakerAccounts.map((account) => api.getBalance(account)))
 
   // wait k = 10 blocks
-  await api.untilBlock(nBlocks).then(() => {})
-
-  // attempt to claim payout for ALL validators
-  (await Promise.all(
-    stakerAccounts.concat(authorities).map(async (account) => {
-      const claimingPayoutStakersSucceedsFixture = new ClaimingPayoutStakersSucceedsFixture(api, account, claimingEra)
-      const fixtureRunner = new FixtureRunner(claimingPayoutStakersSucceedsFixture)
-      fixtureRunner.run()
-    })
-  )).map(() => {})
+  await api
+    .untilBlock(nBlocks)
+    .then(() => {})
+    (
+      // attempt to claim payout for ALL validators
+      await Promise.all(
+        stakerAccounts.concat(authorities).map(async (account) => {
+          const claimingPayoutStakersSucceedsFixture = new ClaimingPayoutStakersSucceedsFixture(
+            api,
+            account,
+            claimingEra
+          )
+          const fixtureRunner = new FixtureRunner(claimingPayoutStakersSucceedsFixture)
+          fixtureRunner.run()
+        })
+      )
+    )
+    .map(() => {})
 
   // each payout (positive pnumber) must be zero iff the sum is zero
   const currentBalances = await Promise.all(stakerAccounts.map((account) => api.getBalance(account)))
--- tests/network-tests/src/flows/carthage/claimingPayoutsDisabled.ts
+++ tests/network-tests/src/flows/carthage/claimingPayoutsDisabled.ts
@@ -23,35 +23,43 @@ export default async function claimingPayoutsDisabled({ api, query, env }: FlowP
   const authorities = (await api.getSessionAuthorities()).map((account) => account.toString())
 
   const previousBalances = await Promise.all(stakerAccounts.map((account) => api.getBalance(account)))
-
-  // such accounts becomes stakers
-  (await Promise.all(
-    stakerAccounts.map(async (account) => {
-      const bondingSucceedsFixture = new BondingSucceedsFixture(api, {
-        stash: account,
-        controller: account,
-        bondAmount: bondAmount,
+  (
+    // such accounts becomes stakers
+    await Promise.all(
+      stakerAccounts.map(async (account) => {
+        const bondingSucceedsFixture = new BondingSucceedsFixture(api, {
+          stash: account,
+          controller: account,
+          bondAmount: bondAmount,
+        })
+        const fixtureRunner = new FixtureRunner(bondingSucceedsFixture)
+        fixtureRunner.run()
       })
-      const fixtureRunner = new FixtureRunner(bondingSucceedsFixture)
-      fixtureRunner.run()
-    })
-  )).map(() => {})
+    )
+  ).map(() => {})
 
   // wait k = 10 blocks
-  await api.untilBlock(nBlocks).then(() => {})
-
-  // attempt to claim payout for ALL validators
-  (await Promise.all(
-    stakerAccounts.concat(authorities).map(async (account) => {
-      const claimingPayoutStakersSucceedsFixture = new ClaimingPayoutStakersSucceedsFixture(api, account, claimingEra)
-      const fixtureRunner = new FixtureRunner(claimingPayoutStakersSucceedsFixture)
-      fixtureRunner.run()
-    })
-  )).map(() => {})
+  await api
+    .untilBlock(nBlocks)
+    .then(() => {})
+    (
+      // attempt to claim payout for ALL validators
+      await Promise.all(
+        stakerAccounts.concat(authorities).map(async (account) => {
+          const claimingPayoutStakersSucceedsFixture = new ClaimingPayoutStakersSucceedsFixture(
+            api,
+            account,
+            claimingEra
+          )
+          const fixtureRunner = new FixtureRunner(claimingPayoutStakersSucceedsFixture)
+          fixtureRunner.run()
+        })
+      )
+    )
+    .map(() => {})
 
   const currentBalances = await Promise.all(stakerAccounts.map((account) => api.getBalance(account)))
 
   // previous balances is equal to current balances
   expect(previousBalances).to.be.deep.equal(currentBalances)
-
 }
