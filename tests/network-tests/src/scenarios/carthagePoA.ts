3a1accb31b411e37cff1489f36bb518f1939c6a8
ignazio-bovo <bovoignazio.dev@gmail.com>
2022-10-03 06:20:14 -0400

    chore: test cleanup & formatting

--- tests/network-tests/src/scenarios/carthagePoA.ts
+++ tests/network-tests/src/scenarios/carthagePoA.ts
@@ -1,5 +1,4 @@
 import bondingSucceedsInPoA from '../flows/carthage/bondingSucceedsInPoA'
-// import claimingPayoutsDisabled from '../flows/carthage/claimingPayoutsDisabled'
 import nominateSucceedsInPoA from '../flows/carthage/nominateSucceedsInPoA'
 import validateSucceedsInPoA from '../flows/carthage/validateSucceedsInPoA'
 import constantAuthorities from '../flows/carthage/constantAuthorities'
@@ -10,7 +9,6 @@ import nextActiveEraIsNoneInPoA from '../flows/carthage/nextActiveEraIsNoneInPoA
 scenario('Carthage PoA setup', async ({ job }) => {
   job('authority set is constant', constantAuthorities)
   job('next active era is none', nextActiveEraIsNoneInPoA)
-  // job('claiming payout disabled in PoA', claimingPayoutsDisabled)
   job('nominate succeeds in PoA', nominateSucceedsInPoA)
   job('validate succeeds in PoA', validateSucceedsInPoA)
   job('bonding succeeds in PoA', bondingSucceedsInPoA)
