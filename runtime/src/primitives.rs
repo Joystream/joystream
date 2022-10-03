d4d812701329c85b5cd9ccdc168bfe3296c08d15
ignazio-bovo <bovoignazio.dev@gmail.com>
2022-10-03 15:08:39 -0400

    fix: CI errors

--- runtime/src/primitives.rs
+++ runtime/src/primitives.rs
@@ -1,7 +1,7 @@
 //! Low-level types used throughout the Substrate code.
 
 #![warn(missing_docs)]
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"))]
 
 use sp_runtime::{
     traits::{Convert, IdentifyAccount, Verify},
