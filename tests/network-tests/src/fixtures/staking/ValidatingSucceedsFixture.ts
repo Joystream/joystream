a92759ee5da36adf4f20a7ff67cb51a0a3fd0996
Lezek123 <leszek@jsgenesis.com>
2022-10-13 15:44:06 +0200

    A few POA->NPoS tests adjustments

--- tests/network-tests/src/fixtures/staking/ValidatingSucceedsFixture.ts
+++ tests/network-tests/src/fixtures/staking/ValidatingSucceedsFixture.ts
@@ -1,22 +1,45 @@
-import { PalletStakingValidatorPrefs } from '@polkadot/types/lookup'
+import { JoystreamNodeRuntimeSessionKeys, PalletStakingValidatorPrefs } from '@polkadot/types/lookup'
+import { assert } from 'chai'
 import { Api } from '../../Api'
 import { BaseFixture } from '../../Fixture'
 
 export class ValidatingSucceedsFixture extends BaseFixture {
   protected preferences: PalletStakingValidatorPrefs
   protected controller: string
+  protected sessionKeys: JoystreamNodeRuntimeSessionKeys | undefined
 
-  public constructor(api: Api, preferences: PalletStakingValidatorPrefs, controller: string) {
+  public constructor(
+    api: Api,
+    preferences: PalletStakingValidatorPrefs,
+    controller: string,
+    sessionKeys?: JoystreamNodeRuntimeSessionKeys
+  ) {
     super(api)
     this.controller = controller
     this.preferences = preferences
+    this.sessionKeys = sessionKeys
   }
 
   async execute(): Promise<void> {
-    const validateTx = this.api.tx.staking.validate(this.preferences)
-    const fees = await this.api.estimateTxFee(validateTx, this.controller)
-    await this.api.treasuryTransferBalance(this.controller, fees)
+    const transactions = [this.api.tx.staking.validate(this.preferences)]
+    if (this.sessionKeys) {
+      transactions.push(this.api.tx.session.setKeys(this.sessionKeys, ''))
+    }
+    await this.api.prepareAccountsForFeeExpenses(this.controller, transactions)
+    const results = await this.api.sendExtrinsicsAndGetResults(transactions, this.controller)
+    assert(results.every((r) => r.isInBlock))
 
-    await this.api.signAndSend(validateTx, this.controller)
+    const validatorEntry = (await this.api.query.staking.validators.entries()).find(
+      ([sKey, prefs]) => sKey.args[0].eq(this.controller) && prefs.eq(this.preferences)
+    )
+    assert(
+      validatorEntry,
+      `validator entry not found (${this.controller}, ${JSON.stringify(this.preferences.toHuman())})`
+    )
+
+    if (this.sessionKeys) {
+      const nextControllerSessionKeys = await this.api.query.session.nextKeys(this.controller)
+      assert(nextControllerSessionKeys.eq(this.sessionKeys), 'invalid session.nextKeys')
+    }
   }
 }
--- tests/network-tests/src/flows/carthage/carthagePoAAssertions.ts
+++ tests/network-tests/src/flows/carthage/carthagePoAAssertions.ts
@@ -6,19 +6,22 @@ import { BondingSucceedsFixture } from '../../fixtures/staking/BondingSucceedsFi
 import { ValidatingSucceedsFixture } from '../../fixtures/staking/ValidatingSucceedsFixture'
 import { NominatingSucceedsFixture } from '../../fixtures/staking/NominatingSucceedsFixture'
 
-export default async function carthagePoAAssertions({ api, query, env }: FlowProps): Promise<void> {
+export default async function carthagePoAAssertions({ api }: FlowProps): Promise<void> {
   const debug = extendDebug('flow: constant Authorities in PoA')
   debug('started')
   api.enableDebugTxLogs()
 
   // ----------------------- ARRANGE -----------------------------
-  const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-  const sleepTimeSeconds = 10
+  const currentTopValidatorStake = (await api.query.staking.erasStakers.entries(0))
+    .map(([, v]) => v.total.toBn())
+    .reduce((a, b) => (b.gt(a) ? b : a))
   const nominatorBond = await api.query.staking.minNominatorBond()
-  const validatorBond = await api.query.staking.minValidatorBond()
+  // New validator's stake will be higher than any existing validator's total stake
+  const validatorBond = currentTopValidatorStake.addn(1)
   const [nominatorAccount, validatorAccount] = (await api.createKeyPairs(2)).map(({ key }) => key.address)
   const pastAuthorities = await api.getBabeAuthorities()
 
+  debug('setting up a new nominator and validator')
   const nominatorBondingSucceedsFixture = new BondingSucceedsFixture(api, {
     stash: nominatorAccount,
     controller: nominatorAccount,
@@ -35,13 +38,15 @@ export default async function carthagePoAAssertions({ api, query, env }: FlowPro
   const validatorFixture = new FixtureRunner(validatorBondingSucceedsFixture)
   await validatorFixture.run()
 
+  const validatorSessionKeys = api.createType('JoystreamNodeRuntimeSessionKeys', await api.rpc.author.rotateKeys())
   const validatorCandidatingSucceedsFixture = new ValidatingSucceedsFixture(
     api,
     api.createType('PalletStakingValidatorPrefs', {
       'commission': 0,
       'blocked': false,
     }),
-    validatorAccount
+    validatorAccount,
+    validatorSessionKeys
   )
   const candidationFixture = new FixtureRunner(validatorCandidatingSucceedsFixture)
   await candidationFixture.run()
@@ -51,48 +56,60 @@ export default async function carthagePoAAssertions({ api, query, env }: FlowPro
   await nominationFixture.run()
 
   // -------------------------- ACT -------------------------------
+  let startedAtSession: number
+  const numberOfSessionsToWait = api.consts.staking.sessionsPerEra.toNumber() * 2
+  const slotsPerSession = api.consts.babe.epochDuration.toNumber()
+  const slotDurationMs = api.consts.babe.expectedBlockTime.toNumber()
+  const timeoutMs = numberOfSessionsToWait * slotsPerSession * slotDurationMs * 2
 
-  // wait SessionPerEra * Epoch length blocks (standard era duration in block)
-  // this is the calculation done by the runtime:
-  // see https://github.com/paritytech/substrate/blob/master/frame/staking/src/pallet/impls.rs#L938
-  const period = api.consts.babe.epochDuration.toBn()
-  const sessionsPerEra = api.consts.staking.sessionsPerEra.toBn()
-  let currentBlock = (await api.getCurrentBlockNumber()).toBn()
-  const lastEraBlock = period.mul(sessionsPerEra).add(currentBlock)
-  while (currentBlock < lastEraBlock) {
-    sleep(sleepTimeSeconds * 1000)
-    currentBlock = (await api.getCurrentBlockNumber()).toBn()
-  }
+  // For each new session, make sure all assertions remain valid, until
+  // current session index === startedAtSession + numberOfSessionsToWait
+  await new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Timeout reached')), timeoutMs)
+    const unsub = api.query.session.currentIndex(async (i) => {
+      if (startedAtSession === undefined) {
+        startedAtSession = i.toNumber()
+      }
+      debug(`current session: ${i.toNumber()} (last one to check: ${startedAtSession + numberOfSessionsToWait})...`)
+      // 0. forceEra is still ForceNone
+      const forceEra = await api.getForceEra()
+      assert(forceEra.isForceNone)
 
-  // -------------------------- ASSERT ----------------------------
+      // 1. Current / Active Era is still `0`
+      const activeEraIndex = (await api.getActiveEra()).unwrap().index.toNumber()
+      const currentEraIndex = (await api.getCurrentEra()).unwrap().toNumber()
+      assert.equal(activeEraIndex, 0, 'unexpected activeEra change')
+      assert.equal(currentEraIndex, 0, 'unexpected currentEra change')
 
-  // 0. Force none state at genesis
-  const forceEra = await api.getForceEra()
-  assert(forceEra.isForceNone)
+      // 2. Authorities are constant
+      // 2.a. babe authorities are constant
+      const currentAuthorities = await api.getBabeAuthorities()
+      assert.deepEqual(pastAuthorities, currentAuthorities, 'babe authorities have changed')
 
-  // 1. Authorities are constant
-  // 1.a. babe authorities are constant
-  const currentAuthorities = await api.getBabeAuthorities()
-  assert.deepEqual(pastAuthorities, currentAuthorities, 'babe authorities have changed')
+      // 2.b. Queued keys (for next session) and current session keys are the same
+      const sessionAuthorities = await api.getSessionAuthorities()
+      const queuedKeys = await api.getQueuedKeys()
+      assert.deepEqual(queuedKeys, sessionAuthorities, 'different validator keys in between sessions ')
 
-  // 1.b. Queued keys (for next session) and current session keys are the same
-  const sessionAuthorities = await api.getSessionAuthorities()
-  const queuedKeys = await api.getQueuedKeys()
-  assert.deepEqual(queuedKeys, sessionAuthorities, 'different validator keys in between sessions ')
+      // 3. Next Era starting session index is none
+      const nextEraIndex = activeEraIndex + 1
+      const nextEraStartSessionIndex = await api.getErasStartSessionIndex(nextEraIndex)
+      assert(nextEraStartSessionIndex.isNone, 'next era doomed to begin')
 
-  // 2. Next Era starting session index is none
-  const activeEraIndex = (await api.getActiveEra()).unwrap().index.toNumber()
-  const nextEraIndex = activeEraIndex + 1
-  const nextEraStartSessionIndex = await api.getErasStartSessionIndex(nextEraIndex)
-  assert.equal(activeEraIndex, 0)
-  assert(nextEraStartSessionIndex.isNone, 'next era doomed to begin')
+      // 4. Elections
+      // 4.a election round is blocked
+      const electionRoundSnapshot = await api.getElectionSnapshot()
+      assert(electionRoundSnapshot.isNone, 'ongoing elections')
 
-  // 3. Elections
-  // 3.a election round is blocked
-  const electionRoundSnapshot = await api.getElectionSnapshot()
-  assert(electionRoundSnapshot.isNone, 'ongoing elections')
+      // 4.b election round is 1 (initial round)
+      const currentElectionRound = (await api.getElectionRounds()).toNumber()
+      assert.equal(currentElectionRound, 1, 'election rounds happened')
 
-  // 3.b election round is 1 (initial round)
-  const currentElectionRound = (await api.getElectionRounds()).toNumber()
-  assert.equal(currentElectionRound, 1, 'election rounds happened')
+      debug('All assertions OK!')
+      if (i.toNumber() === startedAtSession + numberOfSessionsToWait) {
+        ;(await unsub)()
+        resolve(true)
+      }
+    })
+  })
 }
