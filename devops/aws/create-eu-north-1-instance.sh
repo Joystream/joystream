918bc755e14c782801ae59f0eca2b1fa60c86a15
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-10-20 13:24:21 +0400

    carthage-specific-deployment scripts

---
+++ devops/aws/create-eu-north-1-instance.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+source common.sh
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying single node ==========="
+aws cloudformation deploy \
+  --region eu-north-1 \
+  --stack-name polkadot-ref-machine-2 \
+  --template-file cloudformation/single-instance.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=c5d.2xlarge \
+    KeyName=mokhtar-joystream-key \
+    EC2AMI=ami-0126004969884d592
+
