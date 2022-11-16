8efbd136080d80176988b4b17faee7bf827eb9de
Ignazio Bovo <ignazio@jsgenesis.com>
2022-11-16 07:41:16 +0100

    Update runtime-modules/project-token/src/lib.rs
    Co-authored-by: Leszek Wiesner <leszek@jsgenesis.com>

--- runtime-modules/project-token/src/lib.rs
+++ runtime-modules/project-token/src/lib.rs
@@ -890,7 +890,7 @@ decl_module! {
 
             // timestamp deadline check
             if let Some(deadline) = deadline {
-                ensure!(<timestamp::Pallet<T>>::get() < deadline, Error::<T>::DeadlineExpired);
+                ensure!(<timestamp::Pallet<T>>::get() <= deadline, Error::<T>::DeadlineExpired);
             }
 
             // == MUTATION SAFE ==
