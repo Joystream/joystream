43d3b666f217e01ac08d3efafed950ef4a7bf83e
ignazio <bovoignazio.dev@gmail.com>
2022-11-21 11:15:57 +0100

    fix: update token data on benchmarks

--- runtime-modules/project-token/src/benchmarking.rs
+++ runtime-modules/project-token/src/benchmarking.rs
@@ -116,7 +116,6 @@ fn issue_token<T: Config>(
             transfer_policy,
             patronage_rate: DEFAULT_PATRONAGE,
             revenue_split_rate: DEFAULT_REVENUE_SPLIT_RATE,
-            creator_member_id: T::MemberId::zero(),
         },
         UploadContext {
             bag_id: BagId::<T>::default(),
