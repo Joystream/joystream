6a52643428438d4ee4c652b518e7b9f2089c8a1a
ignazio <bovoignazio.dev@gmail.com>
2023-01-18 16:35:41 +0100

    fix: proposal flow

--- tests/network-tests/src/flows/proposals/index.ts
+++ tests/network-tests/src/flows/proposals/index.ts
@@ -3,14 +3,17 @@ import { extendDebug } from '../../Debugger'
 import { FixtureRunner } from '../../Fixture'
 import { AddStakingAccountsHappyCaseFixture, BuyMembershipHappyCaseFixture } from '../../fixtures/membership'
 import { Utils } from '../../utils'
-import {
-  ApplyOnOpeningsHappyCaseFixture,
-  CreateLeadOpeningFixture,
-  createDefaultOpeningParams,
-} from '../../fixtures/workingGroups'
+import { ApplyOnOpeningsHappyCaseFixture, createDefaultOpeningParams } from '../../fixtures/workingGroups'
 import { OpeningMetadata } from '@joystream/metadata-protobuf'
-import { AllProposalsOutcomesFixture, TestedProposal } from '../../fixtures/proposals'
+import {
+  AllProposalsOutcomesFixture,
+  CreateProposalsFixture,
+  DecideOnProposalStatusFixture,
+  TestedProposal,
+} from '../../fixtures/proposals'
 import { createType } from '@joystream/types'
+import { Resource } from '../../Resources'
+import { OpeningId } from '@joystream/types/primitives'
 
 export default async function creatingProposals({ api, query, lock }: FlowProps): Promise<void> {
   const debug = extendDebug('flow:creating-proposals')
@@ -18,20 +21,68 @@ export default async function creatingProposals({ api, query, lock }: FlowProps)
   api.enableVerboseTxLogs()
 
   debug('Creating test lead openings and applications...')
-  const createLeadOpeningsFixture = new CreateLeadOpeningFixture(
-    api,
-    query,
-    [createDefaultOpeningParams(api), createDefaultOpeningParams(api)],
-    'membershipWorkingGroup'
-  )
-  await new FixtureRunner(createLeadOpeningsFixture).run()
-  const [openingToCancelId, openingToFillId] = createLeadOpeningsFixture.getCreatedOpeningIds()
+  // const createLeadOpeningsFixture = new CreateLeadOpeningFixture(
+  //   api,
+  //   query,
+  //   [createDefaultOpeningParams(api), createDefaultOpeningParams(api)],
+  //   'membershipWorkingGroup'
+  // )
+  // await new FixtureRunner(createLeadOpeningsFixture).run()
+  // const [openingToCancelId, openingToFillId] = createLeadOpeningsFixture.getCreatedOpeningIds()
 
   const [applicantControllerAcc, applicantStakingAcc] = (await api.createKeyPairs(2)).map(({ key }) => key.address)
   const buyMembershipFixture = new BuyMembershipHappyCaseFixture(api, query, [applicantControllerAcc])
   await new FixtureRunner(buyMembershipFixture).run()
   const [applicantMemberId] = buyMembershipFixture.getCreatedMembers()
 
+  const unlock = await lock(Resource.Proposals)
+  const openingParams = createDefaultOpeningParams(api)
+  const createLeadOpeningProposalsFixture = new CreateProposalsFixture(api, query, [
+    {
+      type: 'CreateWorkingGroupLeadOpening',
+      details: createType('PalletProposalsCodexCreateOpeningParameters', {
+        'description': createType('Bytes', `Proposal to hire lead membership TO BE KEPT`),
+        'stakePolicy': createType('PalletWorkingGroupStakePolicy', {
+          'stakeAmount': openingParams.stake,
+          'leavingUnstakingPeriod': openingParams.unstakingPeriod,
+        }),
+        'rewardPerBlock': openingParams.reward,
+        'group': createType('PalletCommonWorkingGroupIterableEnumsWorkingGroup', 'Membership'),
+      }),
+      asMember: applicantMemberId,
+      title: 'Proposal to Hired lead TO BE KEPT',
+      description: 'Proposal to hire lead membership',
+    },
+    {
+      type: 'CreateWorkingGroupLeadOpening',
+      details: createType('PalletProposalsCodexCreateOpeningParameters', {
+        'description': createType('Bytes', `Proposal to hire lead membership TO BE CANCELLED`),
+        'stakePolicy': createType('PalletWorkingGroupStakePolicy', {
+          'stakeAmount': openingParams.stake,
+          'leavingUnstakingPeriod': openingParams.unstakingPeriod,
+        }),
+        'rewardPerBlock': openingParams.reward,
+        'group': createType('PalletCommonWorkingGroupIterableEnumsWorkingGroup', 'Membership'),
+      }),
+      asMember: applicantMemberId,
+      title: 'Proposal to Hired lead TO BE CANCELLED',
+      description: 'Proposal to hire lead membership',
+    },
+  ])
+  await new FixtureRunner(createLeadOpeningProposalsFixture).run()
+  const [proposalToCancel, proposalToKeep] = createLeadOpeningProposalsFixture.getCreatedProposalsIds()
+
+  const approveProposalsFixture = new DecideOnProposalStatusFixture(api, query, [
+    { proposalId: proposalToKeep, status: 'Approved', expectExecutionFailure: false },
+    { proposalId: proposalToCancel, status: 'Approved', expectExecutionFailure: false },
+  ])
+  await new FixtureRunner(approveProposalsFixture).run()
+  unlock()
+  const [openingToCancelId, openingToFillId] = (
+    await approveProposalsFixture.getExecutionEvents('membershipWorkingGroup', 'OpeningAdded')
+  ).map((event) => event.data[0]) as OpeningId[]
+
+  // stake to apply
   const addStakingAccountsFixture = new AddStakingAccountsHappyCaseFixture(api, query, [
     { asMember: applicantMemberId, account: applicantStakingAcc, stakeAmount: createDefaultOpeningParams(api).stake },
   ])
@@ -111,6 +162,7 @@ export default async function creatingProposals({ api, query, lock }: FlowProps)
   const testAllOutcomesFixture = new AllProposalsOutcomesFixture(api, query, lock, proposalsToTest)
   await new FixtureRunner(testAllOutcomesFixture).run()
 
+  debug('FIRST LEG OF PROPOSALS DONE')
   // The membership lead should be hired at this point, so we can test lead-related proposals
 
   const leadId = (await api.query.membershipWorkingGroup.currentLead()).unwrap()
@@ -134,6 +186,7 @@ export default async function creatingProposals({ api, query, lock }: FlowProps)
   ]
   const leadProposalsOutcomesFixture = new AllProposalsOutcomesFixture(api, query, lock, leadProposalsToTest)
   await new FixtureRunner(leadProposalsOutcomesFixture).run()
+  debug('SECOND LEG OF PROPOSALS DONE')
 
   const terminateLeadProposalOutcomesFixture = new AllProposalsOutcomesFixture(api, query, lock, [
     {
