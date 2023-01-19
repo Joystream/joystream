1a2d313b8b7a0c98032d2bbfb1f9ebe11e7aad6b
drillprop <drylbartosz@gmail.com>
2023-01-19 11:26:11 +0100

    add new scenarios to full scenario

--- tests/network-tests/src/scenarios/full.ts
+++ tests/network-tests/src/scenarios/full.ts
@@ -37,6 +37,7 @@ import commentsAndReactions from '../flows/content/commentsAndReactions'
 import addAndUpdateVideoSubtitles from '../flows/content/videoSubtitles'
 import { testVideoCategories } from '../flows/content/videoCategories'
 import channelPayouts from '../flows/proposals/channelPayouts'
+import { createApp, updateApp } from '../flows/content/app'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 scenario('Full', async ({ job, env }) => {
@@ -111,6 +112,8 @@ scenario('Full', async ({ job, env }) => {
   const commentsAndReactionsJob = job('video comments and reactions', commentsAndReactions).after(
     nftAuctionAndOffersJob
   )
+  job('create app', createApp).after(sudoHireLead)
+  job('update app', updateApp).after(sudoHireLead)
 
   const contentDirectoryJob = commentsAndReactionsJob // keep updated to last job above
 
