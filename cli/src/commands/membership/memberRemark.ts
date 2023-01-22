89b5fc10949005a76569af3f4f8ee74ad54c13c9
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-01-23 00:15:20 +0500

    decorate membership:memberRemark with payment option

--- cli/src/commands/membership/memberRemark.ts
+++ cli/src/commands/membership/memberRemark.ts
@@ -1,5 +1,9 @@
+import { flags } from '@oclif/command'
+import { formatBalance } from '@polkadot/util'
 import chalk from 'chalk'
+import ExitCodes from '../../ExitCodes'
 import MembershipsCommandBase from '../../base/MembershipsCommandBase'
+import { validateAddress } from '../../helpers/validation'
 
 export default class MemberRemarkCommand extends MembershipsCommandBase {
   static description = 'Member remarks'
@@ -12,16 +16,51 @@ export default class MemberRemarkCommand extends MembershipsCommandBase {
   ]
 
   static flags = {
+    account: flags.string({
+      description: 'Account where JOY needs to be transferred',
+      dependsOn: ['amount'],
+    }),
+    amount: flags.string({
+      description: 'JOY amount to be transferred',
+    }),
     ...MembershipsCommandBase.flags,
   }
 
   async run(): Promise<void> {
     const { message } = this.parse(MemberRemarkCommand).args
-    const { id: memberId, membership } = await this.getRequiredMemberContext(true)
-    const keypair = await this.getDecodedPair(membership.controllerAccount)
+    let { account, amount } = this.parse(MemberRemarkCommand).flags
+    const {
+      id: memberId,
+      membership: { controllerAccount },
+    } = await this.getRequiredMemberContext(true)
 
-    await this.sendAndFollowNamedTx(keypair, 'members', 'memberRemark', [memberId, message, null])
+    if (amount) {
+      if (!account) {
+        account = await this.promptForAnyAddress(`Select recipient for 'member_remark' transfer`)
+      } else if (validateAddress(account) !== true) {
+        this.error('Invalid recipient address', { exit: ExitCodes.InvalidInput })
+      }
+      await this.ensureJoyTransferIsPossible(controllerAccount.toString(), account, amount)
 
-    this.log(chalk.green(`Member remarked successfully`))
+      await this.requireConfirmation(
+        `Do you confirm transfer of ${chalk.cyan(formatBalance(amount))} to ${chalk.cyan(account)}?`
+      )
+    }
+
+    const keypair = await this.getDecodedPair(controllerAccount)
+    const result = await this.sendAndFollowNamedTx(keypair, 'members', 'memberRemark', [
+      memberId,
+      message,
+      account && amount ? [account, amount] : null,
+    ])
+
+    const [id, msg, payment] = this.getEvent(result, 'members', 'MemberRemarked').data
+    this.log(
+      chalk.green(
+        `Member ${id} remarked successfully with message: ${msg} ${
+          payment.isSome ? `and payment: ${payment.unwrap()}` : ''
+        }`
+      )
+    )
   }
 }
