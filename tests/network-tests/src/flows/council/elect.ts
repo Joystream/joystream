353deb7e51993ee658152d67c88879704edad704
ignazio-bovo <ignazio@jsgenesis.com>
2023-02-10 14:52:46 +0100

    fix: refactor

--- tests/network-tests/src/flows/council/elect.ts
+++ tests/network-tests/src/flows/council/elect.ts
@@ -4,17 +4,11 @@ import { FixtureRunner } from '../../Fixture'
 import { ElectCouncilFixture } from '../../fixtures/council/ElectCouncilFixture'
 import { assert } from 'chai'
 
-async function createCandidatesAddresses(councilSize: number, extraCandidates: number, { api, env }: FlowProps) {
-  const candidatesNumber = councilSize + extraCandidates
-  if (councilSize > 1) {
-    return (await api.createKeyPairs(candidatesNumber)).map(({ key }) => key.address)
-  } else {
-    assert.equal(extraCandidates, 0, 'no need for extra candidates when testing')
-    const suri = env.COUNCILLOR_SURI
-    assert(suri, 'suri for single councelor not specified')
-    const candidate = api.createCustomKeyPair(suri.toString(), true)
-    return [candidate.address]
-  }
+function createSingleCouncilorAddress({ api, env }: FlowProps) {
+  const suri = env.COUNCILLOR_SURI
+  assert(suri, 'suri for single councelor not specified')
+  const candidate = api.createCustomKeyPair(suri.toString(), true)
+  return [candidate.address]
 }
 export default async function electCouncil(props: FlowProps): Promise<void> {
   const debug = extendDebug('flow:elect-council')
@@ -22,11 +16,11 @@ export default async function electCouncil(props: FlowProps): Promise<void> {
   debug('Started')
   api.enableDebugTxLogs()
 
-  const extraCandidates = api.consts.council.minNumberOfExtraCandidates.toNumber()
   const councilSize = api.consts.council.councilSize.toNumber()
-  const candidatesAddresses = await createCandidatesAddresses(councilSize, extraCandidates, props)
 
-  const electCouncilFixture = new ElectCouncilFixture(api, query, candidatesAddresses)
+  // if single councilor (playground setting) use custom key
+  const candidates = councilSize === 1 ? createSingleCouncilorAddress(props) : []
+  const electCouncilFixture = new ElectCouncilFixture(api, query, candidates)
   await new FixtureRunner(electCouncilFixture).run()
 
   debug('Done')
