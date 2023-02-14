cc7d2b6e8b943c9ab541829d22ecd984cb6239b7
ignazio-bovo <ignazio@jsgenesis.com>
2023-02-14 17:24:36 +0100

    fix: electfixture refactor

--- tests/network-tests/src/fixtures/council/ElectCouncilFixture.ts
+++ tests/network-tests/src/fixtures/council/ElectCouncilFixture.ts
@@ -2,13 +2,71 @@ import { BaseQueryNodeFixture, FixtureRunner } from '../../Fixture'
 import { AddStakingAccountsHappyCaseFixture, BuyMembershipHappyCaseFixture } from '../membership'
 import { assertCouncilMembersRuntimeQnMatch } from './common'
 import { blake2AsHex } from '@polkadot/util-crypto'
-import { GenericAccountId } from '@polkadot/types'
+import { GenericAccountId, u64 } from '@polkadot/types'
 import { assert } from 'chai'
 import { createType, registry } from '@joystream/types'
 import { BlackListVoteFixture, VoteFixture } from '../referendum'
 import { AnnounceCandidacyFixture } from './announceCandidacyFixture'
+import { Api } from 'src/Api'
+import { QueryNodeApi } from 'src/QueryNodeApi'
+import BN from 'bn.js'
 
 export class ElectCouncilFixture extends BaseQueryNodeFixture {
+  private _optOutVoters: boolean
+
+  protected async maybeBlackListVoters(votersStakingAccounts: string[]) {
+    if (this._optOutVoters) {
+      // failing case for opted-out voters
+      const optOutVotersFixture = new BlackListVoteFixture(this.api, this.query, votersStakingAccounts)
+      await new FixtureRunner(optOutVotersFixture).run()
+      return
+    }
+  }
+
+  protected async runVoteFixture(
+    votersStakingAccounts: string[],
+    candidatesMemberIds: u64[],
+    voteStake: BN
+  ): Promise<u64> {
+    const cycleId = (await this.api.query.referendum.stage()).asVoting.currentCycleId
+    const numberOfCandidates = candidatesMemberIds.length
+    const commitments = votersStakingAccounts.map((account, i) => {
+      const accountId = new GenericAccountId(registry, account)
+      const optionId = candidatesMemberIds[i % numberOfCandidates]
+      const salt = createType('Bytes', `salt${i}`)
+
+      const payload = Buffer.concat([accountId.toU8a(), optionId.toU8a(), salt.toU8a(), cycleId.toU8a()])
+      const commitment = blake2AsHex(payload)
+      return commitment
+    })
+    const voteFixture = new VoteFixture(
+      this.api,
+      this.query,
+      new Map(
+        votersStakingAccounts.map((account, i) => {
+          return [
+            account,
+            {
+              commitment: commitments[i],
+              stake: voteStake,
+            },
+          ]
+        })
+      ),
+      false
+    )
+    if (this._optOutVoters) {
+      voteFixture.setErrorName('AccountAlreadyOptedOutOfVoting')
+    }
+    await new FixtureRunner(voteFixture).run()
+    return cycleId
+  }
+
+  constructor(api: Api, query: QueryNodeApi, optOutVoters = false) {
+    super(api, query)
+    this._optOutVoters = optOutVoters
+  }
+
   public async execute(): Promise<void> {
     const { api, query } = this
     const { councilSize, minNumberOfExtraCandidates } = api.consts.council
@@ -65,34 +123,14 @@ export class ElectCouncilFixture extends BaseQueryNodeFixture {
     const votersStakingAccounts = (await this.api.createKeyPairs(numberOfVoters)).map(({ key }) => key.address)
     await api.treasuryTransferBalanceToAccounts(votersStakingAccounts, voteStake) // fund accounts
 
-    const cycleId = (await this.api.query.referendum.stage()).asVoting.currentCycleId
-    const commitments = votersStakingAccounts.map((account, i) => {
-      const accountId = new GenericAccountId(registry, account)
-      const optionId = candidatesMemberIds[i % numberOfCandidates]
-      const salt = createType('Bytes', `salt${i}`)
+    // blacklist all voters if flag is set
+    await this.maybeBlackListVoters(votersStakingAccounts)
 
-      const payload = Buffer.concat([accountId.toU8a(), optionId.toU8a(), salt.toU8a(), cycleId.toU8a()])
-      const commitment = blake2AsHex(payload)
-      return commitment
-    })
-
-    const voteFixture = new VoteFixture(
-      this.api,
-      this.query,
-      new Map(
-        votersStakingAccounts.map((account, i) => {
-          return [
-            account,
-            {
-              commitment: commitments[i],
-              stake: voteStake,
-            },
-          ]
-        })
-      ),
-      false
-    )
-    await new FixtureRunner(voteFixture).run()
+    // run vote fixture with assertions
+    const cycleId = await this.runVoteFixture(votersStakingAccounts, candidatesMemberIds, voteStake)
+    if (this._optOutVoters) {
+      return // return as there won't be any votes to reveal
+    }
 
     // Revealing stage
     await this.api.untilCouncilStage('Revealing')
--- tests/network-tests/src/flows/council/elect.ts
+++ tests/network-tests/src/flows/council/elect.ts
@@ -2,7 +2,6 @@ import { FlowProps } from '../../Flow'
 import { extendDebug } from '../../Debugger'
 import { FixtureRunner } from '../../Fixture'
 import { ElectCouncilFixture } from '../../fixtures/council/ElectCouncilFixture'
-import { VotersOptingOut } from '../../fixtures/council/VotersOptingOut'
 
 export default async function electCouncil({ api, query }: FlowProps): Promise<void> {
   const debug = extendDebug('flow:elect-council')
