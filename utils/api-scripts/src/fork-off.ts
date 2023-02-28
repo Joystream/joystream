0d7ed47eee66f16fb9bb5a5c7c6610cbe5493888
Ignazio Bovo <ignazio@jsgenesis.com>
2023-02-28 11:11:32 +0100

    fix: add checks

--- utils/api-scripts/src/fork-off.ts
+++ utils/api-scripts/src/fork-off.ts
@@ -2,6 +2,8 @@ import { xxhashAsHex } from '@polkadot/util-crypto'
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import fs from 'fs'
 import path from 'path'
+import { Metadata } from '@polkadot/types'
+import { assert } from '@polkadot/util/assert'
 
 /**
  * All module prefixes except those mentioned in the skippedModulesPrefix will be added to this by the script.
@@ -18,24 +20,24 @@ import path from 'path'
  */
 const prefixes = [
   '0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9' /* System.Account */,
-  '0x2ce461329fdf4be12bce01afc0af09bc' /* Members */,
-  '0x30f7f927af3a1cd6f254a51fb5ddb9e7' /* Membership WG */,
-  '0xf2bc8460d32f9e3947b72e0e2ac73992' /* OperationsWgGamma */,
-  '0x9a73b960a5d89e29b0a5636837f8df16' /* OperationsWgBeta */,
-  '0xbf38d6d001fcafa118fc9526bee4cb50' /* OperationsWgAlpha */,
-  '0xaa4612cd135c6055b7910d493c5fd4ae' /* Forum */,
-  '0xcf9da36cc34d922a84a3ec231495ea2b' /* Forum WG */,
-  '0x95875cb80ebaf9f918457db6a86ac6ad' /* Storage */,
-  '0xaff74aad5f7ed527360635c9b99b50d2' /* Storage WG */,
-  '0xb5a494c92fa4747cc071573e93b32b87' /* Content */,
-  '0xde7bf41b08da4e09ac0e7c35311f0086' /* ProposalDiscussion */,
-  '0xf546b188d57525d1826b066ad7959643' /* Proposal Engine */,
-  '0x9a4d7ec4b42d2468c1e2ef6de6dfe59f' /* Proposal Codex */,
-  '0x4d91f840794dbb0abe4c14e33733ae2a' /* App WG */,
-  '0xdf66cf37cde77d2a63889732a23c685e' /* BagList */,
-  '0x43a64b3f1b3826a8520d6a2635c4cdbe' /* Bounty */,
-  '0x122856f80f579448b2710bec9ee1f890' /* DistributionWG */,
-  '0xcb732bb8b688ea549fec1838a1350aed' /* Project Token */,
+  // '0x2ce461329fdf4be12bce01afc0af09bc' /* Members */,
+  // '0x30f7f927af3a1cd6f254a51fb5ddb9e7' /* Membership WG */,
+  // '0xf2bc8460d32f9e3947b72e0e2ac73992' /* OperationsWgGamma */,
+  // '0x9a73b960a5d89e29b0a5636837f8df16' /* OperationsWgBeta */,
+  // '0xbf38d6d001fcafa118fc9526bee4cb50' /* OperationsWgAlpha */,
+  // '0xaa4612cd135c6055b7910d493c5fd4ae' /* Forum */,
+  // '0xcf9da36cc34d922a84a3ec231495ea2b' /* Forum WG */,
+  // '0x95875cb80ebaf9f918457db6a86ac6ad' /* Storage */,
+  // '0xaff74aad5f7ed527360635c9b99b50d2' /* Storage WG */,
+  // '0xb5a494c92fa4747cc071573e93b32b87' /* Content */,
+  // '0xde7bf41b08da4e09ac0e7c35311f0086' /* ProposalDiscussion */,
+  // '0xf546b188d57525d1826b066ad7959643' /* Proposal Engine */,
+  // '0x9a4d7ec4b42d2468c1e2ef6de6dfe59f' /* Proposal Codex */,
+  // '0x4d91f840794dbb0abe4c14e33733ae2a' /* App WG */,
+  // '0xdf66cf37cde77d2a63889732a23c685e' /* BagList */,
+  // '0x43a64b3f1b3826a8520d6a2635c4cdbe' /* Bounty */,
+  // '0x122856f80f579448b2710bec9ee1f890' /* DistributionWG */,
+  // '0xcb732bb8b688ea549fec1838a1350aed' /* Project Token */,
 ]
 const skippedModulesPrefix = [
   'System',
@@ -50,6 +52,20 @@ const skippedModulesPrefix = [
   'Council', // empty council
   'Referendum',
 ]
+
+function checkMetadataAndStorageKeyCompatibility(storageKeyPairs: Array<[string, string]>, metadata: Metadata) {
+  let palletNameHashes = metadata.asLatest.pallets.map((pallet) => [
+    xxhashAsHex(pallet.name.toString(), 128),
+    pallet.name.toString(),
+  ])
+  let isMetadataPalletNameInStorageKey = (palletNameHash: string, palletName: string) => {
+    const isPrefix = storageKeyPairs.some(([key]) => key.startsWith(palletNameHash))
+    assert(isPrefix, `error: ${palletName} is not contained in live storage keypairs`)
+  }
+  palletNameHashes.forEach(([palletNameHash, palletName]) =>
+    isMetadataPalletNameInStorageKey(palletNameHash, palletName)
+  )
+}
 
 async function main() {
   // paths & env variables
@@ -70,19 +86,19 @@ async function main() {
   pallets.forEach((pallet) => {
     if (pallet.storage) {
       if (!skippedModulesPrefix.includes(pallet.name.toString())) {
-        console.log(`pallet being copied is ${pallet.name.toString()} ${xxhashAsHex(pallet.name.toString())}`)
         prefixes.push(xxhashAsHex(pallet.name.toString(), 128))
       }
     }
   })
-  console.log(`membership pallet hex: ${xxhashAsHex('membership', 128)}`)
 
   // blank starting chainspec guaranteed to exist
   const storage: Storage = JSON.parse(fs.readFileSync(storagePath, 'utf8'))
   const chainSpec = JSON.parse(fs.readFileSync(specPath, 'utf8'))
 
+  // check that metadata pallets module key-prefix is contained in storage keys
+  checkMetadataAndStorageKeyCompatibility(storage.result, metadata)
+
   // Grab the items to be moved, then iterate through and insert into storage
-  console.log(`${prefixes}`)
   storage.result
     .filter((i) => prefixes.some((prefix) => i[0].startsWith(prefix)))
     .forEach(([key, value]) => (chainSpec.genesis.raw.top[key] = value))
