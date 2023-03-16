26e0994eabf3dcd6214b45018714f2fb12d18250
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2023-03-16 23:17:32 +0400

    cargo-checks covers runtime-benchmarks

--- scripts/cargo-checks.sh
+++ scripts/cargo-checks.sh
@@ -11,13 +11,13 @@ source ./features.sh
 
 export WASM_BUILD_TOOLCHAIN=nightly-2022-11-15
 
-echo 'running clippy (rust linter)'
 # When custom build.rs triggers wasm-build-runner-impl to build we get error:
 # "Rust WASM toolchain not installed, please install it!"
 # So we skip building the WASM binary by setting BUILD_DUMMY_WASM_BINARY=1
 # Aggressive linting
 echo 'running cargo clippy'
-BUILD_DUMMY_WASM_BINARY=1 cargo "+$WASM_BUILD_TOOLCHAIN" clippy --release --all -- -D warnings
+#cargo "+$WASM_BUILD_TOOLCHAIN" clippy --release --all --features "${FEATURES}" -- -D warnings
+cargo "+$WASM_BUILD_TOOLCHAIN" clippy --release --all --features runtime-benchmarks,"${FEATURES}"
 
 echo 'running cargo unit tests'
-cargo "+$WASM_BUILD_TOOLCHAIN" test --release --all --features runtime-benchmarks,"${FEATURES}" 
+cargo "+$WASM_BUILD_TOOLCHAIN" test --release --all --features runtime-benchmarks,"${FEATURES}"
