e3b6dc69649d095c8267208c8811ceeb936c6af7
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2023-05-01 11:42:02 +0400

    runtime spec 2002

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -144,7 +144,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("joystream-node"),
     impl_name: create_runtime_str!("joystream-node"),
     authoring_version: 12,
-    spec_version: 3000,
+    spec_version: 2002,
     impl_version: 0,
     apis: crate::runtime_api::EXPORTED_RUNTIME_API_VERSIONS,
     transaction_version: 1,
