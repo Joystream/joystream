779c774a5b368bac18b6f6f20121ecf3e4c1a226
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2023-05-23 14:17:06 +0400

    setup.sh - rustup update after install

--- setup.sh
+++ setup.sh
@@ -39,6 +39,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source ~/.cargo/env
 
+rustup update
+rustup update nightly
+
 rustup install nightly-2022-11-15
 rustup target add wasm32-unknown-unknown --toolchain nightly-2022-11-15
 
