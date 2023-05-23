c7770198fc623cdcf6b734c58b608cbbb4114e92
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2023-05-23 14:19:51 +0400

    setup.sh - work around outdated protobuf package install from getsubstrate.io

--- setup.sh
+++ setup.sh
@@ -30,9 +30,16 @@ fi
 
 # If OS is supported will install build tools for rust and substrate.
 # Skips installation of substrate and subkey
-curl https://getsubstrate.io -sSf | bash -s -- --fast
+# old script trying to install package 'protobuf' which does not exist
+# curl https://getsubstrate.io -sSf | bash -s -- --fast
+
+# Install Rust toolchain since we no longer use getsubstrate.io script
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source ~/.cargo/env
+
+rustup update
+rustup update nightly
 
 rustup install nightly-2022-05-11
 rustup target add wasm32-unknown-unknown --toolchain nightly-2022-05-11
