b9aa16950e48965047323317835dc32263404577
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-07-13 15:53:19 +0500

    print service token generation error in start-elasticsearch-stach.sh script

--- start-elasticsearch-stack.sh
+++ start-elasticsearch-stack.sh
@@ -26,10 +26,23 @@ sleep 30
 
 # Generate the service token
 # Ref: https://www.elastic.co/guide/en/elasticsearch/reference/current/service-accounts.html#service-accounts-tokens
-response=$(curl -f -s -X POST -u "${ELASTIC_USERNAME}":"${ELASTIC_PASSWORD}" "http://localhost:9200/_security/service/elastic/kibana/credential/token/my_kibana_token" -H 'Content-Type: application/json')
+response=$(curl -s -w "\n%{http_code}\n" -X POST -u "${ELASTIC_USERNAME}":"${ELASTIC_PASSWORD}" "http://localhost:9200/_security/service/elastic/kibana/credential/token/my_kibana_token" -H 'Content-Type: application/json')
+response_body=$(echo "$response" | head -n1)
+status_code=$(echo "$response" | tail -n1)
+
+if [ -z "$status_code" ]; then
+    echo "Error: Did not receive a status code from the server"
+    exit 1
+fi
+
+if [ "$status_code" -ne 200 ]; then
+    error_message=$(echo $response_body | jq -r '.error.root_cause')
+    echo -e "\nError: Failed to generate the service token: \n$error_message"
+    exit 1
+fi
 
 # Extract & export the token from the response
-export ELASTICSEARCH_SERVICEACCOUNTTOKEN=$(echo $response | jq -r '.token.value')
+export ELASTICSEARCH_SERVICEACCOUNTTOKEN=$(echo $response_body | jq -r '.token.value')
 
 echo 'Starting for Kibana...'
 
