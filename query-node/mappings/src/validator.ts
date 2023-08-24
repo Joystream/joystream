9eb304c843e4348227c71f3ec420d78fa00448b4
chrlschwb <chrlschwb77@gmail.com>
2023-08-24 14:58:25 +0200

    change VALIDATOR to DISCORD for push git

--- query-node/mappings/src/validator.ts
+++ query-node/mappings/src/validator.ts
@@ -1,5 +1,11 @@
-import { DatabaseManager, } from '@joystream/hydra-common';
-import { MemberMetadata, Membership, MembershipExternalResource, MembershipExternalResourceType, Validator } from 'query-node/dist/model';
+import { DatabaseManager } from '@joystream/hydra-common'
+import {
+  MemberMetadata,
+  Membership,
+  MembershipExternalResource,
+  MembershipExternalResourceType,
+  Validator,
+} from 'query-node/dist/model'
 
 export async function saveValidator(
   store: DatabaseManager,
@@ -7,19 +13,19 @@ export async function saveValidator(
   externalResources: Pick<MembershipExternalResource, 'type' | 'value'>[] = [],
   memberMetadata: MemberMetadata = member.metadata
 ): Promise<Validator[] | undefined> {
-
-  const newExternalResources = externalResources.filter(d=>d.type=== MembershipExternalResourceType.VALIDATOR).map(
-    ({ type, value }) =>{      
-      return(
-        new Validator({
+  const newExternalResources = externalResources
+    .filter((d) => d.type === MembershipExternalResourceType.DISCORD)
+    .map(
+      // here is d.type=== MembershipExternalResourceType.VALIDATOR
+      ({ type, value }) => {
+        return new Validator({
           id: `${memberMetadata.id}-${type}`,
-          isValidator:false,
-          validatorAccount:value,
+          isValidator: false,
+          validatorAccount: value,
           member: member.handle,
         })
-      )
-  
-    })
+      }
+    )
 
   for (const resource of newExternalResources) {
     await store.save<Validator>(resource)
