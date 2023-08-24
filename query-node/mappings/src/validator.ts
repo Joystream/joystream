e3409424b65ebe14ac451fcf801f5f0f6b159ead
chrlschwb <chrlschwb77@gmail.com>
2023-08-24 13:45:05 +0200

    valditor add and map

--- query-node/mappings/src/validator.ts
+++ query-node/mappings/src/validator.ts
@@ -1,17 +1,29 @@
-import { EventContext, StoreContext } from '@joystream/hydra-common';
-import { Members_MemberCreatedEvent_V1001 } from "generated/types"
-import { MembershipEntryMemberCreated, Validator } from 'query-node/dist/model';
+import { DatabaseManager, } from '@joystream/hydra-common';
+import { MemberMetadata, Membership, MembershipExternalResource, MembershipExternalResourceType, Validator } from 'query-node/dist/model';
 
-export async function validator_created({ store, event }: EventContext & StoreContext): Promise<void> {
-    const [memberId, memberParameters, inviteCount] = new Members_MemberCreatedEvent_V1001(event).params
+export async function saveValidator(
+  store: DatabaseManager,
+  member: Membership,
+  externalResources: Pick<MembershipExternalResource, 'type' | 'value'>[] = [],
+  memberMetadata: MemberMetadata = member.metadata
+): Promise<Validator[] | undefined> {
+
+  const newExternalResources = externalResources.filter(d=>d.type=== MembershipExternalResourceType.VALIDATOR).map(
+    ({ type, value }) =>{      
+      return(
+        new Validator({
+          id: `${memberMetadata.id}-${type}`,
+          isValidator:false,
+          validatorAccount:value,
+          member: member.handle,
+        })
+      )
   
-    const memberEntry = new MembershipEntryMemberCreated()
-
-  
-    const memberCreatedEvent = new Validator({
-
     })
 
-    // await store.save<Validator>(member)
+  for (const resource of newExternalResources) {
+    await store.save<Validator>(resource)
   }
-  
+
+  return newExternalResources
+}
