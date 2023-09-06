dfd8667733fee1b45c8ed8c33872e73dbfd8d95a
Ignazio Bovo <ignazio@jsgenesis.com>
2023-09-06 17:37:45 +0200

    fix: :green_heart: fix orion environment not working
    Fixes the current orion environment not working.
    I have also removed the squid-archive services in favour of our new
    endpoint `https://archive.joystream.org/graphql`

--- start-orion.sh
+++ start-orion.sh
@@ -2,7 +2,6 @@
 set -e
 
 docker-compose up -d orion-db
-docker-compose up -d squid-archive
-docker-compose up -d squid-archive-gateway
 docker-compose up -d orion-processor
-docker-compose up -d orion-api
+docker-compose up -d orion-graphql-api
+docker-compose up -d orion-auth-api
