aee007e1ad0c7ea5a03ab64a0f8ec56d4c24be29
freakstatic <ricardomaltez@gmail.com>
2023-09-21 01:26:49 +0100

    increase forum sub categories runtime limit

--- tests/network-tests/src/misc/postRuntimeUpdateChecks.ts
+++ tests/network-tests/src/misc/postRuntimeUpdateChecks.ts
@@ -10,5 +10,9 @@ export default async function assertValues({ api }: FlowProps): Promise<void> {
   const version = await api.rpc.state.getRuntimeVersion()
   assert.equal(version.specVersion.toNumber(), 2002)
 
+  debug('Check forum sub category limit')
+  const subCategoryLimit = api.consts.forum.maxDirectSubcategoriesInCategory.toNumber()
+  assert.equal(subCategoryLimit, 10)
+
   debug('Done')
 }
