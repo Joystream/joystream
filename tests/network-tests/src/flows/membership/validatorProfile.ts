5ce949ac05b889b9a2ee07ca77573f52b7f203ec
chrlschwb <chrlschwb77@gmail.com>
2023-09-26 16:19:07 +0200

    update test file

--- tests/network-tests/src/flows/membership/validatorProfile.ts
+++ tests/network-tests/src/flows/membership/validatorProfile.ts
@@ -1,41 +1,33 @@
 import { FlowProps } from '../../Flow'
-import {
-  BuyMembershipHappyCaseFixture,
-  MemberProfileData,
-  UpdateProfileHappyCaseFixture,
-} from '../../fixtures/membership'
+import { BuyMembershipHappyCaseFixture } from '../../fixtures/membership'
 
 import { extendDebug } from '../../Debugger'
 import { FixtureRunner } from '../../Fixture'
-import { generateParamsFromAccountId } from '../../fixtures/membership/utils'
-import { WorkingGroupMetadata } from '@joystream/metadata-protobuf'
+import { VerifyValidatorAccountFixture } from '../../fixtures/membership/VerifyValidatorAccountFixture'
 
 export default async function validatorAccount({ api, query }: FlowProps): Promise<void> {
   const debug = extendDebug('flow:validator-account-update')
   debug('Started')
   api.enableDebugTxLogs()
 
-  // const updates: WorkingGroupMetadata[] = [
-  //   // Partial updates
-  //   // FIXME: Currently handle always need to be provided, see: https://github.com/Joystream/joystream/issues/2503
-  //   {
-  //       about:""
-  //   }
-  // ]
-
   const [account] = (await api.createKeyPairs(1)).map(({ key }) => key.address)
   const buyMembershipHappyCaseFixture = new BuyMembershipHappyCaseFixture(api, query, [account])
   await new FixtureRunner(buyMembershipHappyCaseFixture).run()
+
   const [memberId] = buyMembershipHappyCaseFixture.getCreatedMembers()
+  const [verifyAccount] = (await api.createKeyPairs(2)).map(({ key }) => key.address)
 
-  let oldValues: MemberProfileData = generateParamsFromAccountId(account)
-  //   for (const newValues of updates) {
-  //     const context = { account, memberId }
-  //     const updateProfileHappyCaseFixture = new UpdateProfileHappyCaseFixture(api, query, context, oldValues, newValues)
-
-  //     await new FixtureRunner(updateProfileHappyCaseFixture).runWithQueryNodeChecks()
-  //     oldValues = updateProfileHappyCaseFixture.getExpectedValues()
-  //   }
+  console.log('metadata ----------  ', verifyAccount)
+  const updateVerifyAccount = new VerifyValidatorAccountFixture(
+    api,
+    query,
+    {
+      account,
+      memberId,
+    },
+    verifyAccount
+  )
+  // await new FixtureRunner(updateVerifyAccount).runWithQueryNodeChecks()
 
   debug('Done')
 }
