8170034eb4a967b175061df1432b9a655e9e1192
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2023-09-27 20:04:44 +0400

    hire content lead before pre-upgrade runtime tests

--- tests/network-tests/src/scenarios/preRuntimeUpgrade.ts
+++ tests/network-tests/src/scenarios/preRuntimeUpgrade.ts
@@ -1,7 +1,8 @@
 import { scenario } from '../Scenario'
 import pre from '../misc/preRuntimeUpgrade'
+import leadOpening from '../flows/working-groups/leadOpening'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-scenario('Full', async ({ job, env, debug }) => {
-  job('Pre-Upgrade setup and checks', pre)
+scenario('Full', async ({ job }) => {
+  job('Pre-Upgrade setup and checks', pre).after(job('Hire content lead', leadOpening(true, ['contentWorkingGroup'])))
 })
