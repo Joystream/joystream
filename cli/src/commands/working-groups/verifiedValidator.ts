c77c8c5b029447a27f2a7b19eadc3f4dc15fe1d5
chrlschwb <chrlschwb77@gmail.com>
2023-10-15 17:24:34 +0200

    start verified validator command

---
+++ cli/src/commands/working-groups/verifiedValidator.ts
@@ -0,0 +1,42 @@
+import { flags } from '@oclif/command'
+import WorkingGroupsCommandBase from '../../base/WorkingGroupsCommandBase'
+import { WorkingGroups } from '../../Types'
+
+export default class MembershipValidatorAccountCommand extends WorkingGroupsCommandBase {
+  static description = 'Membership lead/worker verifies validator membership profile'
+  static flags = {
+    message: flags.string({
+      required: true,
+      description: 'Memberhip ID of the validator to verify',
+    }),
+
+    ...WorkingGroupsCommandBase.flags,
+  }
+
+  async run(): Promise<void> {
+    const { message } = this.parse(MembershipValidatorAccountCommand).flags
+
+    const members = await this.getApi().groupMembers(WorkingGroups.Membership)
+    const lead = await this.getApi().groupLead(WorkingGroups.Membership)
+    let memberIsLead = false
+
+    const pairs = this.getPairs()
+    const membersRows = members.filter((m) =>
+      pairs.find((p) => p.address === m.roleAccount.toString()) ? m.memberId.toString() : false
+    )
+
+    if (lead) {
+      memberIsLead = pairs.find((p) => p.address === lead.roleAccount.toString()) ? true : false
+    }
+
+    if (!membersRows || membersRows.length === 0) {
+      this.error('Only membership WG lead/worker can perform this command')
+    } else {
+      if (memberIsLead) {
+        this.getOriginalApi().tx.membershipWorkingGroup.leadRemark(message)
+      } else {
+        this.getOriginalApi().tx.membershipWorkingGroup.workerRemark(Number(membersRows[0]), message!)
+      }
+    }
+  }
+}
