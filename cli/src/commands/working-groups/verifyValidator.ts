c95dacac6d32ef6e4c3316882f1c669befd52420
chrlschwb <chrlschwb77@gmail.com>
2023-10-24 18:48:39 +0200

    update review

--- cli/src/commands/working-groups/verifyValidator.ts
+++ cli/src/commands/working-groups/verifyValidator.ts
@@ -1,7 +1,6 @@
 import { flags } from '@oclif/command'
 import WorkingGroupsCommandBase from '../../base/WorkingGroupsCommandBase'
 import { WorkingGroups } from '../../Types'
-import { u64 } from '@polkadot/types'
 
 export default class VerifyValidatorAccountCommand extends WorkingGroupsCommandBase {
   static description = 'Membership lead/worker verifies validator membership profile'
@@ -18,21 +17,14 @@ export default class VerifyValidatorAccountCommand extends WorkingGroupsCommandB
     const { memberId } = this.parse(VerifyValidatorAccountCommand).flags
 
     const members = await this.getApi().groupMembers(WorkingGroups.Membership)
-    const lead = await this.getApi().groupLead(WorkingGroups.Membership)
+    const worker = await this.getRequiredWorkerContext()
 
-    const membersRows = members.filter((m) => {
-      const arrayValue: u64[] = [m.memberId]
-      return this.getRequiredWorkerContext('MemberController', arrayValue)
-    })
+    const membersRows = members.find(((m) => m === worker))
 
-    if (!membersRows || membersRows.length === 0) {
+    if (!membersRows) {
       this.error('Only membership WG lead/worker can perform this command')
     } else {
-      if (!lead) {
-        this.getOriginalApi().tx.membershipWorkingGroup.workerRemark(Number(membersRows[0]), memberId!)
-      } else {
-        this.getOriginalApi().tx.membershipWorkingGroup.leadRemark(memberId)
-      }
+      this.getOriginalApi().tx.membershipWorkingGroup.workerRemark(Number(membersRows), memberId!)
     }
   }
 }
