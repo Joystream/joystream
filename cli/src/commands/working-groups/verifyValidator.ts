998eb6dcdb41a1b64ed107ffca7ca93b8a2c1d45
chrlschwb <chrlschwb77@gmail.com>
2023-10-23 18:34:28 -0700

    update

--- cli/src/commands/working-groups/verifyValidator.ts
+++ cli/src/commands/working-groups/verifyValidator.ts
@@ -1,35 +1,34 @@
 import { flags } from '@oclif/command'
 import WorkingGroupsCommandBase from '../../base/WorkingGroupsCommandBase'
 import { WorkingGroups } from '../../Types'
+import { u64 } from '@polkadot/types'
 
-export default class MembershipValidatorAccountCommand extends WorkingGroupsCommandBase {
+export default class VerifyValidatorAccountCommand extends WorkingGroupsCommandBase {
   static description = 'Membership lead/worker verifies validator membership profile'
   static flags = {
     message: flags.string({
       required: true,
-      description: 'Memberhip ID of the validator to verify',
+      description: 'Membership ID of the validator to verify',
     }),
 
     ...WorkingGroupsCommandBase.flags,
   }
 
   async run(): Promise<void> {
-    const { message } = this.parse(MembershipValidatorAccountCommand).flags
+    const { message } = this.parse(VerifyValidatorAccountCommand).flags
 
     const members = await this.getApi().groupMembers(WorkingGroups.Membership)
     const lead = await this.getApi().groupLead(WorkingGroups.Membership)
-    // const lead = await this.getRequiredLeadContext()
-    const pairs = this.getPairs()
 
-    const membersRows = members.filter((m) =>
-      pairs.find((p) => p.address === m.roleAccount.toString()) ? m.memberId.toString() : undefined
-    )
+    const membersRows = members.filter((m) => {
+      let arrayValue: u64[] = [m.memberId]
+      return this.getRequiredWorkerContext('MemberController', arrayValue)
+    })
 
     if (!membersRows || membersRows.length === 0) {
       this.error('Only membership WG lead/worker can perform this command')
     } else {
       if (!lead) {
-        this.log("You are lead");
         this.getOriginalApi().tx.membershipWorkingGroup.workerRemark(Number(membersRows[0]), message!)
       } else {
         this.getOriginalApi().tx.membershipWorkingGroup.leadRemark(message)
