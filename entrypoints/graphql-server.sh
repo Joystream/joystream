347646aaf53cd58ddaf78b4891db118340e8866a
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-11-17 15:14:54 +0500

    fix: load env vars from .env file in @joystream/opentelemetry module

--- entrypoints/graphql-server.sh
+++ entrypoints/graphql-server.sh
@@ -2,6 +2,7 @@
 
 # docker entrypoint fot graphql-server, to allow running with telemetry
 if [[ "$TELEMETRY_ENABLED" = "yes" ]]; then
+    export OTEL_APPLICATION=query-node
     yarn workspace query-node-root query-node:start:prod:with-instrumentation $*
 else
     yarn workspace query-node-root query-node:start:prod:pm2 $*
