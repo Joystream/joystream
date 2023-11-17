a91634aae25735e9d5baf73d56d1496306493ce0
Zeeshan Akram <97m.zeeshan@gmail.com>
2023-11-17 15:16:55 +0500

    add 'nodeId' attribute to all argus spans

--- opentelemetry/instrumentations/distributor-node.ts
+++ opentelemetry/instrumentations/distributor-node.ts
@@ -5,13 +5,19 @@ import { FsInstrumentation } from '@opentelemetry/instrumentation-fs'
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
 import { NodeSDK } from '@opentelemetry/sdk-node'
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node'
+import { BatchSpanProcessor, Span } from '@opentelemetry/sdk-trace-node'
 import { ClientRequest, ServerResponse } from 'http'
 
 /** Opentelemetry Instrumentation for Joystream Distributor Node */
 
+class CustomSpanProcessor extends BatchSpanProcessor {
+  onStart(span: Span) {
+    span.setAttribute('nodeId', process.env.JOYSTREAM_DISTRIBUTOR__ID)
+  }
+}
+
 export const DistributorNodeInstrumentation = new NodeSDK({
-  spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter(), {
+  spanProcessor: new CustomSpanProcessor(new OTLPTraceExporter(), {
     maxQueueSize: parseInt(process.env.OTEL_MAX_QUEUE_SIZE || '8192'),
     maxExportBatchSize: parseInt(process.env.OTEL_MAX_EXPORT_BATCH_SIZE || '1024'),
   }),
