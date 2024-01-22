972a6187f02e113dce2f628eb1c94123c845189d
Theophile Sandoz <theophile.sandoz@gmail.com>
2024-01-22 15:56:22 +0100

    start verified validator command Co-authored-by: chrlschwb <chrlschwb77@gmail.com>

---
+++ cli/src/commands/working-groups/verifyValidator.ts
@@ -0,0 +1,61 @@
+import { flags } from '@oclif/command'
+import WorkingGroupsCommandBase from '../../base/WorkingGroupsCommandBase'
+import { WorkingGroups } from '../../Types'
+import { VerifyValidator, RemarkMetadataAction } from '@joystream/metadata-protobuf'
+import { metadataToString } from '../../helpers/serialization'
+import Long from 'long'
+import chalk from 'chalk'
+export default class VerifyValidatorAccountCommand extends WorkingGroupsCommandBase {
+  static description = 'Membership lead/worker verifies validator membership profile'
+  static flags = {
+    memberId: flags.integer({
+      required: true,
+      description: 'Membership ID of the validator to verify',
+    }),
+    isVerified: flags.boolean({
+      required: true,
+      description: 'Verification state of the validator',
+    }),
+
+    ...WorkingGroupsCommandBase.flags,
+  }
+
+  async run(): Promise<void> {
+    const { memberId, isVerified } = this.parse(VerifyValidatorAccountCommand).flags
+
+    const id = Long.fromNumber(memberId)
+    const meta = new RemarkMetadataAction({
+      verifyValidator: new VerifyValidator({
+        memberId: id,
+        isVerified,
+      }),
+    })
+
+    const message = metadataToString(RemarkMetadataAction, meta)
+
+    const nowGroup = this.group
+
+    await this.setPreservedState({ defaultWorkingGroup: WorkingGroups.Membership })
+
+    const worker = await this.getRequiredWorkerContext()
+
+    const [Id, controllerAccount] = await this.getValidatedMemberRemarkParams()
+
+    if (!worker) {
+      this.error('Only membership WG lead/worker can perform this command')
+    } else {
+      const keypair = await this.getDecodedPair(controllerAccount)
+      const result = await this.sendAndFollowNamedTx(keypair, 'membershipWorkingGroup', 'workerRemark', [Id, message])
+
+      const [workerid] = this.getEvent(result, 'membershipWorkingGroup', 'WorkerRemarked').data
+
+      this.log(
+        chalk.green(
+          `member ${memberId} verified successfully by membership WG worker ${workerid}!`
+        )
+      )
+    }
+
+    await this.setPreservedState({ defaultWorkingGroup: nowGroup })
+  }
+}
