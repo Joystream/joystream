e4b4e9992fea5b8f963ecb1a08959c89a6ddb8de
Theophile Sandoz <theophile.sandoz@gmail.com>
2024-04-15 12:52:34 +0200

    Validate the QN benchmarking responses

--- query-node/benchmark.sh
+++ query-node/benchmark.sh
@@ -6,6 +6,7 @@ T=60
 # Pass the endpoint as the first argument, default to local
 endpoint=${1:-'http://localhost:8081/graphql'}
 
+invalidWhen=".errors != null or (.data.proposals | length == 0)"
 query=$(
   cat <<EOF | awk '{ print NR == 1 ? "" : "\\n" } 1' ORS=''
 {"query":"{
@@ -44,7 +45,11 @@ i=-1
 until=$(($(date +%s%N) + $T * $SEC))
 while [ $(date +%s%N) -lt $until ]; do
   i=$((i + 1))
-  curl -s "$endpoint" -H 'Content-Type: application/json' --data-binary "$query" >/dev/null
+  res=$(curl -s "$endpoint" -H 'Content-Type: application/json' --data-binary "$query")
+  if $(echo "$res" | jq -e "$invalidWhen"); then
+    echo "Failed with response $res"
+    exit 1
+  fi
 done
 
 echo "Sent $i queries in $T seconds"
