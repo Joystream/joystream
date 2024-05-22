978b21f66fe237a941b0549e45478ca6bd458d6b
Ignazio Bovo <ignazio@jsgenesis.com>
2024-05-22 15:11:35 +0200

    test: :white_check_mark: add unhappy path for move To accepted location

--- storage-node/src/services/storageProviders/tests/integration/acceptObjects.test.ts
+++ storage-node/src/services/storageProviders/tests/integration/acceptObjects.test.ts
@@ -34,20 +34,18 @@ async function createMockAcceptPendingObjectsService() {
 
 describe('AcceptPendingObjectsService::moveToAcceptedLocation', () => {
   AcceptPendingObjectsService.prototype.runWithInterval = jest.fn()
-  let mockStorageProvider: jest.Mocked<IConnectionHandler>
-  beforeAll(() => {
-    mockStorageProvider = {
-      uploadFileToRemoteBucket: jest.fn(),
-      getRedirectUrlForObject: jest.fn(),
-      listFilesOnRemoteBucket: jest.fn(),
-      removeObject: jest.fn(),
-    }
-  })
+  let mockStorageProvider: jest.Mocked<IConnectionHandler> = {
+    uploadFileToRemoteBucket: jest.fn(),
+    getRedirectUrlForObject: jest.fn(),
+    listFilesOnRemoteBucket: jest.fn(),
+    removeObject: jest.fn(),
+  }
+  let service: AcceptPendingObjectsService
+  let oldPath = PENDING_DIRECTORY + '/' + FILENAME
+
   describe('happy path connection enabled', () => {
-    let service: AcceptPendingObjectsService
-    let oldPath = PENDING_DIRECTORY + '/' + FILENAME
     beforeAll(async () => {
-      //given
+      jest.clearAllMocks()
       ;(isStorageProviderConnectionEnabled as jest.Mock) = jest.fn().mockReturnValue(true)
       ;(access as jest.Mock) = jest.fn().mockRejectedValue(undefined as never) // file should not exists
       ;(unlink as jest.Mock) = jest.fn().mockResolvedValue(undefined as never)
@@ -55,17 +53,19 @@ describe('AcceptPendingObjectsService::moveToAcceptedLocation', () => {
       ;(getStorageProviderConnection as jest.Mock) = jest.fn().mockReturnValue(mockStorageProvider)
       ;(registerNewDataObjectId as jest.Mock) = jest.fn().mockReturnValue(undefined as never)
       ;(addDataObjectIdToCache as jest.Mock) = jest.fn().mockResolvedValue(undefined as never)
+      ;(mockStorageProvider.uploadFileToRemoteBucket as jest.Mock) = jest.fn().mockResolvedValue(undefined as never)
       service = await createMockAcceptPendingObjectsService()
-
-      // when
-      await service.moveToAcceptedLocation(FILENAME)
+    })
+    it('should correctly call the move function without errors', () => {
+      expect(service.moveToAcceptedLocation(FILENAME)).resolves.not.toThrowError()
     })
     // then
     it('should check for storage connection enabled', () => {
       expect(isStorageProviderConnectionEnabled).toHaveBeenCalled()
     })
-    it('should successfully move from pending to uploads buckets when cloud connection enabled', () => {
+    it('should successfully moved from pending to uploads buckets when cloud connection enabled', () => {
       expect(mockStorageProvider.uploadFileToRemoteBucket).toHaveBeenCalledWith(FILENAME, oldPath)
+      expect(isStorageProviderConnectionEnabled).not.toThrowError()
     })
     it('should unlink file after uploading', () => {
       expect(unlink).toHaveBeenCalledWith(oldPath)
@@ -75,6 +75,43 @@ describe('AcceptPendingObjectsService::moveToAcceptedLocation', () => {
     })
     it('should register the file with expiration', () => {
       expect(registerNewDataObjectId).toHaveBeenCalledWith(FILENAME)
+    })
+  })
+  describe('unhappy path connection enabled, upload error', () => {
+    beforeAll(async () => {
+      jest.clearAllMocks()
+      ;(isStorageProviderConnectionEnabled as jest.Mock) = jest.fn().mockReturnValue(true)
+      ;(access as jest.Mock) = jest.fn().mockRejectedValue(undefined as never) // file should not exists
+      ;(mockStorageProvider.uploadFileToRemoteBucket as jest.Mock) = jest.fn().mockImplementation(() => {
+        throw new Error('S3 upload error')
+      })
+      ;(getStorageProviderConnection as jest.Mock) = jest.fn().mockReturnValue(mockStorageProvider)
+      ;(unlink as jest.Mock) = jest.fn().mockResolvedValue(undefined as never)
+      ;(copyFile as jest.Mock) = jest.fn().mockRejectedValue(undefined as never)
+      ;(registerNewDataObjectId as jest.Mock) = jest.fn().mockReturnValue(undefined as never)
+      ;(addDataObjectIdToCache as jest.Mock) = jest.fn().mockResolvedValue(undefined as never)
+      service = await createMockAcceptPendingObjectsService()
+    })
+    it('should throw error when upload fails', async () => {
+      try {
+        await service.moveToAcceptedLocation(FILENAME)
+      } catch (err) {
+        expect(err).toEqual(new Error('S3 upload error'))
+      }
+    })
+
+    it('should throw error when upload fails', () => {
+      expect(mockStorageProvider.uploadFileToRemoteBucket).toHaveBeenCalledWith(FILENAME, oldPath)
+      expect(mockStorageProvider.uploadFileToRemoteBucket).toThrowError()
+    })
+    it('should not unlink file after failed upload', () => {
+      expect(unlink).not.toHaveBeenCalled()
+    })
+    it('should not add the file in the object cache', () => {
+      expect(addDataObjectIdToCache).not.toHaveBeenCalled()
+    })
+    it('should not register the file with expiration', () => {
+      expect(registerNewDataObjectId).not.toHaveBeenCalled()
     })
   })
 })
